4aa3f0ed942ba0f937e70d5b94c5647c587ff002
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-30 19:22:06 +0800

    Update xbox360-controller-driver.rb

--- Casks/xbox360-controller-driver.rb
+++ Casks/xbox360-controller-driver.rb
@@ -4,7 +4,7 @@ cask 'xbox360-controller-driver' do
 
   url "https://github.com/360Controller/360Controller/releases/download/v#{version}/360ControllerInstall_#{version}.dmg"
   appcast 'https://github.com/360Controller/360Controller/releases.atom',
-          checkpoint: 'ea89ea749b89a985b663d1bee677a06acc0b1afebbd58258d62ea5d92c5f4f7f'
+          checkpoint: '5352493bf00e50b6ad74f71a1f009e01a593e9ae3becb3856e2ae5cd75a9bd17'
   name 'TattieBogle Xbox 360 Controller Driver (with improvements)'
   homepage 'https://github.com/360Controller/360Controller'
 
