fdde40f8aae59c776d5f70f709abc408b7e18f6f
Robin Breathe <robin@isometry.net>
2017-02-19 10:29:59 +0000

    Update silicon-labs-vcp-driver URLs
    * Update download and homepage URLs
    * Add version, checksum and appcast checkpoint

--- Casks/silicon-labs-vcp-driver.rb
+++ Casks/silicon-labs-vcp-driver.rb
@@ -1,11 +1,13 @@
 cask 'silicon-labs-vcp-driver' do
-  version :latest
-  sha256 :no_check
+  version '4.10.15'
+  sha256 'b5532118d3ea97080ff57d04e4941295e2542df7d6180cebe3d5684aed13072b'
 
-  url 'https://www.silabs.com/Support%20Documents/Software/Mac_OSX_VCP_Driver.zip'
+  url 'https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver.zip'
+  appcast 'https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver_Release_Notes.txt',
+          checkpoint: '080631469bed4a8749d2ac007a2571102b2494cefa4a4530013edbb08d4be940'
   name 'Silicon Labs VCP Driver'
   name 'CP210x USB to UART Bridge VCP Driver'
-  homepage 'https://www.silabs.com/products/mcu/pages/usbtouartbridgevcpdrivers.aspx'
+  homepage 'https://www.silabs.com/products/development-tools/software/usb-to-uart-bridge-vcp-drivers'
 
   container nested: 'SiLabsUSBDriverDisk.dmg'
 
