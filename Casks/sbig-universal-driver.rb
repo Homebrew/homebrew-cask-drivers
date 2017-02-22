faf132ae2dc0f0ce61b244e059d7c3651d24d3b2
Sean Houghton <sean.houghton@activision.com>
2017-02-21 18:21:48 -0800

    Add SBIG Universal Driver (latest version)

---
+++ Casks/sbig-universal-driver.rb
@@ -0,0 +1,13 @@
+cask 'sbig-universal-driver' do
+  version :latest
+  sha256 :no_check
+
+  # ftp.sbig.com was verified as official when first introduced to the cask
+  url 'ftp://ftp.sbig.com/pub/SBIGDriverInstallerUniv.dmg'
+  name 'SBIG Universal Driver'
+  homepage 'http://diffractionlimited.com/'
+
+  pkg 'SBIG Universal Driver Installer 4r84.pkg', allow_untrusted: true
+
+  uninstall pkgutil: ['com.sbig']
+end
