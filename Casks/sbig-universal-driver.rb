2543026ddd1f116d6b797d14b96809d09d82178c
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-22 16:19:30 +0800

    Update sbig-universal-driver.rb

--- Casks/sbig-universal-driver.rb
+++ Casks/sbig-universal-driver.rb
@@ -2,7 +2,7 @@ cask 'sbig-universal-driver' do
   version :latest
   sha256 :no_check
 
-  # ftp.sbig.com/pub was verified as official when first introduced to the cask
+  # ftp.sbig.com was verified as official when first introduced to the cask
   url 'ftp://ftp.sbig.com/pub/SBIGDriverInstallerUniv.dmg'
   name 'SBIG Universal Driver'
   homepage 'http://diffractionlimited.com/'
