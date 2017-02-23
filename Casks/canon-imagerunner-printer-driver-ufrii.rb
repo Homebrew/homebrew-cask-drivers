09e6b590c7be62097104b8d009831f4a8dfac522
Vítor Galvão <info@vitorgalvao.com>
2017-02-23 18:21:33 +0000

    Update canon-imagerunner-printer-driver-ufrii.rb

--- Casks/canon-imagerunner-printer-driver-ufrii.rb
+++ Casks/canon-imagerunner-printer-driver-ufrii.rb
@@ -3,6 +3,8 @@ cask 'canon-imagerunner-printer-driver-ufrii' do
   sha256 'f00b5dff45b0e6642fb17714a180d78eb93b352b21a5fef4b96d3f798a286126'
 
   url "https://downloads.canon.com/bisg2016/drivers/mac/UFRII_#{version}_Mac.zip"
+  appcast 'https://www.usa.canon.com/internet/PA_NWSupport/driversDownloads?model=15802&os=MACOS_V10_12&type=DS&lang=English',
+           checkpoint: '3c848ecfe8545ac2c98964b5db3d5078d649b713a7ad8f717ec84f3bb55bcce2'
   name 'Canon imageRUNNER UFRII Printer Driver'
   homepage 'https://www.usa.canon.com/internet/portal/us/home/support/details/copiers-mfps-fax-machines/support-multifunction/imagerunner-2018'
 
