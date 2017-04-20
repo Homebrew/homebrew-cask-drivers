c488f022ec286271e9c0a29b7947d6d44a9db3f2
commitay <commitay1@gmail.com>
2017-04-21 07:59:54 +1000

    Update and rename tomtom-mysports-connect.rb to tomtom-sports-connect.rb

--- Casks/tomtom-sports-connect.rb
+++ Casks/tomtom-sports-connect.rb
@@ -1,18 +1,17 @@
-cask 'tomtom-mysports-connect' do
+cask 'tomtom-sports-connect' do
   version :latest
   sha256 :no_check
 
   # sports.tomtom-static.com was verified as official when first introduced to the cask
-  url 'https://sports.tomtom-static.com/downloads/desktop/mysportsconnect/latest/TomTomMySportsConnectInstaller.pkg'
+  url 'https://sports.tomtom-static.com/downloads/desktop/mysportsconnect/latest/TomTomSportsConnectInstaller.pkg'
   name 'TomTom MySports Connect'
   homepage 'https://www.tomtom.com/mysports/getstarted'
 
-  pkg 'TomTomMySportsConnectInstaller.pkg'
+  pkg 'TomTomSportsConnectInstaller.pkg'
 
   uninstall quit:    'com.tomtom.mysportsconnect',
             pkgutil: [
                        'com.tomtom.tomtomfa.pkg',
                        'com.tomtom.tomtomfa.temp.pkg',
-                     ],
-            delete:  '/Applications/TomTom MySports Connect.app'
+                     ]
 end
