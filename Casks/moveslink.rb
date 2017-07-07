5c3c4735d1f4c0bf84c57e51f5285f363b99d982
Janne Vaittinen <janne.vaittinen@iki.fi>
2017-07-07 20:45:37 +0300

    Add Suunto Moveslink v1.0.66 (#90)
    * Add Suunto Moveslink v1.0.66
    
    * url
    
    * Rename suunto-moveslink.rb to moveslink.rb
    
    * uninstall

---
+++ Casks/moveslink.rb
@@ -0,0 +1,14 @@
+cask 'moveslink' do
+  version '1.0.66'
+  sha256 '5897fe23239a4400f9f23929f2eadcc424bdc5f3f3b3da74a1c17b8ec778997c'
+
+  url "http://content.static.movescount.com/downloads/Moveslink_setup_#{version.dots_to_underscores}.dmg"
+  name 'Suunto Moveslink'
+  homepage 'http://www.movescount.com/connect/moveslink?os=mac'
+
+  pkg 'Moveslink_setup.pkg'
+
+  uninstall login_item: 'Moveslink',
+            pkgutil:    'com.suunto.moveslink.moveslink.pkg',
+            quit:       'com.yourcompany.Moveslink'
+end
