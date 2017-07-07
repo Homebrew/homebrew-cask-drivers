3d6b5006a6f2d12bbd61cb78f1792a68d008b3a1
commitay <commitay1@gmail.com>
2017-07-07 20:07:01 +1000

    url

--- Casks/suunto-moveslink.rb
+++ Casks/suunto-moveslink.rb
@@ -1,8 +1,8 @@
-cask 'suunto-moveslink' do
+cask 'moveslink' do
   version '1.0.66'
   sha256 '5897fe23239a4400f9f23929f2eadcc424bdc5f3f3b3da74a1c17b8ec778997c'
 
-  url 'http://content.static.movescount.com/downloads/Moveslink_setup_1_0_66.dmg'
+  url "http://content.static.movescount.com/downloads/Moveslink_setup_#{version.dots_to_underscores}.dmg"
   name 'Suunto Moveslink'
   homepage 'http://www.movescount.com/connect/moveslink?os=mac'
 
