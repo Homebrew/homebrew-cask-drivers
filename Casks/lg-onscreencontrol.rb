15d3c954554f335272cec801a3d958b6aadfbac2
Jeremy Gaither <jeremy@jeremygaither.com>
2017-07-08 15:44:14 -0500

    lg-onscreencontrol remove extra variables

--- Casks/lg-onscreencontrol.rb
+++ Casks/lg-onscreencontrol.rb
@@ -1,12 +1,9 @@
 cask 'lg-onscreencontrol' do
-  version '2.46'
+  version '2.46,'
   sha256 '943cfd2d59d56b47ae244d175eaa4344cb49e5e39a4efd812ba78939970e69e8'
   
-  lgosc_doc_id = '20150320442554'
-  lgosc_file_id = 'q0YuHZx6oufr6zJ6znVLmg'
-  lgosc_original_name = "MAC_OSC.#{version}.zip"
-
-  url "http://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=#{lgosc_doc_id}&what=MANUAL&fromSystem=LG.COM&fileId=#{lgosc_file_id}&ORIGINAL_NAME_b1_a1=#{lgosc_original_name}"
+  # http://www.lg.com/us/support-product/lg-27UD69P-W#manuals retrieved 2017-07-08
+  url "http://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150320442554&what=MANUAL&fromSystem=LG.COM&fileId=q0YuHZx6oufr6zJ6znVLmg&ORIGINAL_NAME_b1_a1=MAC_OSC.#{version.major_minor_patch}.zip"
   name 'LG OnScreen Control'
   homepage 'http://www.lg.com/'
 
