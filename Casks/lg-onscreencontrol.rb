fb7a865e86239100bd1b2b5058ed6dcc393c945c
Jeremy Gaither <jeremy@jeremygaither.com>
2017-07-10 18:34:39 -0500

    Update LG OnScreen Control to v2.46 (#92)
    * Update lg_onscreencontrol to latest version
    
    Updating to latest package version, retrieved from http://www.lg.com/us/support-product/lg-27UD69P-W on 2017-07-08.
    
    * Version now 2.46
    * todo: verify uninstall params
    
    * fix lg-oncreencontrol 2.46 hash
    
    * lg-onscreencontrol remove extra variables
    
    * lg-onscreencontrol fix version
    
    * Update lg-onscreencontrol.rb
    
    * update uninstall

--- Casks/lg-onscreencontrol.rb
+++ Casks/lg-onscreencontrol.rb
@@ -1,13 +1,17 @@
 cask 'lg-onscreencontrol' do
-  version '1.37'
-  sha256 '7263b8cb714cf25db16cee7310ba03a79cc28754cb18f55c30642cc745d64740'
+  version '2.46'
+  sha256 '943cfd2d59d56b47ae244d175eaa4344cb49e5e39a4efd812ba78939970e69e8'
 
-  url "http://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150116943155&what=MANUAL&fromSystem=LG.COM&fileId=kF2MFoPcI9YSuCcD23D9w&ORIGINAL_NAME_b1_a1=MAC_OnScreenControl.#{version}.zip"
+  url "http://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150320442554&what=MANUAL&fromSystem=LG.COM&fileId=q0YuHZx6oufr6zJ6znVLmg&ORIGINAL_NAME_b1_a1=MAC_OSC.#{version}.zip"
   name 'LG OnScreen Control'
   homepage 'http://www.lg.com/'
 
-  pkg "OnScreenControl.#{version}.pkg"
+  pkg "OnScreenControl_V#{version}.pkg"
 
-  uninstall quit:    'com.LGSI.-.OnScreen-Control',
-            pkgutil: 'com.lge.onscreenControl.*'
+  uninstall login_item: 'OnScreen Control',
+            pkgutil:    [
+                          'com.lge.onscreenControl.*',
+                          'com.lge.OnscreenControl.*',
+                        ],
+            quit:       'com.LGSI.OnScreen-Control'
 end
