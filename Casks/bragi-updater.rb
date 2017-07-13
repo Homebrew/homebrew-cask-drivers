acb1f20efbdccf848ddbb2140c221886ac9c7a6f
Gobinath Mallaiyan <i@gobinath.com>
2017-07-13 12:55:55 -0400

    Update bragi-updater to 1.1.2

--- Casks/bragi-updater.rb
+++ Casks/bragi-updater.rb
@@ -1,6 +1,6 @@
 cask 'bragi-updater' do
-  version '1.1.0'
-  sha256 '305fd036fae31b765455c8dab9176f12b8f98a526d45ead0bb9edc41a2940c86'
+  version '1.1.2'
+  sha256 '7430e20897da336aba59849ce23e4815dfe24b05772106c47254e2269e7022a0'
 
   url "http://update.bragi.com/bin/Bragi%20Updater-#{version}.dmg"
   appcast 'http://update.bragi.com/',
