e35804aae89580d9e43db946e8652eac2f2e427a
Zach Schneider <zach@schneid.io>
2017-07-13 21:40:14 -0400

    Add Mazda Toolbox.app v4.2 (#93)
    * Add Mazda Toolbox.app v4.2
    
    * url & zap

---
+++ Casks/mazda-toolbox.rb
@@ -0,0 +1,21 @@
+cask 'mazda-toolbox' do
+  version '2017_05_16__14_25_TB4'
+  sha256 'bbaa78c28c8622a9e44c955567aa941ff77a01948364e68c2ce8179ace6f2747'
+
+  url "https://cdns.distrib.naviextras.com/content/!application/TOOLBOX/Agents/Mazda/OSX/#{version}/Mazda-Toolbox.dmg"
+  name 'Mazda Toolbox'
+  homepage 'https://mazda-na.naviextras.com/shop/portal/downloads'
+
+  app 'Mazda Toolbox.app'
+
+  zap delete: [
+                '~/Library/Application Support/Mazda Toolbox',
+                '~/Library/Preferences/com.mazda.toolbox.plist',
+              ],
+      trash:  [
+                '~/Library/Caches/com.mazda.toolbox',
+                '~/Library/Caches/Mazda Toolbox',
+                '~/Library/Logs/Mazda Toolbox',
+                '~/Library/Saved Application State/com.mazda.toolbox.savedState',
+              ]
+end
