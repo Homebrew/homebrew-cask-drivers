6e96f4d203960ecadafac712cb106c3d4195f760
Ron Mordechai <ronmrdechai@gmail.com>
2017-07-15 17:48:37 +0300

    Added saleae-logic version 1.2.14 (#99)
    * Added saleae-logic version 1.2.14
    
    * zap

---
+++ Casks/saleae-logic.rb
@@ -0,0 +1,13 @@
+cask 'saleae-logic' do
+  version '1.2.14'
+  sha256 '42c7d5dc6f8193535ae578b634a1941c769eab29cf641e127459032a6a53ceb7'
+
+  url "http://downloads.saleae.com/logic/#{version}/Logic-#{version}-Darwin.dmg"
+  name 'Saleae Logic'
+  homepage 'https://www.saleae.com/'
+
+  app 'Logic.app'
+
+  zap delete: '~/Library/Saved Application State/com.saleae.Logic.savedState',
+      trash:  '~/Library/Preferences/Saleae'
+end
