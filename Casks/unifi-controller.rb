49e300ec6fc122bba06236e6eb69a796fe6573de
Sven Ertel <sven@ertel-net.de>
2017-07-21 18:46:09 +0200

    updated unifi-controller to 5.5.19

--- Casks/unifi-controller.rb
+++ Casks/unifi-controller.rb
@@ -1,6 +1,6 @@
 cask 'unifi-controller' do
-  version '5.4.16'
-  sha256 'ca22aed22772e3c2c9e26146d8984bf2373a788131f848952bd68f10d15887bb'
+  version '5.5.19'
+  sha256 'f3f3e94185cd43a1f21a4483afc161ebba5c905fd3680d26b159f3e97f741adc'
 
   url "https://dl.ubnt.com/unifi/#{version}/UniFi.pkg"
   name 'UniFi Controller'
