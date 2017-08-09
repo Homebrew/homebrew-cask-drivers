0173a88b3343b8d13e5392c242f761eb7f6ecb76
Federico <shatteringlass@users.noreply.github.com>
2017-08-09 13:19:22 +0200

    Canon pixma mx510 drivers (#116)
    * New driver for Mx510 series Canon Printers
    
    * Merged branches for printer and scanner drivers
    
    * Update canon-pixma-mx510-scanner-driver.rb

---
+++ Casks/canon-pixma-mx510-printer-driver.rb
@@ -0,0 +1,14 @@
+cask 'canon-pixma-mx510-printer-driver' do
+  version '16.10.0.0'
+  sha256 'ea4c1340ba60d1e9845f17ee153e887ba68337f85d5b09dd49c19a7386d73a25'
+
+  # gdlp01.c-wss.com was verified as official when first introduced to the cask
+  url "http://gdlp01.c-wss.com/gds/5/0100005695/03/mcpd-mac-mx510-#{version.dots_to_underscores}-ea21_3.dmg"
+  name 'Canon PIXMA MX510 series CUPS Printer Driver'
+  homepage 'https://www.usa.canon.com/internet/portal/us/home/support/details/printers/support-inkjet-printer/mx-series/pixma-mx510'
+
+  pkg "PrinterDriver_MX510 series_#{version.no_dots}.pkg"
+
+  uninstall pkgutil: "jp.co.canon.pkg.MX510-#{version.no_dots}",
+            delete:  '/Library/Printers/PPDs/Contents/Resources/CanonIJMX510series.ppd.gz'
+end
