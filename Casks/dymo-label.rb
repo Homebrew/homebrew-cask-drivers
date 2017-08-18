df27cd58957b1a3141f2952d1d5238854161a777
kerouanton <0github@9c.re>
2017-08-18 20:06:04 +0200

    Add Dymo Label v8.6.2

---
+++ Casks/dymo-label.rb
@@ -0,0 +1,10 @@
+cask 'dymo-label' do
+  version '8.6.2'
+  sha256 'efda7c1428974261c77b765d94751502eaf58521f179c8bc169362f9fddaee6e'
+
+  url 'http://download.dymo.com/dymo/Software/Mac/DLS8Setup.8.6.2.dmg'
+  name 'Dymo LabelWriter Manager'
+  homepage 'http://www.dymo.com/en-US/dymo-user-guides'
+
+  pkg 'DYMO Label v.8.pkg'
+end
