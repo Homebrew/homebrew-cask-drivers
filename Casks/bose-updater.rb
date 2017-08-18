6b9119f581eaf0bf9db203b402c38ab6ac4c5d7e
kerouanton <0github@9c.re>
2017-08-18 18:12:23 +0200

    Add Bose Updater.app v1.5.4.1309.6342

---
+++ Casks/bose-updater.rb
@@ -0,0 +1,10 @@
+cask 'bose-updater' do
+  version '1.5.4.1309.6342'
+  sha256 '027fa124ce45342cc925e584fc1d76cd3a8f7761e3a6db0191f82cff3e6b967a'
+
+  url 'http://downloads.bose.com/ced/boseupdater/mac/BoseUpdater_1.5.4.1309.6342.dmg'
+  name 'Bose Updater'
+  homepage 'http://btu.bose.com/'
+
+  app 'Bose Updater.app'
+end
