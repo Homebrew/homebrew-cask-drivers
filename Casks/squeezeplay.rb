0ec4b436bba942eb0673d91712c82f69aee4fc42
commitay <commitay@users.noreply.github.com>
2017-08-23 08:40:09 +1000

    update url, add appcast

--- Casks/squeezeplay.rb
+++ Casks/squeezeplay.rb
@@ -2,8 +2,10 @@ cask 'squeezeplay' do
   version '7.8.0r984'
   sha256 'a92808906a708169bc1d9107329cfb7d95ba1f00502a807e03e056b8f50bdfac'
 
-  # URL_SECTION was verified as official when first introduced to the cask
-  url "https://sourceforge.net/projects/lmsclients/files/squeezeplay/osx/SqueezePlay-#{version}.dmg"
+  # downloads.sourceforge.net/lmsclients was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/lmsclients/SqueezePlay-#{version}.dmg"
+  appcast 'https://sourceforge.net/projects/lmsclients/rss?path=/squeezeplay/osx',
+          checkpoint: '9de25ee8d7e6cfd6ccb9b83de2b2796c776d36eb88d825fb973c62a44b86aac7'
   name 'Logitech LMS SqueezePlay Client'
   homepage 'http://forums.slimdevices.com/showthread.php?96328-ANNOUNCE-SqueezePlay-for-Mac-OSX'
 
