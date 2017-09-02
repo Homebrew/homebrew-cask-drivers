07479df79f368cdcda059801bc74bcd6ee1e2ab6
commitay <commitay@users.noreply.github.com>
2017-09-02 21:16:55 +1000

    hp-easy-start

--- Casks/hp-easy-start.rb
+++ Casks/hp-easy-start.rb
@@ -1,11 +1,10 @@
 cask 'hp-easy-start' do
-  version '2.6.1.164'
-  sha256 'bcf5a7f2e173ba00fd802d6b25179008373c83f7511e0a4d68be8c002f5ea0a8'
+  version :latest
+  sha256 :no_check
 
-  url "http://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Applications/HP_Easy_Start.app.zip"
-
+  url 'http://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Applications/HP_Easy_Start.app.zip'
   name 'HP Easy Start'
-  homepage 'https://support.hp.com/us-en/drivers/selfservice/swdetails/hp-deskjet-2130-all-in-one-printer-series/7174550/model/7174551/swItemId/mp-166618-4'
+  homepage 'https://support.hp.com/'
 
-  installer manual: 'HP Easy Start.app'
+  app 'HP Easy Start.app'
 end
