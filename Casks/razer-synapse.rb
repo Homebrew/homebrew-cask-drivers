8cb46f95dd9e97e80a5b45f4dc0b6ee1d5c8a367
Victor Popkov <victor@popkov.me>
2017-09-04 15:54:21 +0300

    Update razer-synapse to 1.76

--- Casks/razer-synapse.rb
+++ Casks/razer-synapse.rb
@@ -1,6 +1,6 @@
 cask 'razer-synapse' do
-  version '1.75'
-  sha256 '9aa4b3f2dc9eaa756e7baaa8ea314fc9300585addda3f9b9009fba34d68328fc'
+  version '1.76'
+  sha256 'ec4607dc1daf606e68c4afbf9eaedf7ce7b42e4b1b2285a19681910d0332f17a'
 
   url "http://dl.razerzone.com/drivers/Synapse2/mac/Razer_Synapse_Mac_Driver_v#{version}.dmg"
   name 'Razer Synapse'
