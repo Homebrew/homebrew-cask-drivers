09889f2f1ee8802940860781314156f9023438aa
Vítor Galvão <info@vitorgalvao.com>
2017-09-05 17:50:25 +0100

    Update navdy.rb

--- Casks/navdy.rb
+++ Casks/navdy.rb
@@ -2,17 +2,15 @@ cask 'navdy' do
   version '1.0.146'
   sha256 'f30f84016e435239cc2ca63a8ed5b43ec858c815310cfbfa147cea9bb4f940c4'
 
-  url 'https://downloads.navdy.com/desktop_support/release/navdy_desktop_mac.dmg'
+  url "https://downloads.navdy.com/desktop_support/release/navdy_desktop_mac_#{version}.dmg"
   name 'Navdy'
   homepage 'https://navdy.zendesk.com/hc/en-us/articles/115001590703'
 
   app 'Navdy.app'
 
   zap delete: [
-                '~/Library/Application Support/Navdy',
+                '~/Library/Saved Application State/com.navdy.application.desktop.savedState',
                 '~/Library/Preferences/com.navdy.application.desktop.plist',
               ],
-      trash:  [
-                '~/Library/Saved Application State/com.navdy.application.desktop.savedState',
-              ]
+      trash:  '~/Library/Application Support/Navdy'
 end
