609ec23be417f3c99150e4660e60d60a5d93148c
commitay <commitay@users.noreply.github.com>
2017-09-06 08:53:25 +1000

    Update navdy.rb

--- Casks/navdy.rb
+++ Casks/navdy.rb
@@ -2,15 +2,16 @@ cask 'navdy' do
   version '1.0.146'
   sha256 'f30f84016e435239cc2ca63a8ed5b43ec858c815310cfbfa147cea9bb4f940c4'
 
+  # downloads.navdy.com/desktop_support/release was verified as official when first introduced to the cask
   url "https://downloads.navdy.com/desktop_support/release/navdy_desktop_mac_#{version}.dmg"
   name 'Navdy'
   homepage 'https://navdy.zendesk.com/hc/en-us/articles/115001590703'
 
   app 'Navdy.app'
 
-  zap delete: [
-                '~/Library/Saved Application State/com.navdy.application.desktop.savedState',
+  zap delete: '~/Library/Saved Application State/com.navdy.application.desktop.savedState',
+      trash:  [
+                '~/Library/Application Support/Navdy',
                 '~/Library/Preferences/com.navdy.application.desktop.plist',
-              ],
-      trash:  '~/Library/Application Support/Navdy'
+              ]
 end
