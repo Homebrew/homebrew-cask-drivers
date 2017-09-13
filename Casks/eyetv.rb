996e93d153fb1edb6ca33c0199a2f14d206d8b23
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-09-13 23:47:55 +0100

    Update eyetv to 3.6.9,7520:20170730 (#151)

--- Casks/eyetv.rb
+++ Casks/eyetv.rb
@@ -1,11 +1,11 @@
 cask 'eyetv' do
-  version '3.6.9_7518'
-  sha256 'f6108cd5c2d6626c40b21e01b585d8a4f4a681cffd39b236376fce618133e974'
+  version '3.6.9,7520:20170730'
+  sha256 '41c092b92dd7470095d1748dde357de76f2331bd5e142f33d2aa3865fb5ce7a5'
 
   # file.geniatech.com/eyetv3 was verified as official when first introduced to the cask
-  url "http://file.geniatech.com/eyetv3/EyeTV#{version}.dmg"
+  url "http://file.geniatech.com/eyetv3/EyeTV#{version.before_comma}(#{version.before_colon.after_comma})#{version.after_colon}.dmg"
   appcast "https://www.geniatech.eu/eyetv/support/eyetv-#{version.major}-en/",
-          checkpoint: '3e6674a84a0329cf38da68c4de888af358441c11235913ae0dfa968b98de763e'
+          checkpoint: '99594ce7d7f905c2d8e533ca6d888e0ae3aa2d22a26a7382f35c9d4ce893c76a'
   name 'EyeTV'
   homepage 'https://www.geniatech.eu/eyetv/'
 
