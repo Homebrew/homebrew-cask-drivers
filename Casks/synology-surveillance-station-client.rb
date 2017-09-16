f912a5a716763b41579aa20109489b4d3902338d
Xo Wang <xo@geekshavefeelings.com>
2017-09-16 14:34:52 -0700

    Update synology-surveillance-station-client to 1.1.0-0324

--- Casks/synology-surveillance-station-client.rb
+++ Casks/synology-surveillance-station-client.rb
@@ -1,6 +1,6 @@
 cask 'synology-surveillance-station-client' do
-  version '1.0.5-0232'
-  sha256 '117ff0ffbdffe945297389980cc4744eb531949fe428cb7db3f20e7b4786f4e0'
+  version '1.1.0-0324'
+  sha256 '73d95edcbb7df4a7534ea5020cda00f6099b350d0059b8a917e95461526508ec'
 
   url "https://global.download.synology.com/download/Tools/SurveillanceStationClient/#{version}/Mac/Synology%20Surveillance%20Station%20Client-#{version}.dmg"
   name 'Synology Surveillance Station Client'
