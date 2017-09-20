df36bb5b1a6074443bffe7af7d2fa936d5a5036a
commitay <commitay@users.noreply.github.com>
2017-09-20 10:49:21 +1000

    Update nttcom-smart-card-reader-driver.rb

--- Casks/nttcom-smart-card-reader-driver.rb
+++ Casks/nttcom-smart-card-reader-driver.rb
@@ -2,7 +2,8 @@ cask 'nttcom-smart-card-reader-driver' do
   version '111'
   sha256 'f4decf3e2fbd08c86d6a9e6bede29661caa95581625795f97f28cb8a7ba94137'
 
-  url "http://www.ntt.com/content/dam/nttcom/hq/jp/business/services/application/authentication/jpki/etc/AcrMacDriver#{version}.dmg", user_agent: :fake
+  url "http://www.ntt.com/content/dam/nttcom/hq/jp/business/services/application/authentication/jpki/etc/AcrMacDriver#{version}.dmg",
+      user_agent: :fake
   name 'NTTCom Smart Card Reader Driver'
   homepage 'http://www.ntt.com/business/services/application/authentication/jpki.html'
 
@@ -11,4 +12,8 @@ cask 'nttcom-smart-card-reader-driver' do
   pkg 'NTTCom Smart Card Reader Driver.pkg'
 
   uninstall pkgutil: 'com.acs.acsccid'
+
+  caveats do
+    reboot
+  end
 end
