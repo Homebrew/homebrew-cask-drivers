60865b69265b0b0393edfa6f4e45c4c7d1a22b6c
Richard B. Wagner <breckwagner@gmail.com>
2017-09-28 16:05:13 -0400

    Updated nvidia-web-driver to 378.10.10.10.15.114
    Updated version but had to patch url pattern because last minor version number wasn't presant.

--- Casks/nvidia-web-driver.rb
+++ Casks/nvidia-web-driver.rb
@@ -1,8 +1,8 @@
 cask 'nvidia-web-driver' do
-  version '378.05.05.25f01'
-  sha256 '79b831457e0ba0b7f99ee69f49f1abcd106446713bfa2ffc09e4058b4dec501d'
+  version '378.10.10.10.15.114'
+  sha256 '02fafe29be21923ea9f647a9075171b37b2714a8f59cc28570b52311240e6ebb'
 
-  url "http://us.download.nvidia.com/Mac/Quadro_Certified/#{version}/WebDriver-#{version}.pkg"
+  url "http://us.download.nvidia.com/Mac/Quadro_Certified/#{version[0..14]}/WebDriver-#{version}.pkg"
   name 'NVIDIA Web Driver'
   homepage 'https://www.nvidia.com/download/driverResults.aspx/107369/en-us'
 
