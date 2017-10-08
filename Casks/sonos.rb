1aa5169502db0d685a5bd5f8ae47b76c406fc34b
Marco Slater <marco@marcoslater.com>
2017-10-08 20:49:16 +0100

    Updated Sonos Cask to Version 8.0 (#173)
    * Updated Sonos Cask to Version 8.0
    
    * Updated to use versioned URL.
    * Added SHA verification.
    * Added Zap for application folder.
    
    * sonos

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,12 +1,14 @@
 cask 'sonos' do
-  version :latest
-  sha256 :no_check
+  version '8.0'
+  sha256 '4362657796f74c66c1f342d50cc40233a04bc51a34bcc069bab57434c43541b7'
 
-  url 'http://www.sonos.com/redir/controller_software_mac'
+  url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   name 'Sonos'
   homepage 'http://www.sonos.com/'
 
   auto_updates true
 
   app 'Sonos.app'
+
+  zap trash: '~/Library/Application Support/Sonos'
 end
