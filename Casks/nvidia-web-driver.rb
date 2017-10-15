69bf42a5d0f85369ad44dfe333f86238cfa21eec
ickc <ickc@users.noreply.github.com>
2017-10-14 19:43:45 -0700

    Update nvidia-web-driver to 378.10.10.10.15.117 (#187)
    * Update nvidia-web-driver to 378.05.05.25f01
    
    * Update nvidia-web-driver.rb

--- Casks/nvidia-web-driver.rb
+++ Casks/nvidia-web-driver.rb
@@ -2,15 +2,14 @@ cask 'nvidia-web-driver' do
   if MacOS.version <= :sierra
     version '378.05.05.25f01'
     sha256 '79b831457e0ba0b7f99ee69f49f1abcd106446713bfa2ffc09e4058b4dec501d'
-    url "http://us.download.nvidia.com/Mac/Quadro_Certified/#{version}/WebDriver-#{version}.pkg"
   else
-    version '378.10.10.10.15.114'
-    sha256 '02fafe29be21923ea9f647a9075171b37b2714a8f59cc28570b52311240e6ebb'
-    url "http://us.download.nvidia.com/Mac/Quadro_Certified/#{version[0..14]}/WebDriver-#{version}.pkg"
+    version '378.10.10.10.15.117'
+    sha256 '9fe60527f3c93b1699505c6447567e8677120ef204bfc35ec0668b3a48edf4bc'
   end
 
+  url "http://us.download.nvidia.com/Mac/Quadro_Certified/#{version}/WebDriver-#{version}.pkg"
   name 'NVIDIA Web Driver'
-  homepage 'http://www.nvidia.com/download/driverResults.aspx/125379/en-us'
+  homepage 'https://www.nvidia.com/Download/index.aspx'
 
   pkg "WebDriver-#{version}.pkg"
 
