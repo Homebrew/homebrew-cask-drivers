33da5f50b49dd309472d9b531f0230e8d7801778
Kolen Cheung <christian.kolen@gmail.com>
2017-10-14 19:11:45 -0700

    Update nvidia-web-driver to 378.05.05.25f01

--- Casks/nvidia-web-driver.rb
+++ Casks/nvidia-web-driver.rb
@@ -4,8 +4,8 @@ cask 'nvidia-web-driver' do
     sha256 '79b831457e0ba0b7f99ee69f49f1abcd106446713bfa2ffc09e4058b4dec501d'
     url "http://us.download.nvidia.com/Mac/Quadro_Certified/#{version}/WebDriver-#{version}.pkg"
   else
-    version '378.10.10.10.15.114'
-    sha256 '02fafe29be21923ea9f647a9075171b37b2714a8f59cc28570b52311240e6ebb'
+    version '378.10.10.10.15.117'
+    sha256 '79b831457e0ba0b7f99ee69f49f1abcd106446713bfa2ffc09e4058b4dec501d'
     url "http://us.download.nvidia.com/Mac/Quadro_Certified/#{version[0..14]}/WebDriver-#{version}.pkg"
   end
 
