27265eca21c16f0ecc1c7075211cebe76c305015
commitay <commitay@users.noreply.github.com>
2017-10-18 14:47:17 +1000

    update blackvue-viewer to 1.13,74331

--- Casks/blackvue-viewer.rb
+++ Casks/blackvue-viewer.rb
@@ -1,6 +1,6 @@
 cask 'blackvue-viewer' do
-  version '1.10,74331'
-  sha256 '138e6db6dfa737fe4f00497d5296eb5464e888181979e83511d218ba0b5106e6'
+  version '1.13,74331'
+  sha256 '0eee70b2492d029fc959cb560d44be110f11355b9644f9e1930c2dba0d00326d'
 
   url "https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=#{version.after_comma}"
   name 'BlackVue Viewer'
