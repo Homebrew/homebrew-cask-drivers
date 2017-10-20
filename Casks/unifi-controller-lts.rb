54715d96d5629997d9883e5b2b823c047061ed5a
Lorenzo Villani <lorenzo@villani.me>
2017-10-20 04:43:28 +0200

    Add unifi-controller-lts 5.4.18 (#199)

--- Casks/unifi-controller-lts.rb
+++ Casks/unifi-controller-lts.rb
@@ -1,12 +1,12 @@
-cask 'unifi-controller' do
-  version '5.5.24'
-  sha256 'dfa72d9ac7eac57be622b539dca3ba958c5c9a2e496aab5226725f00771f6faf'
+cask 'unifi-controller-lts' do
+  version '5.4.18'
+  sha256 '31069768e6761e50ce4f1df022b31d851b8ead7cb0dc30d24df3a32a313b4f90'
 
   url "https://dl.ubnt.com/unifi/#{version}/UniFi.pkg"
-  name 'UniFi Controller'
+  name 'UniFi Controller - LTS version'
   homepage 'https://www.ubnt.com/download/unifi/'
 
-  conflicts_with cask: 'unifi-controller-lts'
+  conflicts_with cask: 'unifi-controller'
 
   pkg 'UniFi.pkg'
 
