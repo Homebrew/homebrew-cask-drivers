8404fd97e8c1e0ce030b8d48040a0fd33f40c697
commitay <commitay@users.noreply.github.com>
2017-10-22 18:41:48 +1000

    Rename yubiswitch to pallotron-yubiswitch (#207)

--- Casks/pallotron-yubiswitch.rb
+++ Casks/pallotron-yubiswitch.rb
@@ -1,10 +1,10 @@
-cask 'yubiswitch' do
+cask 'pallotron-yubiswitch' do
   version '0.12'
   sha256 '691ff7017c000211587217c60a6a9f352b3cb8d9ee2ce099f01d096d794a01ff'
 
   url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}/yubiswitch_#{version}.dmg"
   appcast 'https://github.com/pallotron/yubiswitch/releases.atom',
-          checkpoint: '8bfb9fd967ee7372c6df67f4e8666e9da5f38c42aa5fdb82abb54d1c49b1752e'
+          checkpoint: '31624b172828e2b1d3a2e2dabbe35f6243957eadf84d631a86c2684032db4710'
   name 'yubiswitch'
   homepage 'https://github.com/pallotron/yubiswitch'
 
