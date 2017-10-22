6ef57ffc3e30cd539958410d9659d7080e461df7
commitay <commitay@users.noreply.github.com>
2017-10-22 20:25:36 +1000

    Rename squeezeplay to lmsclients-squeezeplay

--- Casks/lmsclients-squeezeplay.rb
+++ Casks/lmsclients-squeezeplay.rb
@@ -1,11 +1,11 @@
-cask 'squeezeplay' do
+cask 'lmsclients-squeezeplay' do
   version '7.8.0r984'
   sha256 'a92808906a708169bc1d9107329cfb7d95ba1f00502a807e03e056b8f50bdfac'
 
   # downloads.sourceforge.net/lmsclients was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/lmsclients/SqueezePlay-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/lmsclients/rss?path=/squeezeplay/osx',
-          checkpoint: '9de25ee8d7e6cfd6ccb9b83de2b2796c776d36eb88d825fb973c62a44b86aac7'
+          checkpoint: '40e3dbf7f953949f6286347fd1caa25fa181079340b8aac312f87f483a45f990'
   name 'Logitech LMS SqueezePlay Client'
   homepage 'http://forums.slimdevices.com/showthread.php?96328-ANNOUNCE-SqueezePlay-for-Mac-OSX'
 
