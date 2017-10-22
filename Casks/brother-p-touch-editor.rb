096367e9d3781cf9646026c408de423feb25af01
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-10-22 22:25:06 +0200

    Add Brother QL-720NW label printer utilities and labeling app
    P-touch Editor is a labeling application which can be used with other Brother
    label printers, but useless without one, so it is submitted to the drivers
    repository.
    
    P-touch Update Software naming is unfortunate: it updates both label printer
    firmware (useful), and P-touch Editor (dubious).

---
+++ Casks/brother-p-touch-editor.rb
@@ -0,0 +1,15 @@
+cask 'brother-p-touch-editor' do
+  version '5.1.109'
+  sha256 'a1ecebc4688e5d44e5ca761501387f77c092ae96ed4dbc6fe5c6ce242fdf76af'
+
+  url "https://download.brother.com/pub/com/ptouch-su/editor/pem#{version.no_dots}x12us.dmg"
+  name 'Brother P-Touch Editor'
+  homepage 'http://www.brother.com/product/dev/label/editor/index.htm'
+
+  pkg "BrotherPtEdit#{version.major}.pkg"
+
+  uninstall pkgutil: [
+                       "com.brother.P-touchEditor#{version.major}.pkg",
+                       'com.brother.brotherptdriver.BrotherFonts_Common.pkg',
+                     ]
+end
