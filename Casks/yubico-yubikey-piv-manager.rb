86ca84928a6da6bb504591806a3f84f8a08280ad
commitay <commitay@users.noreply.github.com>
2017-10-22 13:45:49 +1000

    yubico-yubikey-piv-manager

--- Casks/yubico-yubikey-piv-manager.rb
+++ Casks/yubico-yubikey-piv-manager.rb
@@ -1,10 +1,10 @@
-cask 'yubikey-piv-manager' do
+cask 'yubico-yubikey-piv-manager' do
   version '1.4.2'
   sha256 'a8bb9b8dcd11616a607ab6f5cb7de55f00c4e17e4cbab725884f34d952944c99'
 
   url "https://developers.yubico.com/yubikey-piv-manager/Releases/yubikey-piv-manager-#{version}-mac.pkg"
   appcast 'https://developers.yubico.com/yubikey-piv-manager/Release_Notes.html',
-          checkpoint: 'f6a7b3b5eed1efb04fa174519ae3816a6beaf36c044d6d695a5264b202337b83'
+          checkpoint: '5583b200fe61e5c6871953416523b186fb8a62eccf93982631a99e3f91a36686'
   name 'YubiKey PIV Manager'
   homepage 'https://developers.yubico.com/yubikey-piv-manager/'
 
