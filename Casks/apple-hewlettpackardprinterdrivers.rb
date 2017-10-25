e323067c4eceb922cbb97f6f76201e9980a9953a
Bret Martin <bret_martin@h3biomedicine.com>
2017-10-25 14:36:39 -0400

    Update apple-hewlettpackardprinterdrivers to 5.1

--- Casks/apple-hewlettpackardprinterdrivers.rb
+++ Casks/apple-hewlettpackardprinterdrivers.rb
@@ -1,8 +1,8 @@
 cask 'apple-hewlettpackardprinterdrivers' do
-  version '5.0'
-  sha256 '02aba5aacbc812d995a3818a1b032f5b1e6aa124060ed5405f0e73d8be726667'
+  version '5.1'
+  sha256 '788e26e8afbfcf03d36f45e8563b1cd24183d6a8772b995914072189a714bd22'
 
-  url "http://support.apple.com/downloads/DL1888/en_US/hpprinterdrivers#{version}.dmg"
+  url "http://support.apple.com/downloads/DL1888/en_US/HewlettPackardPrinterDrivers#{version}.dmg"
   name 'HP Printer Drivers'
   homepage 'https://support.apple.com/kb/DL1888'
 
