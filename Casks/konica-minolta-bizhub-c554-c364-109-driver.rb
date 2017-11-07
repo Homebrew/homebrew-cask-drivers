1f121302852363ac5f86052358bd8ec5d8c658e4
commitay <commitay@users.noreply.github.com>
2017-11-07 14:37:00 +1000

    konica-minolta-bizhub-c554-c364-109-driver

--- Casks/konica-minolta-bizhub-c554-c364-109-driver.rb
+++ Casks/konica-minolta-bizhub-c554-c364-109-driver.rb
@@ -3,10 +3,8 @@ cask 'konica-minolta-bizhub-c554-c364-109-driver' do
   sha256 '5400c5c971baaccf1f5fdb67aef90a516faf07f39a195b00bb1f03126266ec21'
 
   url 'https://o.cses.konicaminolta.com/file/Default.aspx?fileid=2BECF238-AC0A-43BF-AC57-82E8E252A9BD&filetype=DA.dmg'
-  name 'Konica Minolta bizhub C554/C364 Postscript Printer Driver'
+  name 'Konica Minolta Bizhub C554/C364 Postscript Printer Driver'
   homepage 'https://www.biz.konicaminolta.com/download/driver.html'
-
-  depends_on macos: '>= :sierra'
 
   pkg 'bizhub_C554_C364_109.pkg'
 
