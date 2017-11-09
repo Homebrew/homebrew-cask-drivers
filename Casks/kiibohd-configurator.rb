7bfe432440ad8be35717d0020245293e72258168
Marco De Bortoli <marco.debo.debortoli@gmail.com>
2017-11-09 23:02:12 +0000

    Update Kiibohd Configurator to v0.3.1

--- Casks/kiibohd-configurator.rb
+++ Casks/kiibohd-configurator.rb
@@ -1,11 +1,11 @@
 cask 'kiibohd-configurator' do
-  version '0.3.0'
-  sha256 'bacc8c5e735d1cd851aebcba6b92bc772a1332a572361a6644b59808f012d07d'
+  version '0.3.1'
+  sha256 '2e529b465f99553cad8a79d8fa158cf2ba72fb3b0efea53e007a3d91e888bfe2'
 
   # github.com/kiibohd/configurator was verified as official when first introduced to the cask
   url "https://github.com/kiibohd/configurator/releases/download/v#{version}/kiibohd-configurator-#{version}-mac.dmg"
   appcast 'https://github.com/kiibohd/configurator/releases.atom',
-          checkpoint: '3003b21a8545599ab68076ef7ca06dab5e7609c629566b15a3f6edd1e146743b'
+          checkpoint: 'aff44721d184c9878c4007abd60bb2a4a00f5048bc27dbf923385bcf5051303e'
   name 'Kiibohd Configurator'
   homepage 'http://kiibohd.com/'
 
