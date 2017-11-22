d64d4eb78bdaaf6de007a6d19f19a403c343cc32
Kevin Elliott <kevin@welikeinc.com>
2017-11-22 00:25:36 -0800

    Add cask for 3dconnexion driver version r2688. (#250)
    * Add cask for 3dconnexion driver.
    
    * 3dconnexion

---
+++ Casks/3dconnexion.rb
@@ -0,0 +1,12 @@
+cask '3dconnexion' do
+  version '10-5-0_r2688'
+  sha256 '9620ef4895d0ed118bf950566c72bae421cc84de55bb2f301868e5654beee999'
+
+  url "https://www.3dconnexion.com/index.php?eID=sdl&ext=tx_iccsoftware&oid=dd910822-3bf1-5ce9-8145-59d4caa82238&filename=3DxWareMac_v#{version}.dmg"
+  name '3Dconnexion'
+  homepage 'https://www.3dconnexion.com/service/drivers.html'
+
+  pkg 'Install 3Dconnexion software.pkg'
+
+  uninstall pkgutil: 'com.3dconnexion.*'
+end
