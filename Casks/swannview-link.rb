9a6e4df7c4559c8a87bb36d06459ce01bc78cb23
Craig Gardener <CraigGardener@users.noreply.github.com>
2017-11-23 06:01:55 +0000

    Add SwannView Link 2.1.4.14 (#247)
    * Add SwannView Link 2.1.4.14
    
    * Fix download URL for SwannView Link 2.1.4.14

---
+++ Casks/swannview-link.rb
@@ -0,0 +1,10 @@
+cask 'swannview-link' do
+  version '2.1.4.14'
+  sha256 '6a695460bc886a36ca6e4e6a4446fb50078b998fe3a75b5077de347b49742aa3'
+
+  url 'http://support.swann.com/customer/portal/kb_article_attachments/101292/original.dmg?1481063317'
+  name 'SwannView Link'
+  homepage 'http://support.swann.com/customer/en/portal/articles/2171357-swannview-link-for-mac'
+
+  app 'SwannView Link.app'
+end
