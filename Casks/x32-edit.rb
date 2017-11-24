cac79311431600f991e0246bd494346dc127d2c6
Marco Slater <marco@marcoslater.com>
2017-11-24 13:58:54 +0000

    First attempt.

---
+++ Casks/x32-edit.rb
@@ -0,0 +1,12 @@
+cask 'x32-edit' do
+  version '3.2'
+  sha256 '2dee264d0df054e4f9b689af99046b7b28f5b8519cf1fba8df103ca30edcdc0a'
+
+  # Taken from http://www.music-group.com/brand/c/Behringer/downloads?active=Downloads
+  url "https://downloads.music-group.com/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
+  name 'X32-Edit'
+  homepage 'http://www.music-group.com/Categories/Behringer/Mixers/Digital-Mixers/X32/p/P0ASF'
+
+  app 'X32-Edit.app'
+  
+end
