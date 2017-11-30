85f8af28a7102fe5accf629ab7db550f4c0e94bd
Sergey Parhomenko <sergey@parhomenko.net>
2017-11-30 17:11:29 +0100

    Update canon-pixma-mg6200-printer-driver to 16.20.0.0

--- Casks/canon-pixma-mg6200-printer-driver.rb
+++ Casks/canon-pixma-mg6200-printer-driver.rb
@@ -1,9 +1,9 @@
 cask 'canon-pixma-mg6200-printer-driver' do
-  version '16.10.0.0'
-  sha256 'af0258d083e5dccb90d215925710688001e210181fca36833095c1d182b340e0'
+  version '16.20.0.0'
+  sha256 '2f45d1884892762238f389b635b4f76906baed4deb5277b4e14f22420d4f6ea4'
 
   # gdlp01.c-wss.com was verified as official when first introduced to the cask
-  url "http://gdlp01.c-wss.com/gds/5/0100005655/03/mcpd-mac-mg6200-#{version.dots_to_underscores}-ea21_3.dmg"
+  url "http://gdlp01.c-wss.com/gds/5/0100005655/05/mcpd-mac-mg6200-#{version.dots_to_underscores}-ea21_3.dmg"
   name 'Canon PIXMA MG6200 series CUPS Printer Driver'
   homepage 'https://www.usa.canon.com/internet/portal/us/home/support/details/printers/support-inkjet-printer/mg-series/pixma-mg6220'
 
