a9332f9c298356aad9fb8893a400d16abc619fbe
Sergey Parhomenko <sparhomenko@users.noreply.github.com>
2017-12-01 10:28:35 +0100

    Update canon-pixma-scanner-driver-ica to 4.0.0a (#260)

--- Casks/canon-pixma-scanner-driver-ica.rb
+++ Casks/canon-pixma-scanner-driver-ica.rb
@@ -1,9 +1,9 @@
 cask 'canon-pixma-scanner-driver-ica' do
-  version '4.0.0'
-  sha256 '0ea9a9a92ab90f60b0929803f7dc4b617d5225432c9316582b063178267d34cb'
+  version '4.0.0a'
+  sha256 '4c6c7b1549b8c400addc5d2bd2454b01dc8cff567bb17e2343240d9cba201d6a'
 
   # gdlp01.c-wss.com/gds/ was verified as official when first introduced to the cask
-  url "http://gdlp01.c-wss.com/gds/7/0100006587/01/misd-mac-ijscanner1-#{version.dots_to_underscores}-ea19_2.dmg"
+  url "http://gdlp01.c-wss.com/gds/7/0100006587/02/misd-mac-ijscanner1-#{version.dots_to_underscores.delete('^0-9_')}-ea19_2.dmg"
   name 'Canon PIXMA ICA Scanner Driver'
   homepage 'https://www.usa.canon.com/internet/portal/us/home/support/details/printers/support-inkjet-printer/mg-series/pixma-mg6220'
 
