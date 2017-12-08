4dfc62db984d0d9a747c9f269a72efd8d1470cf4
Christian Inzinger <christian@inz.io>
2017-12-08 14:38:36 +0100

    Update bose-soundtouch to 17.170.80-1819-25804d2,mr4_2017_73e7b563 (#271)

--- Casks/bose-soundtouch.rb
+++ Casks/bose-soundtouch.rb
@@ -1,6 +1,6 @@
 cask 'bose-soundtouch' do
-  version '16.140.46-1747-f4480ce,mr3_2017-fefda4ce'
-  sha256 'ca65da84a4c35f319f8dc17c98a011e9bcfac53869ccec131c1e87e012fdd793'
+  version '17.170.80-1819-25804d2,mr4_2017_73e7b563'
+  sha256 '6a02275b5373b0ec4c9af723f8b540e0174fbdb75b6ac28312bd32f70aa9b6d2'
 
   # bose.com was verified as official when first introduced to the cask
   url "https://downloads.bose.com/ced/soundtouch/#{version.after_comma}/SoundTouch-app-installer-#{version.before_comma}.dmg"
