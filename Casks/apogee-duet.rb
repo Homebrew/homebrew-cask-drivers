9141a56c43f8c1d8af89a3f23b573ef41fc3e143
Kris Anderson <kris@xsmail.com>
2017-12-11 20:59:06 -0800

    used https for homepage url

--- Casks/apogee-duet.rb
+++ Casks/apogee-duet.rb
@@ -1,10 +1,10 @@
 cask 'apogee-duet' do
   version 'June_2015'
-  sha256 '8873b43fbf8b354ba5adebaf8ce79eb0bb572c90ac86950c39b42d8f504649e3'
+  sha256 '4269af249372df7a50006f91a7dc1d73dddce9a878a247f98bd0a46d5e2619ee'
 
   url "https://www.apogeedigital.com/drivers/Duet_#{version}.dmg"
   name 'Apogee Duet'
-  homepage 'http://www.apogeedigital.com/support/software-downloads#tab-id-8'
+  homepage 'https://www.apogeedigital.com/support/software-downloads#tab-id-8'
 
   depends_on macos: '>= :mavericks'
 
