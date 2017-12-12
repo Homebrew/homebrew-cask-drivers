c32b28bda3cc37b87205b768573d6977394317f3
Kris Anderson <kris@xsmail.com>
2017-12-11 21:33:44 -0800

    correct the shasum 256 value for apogee-duet (#273)
    * used https for homepage url
    
    * downgrades to http

--- Casks/apogee-duet.rb
+++ Casks/apogee-duet.rb
@@ -1,6 +1,6 @@
 cask 'apogee-duet' do
   version 'June_2015'
-  sha256 '8873b43fbf8b354ba5adebaf8ce79eb0bb572c90ac86950c39b42d8f504649e3'
+  sha256 '4269af249372df7a50006f91a7dc1d73dddce9a878a247f98bd0a46d5e2619ee'
 
   url "https://www.apogeedigital.com/drivers/Duet_#{version}.dmg"
   name 'Apogee Duet'
