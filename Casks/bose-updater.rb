6392ae9181d5f1d6c5db69490ba831d922d179e6
commitay <commitay@users.noreply.github.com>
2017-12-15 13:03:50 +1000

    Update bose-updater to 1.5.4.1309.6342

--- Casks/bose-updater.rb
+++ Casks/bose-updater.rb
@@ -2,9 +2,9 @@ cask 'bose-updater' do
   version '1.5.4.1309.6342'
   sha256 '027fa124ce45342cc925e584fc1d76cd3a8f7761e3a6db0191f82cff3e6b967a'
 
-  url "http://downloads.bose.com/ced/boseupdater/mac/BoseUpdater_#{version}.dmg"
+  url "https://downloads.bose.com/ced/boseupdater/mac/BoseUpdater_#{version}.dmg"
   name 'Bose Updater'
-  homepage 'http://btu.bose.com/'
+  homepage 'https://btu.bose.com/'
 
   app 'Bose Updater.app'
 end
