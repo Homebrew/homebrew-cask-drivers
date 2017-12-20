f48c66468f0bb3036f02a192ee117266a21b0c39
Erick Guan <fantasticfears@gmail.com>
2017-12-20 14:21:15 +0100

    Add Handelsbanken card reader 1.0.0.7

---
+++ Casks/handelsbanken-card-reader.rb
@@ -0,0 +1,12 @@
+cask 'handelsbanken-card-reader' do
+  version '1.0.0.7'
+  sha256 '119394abf75fb85a08ba7c0c96272e61584b8a1666ce08fd64380a583622e569'
+
+  url 'https://www.handelsbanken.se/shb/inet/icentsv.nsf/vlookuppics/installation_handelsbanken_card_reader_mac_el_capitan/$file/shb_elcapitan.pkg'
+  name 'Handelsbanken Card Reader'
+  homepage 'https://www.handelsbanken.se/sv/privat/digitala-tjanster/installera-bankid'
+
+  pkg 'shb_elcapitan.pkg'
+
+  uninstall pkgutil: 'com.gemalto.handelsbanken1007.shbecr.pkg'
+end
