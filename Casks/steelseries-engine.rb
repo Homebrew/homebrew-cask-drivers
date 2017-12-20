bc88f496a97d5a3120ec5b04671bd1cd03085b96
Pontus Malm <malm@chalmers.it>
2017-12-20 16:37:59 +0100

    Update steelseries-engine to 3.11.8

--- Casks/steelseries-engine.rb
+++ Casks/steelseries-engine.rb
@@ -1,6 +1,6 @@
 cask 'steelseries-engine' do
-  version '3.11.7'
-  sha256 'fd72c73c1843777fe8508e79ade5b7f34110eeca90d77ca8c80df1684fc52988'
+  version '3.11.8'
+  sha256 '5565f1f2c3af60bc6767f20e3694137ec6b4680dce03a560634365a9075cbe15'
 
   # steelseriescdn.com was verified as official when first introduced to the cask
   url "https://downloads.steelseriescdn.com/drivers/engine/SteelSeriesEngine#{version}.pkg"
