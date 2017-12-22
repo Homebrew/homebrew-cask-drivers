204ecdba753db76c9ac38914fd0863e0b85dc9c6
Xo Wang <xo@geekshavefeelings.com>
2017-12-22 11:44:33 -0800

    Update nvidia-web-driver to 378.10.10.10.25.103

--- Casks/nvidia-web-driver.rb
+++ Casks/nvidia-web-driver.rb
@@ -1,10 +1,10 @@
 cask 'nvidia-web-driver' do
-  version '378.10.10.10.25.102'
-  sha256 '5eca1fb15b5bf14083b54fcc667ab57353f0600503d3d9daa317dbfe8b3941af'
+  version '378.10.10.10.25.103'
+  sha256 '815294b7229fd69679e6d96c1a4898e23f97b1b67a779a742ee488e25672a7ed'
 
   url "https://images.nvidia.com/mac/pkg/#{version.major}/WebDriver-#{version}.pkg"
   appcast 'https://gfe.nvidia.com/mac-update',
-          checkpoint: '9fd6c3b53687a3891ed1cbb6d52473e87039f6035bcfe85dae8df29c9a5a59c6'
+          checkpoint: '135278c3876fdc194b281293839d0e04421a0ac97545ae3bc8073eb81aacdb5a'
   name 'NVIDIA Web Driver'
   homepage 'https://www.nvidia.com/Download/index.aspx'
 
