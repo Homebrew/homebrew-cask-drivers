c1c8a77d1544e3ee79006e7928fe3fed71fc70b4
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2017-12-27 22:39:08 +0000

    Update yubico-yubikey-manager to 0.4.0 (#290)

--- Casks/yubico-yubikey-manager.rb
+++ Casks/yubico-yubikey-manager.rb
@@ -1,10 +1,10 @@
 cask 'yubico-yubikey-manager' do
-  version '0.3.1'
-  sha256 '55190b911e44a106b5b6f288a0ccdbd853d82e1c47ef0efd1029ef8dd24b5630'
+  version '0.4.0'
+  sha256 'f8cfac5d9d00f1f003377ddfe41bbc251e22def148c5fd92a3163cbef63b3486'
 
   url "https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-#{version}-mac.pkg"
   appcast 'https://developers.yubico.com/yubikey-manager-qt/Release_Notes.html',
-          checkpoint: '049557d1ee8dad39dbe2a2a50605219d261dedf0d54c96099c73c1643cea2a9f'
+          checkpoint: 'c9948fc0081554f24afddc0bfec81d288b83242b2554f61a06aa8f9c4880395e'
   name 'Yubikey Manager'
   homepage 'https://developers.yubico.com/yubikey-manager-qt/'
 
