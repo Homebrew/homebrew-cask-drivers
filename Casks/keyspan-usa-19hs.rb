0653a2890dae5c0947e208c5271dc22839e0a359
Anver Housseini <contact@anver.ch>
2018-01-02 22:22:13 +0100

    Update keyspan-usa-19hs.rb (#293)
    * Update keyspan-usa-19hs.rb
    
    * Update keyspan-usa-19hs.rb

--- Casks/keyspan-usa-19hs.rb
+++ Casks/keyspan-usa-19hs.rb
@@ -2,9 +2,9 @@ cask 'keyspan-usa-19hs' do
   version '4'
   sha256 '9a07a07d10c3cee5ea1cf3e67b091c30b26a7e24959602b258ae8060a7bf6ce3'
 
-  url "http://www.tripplite.com/shared/software/Driver/USA-19HS-Driver-v#{version}-Mac-OS-X-10.9-10.11.zip"
+  url "https://assets.tripplite.com/drivers/usa-19hs-driver-v#{version}-mac-os-x-10.9-10.11.zip"
   name 'Keyspan USA-19HS High-Speed USB-to-Serial Adapter Driver'
-  homepage 'http://www.tripplite.com/keyspan-high-speed-usb-to-serial-adapter~USA19HS/'
+  homepage 'https://www.tripplite.com/keyspan-high-speed-usb-to-serial-adapter~USA19HS/'
 
   depends_on macos: '>= :mavericks'
 
