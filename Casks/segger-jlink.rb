a68f591235ad40724678a3417af09a37caafb790
Joe Schneider <josschne@gmail.com>
2018-01-06 18:41:23 -0600

    Add Segger J-Link Command Line Tools v6.22d (#298)
    * Add Segger J-Link Command Line Tools v6.22d
    
    * Added caveats section
    
    * Update segger-jlink.rb

---
+++ Casks/segger-jlink.rb
@@ -0,0 +1,23 @@
+cask 'segger-jlink' do
+  version '6.22d'
+  sha256 '5449d51e7c84a17ce768c00a4788f7a9c9f902c37853e6923e3820fded409cd4'
+
+  url "https://www.segger.com/downloads/flasher/JLink_MacOSX_V#{version.no_dots}.pkg",
+      using: :post,
+      data:  {
+               'accept_license_agreement' => 'accepted',
+             }
+  name 'Segger J-Link Command Line Tools'
+  homepage 'https://www.segger.com/downloads/flasher'
+
+  pkg "JLink_MacOSX_V#{version.no_dots}.pkg"
+
+  uninstall pkgutil: 'com.segger.pkg.JLink'
+
+  caveats <<~EOS
+    Installing this Cask means you have AGREED to the
+    Segger Downloads License agreement available at
+
+      #{url}
+  EOS
+end
