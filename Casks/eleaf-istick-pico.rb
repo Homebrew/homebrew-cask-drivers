dd5f4976e820131c659193d21e3bae6e5727cada
Ted Strzalkowski <tedski@gmail.com>
2018-01-08 09:53:07 -0800

    Add UpdateEleaf.app v1.0.3 (#301)
    * Add UpdateEleaf.app v1.0.3
    
    Pico iStick Eleaf firmware updater.
    
    * eleaf-istick-pico

---
+++ Casks/eleaf-istick-pico.rb
@@ -0,0 +1,12 @@
+cask 'eleaf-istick-pico' do
+  version '1.03'
+  sha256 '0f9946cb9f7ce4be78778f5f3d4ba8d8e53f97d057c6c6f90ad97217f70fc9df'
+
+  url "http://www.eleafworld.com/wp-content/uploads/download/iStick_Pico_V#{version}.pkg.zip"
+  name 'UpdateEleaf iStick Pico'
+  homepage 'http://www.eleafworld.com/softwares-for-istick-pico/'
+
+  pkg 'UpdateEleaf.pkg'
+
+  uninstall pkgutil: 'com.mygreatcompany.pkg.UpdateEleaf'
+end
