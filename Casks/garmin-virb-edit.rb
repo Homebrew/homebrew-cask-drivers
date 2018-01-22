708f1d1b0fc8f4fd3decf84fff9b62ea5f07a9e3
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-22 07:39:50 +0000

    Update garmin-virb-edit to 5.2.1 (#312)

--- Casks/garmin-virb-edit.rb
+++ Casks/garmin-virb-edit.rb
@@ -1,12 +1,12 @@
 cask 'garmin-virb-edit' do
-  version '5.1.3'
-  sha256 '8bd79d4efdbb641c54b2a7f6044dcd9f2fb6cefb79e5431651af85be7df3cfa2'
+  version '5.2.1'
+  sha256 '02e997036a2aac4a519f9f799678e46ddb952df632ee7b51afe2bcd03a28abee'
 
   url "http://download.garmin.com/software/VIRBEditforMac_#{version.no_dots}.dmg"
   name 'Garmin VIRB Edit'
   homepage 'https://buy.garmin.com/en-US/US/p/573412'
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: '>= :el_capitan'
 
   pkg 'Install VIRB Edit.pkg'
 
