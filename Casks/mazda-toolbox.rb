00926d06172bd17cac4f1a8ffc905157127c23c3
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-29 22:06:27 +0000

    Update mazda-toolbox to 2017_05_16__14_25_TB4 (#322)

--- Casks/mazda-toolbox.rb
+++ Casks/mazda-toolbox.rb
@@ -2,9 +2,11 @@ cask 'mazda-toolbox' do
   version '2017_05_16__14_25_TB4'
   sha256 'bbaa78c28c8622a9e44c955567aa941ff77a01948364e68c2ce8179ace6f2747'
 
-  url "https://cdns.distrib.naviextras.com/content/!application/TOOLBOX/Agents/Mazda/OSX/#{version}/Mazda-Toolbox.dmg"
+  url "http://azure.distrib.naviextras.com/content/!application/TOOLBOX/Agents/Mazda/OSX/#{version}/Mazda-Toolbox.dmg"
   name 'Mazda Toolbox'
   homepage 'https://mazda-na.naviextras.com/shop/portal/downloads'
+
+  depends_on macos: '>= :yosemite'
 
   app 'Mazda Toolbox.app'
 
