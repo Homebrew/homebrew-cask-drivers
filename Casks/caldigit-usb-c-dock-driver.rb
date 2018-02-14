bb924884bd0c34bb6ddfacb61ac96bc9ef210a0b
Markus Reiter <me@reitermark.us>
2018-02-14 03:03:12 +0100

    Fix CalDigit USB-C Dock Driver.

--- Casks/caldigit-usb-c-dock-driver.rb
+++ Casks/caldigit-usb-c-dock-driver.rb
@@ -6,11 +6,17 @@ cask 'caldigit-usb-c-dock-driver' do
   name 'CalDigit USB-C Dock Driver'
   homepage 'http://www.caldigit.com/usb-3-1-usb-c-dock/faq.asp'
 
-  pkg 'USB-C-Dock-mac/CalDigit-USB-C-Dock-Driver.pkg'
+  pkg 'USB-C-Dock-mac/CalDigit-USB-C-Dock-Ethernet-Driver.pkg'
+  pkg 'USB-C-Dock-mac/CalDigit-USB-Hub-Support-Driver.pkg'
 
-  uninstall pkgutil: [
+  uninstall script:  'USB-C-Dock-mac/UnInstaller/CalDigit-USBC-Dock-Ethernet-Uninstaller',
+            kext:    'com.cypress.driver.CYUSB3610',
+            pkgutil: [
                        'com.CalDigit.caldigitUsbcDockEthernetHubSupportDrivers.CalDigitUSBHubSupport.pkg',
                        'com.CalDigit.caldigitUsbcDockEthernetHubSupportDrivers.postflight.pkg',
                        'com.CalDigit.caldigitUsbcDockEthernetHubSupportDrivers.preflight.pkg',
+                       'com.CalDigit.caldigitUsbcDockHubSupportDriver.CalDigitUSBHubSupport.pkg',
+                       'com.CalDigit.caldigitUsbcDockHubSupportDriver.postflight.pkg',
+                       'com.CalDigit.caldigitUsbcDockHubSupportDriver.preflight.pkg',
                      ]
 end
