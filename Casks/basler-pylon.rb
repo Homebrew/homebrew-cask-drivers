5286fc285f301f65c82b4920b155621983bc3715
Christopher Krooß <c.krooss@precibake.com>
2018-02-19 15:35:29 +0100

    Remove identifying information from basler-pylon download URL

--- Casks/basler-pylon.rb
+++ Casks/basler-pylon.rb
@@ -2,7 +2,7 @@ cask 'basler-pylon' do
   version '5.0.5.8973'
   sha256 '4f17df1c89337a49cf423775823d5fd31101409bd5c8c42751b9ac92c16d921d'
 
-  url "https://www.baslerweb.com/fp-1478427503/media/downloads/software/pylon_software/pylon-#{version}.dmg"
+  url "https://www.baslerweb.com/media/downloads/software/pylon_software/pylon-#{version}.dmg"
   name 'Basler Pylon Camera Software Suite'
   homepage 'https://www.baslerweb.com/'
 
