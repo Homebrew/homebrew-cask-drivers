10cfbbdeb11a88098e2c735ec0e2d62feb52f9ab
Christopher Krooß <c.krooss@precibake.com>
2018-02-19 01:24:10 +0100

    Rename pylon to basler-pylon and apply style changes

---
+++ Casks/basler-pylon.rb
@@ -0,0 +1,12 @@
+cask 'basler-pylon' do
+  version '5.0.5.8973'
+  sha256 '4f17df1c89337a49cf423775823d5fd31101409bd5c8c42751b9ac92c16d921d'
+
+  url "https://www.baslerweb.com/fp-1478427503/media/downloads/software/pylon_software/pylon-#{version}.dmg"
+  name 'Basler Pylon Camera Software Suite'
+  homepage 'https://www.baslerweb.com/'
+
+  pkg "pylon-#{version}.pkg"
+
+  uninstall pkgutil: 'com.baslerweb.pylon.*'
+end
