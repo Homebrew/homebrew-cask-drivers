414321e534b80bc557579515e38a98290f919528
ckrooss <ckrooss@users.noreply.github.com>
2018-02-19 17:50:18 +0100

    Add Basler Pylon Camera Suite 5.0.5 (#337)
    * Add Basler Pylon Camera Suite 5.0.5
    
    * Rename pylon to basler-pylon and apply style changes
    
    * add eula caveat
    
    * Remove identifying information from basler-pylon download URL

---
+++ Casks/basler-pylon.rb
@@ -0,0 +1,19 @@
+cask 'basler-pylon' do
+  version '5.0.5.8973'
+  sha256 '4f17df1c89337a49cf423775823d5fd31101409bd5c8c42751b9ac92c16d921d'
+
+  url "https://www.baslerweb.com/media/downloads/software/pylon_software/pylon-#{version}.dmg"
+  name 'Basler Pylon Camera Software Suite'
+  homepage 'https://www.baslerweb.com/'
+
+  pkg "pylon-#{version}.pkg"
+
+  uninstall pkgutil: 'com.baslerweb.pylon.*'
+
+  caveats <<~EOS
+    Installing this Cask means you have AGREED to the
+    PYLON END-USER LICENSE AGREEMENT available at:
+
+      https://www.baslerweb.com/en/service/pylon-eula/
+  EOS
+end
