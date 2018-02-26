5ef02311fcef3cb3dbab2814d3eeabcfde554ef4
Claudia <claui@users.noreply.github.com>
2018-02-26 19:16:14 +0100

    Fix lg-onscreencontrol 2.82 (#342)
    * Fix lg-onscreencontrol 2.82
    
    This commit adds the following improvements to the cask:
    
    - Change the `homepage` to a page that is more related to the app
    
    - Add the `accessibility_access` stanza to inform the user that
      accessibility needs to be enabled for the app to function
      properly
    
    - The PKG installer changes the permissions of both `/usr/local`
      and `/usr/local/lmm` to world-writable on install time for no
      good reason. Because this is very unsafe and introduces a
      needless security risk, this commit introduces a `postflight`
      block, which immediately sets those permissions back to sane
      values.
    
    - Remove the OSCMultiMonitor login item when uninstalling
    
    - Quit OSCMultiMonitor.app before uninstalling
    
    - Add `zap` stanza for cache files and preferences
    
    * Update lg-onscreencontrol.rb
    
    * Update lg-onscreencontrol.rb
    
    * Update lg-onscreencontrol.rb
    
    * Update lg-onscreencontrol.rb

--- Casks/lg-onscreencontrol.rb
+++ Casks/lg-onscreencontrol.rb
@@ -4,14 +4,35 @@ cask 'lg-onscreencontrol' do
 
   url "https://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150320442554&what=MANUAL&fromSystem=LG.COM&fileId=A3JIVu40gLdP5icqVH59Q&ORIGINAL_NAME_b1_a1=Mac_OSC_#{version}.zip"
   name 'LG OnScreen Control'
-  homepage 'http://www.lg.com/'
+  homepage 'https://www.lg.com/us/support/monitors'
+
+  accessibility_access true
 
   pkg "OnScreenControl_V#{version}.pkg"
 
-  uninstall login_item: 'OnScreen Control',
+  postflight do
+    system_command '/bin/chmod',
+                   args: ['755', '/usr/local', '/usr/local/lmm'],
+                   sudo: true
+  end
+
+  uninstall login_item: [
+                          'OnScreen Control',
+                          'OSCMultiMonitor',
+                        ],
+            quit:       [
+                          'com.LGSI.OnScreen-Control',
+                          'com.LGSI.OSCMultiMonitor',
+                        ],
             pkgutil:    [
                           'com.lge.onscreenControl.*',
                           'com.lge.OnscreenControl.*',
-                        ],
-            quit:       'com.LGSI.OnScreen-Control'
+                        ]
+
+  zap trash: [
+               '~/Library/Caches/com.LGSI.OnScreen-Control',
+               '~/Library/Caches/com.LGSI.OSCMultiMonitor',
+               '~/Library/Preferences/com.LGSI.OnScreen-Control.plist',
+               '~/Library/Preferences/com.LGSI.OSCMultiMonitor.plist',
+             ]
 end
