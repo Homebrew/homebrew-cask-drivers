d7a69847e5de0be8389f266f75bc187853b9fe9e
Alexander Pakulov <a.pakulov@gmail.com>
2018-03-24 22:33:56 -0700

    Update LG Screen Manager to 2.09 (#372)

--- Casks/lg-screen-manager.rb
+++ Casks/lg-screen-manager.rb
@@ -1,8 +1,8 @@
 cask 'lg-screen-manager' do
-  version '2.08'
-  sha256 'b4b757d2d79e81c18011965267a12a7d774778b3d850c4f7c0e6482d628ed965'
+  version '2.09'
+  sha256 '6e8102c56ea2480dc880e816ca2c2073a8e01f846d55e6337f72dab22b4727aa'
 
-  url "https://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150222793490&what=MANUAL&fromSystem=LG.COM&fileId=uni0URRVZmqPSUpDV7E05g&ORIGINAL_NAME_b1_a1=LGScreenManager_#{version}.zip"
+  url "https://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150222793490&what=MANUAL&fromSystem=LG.COM&fileId=ZU4yJWlyb8WWcDVbmOgCJg&ORIGINAL_NAME_b1_a1=LGScreenManager_#{version}.zip"
   name 'LG Screen Manager'
   homepage 'http://www.lg.com/us/support-product/lg-27MD5KA-B'
 
