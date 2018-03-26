8659c949cf1be8b4e6cc9e288f003840460f1851
Markus Reiter <me@reitermark.us>
2018-03-26 08:10:20 +0200

    Update LG OnScreen Control to 2.86.

--- Casks/lg-onscreen-control.rb
+++ Casks/lg-onscreen-control.rb
@@ -1,14 +1,14 @@
-cask 'lg-onscreencontrol' do
-  version '2.82'
-  sha256 '2cc18bea2b9380f508ea4032adbdff3848bd4c2759fe37c1dd15c9080d2ba63b'
+cask 'lg-onscreen-control' do
+  version '2.86,Xo0dELqtvVrShJQdcnY3Lw'
+  sha256 '6c51af51f08094ab9065eced026408fc169fdf563289fd278a579e9889f2fa28'
 
-  url "https://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150320442554&what=MANUAL&fromSystem=LG.COM&fileId=A3JIVu40gLdP5icqVH59Q&ORIGINAL_NAME_b1_a1=Mac_OSC_#{version}.zip"
+  url "https://www.lg.com/us/lgecs.downloadFile.ldwf?DOC_ID=20150320442554&what=MANUAL&fromSystem=LG.COM&fileId=#{version.after_comma}&ORIGINAL_NAME_b1_a1=Mac_OSC_#{version.before_comma}.zip"
   name 'LG OnScreen Control'
   homepage 'https://www.lg.com/us/support/monitors'
 
   accessibility_access true
 
-  pkg "OnScreenControl_V#{version}.pkg"
+  pkg "OSC_V#{version.before_comma}_Patch8.pkg"
 
   postflight do
     system_command '/bin/chmod',
