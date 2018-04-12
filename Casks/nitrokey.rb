ed03e991d8effed9f7635fae7b28271e0c19cc51
Szczepan Zalega <szczepan@nitrokey.com>
2018-04-12 12:52:55 +0200

    Remove commented full url
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- Casks/nitrokey.rb
+++ Casks/nitrokey.rb
@@ -3,7 +3,6 @@ cask 'nitrokey' do
   sha256 '6fe4540441f637c8ecc440f1564866295de8cb1cd5f9e2f5f7adaf13d2575b89'
 
   # github.com/Nitrokey/nitrokey-app was verified as official when first introduced to the cask
-  # full url: 'https://github.com/Nitrokey/nitrokey-app/releases/download/v1.3/Nitrokey.App.dmg'
   url "https://github.com/Nitrokey/nitrokey-app/releases/download/v#{version}/Nitrokey.App.dmg"
   appcast 'https://github.com/Nitrokey/nitrokey-app/releases.atom',
           checkpoint: '557a05e9b7a326526bfadda52d670aa59097d2fb2c8ad42bdc1a05f119d19a23'
