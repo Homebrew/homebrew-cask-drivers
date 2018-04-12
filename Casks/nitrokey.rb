c5baca25e0be29a9ca204f1ca197324af3443db2
Szczepan Zalega <szszszsz@users.noreply.github.com>
2018-04-12 13:39:22 +0200

    Update Nitrokey App to v1.3 (#383)

--- Casks/nitrokey.rb
+++ Casks/nitrokey.rb
@@ -1,12 +1,12 @@
 cask 'nitrokey' do
-  version '1.2'
-  sha256 'f137979318b91915cadbb8e52fbea3c529b3dfdfd807a974b54b06e3582575be'
+  version '1.3'
+  sha256 '6fe4540441f637c8ecc440f1564866295de8cb1cd5f9e2f5f7adaf13d2575b89'
 
   # github.com/Nitrokey/nitrokey-app was verified as official when first introduced to the cask
-  url "https://github.com/Nitrokey/nitrokey-app/releases/download/v#{version}/nitrokey-app-v#{version}.dmg"
+  url "https://github.com/Nitrokey/nitrokey-app/releases/download/v#{version}/Nitrokey.App.dmg"
   appcast 'https://github.com/Nitrokey/nitrokey-app/releases.atom',
-          checkpoint: '557a05e9b7a326526bfadda52d670aa59097d2fb2c8ad42bdc1a05f119d19a23'
-  name 'Nitrokey'
+          checkpoint: '4ffd829b3bc2062a7d807bdc34a441f4b37f73b91cb3943e8ffdcfffad0fccc3'
+  name 'Nitrokey App'
   homepage 'https://www.nitrokey.com/download/macos'
 
   app "Nitrokey App v#{version}.app"
