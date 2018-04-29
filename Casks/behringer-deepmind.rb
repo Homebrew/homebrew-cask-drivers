c9958ddffe050cfaea2f54dda460e3300765de2c
Joshua McKinney <joshka@users.noreply.github.com>
2018-04-28 23:51:40 -0700

    Add Behringer DeepMind 12 app
    Note the app is named DeepMind 12 (this is not the version).
    App works for Behringer DeepMind 6, DeepMind 12, DeepMind 12D synths
    Confirm url is linked from
    https://www.musictri.be/Categories/Behringer/Keyboards/Synthesizers-and-Samplers/DEEPMIND-12/p/P0AC5/downloads

---
+++ Casks/behringer-deepmind.rb
@@ -0,0 +1,11 @@
+cask 'behringer-deepmind' do
+  version :latest
+  sha256 :no_check
+
+  # s3.amazonaws.com/cdn.mymcloud.xyz/behringer was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/cdn.mymcloud.xyz/behringer/application/DeepMindAppMacOS.app.zip'
+  name 'Behringer Deepmind 12'
+  homepage 'https://www.musictri.be/brand/behringer/home'
+
+  app 'DeepMind12.app'
+end
