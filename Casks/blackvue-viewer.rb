11f18e25d98b533797dd88cbae797218426ee305
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-03 20:31:46 +0100

    Update blackvue-viewer to 1.20,74331

--- Casks/blackvue-viewer.rb
+++ Casks/blackvue-viewer.rb
@@ -1,10 +1,12 @@
 cask 'blackvue-viewer' do
-  version '1.13,74331'
-  sha256 '0eee70b2492d029fc959cb560d44be110f11355b9644f9e1930c2dba0d00326d'
+  version '1.20,74331'
+  sha256 '82784dcb6c006998a06acb94bd4eab8030958a70f3fbff290793c7722d65a58b'
 
   url "https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=#{version.after_comma}"
   name 'BlackVue Viewer'
   homepage 'https://www.blackvue.com/'
+
+  depends_on macos: '>= :yosemite'
 
   app 'BlackVue Viewer.app'
 end
