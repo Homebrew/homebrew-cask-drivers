3bc782988909c6be136bc88bb478e5292e0c9675
Vítor Galvão <info@vitorgalvao.com>
2018-05-06 18:29:25 +0100

    Update ps4-remote-play.rb

--- Casks/ps4-remote-play.rb
+++ Casks/ps4-remote-play.rb
@@ -5,7 +5,7 @@ cask 'ps4-remote-play' do
   # remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg was verified as official when first introduced to the cask
   url 'https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg'
   name 'PS4 Remote Play'
-  homepage 'https://www.playstation.com/'
+  homepage 'https://www.playstation.com/en-au/get-help/help-library/apps---features/playstation-apps---features/ps4--remote-play-for-pc-and-mac/'
 
   pkg 'RemotePlayInstaller.pkg'
 
