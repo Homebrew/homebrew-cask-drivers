5450cfd6925725aeb7e8e09cf11589218c8fa096
Ilya Medvedev <medved55rus@gmail.com>
2018-05-07 07:15:22 +0000

    Evolv EScribe Suite 2_SP15_1
    EScribe Suite 2.0 SP15.1
    
    --- EScribe ---
    File -> New now asks questions to avoid some configuration when resetting a device's settings. (This should ease Replay setup on DNA 75 Color.)
    --- EScribe Suite ---
    Minor bug fixes, cosmetic, and usability improvements.
     
    
    EScribe Suite 2.0 SP15
    
    --- DNA 75 Color (firmware 1.1 SP33.2) ---
    Replay (International Edition only). To use Replay:
    (1) Apply the Service Pack.
    (2) Choose a profile to put Replay on. Check "Use Replay on this profile", and name it how you like. (For most people, the easiest way to use Replay is to choose a profile whose material is Watts.)
    (3) Load on a theme with Replay support. (The default theme has Replay support. Go to the Theme tab and click Default Theme, and upload.)
    --- DNA 250 Color (firmware 1.1 SP33.2) ---
    Minor bug fixes and reorganization.
    --- Theme Designer ---
    Double-clicking now works when a control has multiple conditions.
    The default DNA Color theme now uses 0.5W increments instead of 0.1W increments.
    --- EScribe Suite ---
    Made tooltips show up on fields now instead of just their labels, and added some new tooltips.
    Permission errors on save/load are now more descriptive.
    Upgraded many icons to high-DPI variants.
    Minor bug fixes and usability improvements.

--- Casks/evolv-escribe-suite.rb
+++ Casks/evolv-escribe-suite.rb
@@ -1,12 +1,12 @@
 cask 'evolv-escribe-suite' do
-  version '2_SP13'
-  sha256 '6c9feaf534faf5e14a3521ff24df921e189fd26fed932e9c6b29850f8fc29bc5'
+  version '2_SP15_1'
+  sha256 '2d2b636ae9c7ec001667f98ae585f48affb301209a1a7398421efb9bad342adf'
 
-  url "https://downloads.evolvapor.com/SetupEScribe#{version}_INTL.pkg"
+  url "https://downloads.evolvapor.com/SetupEScribe#{version}_INT.pkg"
   name 'EScribe Suite'
   homepage 'https://www.evolvapor.com/'
 
-  pkg "SetupEScribe#{version}_INTL.pkg"
+  pkg "SetupEScribe#{version}_INT.pkg"
 
   uninstall pkgutil: [
                        'org.ecigstats.Main',
