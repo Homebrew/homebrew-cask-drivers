3d251ab5ac445201117a6ba093c665480b66a7f9
João Paulo Cercal <jpcercal@gmail.com>
2018-05-08 15:53:29 +0200

    Add RemotePlayInstaller.pkg. (#410)
    * Add RemotePlayInstaller.pkg.Add RemotePlayInstaller.pkg.
    
    Add the latest version of the "PS4 Remote Play" for the console Sony PlayStation 4 from the official source.
    
    * Update ps4-remote-play.rb
    
    * Update and rename ps4-remote-play.rb to sony-remoteplay.rb

---
+++ Casks/sony-remoteplay.rb
@@ -0,0 +1,21 @@
+cask 'sony-remoteplay' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg'
+  name 'PS4 Remote Play'
+  homepage 'https://remoteplay.dl.playstation.net/remoteplay/'
+
+  depends_on macos: '>= :yosemite'
+
+  pkg 'RemotePlayInstaller.pkg'
+
+  uninstall pkgutil: 'com.playstation.RemotePlay.pkg'
+
+  zap trash: [
+               '~/Library/Application Support/Sony Corporation/PS4 Remote Play',
+               '~/Library/Caches/com.playstation.RemotePlay',
+               '~/Library/Cookies/com.playstation.RemotePlay.binarycookies',
+               '~/Library/Preferences/com.playstation.RemotePlay.plist',
+             ]
+end
