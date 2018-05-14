23c09cfe1de041656b70cacfb9fd57a40e9183f8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-14 23:39:21 +0100

    Update gutenprint to 5.2.14 (#421)

--- Casks/gutenprint.rb
+++ Casks/gutenprint.rb
@@ -1,10 +1,10 @@
 cask 'gutenprint' do
-  version '5.2.13'
-  sha256 'e2af8941ee2ec2aa244591caec95c1cd89718ca77007eeaa0fca07328b0648bf'
+  version '5.2.14'
+  sha256 'b1a58df72e3719b48de3b802d7e5c5a4d7d355170d8a99a4c45458a2bf871cb9'
 
   url "https://downloads.sourceforge.net/gimp-print/gutenprint-#{version.major_minor}/#{version}/gutenprint-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/gimp-print/rss',
-          checkpoint: '99893d15cc4d8019254b00cbe0f600c91bfa8a718a3904a27fa60d6c93550640'
+          checkpoint: '0a4faf30408fb2346655a995b383888599b682e8306112d75ec872b9aaee4ff0'
   name 'Gutenprint'
   homepage 'http://gimp-print.sourceforge.net/'
 
