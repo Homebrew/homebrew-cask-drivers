62b3d5386dc861e3f00f530acccd786a635fd8a1
commitay <commitay@users.noreply.github.com>
2018-05-24 22:14:09 +1000

    Update hobbyquaker-arcticfox-config.rb

--- Casks/hobbyquaker-arcticfox-config.rb
+++ Casks/hobbyquaker-arcticfox-config.rb
@@ -1,10 +1,10 @@
 cask 'hobbyquaker-arcticfox-config' do
-  version '1.11.9'
-  sha256 'd70536342088b6e592a484d61425e94c3309f642b15acb3e1a03a79bc8b31f69'
+  version '1.11.10'
+  sha256 '339279b0d46e2bde414b63d4ab349fb9e9206c953148c49eb303daf42aee3d7f'
 
   url "https://github.com/hobbyquaker/arcticfox-config/releases/download/v#{version}/arcticfox-config-#{version}.dmg"
   appcast 'https://github.com/hobbyquaker/arcticfox-config/releases.atom',
-          checkpoint: 'ef6c545fd5b45177cdf44f945bd728ce2925a5ba4aa1fca1dce51311ff412890'
+          checkpoint: '5c16937012c13e082078543176ef4bb72b22f673d05f485e3514c75be69e5926'
   name 'Arcticfox Config'
   homepage 'https://github.com/hobbyquaker/arcticfox-config'
 
