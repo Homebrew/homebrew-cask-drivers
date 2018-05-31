79a22135b0b047d706e7e0fd67faaf05db6e64da
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2018-05-31 14:23:40 +0200

    Add Philips Hue Sync v1.0.0.3

---
+++ Casks/philips-hue-sync.rb
@@ -0,0 +1,21 @@
+cask 'philips-hue-sync' do
+  version '1.0.0.3'
+  sha256 '8ca61ab7fb9b40543953a4ffdf4189a1b46c4c0cdee8f6fae0bc3c2daef4a989'
+
+  url do
+    require 'open-uri'
+    require 'net/http'
+    # No known stable URL; fetching disposable URL from landing site
+    uri = URI('https://flex1548-esd.flexnetoperations.com/flexnet/operations/WebContent?fileID=HueSyncMac')
+    res = Net::HTTP.get_response(uri)
+    res['location']
+  end
+  name 'Philips Hue Sync'
+  homepage 'https://www2.meethue.com/en-us/entertainment/hue-sync'
+
+  accessibility_access true
+
+  pkg "HueSyncInstaller_#{version}.pkg"
+
+  uninstall pkgutil: 'com.lighting.huesync'
+end
