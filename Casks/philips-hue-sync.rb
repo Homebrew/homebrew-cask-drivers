2cef9196c4a0a1b1031c53cf69946f2d61fbf44f
commitay <commitay@users.noreply.github.com>
2018-06-06 12:52:11 +1000

    Update philips-hue-sync.rb

--- Casks/philips-hue-sync.rb
+++ Casks/philips-hue-sync.rb
@@ -1,21 +1,22 @@
 cask 'philips-hue-sync' do
-  version '1.0.0.3'
-  sha256 '8ca61ab7fb9b40543953a4ffdf4189a1b46c4c0cdee8f6fae0bc3c2daef4a989'
+  version :latest
+  sha256 :no_check
 
-  url do
-    require 'open-uri'
-    require 'net/http'
-    # flex1548-esd.flexnetoperations.com/flexnet/operations was verified as official when first introduced to the cask
-    uri = URI('https://flex1548-esd.flexnetoperations.com/flexnet/operations/WebContent?fileID=HueSyncMac')
-    res = Net::HTTP.get_response(uri)
-    res['location']
-  end
+  # flex1548-esd.flexnetoperations.com/flexnet/operations was verified as official when first introduced to the cask
+  url 'https://flex1548-esd.flexnetoperations.com/flexnet/operations/WebContent?fileID=HueSyncMac'
   name 'Philips Hue Sync'
   homepage 'https://www2.meethue.com/en-us/entertainment/hue-sync'
 
   accessibility_access true
+  container type: :naked
 
-  pkg "HueSyncInstaller_#{version}.pkg"
+  pkg 'philips-hue-sync.pkg'
+
+  # This is a horrible hack to force the file extension.
+  # The backend code should be fixed so that this is not needed.
+  preflight do
+    system_command '/bin/mv', args: ['--', staged_path.join('WebContent'), staged_path.join('philips-hue-sync.pkg')]
+  end
 
   uninstall pkgutil: 'com.lighting.huesync'
 end
