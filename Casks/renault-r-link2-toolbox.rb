c1c6977813de2cfcf510a5fc3ebc9a02f1bd3aa0
Work <brigitte@corecode.io>
2018-06-08 12:39:20 +0200

    cask renamed from r-link2-toolbox to renault-r-link2-toolbox, comment added

--- Casks/renault-r-link2-toolbox.rb
+++ Casks/renault-r-link2-toolbox.rb
@@ -1,4 +1,5 @@
-cask 'r-link2-toolbox' do
+cask 'renault-r-link2-toolbox' do
+  # note: "2" is not a version number, but an intrinsic part of the product name
   version :latest
   sha256 :no_check
 
