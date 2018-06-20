1ee56b93a9f9a86d2194dbc64e9bb624b943e038
suschizu <brigitte@corecode.io>
2018-06-20 04:51:48 +0200

    Add R-Link 2 Toolbox.app Latest (#445)

---
+++ Casks/renault-r-link-2-toolbox.rb
@@ -0,0 +1,12 @@
+cask 'renault-r-link-2-toolbox' do
+  # note: "2" is not a version number, but an intrinsic part of the product name
+  version :latest
+  sha256 :no_check
+
+  # rcs-rlmanager.aw.atos.net was verified as official when first introduced to the cask
+  url 'https://rcs-rlmanager.aw.atos.net/archives/mac/RLink2Toolbox-latest.dmg'
+  name 'R-Link 2 Toolbox'
+  homepage 'https://www.renault-multimedia.com/gbr_EN/estore'
+
+  app 'R-Link 2 Toolbox.app'
+end
