4952058e34845c7cd425e5b8d1ed59b36236b6ad
Jerry Yang <zhenyoung@gmail.com>
2018-06-25 09:11:56 -0700

    Add Reolink client 3.4.2.49

---
+++ Casks/reolink.rb
@@ -0,0 +1,13 @@
+cask 'reolink' do
+  version '3.4.2.49'
+  sha256 '9a0f882ca429cf0fef4162aa51d6de751aac674670fedcb346a1a0b3a85ae30e'
+
+  # s3.amazonaws.com/reolink-storage was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/reolink-storage/website/client/Mac-ReolinkClient.zip'
+  name 'Reolink Client'
+  homepage 'https://reolink.com/software-and-manual/'
+
+  container nested: "Mac-ReolinkClient/Mac-ReolinkClient-#{version}.dmg"
+
+  app 'Reolink Client.app'
+end
