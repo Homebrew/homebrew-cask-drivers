9e953b2facd73a0a5e30880031cea1018de1cc97
Beat Rupp <beatrupp@posteo.net>
2018-07-19 06:14:57 +0200

    Update bragi-updater to 1.1.4 (#475)

--- Casks/bragi-updater.rb
+++ Casks/bragi-updater.rb
@@ -1,6 +1,6 @@
 cask 'bragi-updater' do
-  version '1.1.3'
-  sha256 'f3b082989cf1c178a66287f801933c2248f8c9d048e7654bc35081d42e28aeaa'
+  version '1.1.4'
+  sha256 'ab3e12163bf4d73c641c1b249bc7a290d8eeb2034465d243b1e194f4e2f76f94'
 
   url "http://update.bragi.com/bin/Bragi%20Updater-#{version}.dmg"
   appcast 'http://update.bragi.com/'
