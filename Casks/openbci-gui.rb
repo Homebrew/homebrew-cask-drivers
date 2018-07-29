47d2f880d427ee4cb36ce78293de045dbbe7b830
Vítor Galvão <info@vitorgalvao.com>
2018-07-29 14:11:31 +0100

    Update openbci-gui.rb

--- Casks/openbci-gui.rb
+++ Casks/openbci-gui.rb
@@ -2,7 +2,7 @@ cask 'openbci-gui' do
   version '3.3.2'
   sha256 '02938f40abf5382fb00472f454ce455ad79337e03170c80bcd4ec4395152f095'
 
-  # github.com/OpenBCI/OpenBCI_GUI/releases was verified as official when first introduced to the cask
+  # github.com/OpenBCI/OpenBCI_GUI was verified as official when first introduced to the cask
   url "https://github.com/OpenBCI/OpenBCI_GUI/releases/download/v#{version}/application.macosx.zip"
   appcast 'https://github.com/OpenBCI/OpenBCI_GUI/releases.atom'
   name 'OpenBCI GUI'
