ecc40cadd23d0980705d2a026289ee0d7a31879a
Julian Mayer <julian@corecode.io>
2018-08-28 14:46:15 +0200

    Update synology-drive to 1.1.2-10562

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -1,6 +1,6 @@
 cask 'synology-drive' do
-  version '1.1.1-10551'
-  sha256 '5cd4d88bc40853a24901ef82e85257de98f5240f5244e9d2760210bd8e95f8f8'
+  version '1.1.2-10562'
+  sha256 '335a5a379f25c9e23b6988b8eea4e9dbd538d3f325f90bc3c219a1dbfb1a4f3d'
 
   url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version}/Mac/Installer/synology-drive-#{version.sub(%r{.*-}, '')}.dmg"
   appcast 'https://www.synology.com/en-global/releaseNote/SynologyDriveClient'
