2b4ac4bae923762fb20aef290750293215430980
Corey Gouker <corey@coreygo.com>
2018-08-28 12:10:07 -0700

    Update openbci-gui to 3.4.0

--- Casks/openbci-gui.rb
+++ Casks/openbci-gui.rb
@@ -1,9 +1,9 @@
 cask 'openbci-gui' do
-  version '3.3.2'
-  sha256 '02938f40abf5382fb00472f454ce455ad79337e03170c80bcd4ec4395152f095'
+  version '3.4.0'
+  sha256 'f0c197590193bd7af00bc56c1e1ff8789ef4bd962cbd7abee1ab7f81558e1c34'
 
   # github.com/OpenBCI/OpenBCI_GUI was verified as official when first introduced to the cask
-  url "https://github.com/OpenBCI/OpenBCI_GUI/releases/download/v#{version}/application.macosx.zip"
+  url "https://github.com/OpenBCI/OpenBCI_GUI/releases/download/#{version}/application.macosx.zip"
   appcast 'https://github.com/OpenBCI/OpenBCI_GUI/releases.atom'
   name 'OpenBCI GUI'
   homepage 'https://openbci.com/'
