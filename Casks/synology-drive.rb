9fd3525f66caa46465065488f6046b883af67459
Julian Mayer <julian@corecode.io>
2018-08-31 23:12:18 +0200

    update synology-drive

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -11,7 +11,7 @@ cask 'synology-drive' do
 
   pkg 'Install Synology Drive.pkg'
 
-  uninstall quit: 'io.com.synology.CloudStationUI',
+  uninstall quit:      'io.com.synology.CloudStationUI',
             pkgutil:   'com.synology.CloudStation',
             launchctl: 'com.synology.Synology Cloud Station'
 end
