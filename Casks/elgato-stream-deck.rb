8b6f579f835cf779c322fa0094cb6b5176724d12
CoreCode <core-code@users.noreply.github.com>
2018-09-01 11:33:38 +0200

    Update elgato-stream-deck to 3.1.1.7457 (#507)

--- Casks/elgato-stream-deck.rb
+++ Casks/elgato-stream-deck.rb
@@ -1,6 +1,6 @@
 cask 'elgato-stream-deck' do
-  version '3.0.1.6842'
-  sha256 '49844039fb48cf9774ef3a555c0b8f49b5b1904540fd2c1f0bf7162b0fbaa027'
+  version '3.1.1.7457'
+  sha256 '28b9a79313581fcd60fd4776d12f1305dc084a8e10e73ea58c1982536fd15553'
 
   url "https://edge.elgato.com/egc/macos/sd/Stream_Deck_#{version}.pkg"
   name 'Elgato Stream Deck'
