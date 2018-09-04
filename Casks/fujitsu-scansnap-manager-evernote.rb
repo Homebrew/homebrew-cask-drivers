05d4058f9b8a7d035ee2c4924b64c93085a6344f
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2018-09-03 21:59:56 -0400

    Update fujitsu-scansnap-manager-evernote.rb
    Quit running applications and remove remaining packages.

--- Casks/fujitsu-scansnap-manager-evernote.rb
+++ Casks/fujitsu-scansnap-manager-evernote.rb
@@ -8,6 +8,15 @@ cask 'fujitsu-scansnap-manager-evernote' do
   homepage 'https://evernote.com/scansnap'
 
   pkg "ScanSnap Evernote EditionV#{version.no_dots}.pkg"
+  
+  uninstall quit: [ 
+    com.fujitsu.pfu.ScanSnap.AOUMonitor
+    com.fujitsu.pfu.ScanSnap.OnlineUpdate
+    com.fujitsu.pfu.ScansnapEvernoteEdition
+    ]
 
-  uninstall pkgutil: "com.fujitsu.pfu.ScansnapEvernoteEditionV#{version.no_dots}"
+  uninstall pkgutil: [
+    "com.fujitsu.pfu.ScansnapEvernoteEditionV#{version.no_dots}"
+    'com.fujitsu.pfu.ScanSnap.OnlineUpdate.pkg.*'
+    ]
 end
