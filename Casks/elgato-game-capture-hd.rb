c41f911d911235dfebc143d25416bd75fbf1e3e6
CoreCode <core-code@users.noreply.github.com>
2018-09-08 14:28:08 +0200

    Update elgato-game-capture-hd to 2.9.1,1325 (#533)

--- Casks/elgato-game-capture-hd.rb
+++ Casks/elgato-game-capture-hd.rb
@@ -1,5 +1,5 @@
 cask 'elgato-game-capture-hd' do
-  if MacOS.version <= ':yosemite'
+  if MacOS.version <= :yosemite
     version '2.0.5_983'
     sha256 '4803bcac9069e1e63a89e9053fdf2285487acf9e608e84f7610555075489ad5a'
 
