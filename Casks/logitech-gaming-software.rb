b79166b74a8f8eba7a789710d28b528c930308db
Julian Mayer <julian@corecode.io>
2018-09-09 17:42:42 +0200

    Update logitech-gaming-software to 9.00.20

--- Casks/logitech-gaming-software.rb
+++ Casks/logitech-gaming-software.rb
@@ -1,6 +1,6 @@
 cask 'logitech-gaming-software' do
-  version '8.98.306'
-  sha256 'b8fa2cf0caf3b429eb6f4eb5c073caad7ce4b2aa956a08f964e5472126d115d7'
+  version '9.00.20'
+  sha256 '395c6e7d4c0d6d2ce056b71cfdcc532778cd15e181d6b6f047ae838f31c776ce'
 
   url "https://download01.logitech.com/web/ftp/pub/techsupport/gaming/LogitechSetup_#{version}.zip"
   name 'Logitech Gaming Software'
