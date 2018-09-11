7e39740f43187ecc4c101b88ccaf16527203d23f
Ramón Torres Bátiz <ramon.torres@edgebound.com>
2018-09-11 18:06:21 +0000

    Update yubico-yubikey-piv-manager to 1.4.2f (#538)

--- Casks/yubico-yubikey-piv-manager.rb
+++ Casks/yubico-yubikey-piv-manager.rb
@@ -1,6 +1,6 @@
 cask 'yubico-yubikey-piv-manager' do
-  version '1.4.2e'
-  sha256 '19aa4a9d26cd559c31518255fe26cd322ec1c5e6bc280623a34e198c77c54095'
+  version '1.4.2f'
+  sha256 'b167425753dcac2c981e5e0da121c30c9a485600ab2c588c40eaec4456a4a846'
 
   url "https://developers.yubico.com/yubikey-piv-manager/Releases/yubikey-piv-manager-#{version}-mac.pkg"
   appcast 'https://developers.yubico.com/yubikey-piv-manager/Release_Notes.html'
