2687fd8a1456754cd779fd20b3c0113aecc77837
CoreCode <core-code@users.noreply.github.com>
2018-10-03 16:22:14 +0200

    Update synology-surveillance-station-client to 1.2.0-0512 (#530)

--- Casks/synology-surveillance-station-client.rb
+++ Casks/synology-surveillance-station-client.rb
@@ -1,12 +1,13 @@
 cask 'synology-surveillance-station-client' do
-  version '1.1.4-0406'
-  sha256 '1e4809ae9c58e25d564393b8a19fc153954fbdcd157eaeeaa8eb982e04b91feb'
+  version '1.2.0-0512'
+  sha256 '124e5e97b75aed830dfb958c8b18c34737a25e05f46c941101be1605fe984cfd'
 
   url "https://global.download.synology.com/download/Tools/SurveillanceStationClient/#{version}/Mac/Synology%20Surveillance%20Station%20Client-#{version}.dmg"
+  appcast 'https://archive.synology.com/download/Tools/SurveillanceStationClient/'
   name 'Synology Surveillance Station Client'
   homepage 'https://www.synology.com/surveillance/'
 
-  pkg 'Surveillance Station Client.pkg'
+  pkg 'Install Surveillance Station Client.pkg'
 
   uninstall launchctl: 'com.synology.ssc.launch.agent',
             delete:    '/Applications/Surveillance Station Client.app',
@@ -16,8 +17,10 @@ cask 'synology-surveillance-station-client' do
                          'com.synology.svsclient-Timeline',
                          'svsclient',
                          'SurveillanceStationClient',
+                         'com.synology.svsclient-SurveillanceStationClient',
                        ],
             quit:      [
+                         'com.synology.svsclient-SurveillanceStationClient',
                          'svsclient',
                          'SurveillanceStationClient',
                        ]
