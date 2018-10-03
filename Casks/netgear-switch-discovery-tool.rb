d5e001b3cea2a24804cc866aa949c2e086e4b66e
CoreCode <core-code@users.noreply.github.com>
2018-10-03 17:17:27 +0200

    Update netgear-switch-discovery-tool to 1.2.101 (#513)

--- Casks/netgear-switch-discovery-tool.rb
+++ Casks/netgear-switch-discovery-tool.rb
@@ -1,12 +1,12 @@
 cask 'netgear-switch-discovery-tool' do
-  version '1.1.115'
-  sha256 'e2988b8ebb58e8953885a46c4f9e94ba6ebebfc34b93cc902fbc44540d798bf8'
+  version '1.2.101'
+  sha256 'f0ed6cd9eb2f3ec3bf6e27bc4279777640c26143d9e6c635ec7b3d18dcd66042'
 
-  url "https://www.downloads.netgear.com/files/GDC/NetgearSDT-V#{version}.zip"
+  url "https://www.downloads.netgear.com/files/GDC/NSDT/NetgearSDT-V#{version}-Mac.zip"
   name 'NETGEAR Switch Discovery Tool'
   homepage 'https://www.netgear.com/support/download/'
 
-  container nested: "NetgearSDT-V#{version}/NetgearSDT-V#{version}.dmg"
+  container nested: "NetgearSDT-V#{version}-Mac/NetgearSDT-V#{version}.dmg"
 
   app 'NETGEAR Switch Discovery Tool.app'
 end
