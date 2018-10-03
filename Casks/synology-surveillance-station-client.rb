df4190920956a8c7a0ff67ac4d8e6d722ccc12f9
Xo Wang <xo@geekshavefeelings.com>
2018-10-03 07:36:44 -0700

    Update synology-surveillance-station-client to 1.2.0-0553 (#544)

--- Casks/synology-surveillance-station-client.rb
+++ Casks/synology-surveillance-station-client.rb
@@ -1,27 +1,21 @@
 cask 'synology-surveillance-station-client' do
-  version '1.2.0-0512'
-  sha256 '124e5e97b75aed830dfb958c8b18c34737a25e05f46c941101be1605fe984cfd'
+  version '1.2.0-0553'
+  sha256 '08d28121d019556aa4cfdf4e395e7ac9cdc987be765c5f92d570fe500b01406c'
 
   url "https://global.download.synology.com/download/Tools/SurveillanceStationClient/#{version}/Mac/Synology%20Surveillance%20Station%20Client-#{version}.dmg"
-  appcast 'https://archive.synology.com/download/Tools/SurveillanceStationClient/'
+  appcast 'https://www.synology.com/releaseNote/SurveillanceStationClient'
   name 'Synology Surveillance Station Client'
   homepage 'https://www.synology.com/surveillance/'
 
   pkg 'Install Surveillance Station Client.pkg'
 
-  uninstall launchctl: 'com.synology.ssc.launch.agent',
+  uninstall launchctl: 'com.synology.svsclient-SurveillanceStationClient',
             delete:    '/Applications/Surveillance Station Client.app',
+            quit:      'com.synology.svsclient-SurveillanceStationClient',
             pkgutil:   [
                          'com.synology.svsclient-Live-View',
                          'com.synology.svsclient-Recording',
                          'com.synology.svsclient-Timeline',
-                         'svsclient',
-                         'SurveillanceStationClient',
                          'com.synology.svsclient-SurveillanceStationClient',
-                       ],
-            quit:      [
-                         'com.synology.svsclient-SurveillanceStationClient',
-                         'svsclient',
-                         'SurveillanceStationClient',
                        ]
 end
