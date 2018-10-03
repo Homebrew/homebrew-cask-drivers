461d2815c7d15c437c3c764798f42eef0acaec5d
Victor Popkov <victor@popkov.me>
2018-10-03 18:56:14 +0300

    Update reolink-client version

--- Casks/reolink-client.rb
+++ Casks/reolink-client.rb
@@ -1,5 +1,5 @@
 cask 'reolink-client' do
-  version '3.5.0.50'
+  version '3.5.1.53'
   sha256 '70d114e02ec772d3cae83f4a31b826b26ad86354811abef929593b63bc7638a4'
 
   # s3.amazonaws.com/reolink-storage was verified as official when first introduced to the cask
