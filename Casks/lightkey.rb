de2af4afe970aed7c235fa5db7465bfdc3549852
Victor Popkov <victor@popkov.me>
2018-10-08 08:04:41 +0300

    Update lightkey to 2.6.3 (#578)

--- Casks/lightkey.rb
+++ Casks/lightkey.rb
@@ -1,6 +1,6 @@
 cask 'lightkey' do
-  version '2.6.1'
-  sha256 'de5f3aa25909c285545eff325541d40001e7774799151a07ce2754b29ee6042a'
+  version '2.6.3'
+  sha256 'b29196d1b39725db2e197ed57fa180d7033e34d6392833e2dc72d7682afa6dd4'
 
   url "https://lightkeyapp.com/content/06-download/Lightkey-#{version.dots_to_hyphens}/LightkeyInstaller.zip"
   appcast 'https://lightkeyapp.com/en/update'
