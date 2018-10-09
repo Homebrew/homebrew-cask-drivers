0ad2bb42279c723219d285964310106e71cf0455
brigitte zmoelnig <brigitte@corecode.io>
2018-10-09 12:40:27 +0200

    Update reolink-client to 3.5.1.53

--- Casks/reolink-client.rb
+++ Casks/reolink-client.rb
@@ -1,6 +1,6 @@
 cask 'reolink-client' do
-  version '3.4.2.49'
-  sha256 '9a0f882ca429cf0fef4162aa51d6de751aac674670fedcb346a1a0b3a85ae30e'
+  version '3.5.1.53'
+  sha256 '70d114e02ec772d3cae83f4a31b826b26ad86354811abef929593b63bc7638a4'
 
   # s3.amazonaws.com/reolink-storage was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/reolink-storage/website/client/Mac-ReolinkClient.zip'
