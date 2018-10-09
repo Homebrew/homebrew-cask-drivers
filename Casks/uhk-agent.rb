e686733fa0bc5f7d3d512555483304b39a2fec51
Evgen Druzhynin <evgen.druzhynin@gmail.com>
2018-10-09 11:43:51 +0300

    Update uhk-agent to 1.2.11

--- Casks/uhk-agent.rb
+++ Casks/uhk-agent.rb
@@ -1,6 +1,6 @@
 cask 'uhk-agent' do
-  version '1.2.2'
-  sha256 '8acdd3f2a2e0a78262c7e167115def3379cd13054452353175ae9626582472cd'
+  version '1.2.11'
+  sha256 '6080c730d9f679013461cf434d404274862c411375b41c364f562fd5f6da8c90'
 
   url "https://github.com/UltimateHackingKeyboard/agent/releases/download/v#{version}/UHK.Agent-#{version}-mac.#{version}.dmg"
   appcast 'https://github.com/UltimateHackingKeyboard/agent/releases.atom'
