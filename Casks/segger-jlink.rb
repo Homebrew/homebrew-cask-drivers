abca4b4e815b3fc7a6d62f96fc01cbe8c0c72248
yuhki50 <yuhki50@gmail.com>
2018-10-12 20:31:18 +0900

    Update segger-jlink to 6.34g (#580)

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,6 +1,6 @@
 cask 'segger-jlink' do
-  version '6.34f'
-  sha256 'f8763eff6c1745088593d6fb7b495b4b9d8741010ddd5849e0473961973306de'
+  version '6.34g'
+  sha256 '30d5df5750ddc67c100f2ae06546316a8b8465facbdaa9efd5bf137e5ed14654'
 
   url "https://www.segger.com/downloads/flasher/JLink_MacOSX_V#{version.no_dots}.pkg",
       using: :post,
