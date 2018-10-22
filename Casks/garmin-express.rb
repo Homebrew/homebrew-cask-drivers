aeb86ff1ba33a0cd7bf8aa2221ce4e418d437bea
Sebastian Riehm <s.riehm@iwm.rwth-aachend.e>
2018-10-22 14:23:25 +0200

    Update garmin-express to 6.8.0.0

--- Casks/garmin-express.rb
+++ Casks/garmin-express.rb
@@ -1,6 +1,6 @@
 cask 'garmin-express' do
-  version :latest
-  sha256 :no_check
+  version '6.8.0.0'
+  sha256 '7b50dd98e218fe6c6c52240dbe907df1a5681537d3f696cbba5ba60e1e48f924'
 
   url 'https://download.garmin.com/omt/express/GarminExpress.dmg'
   name 'Garmin Express'
