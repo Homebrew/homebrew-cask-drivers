d50c3740382226a6a796b79e686dbfed917dbfc4
Kirk Klobe <kirk@habitaware.com>
2018-10-22 10:37:05 -0500

    Update nordic-nrf-connect to 56320.17.9112767

--- Casks/nordic-nrf-connect.rb
+++ Casks/nordic-nrf-connect.rb
@@ -1,5 +1,5 @@
 cask 'nordic-nrf-connect' do
-  version '56320.17.8035140.108238'
+  version '56320.17.9112767'
   sha256 '689c828042799b40750e3a07728c463116babb477e7e07f814a8e410edca276c'
 
   url "https://www.nordicsemi.com/eng/nordic/download_resource/#{version.dots_to_slashes}"
