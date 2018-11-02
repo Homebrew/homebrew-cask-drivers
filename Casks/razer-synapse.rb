7b87e7f2099d20547c301a75eacc88c64a51b61e
Julian Mayer <julian@corecode.io>
2018-11-02 15:57:32 +0100

    Update razer-synapse to 1.85.2

--- Casks/razer-synapse.rb
+++ Casks/razer-synapse.rb
@@ -1,6 +1,6 @@
 cask 'razer-synapse' do
-  version '1.84.2'
-  sha256 'ae47c9954704a79914aa48b24f6e85553fc341aaea719be689bec5550b955364'
+  version '1.85.2'
+  sha256 'e1251c6007b3b659f0594c8cc39d98c0730eb0742ba82d378af1c3b7085edcd3'
 
   url "https://dl.razerzone.com/drivers/Synapse2/mac/Razer_Synapse_Mac_Driver_v#{version.major_minor}.dmg"
   name 'Razer Synapse'
