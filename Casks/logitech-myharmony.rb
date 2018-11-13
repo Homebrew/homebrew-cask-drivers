4c96625430f893f6341112bb29af5c40c491bc4a
Julian Mayer <julian@corecode.io>
2018-11-13 14:45:12 +0100

    Update logitech-myharmony to 1.0.0.97

--- Casks/logitech-myharmony.rb
+++ Casks/logitech-myharmony.rb
@@ -3,6 +3,7 @@ cask 'logitech-myharmony' do
   sha256 'c5950b7c0461b5713f8896892291689e227046f7a4fc56fec790850f4b938fe0'
 
   url "https://app.myharmony.com/prod/mac/#{version.major_minor}/MyHarmony-App.dmg"
+  appcast 'https://app.myharmony.com/prod/mac/harmonycast.xml'
   name 'MyHarmony'
   homepage 'https://setup.myharmony.com/'
 
