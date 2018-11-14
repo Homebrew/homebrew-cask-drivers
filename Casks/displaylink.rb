1e074523b0e5dfd9ed8899a6b957e3e95d67260d
Yvo Mulder <ytbmulder@gmail.com>
2018-11-14 16:08:53 +0100

    Update displaylink to 5.0.1 (#626)
    * Update displaylink to 4.3.1 for High Sierra.
    
    * Update displaylink to 5.0.1 for Mojave.
    
    * Remove container stanza from displaylink.rb.
    
    * Update displaylink to 3.1.1 for Yosemite.
    
    * Update displaylink to 2.6 for Mavericks.
    
    * Fix requested changes for displaylink.rb.
    
    * Remove unnecessary container stanza.
    
    * Fix missing launchctl entry and add dependency on Mountain Lion or newer..

--- Casks/displaylink.rb
+++ Casks/displaylink.rb
@@ -1,6 +1,17 @@
 cask 'displaylink' do
-  version '4.1.11,1085'
-  sha256 '14b6186fc3a9b202d0fbafde742e8d357e60427b9a6f1f6ee52ebbd478e729d0'
+  if MacOS.version <= :mavericks
+    version '2.6,707'
+    sha256 '5b1c7c5ba941a62a230316df13cdbe9be7559754e808d3480a6197c1a11a779a'
+  elsif MacOS.version <= :yosemite
+    version '3.1.1,1252'
+    sha256 'cc4e73357841463509f1430520eec04792cbfbf9a2a564ea17d5cc78774372d0'
+  elsif MacOS.version <= :high_sierra
+    version '4.3.1,1251'
+    sha256 'd5cd6787d6c4ca6a2425984bcbab607e618e9803335455e24196e14e35657b97'
+  else
+    version '5.0.1,1257'
+    sha256 'deb7ad535987bdaf1358f2a806b8f66a5caa5b7c7843e9029b33e30cf8435b4d'
+  end
 
   url "https://www.displaylink.com/downloads/file?id=#{version.after_comma}",
       data:  {
@@ -11,7 +22,7 @@ cask 'displaylink' do
   name 'DisplayLink USB Graphics Software'
   homepage 'https://www.displaylink.com/'
 
-  container nested: "DisplayLink Installer #{version.before_comma}.dmg"
+  depends_on macos: '>= :mountain_lion'
 
   pkg 'DisplayLink Software Installer.pkg'
 
@@ -26,6 +37,7 @@ cask 'displaylink' do
             #              'com.displaylink.dlusbncm'
             #            ],
             launchctl: [
+                         '73YQY62QM3.com.displaylink.DisplayLinkAPServer',
                          'com.displaylink.useragent-prelogin',
                          'com.displaylink.useragent',
                          'com.displaylink.displaylinkmanager',
