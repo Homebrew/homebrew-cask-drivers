00beddf95507b2882f3048f93e1c3adf98d0e88b
Markus Reiter <me@reitermark.us>
2018-11-16 08:27:18 +0100

    Update segger-ozone.rb

--- Casks/segger-ozone.rb
+++ Casks/segger-ozone.rb
@@ -3,7 +3,7 @@ cask 'segger-ozone' do
   sha256 '4310397a8178e35d51b5c799e27fa286e8c19c51ef1d2be37a1732a81d3a4aad'
 
   url "https://www.segger.com/downloads/jlink/Ozone_MacOSX_V#{version.no_dots}_Universal.pkg"
-  name 'Ozone - the J-Link Debugger'
+  name 'Ozone'
   homepage 'https://www.segger.com/products/development-tools/ozone-j-link-debugger'
 
   pkg "Ozone_MacOSX_V#{version.no_dots}_Universal.pkg"
