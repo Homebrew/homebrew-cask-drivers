be749d3765f055b39abee3fcc4568f08960b3e28
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2018-11-19 07:58:59 +0100

    Add nvidia-cuda for Sierra

--- Casks/nvidia-cuda.rb
+++ Casks/nvidia-cuda.rb
@@ -1,12 +1,17 @@
 cask 'nvidia-cuda' do
-  version '10.0.130'
-  sha256 '4f76261ed46d0d08a597117b8cacba58824b8bb1e1d852745658ac873aae5c8e'
+  if MacOS.version <= :sierra
+    version '9.0.176'
+    sha256 '8fad950098337d2611d64617ca9f62c319d97c5e882b8368ed196e994bdaf225'
+  elsif MacOS.version <= :high_sierra
+    version '10.0.130'
+    sha256 '4f76261ed46d0d08a597117b8cacba58824b8bb1e1d852745658ac873aae5c8e'
+  end
 
   url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac"
   name 'Nvidia CUDA'
   homepage 'https://developer.nvidia.com/cuda-zone'
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: '<= :high_sierra'
 
   installer script: {
                       executable: 'CUDAMacOSXInstaller.app/Contents/MacOS/CUDAMacOSXInstaller',
