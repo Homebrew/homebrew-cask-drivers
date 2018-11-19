bff868f7ca35611ff2ddfd1c47cc797e9b92e52e
Vítor Galvão <info@vitorgalvao.com>
2018-11-19 13:17:11 +0000

    Update nvidia-cuda.rb

--- Casks/nvidia-cuda.rb
+++ Casks/nvidia-cuda.rb
@@ -2,7 +2,7 @@ cask 'nvidia-cuda' do
   if MacOS.version <= :sierra
     version '9.0.176'
     sha256 '8fad950098337d2611d64617ca9f62c319d97c5e882b8368ed196e994bdaf225'
-  elsif MacOS.version <= :high_sierra
+  else
     version '10.0.130'
     sha256 '4f76261ed46d0d08a597117b8cacba58824b8bb1e1d852745658ac873aae5c8e'
   end
