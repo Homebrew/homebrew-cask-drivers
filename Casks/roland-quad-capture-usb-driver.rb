cce29412e38cb19da1762e113d143846efacfa85
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2018-11-21 14:57:38 +0100

    Fix version colon bug introduced with the previous commit

--- Casks/roland-quad-capture-usb-driver.rb
+++ Casks/roland-quad-capture-usb-driver.rb
@@ -2,15 +2,15 @@ cask 'roland-quad-capture-usb-driver' do
   if MacOS.version <= :yosemite
     version '1.5.2,10:24c21855-3c40-469c-a40c-7647a52bf022'
     sha256 'da575f4b3874e0042fddffb4462f4521019ceaa3f44942f64bce099b426cf2d1'
-    url "https://static.roland.com/assets/media/tgz/quad_mx#{version.after_comma}d#{version.before_comma.no_dots}.tgz"
+    url "https://static.roland.com/assets/media/tgz/quad_mx#{version.after_comma.before_colon}d#{version.before_comma.no_dots}.tgz"
   elsif MacOS.version <= :sierra
     version '1.5.3,12:199aee04-dd6b-4a9c-9945-ba9ffb295113'
     sha256 '712b27a25275d748e35c174b242f21a2967f5caca013f6cb09330b9232288770'
-    url "https://static.roland.com/assets/media/tgz/quad_m#{version.after_comma}d#{version.before_comma.no_dots}.tgz"
+    url "https://static.roland.com/assets/media/tgz/quad_m#{version.after_comma.before_colon}d#{version.before_comma.no_dots}.tgz"
   else
     version '1.5.4,13:53d8a307-a8ae-4f9b-9a59-a1adb8c67012'
     sha256 'd3524d7844d24805c3a1c25c09dce62ab87d4c8dd6941a0b1d3653c696563117'
-    url "https://static.roland.com/assets/media/tgz/quad_m#{version.after_comma}d#{version.before_comma.no_dots}.tgz"
+    url "https://static.roland.com/assets/media/tgz/quad_m#{version.after_comma.before_colon}d#{version.before_comma.no_dots}.tgz"
   end
 
   name 'Roland Quad-Capture USB Driver'
@@ -18,13 +18,13 @@ cask 'roland-quad-capture-usb-driver' do
 
   depends_on macos: '>= :yosemite'
 
-  pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver10#{version.after_comma}.pkg"
+  pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver10#{version.after_comma.before_colon}.pkg"
 
   uninstall pkgutil:   [
-                         "jp.co.roland.QuadCapture.app.10#{version.after_colon}.pkg",
-                         "jp.co.roland.QuadCapture.kext.10#{version.after_colon}.pkg",
-                         "jp.co.roland.QuadCapture.midi.10#{version.after_colon}.pkg",
-                         "jp.co.roland.QuadCapture.start.10#{version.after_colon}.pkg",
+                         "jp.co.roland.QuadCapture.app.10#{version.after_comma.before_colon}.pkg",
+                         "jp.co.roland.QuadCapture.kext.10#{version.after_comma.before_colon}.pkg",
+                         "jp.co.roland.QuadCapture.midi.10#{version.after_comma.before_colon}.pkg",
+                         "jp.co.roland.QuadCapture.start.10#{version.after_comma.before_colon}.pkg",
                        ],
             launchctl: [
                          'jp.co.roland.RDUSB012FSetupd',
