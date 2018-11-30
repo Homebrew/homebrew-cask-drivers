593054888b358b7f10049d79d8b8a3b036863b70
suschizu <brigitte@corecode.io>
2018-11-30 20:39:59 +0100

    Update jabra-suite.rb
    this cask need an update: jabra-suite is now called jabra direct and the newest version is 4.0.3545

--- Casks/jabra-suite.rb
+++ Casks/jabra-suite.rb
@@ -1,60 +1,12 @@
-cask 'jabra-suite' do
-  version '2.13.0'
-  sha256 '056f34951ee4b47b0eaa85c7f2fdded616d3039613b53ba212a898f961cd3627'
+cask 'jabra-direct' do
+  version '4.0.3545'
+  sha256 'eaa0150c8728be1c413b72bc201af69e2712a1ee8e2d5ac0f85e0bba7229dd40'
 
-  url "https://www.jabra.com/macsuite/#{version}/Jabra%20Suite%20for%20Mac.dmg"
-  appcast 'https://www.jabra.com/macsuite/JMSVersionUpdate.xml'
-  name 'Jabra Suite for Mac'
-  homepage 'https://www.jabra.com/software-and-services/jabra-suite-for-mac'
+  url 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.dmg'
+  appcast 'https://www.jabra.com/Support/release-notes/release-note-jabra-direct'
+  name 'Jabra Direct'
+  homepage 'https://www.jabra.com/software-and-services/jabra-direct'
 
-  pkg 'Jabra Suite for Mac.pkg'
-
-  uninstall pkgutil:   [
-                         'com.jabra.firmwareapp',
-                         'com.jabra.kext',
-                         'com.jabra.appservice',
-                         'com.jabra.menubar',
-                         'com.jabra.avaya',
-                         'com.jabra.avaya3',
-                         'com.jabra.bria',
-                         'com.jabra.skype',
-                         'com.jabra.sametime',
-                         'com.jabra.plugin',
-                         'com.jabra.preferencepane',
-                       ],
-            kext:      [
-                         'com.jabra.usbdevice',
-                       ],
-            quit:      [
-                         'com.jabra.AvayaDriver',
-                         'com.yourcompany.Jabra-Sametime-Integration',
-                         'com.jabra.BriaDriver',
-                         'GN.Jabra menu bar item',
-                         'com.jabra.SkypeDriver',
-                         'jabra.com.macsuite.Jabra-App-Service',
-                         'com.jabra.Avaya3Driver',
-                       ],
-            launchctl: [
-                         'com.jabra.AvayaDriver',
-                         'com.yourcompany.Jabra-Sametime-Integration',
-                         'com.jabra.BriaDriver',
-                         'GN.Jabra menu bar item',
-                         'com.jabra.SkypeDriver',
-                         'jabra.com.macsuite.Jabra-App-Service',
-                         'com.jabra.Avaya3Driver',
-                       ],
-            delete:    [
-                         '~/Library/Application Support/Jabra/*.log',
-                         '/System/Library/Extensions/JabraUSB.kext',
-                         '/Library/LaunchAgents/com.jabra.bria.plist',
-                         '/Library/LaunchAgents/com.jabra.avaya3.plist',
-                         '/Library/LaunchAgents/com.jabra.skype.plist',
-                         '/Library/LaunchAgents/com.jabra.avaya.plist',
-                         '/Library/LaunchAgents/com.jabra.sametime.plist',
-                         '/Library/LaunchAgents/com.jabra.appservice.plist',
-                       ],
-            rmdir:     [
-                         '~/Library/Application Support/Jabra',
-                         '/Library/Application Support/Jabra',
-                       ]
+  pkg 'JabraDirectSetup.pkg'
 end
+
