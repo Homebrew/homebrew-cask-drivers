498377685badbb41dd1d931404a5588417a30dfa
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 18:00:39 +0000

    Update jabra-direct.rb

--- Casks/jabra-direct.rb
+++ Casks/jabra-direct.rb
@@ -2,7 +2,7 @@ cask 'jabra-direct' do
   version '4.0.3545'
   sha256 'eaa0150c8728be1c413b72bc201af69e2712a1ee8e2d5ac0f85e0bba7229dd40'
 
-  # jabraxpressonlineprdstor.blob.core.windows.net/jdo/ was verified as official when first introduced to the cask
+  # jabraxpressonlineprdstor.blob.core.windows.net/jdo was verified as official when first introduced to the cask
   url 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.dmg'
   appcast 'https://www.jabra.com/Support/release-notes/release-note-jabra-direct'
   name 'Jabra Direct'
