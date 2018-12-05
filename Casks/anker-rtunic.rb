f70a555b17f84a7f78b798fe534adade09644582
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2018-12-06 00:17:55 +0100

    Fix anker-rtunic style offense

--- Casks/anker-rtunic.rb
+++ Casks/anker-rtunic.rb
@@ -3,11 +3,9 @@ cask 'anker-rtunic' do
   sha256 '031b3f3b20d08b75438f258d5a46e07e29a719e634ff03d8d7ae0574e0afeffa'
 
   # cloudfront.net was verified as official when first introduced to the cask
-  url "https://#{version.after_comma.before_colon}.cloudfront.net/spree/accessories/attachments/70581/RTUNICv#{version.before_comma}_disable_flowcontrol_1.zip?#{version.after_colon}"
+  url "https://d2211byn0pk9fi.cloudfront.net/spree/accessories/attachments/70581/RTUNICv#{version.before_comma}_disable_flowcontrol_1.zip?#{version.after_colon}"
   name 'Anker RTUNIC'
   homepage 'https://www.anker.com/store/aluminum-usb-3-0-to-ethernet-adapter/A7611011'
-
-  # depends_on macos: '<= :sierra'
 
   pkg "RTUNICv#{version.before_comma}.pkg"
 
