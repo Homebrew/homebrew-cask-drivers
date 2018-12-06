c6798e1160f331961902937199c0644266569c81
Yvo Mulder <ytbmulder@gmail.com>
2018-12-06 20:42:03 +0100

    Update 3dconnexion to 10.6.0 (#661)

--- Casks/3dconnexion.rb
+++ Casks/3dconnexion.rb
@@ -1,8 +1,8 @@
 cask '3dconnexion' do
-  version '10-5-3_r2794'
-  sha256 '22d03c8465df59afb629a62f24a5b9caef1a8e792902557e632bf386a1d668b6'
+  version '10-6-0_r2876,dac9287b-1f2a-859f-1428-5bd9fb06fe4a'
+  sha256 '8060b6fbad9a3f1fe4e3c693e708f875c5cce3159948423ce414bda56434c9b3'
 
-  url "https://www.3dconnexion.com/index.php?eID=sdl&ext=tx_iccsoftware&oid=c40d972e-0f4d-67ad-f888-5b3e6b422868&filename=3DxWareMac_v#{version}.dmg"
+  url "https://www.3dconnexion.com/index.php?eID=sdl&ext=tx_iccsoftware&oid=#{version.after_comma}&filename=3DxWareMac_v#{version}.dmg"
   name '3Dconnexion'
   homepage 'https://www.3dconnexion.com/service/drivers.html'
 
