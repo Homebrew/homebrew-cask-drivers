089f3b5265bb3a619e5595bc4e8057b5cc94a087
Vítor Galvão <info@vitorgalvao.com>
2018-12-06 19:45:36 +0000

    Update anker-rtunic.rb

--- Casks/anker-rtunic.rb
+++ Casks/anker-rtunic.rb
@@ -2,7 +2,7 @@ cask 'anker-rtunic' do
   version '1.0.19,d2211byn0pk9fi:1533027920'
   sha256 '031b3f3b20d08b75438f258d5a46e07e29a719e634ff03d8d7ae0574e0afeffa'
 
-  # cloudfront.net was verified as official when first introduced to the cask
+  # d2211byn0pk9fi.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2211byn0pk9fi.cloudfront.net/spree/accessories/attachments/70581/RTUNICv#{version.before_comma}_disable_flowcontrol_1.zip?#{version.after_colon}"
   name 'Anker RTUNIC'
   homepage 'https://www.anker.com/store/aluminum-usb-3-0-to-ethernet-adapter/A7611011'
