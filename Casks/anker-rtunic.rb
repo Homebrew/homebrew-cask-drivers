b6c869535c599fa2b47ba3cd1623b50d16790a71
Yvo Mulder <ytbmulder@gmail.com>
2018-12-07 01:48:39 +0100

    Update anker-rtunic to 1.0.19 (#662)
    * Update anker-rtunic to 1.0.19
    
    * Fix anker-rtunic style offense
    
    * Remove anker-rtunic depends_on stanza
    
    * Fix anker-rtunic style offense
    
    * Update anker-rtunic.rb

--- Casks/anker-rtunic.rb
+++ Casks/anker-rtunic.rb
@@ -1,11 +1,11 @@
 cask 'anker-rtunic' do
-  version '1.0.16,515.0010'
-  sha256 '51e1c1790789fcb35ca80c2cc5aa08826d2f24252e20a832124dafb38d85cf4e'
+  version '1.0.19,d2211byn0pk9fi:1533027920'
+  sha256 '031b3f3b20d08b75438f258d5a46e07e29a719e634ff03d8d7ae0574e0afeffa'
 
-  # d2c6jjk3vnoatm.cloudfront.net was verified as official when first introduced to the cask
-  url "https://d2c6jjk3vnoatm.cloudfront.net/spree/products/accessoies/#{version.after_comma.dots_to_slashes}-RTUNICv#{version.before_comma}.zip"
-  name 'rtunic'
-  homepage 'https://www.anker.com/products/taxons/114/Connectivity'
+  # d2211byn0pk9fi.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d2211byn0pk9fi.cloudfront.net/spree/accessories/attachments/70581/RTUNICv#{version.before_comma}_disable_flowcontrol_1.zip?#{version.after_colon}"
+  name 'Anker RTUNIC'
+  homepage 'https://www.anker.com/store/aluminum-usb-3-0-to-ethernet-adapter/A7611011'
 
   pkg "RTUNICv#{version.before_comma}.pkg"
 
@@ -16,6 +16,14 @@ cask 'anker-rtunic' do
             kext:    [
                        'com.realtek.driver.AppleRTL815XComposite',
                        'com.realtek.driver.AppleRTL815XEthernet',
+                     ],
+            script:  [
+                       executable: "#{staged_path}/uninstall/uninstall.command",
+                       sudo:       true,
+                     ],
+            delete:  [
+                       '/Library/Extensions/AppleRTL815X*.kext',
+                       '/System/Library/Extensions/IONetworkingFamily.kext/Contents/PlugIns/AppleRTL815X*.kext',
                      ]
 
   caveats do
