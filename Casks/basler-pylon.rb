6cc02632a89466f11050d1a0902ffe063cfa14c3
Yvo Mulder <ytbmulder@gmail.com>
2018-12-08 14:33:37 +0100

    Update basler-pylon to 5.1.1 (#667)

--- Casks/basler-pylon.rb
+++ Casks/basler-pylon.rb
@@ -1,10 +1,12 @@
 cask 'basler-pylon' do
-  version '5.0.5.8973'
-  sha256 '4f17df1c89337a49cf423775823d5fd31101409bd5c8c42751b9ac92c16d921d'
+  version '5.1.1.13069'
+  sha256 'f98e6c0b3410374155d7a572e55a99d82681c29328cd1f46e9224f245598dd33'
 
   url "https://www.baslerweb.com/media/downloads/software/pylon_software/pylon-#{version}.dmg"
   name 'Basler Pylon Camera Software Suite'
-  homepage 'https://www.baslerweb.com/'
+  homepage 'https://www.baslerweb.com/en/products/software/basler-pylon-camera-software-suite/'
+
+  depends_on macos: '>= :sierra'
 
   pkg "pylon-#{version}.pkg"
 
