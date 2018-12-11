4e06109149e6a39187e4f4e699b22ef89ea77c5f
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2018-12-11 15:10:05 +0100

    Fix audit error

--- Casks/apogee-symphony.rb
+++ Casks/apogee-symphony.rb
@@ -2,11 +2,11 @@ cask 'apogee-symphony' do
   if MacOS.version <= :el_capitan
     version '5.2'
     sha256 '50563eab6ce715981f2d06011bf7836c540bd48e2a3f411ed7e9557dc4545fce'
-    url "http://www.apogeedigital.com/drivers/Symphony_Release_#{version}.dmg"
+    url "https://www.apogeedigital.com/drivers/Symphony_Release_#{version}.dmg"
   else
     version '5.4'
     sha256 'a74ae84061c428a4e4735aecf13d265bb7fe1319d9ed99941fa969eeb5e3fa9e'
-    url "http://www.apogeedigital.com/drivers/symphony-io-#{version}.dmg"
+    url "https://www.apogeedigital.com/drivers/symphony-io-#{version}.dmg"
   end
 
   name 'Apogee Symphony I/O'
