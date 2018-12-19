6384df03c16117c6ce28847827005b141f580dab
Kirk Klobe <kklobe@gmail.com>
2018-12-19 08:28:45 -0600

    Update segger-ozone to 2.60j (#685)

--- Casks/segger-ozone.rb
+++ Casks/segger-ozone.rb
@@ -1,6 +1,6 @@
 cask 'segger-ozone' do
-  version '2.60i'
-  sha256 '23a664b9affc252e955f96ac3aedc91298120ebd3328b8ddcbc8047c02b68f40'
+  version '2.60j'
+  sha256 'aa4e1b0afdcbf76c2ca3a000a91f2b085f31d2087f4ecb5087cadb7b64007ea0'
 
   url "https://www.segger.com/downloads/jlink/Ozone_MacOSX_V#{version.no_dots}_Universal.pkg"
   name 'Ozone'
