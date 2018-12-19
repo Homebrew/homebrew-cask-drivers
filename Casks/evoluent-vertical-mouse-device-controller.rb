091e21f6a9275ef6ae54fc9cfd2364c846071eff
suschizu <brigitte@corecode.io>
2018-12-19 02:57:31 +0100

    Update evoluent-vertical-mouse-device-controller to 1.4.1 (#684)
    * Update evoluent-vertical-mouse-device-controller to 1.4.1
    
    * Update evoluent-vertical-mouse-device-controller.rb

--- Casks/evoluent-vertical-mouse-device-controller.rb
+++ Casks/evoluent-vertical-mouse-device-controller.rb
@@ -1,12 +1,12 @@
 cask 'evoluent-vertical-mouse-device-controller' do
-  version '1.3.0f1'
-  sha256 'aacc74268954edfea38bf9d4704cb7987c84cbedcaa963bbb82413b35497c1ae'
+  version '1.4.1'
+  sha256 '8316bb923d6553b0d383d4aee8a64b53196a9914ee17318e0fc769806ace03d5'
 
-  url "https://evoluent.com/download/EvoluentVerticalMouseDeviceController#{version}.dmg"
+  url "https://evoluent.com/download/Evoluent%20VerticalMouse%20Device%20Controller%20-%20#{version}.pkg"
   name 'Evoluent Vertical Mouse Device Controller'
   homepage 'https://evoluent.com/'
 
-  pkg 'Evoluent VerticalMouse Device Controller.pkg'
+  pkg "Evoluent VerticalMouse Device Controller - #{version}.pkg"
 
   uninstall launchctl: 'com.evoluent.agent',
             pkgutil:   'com.evoluent.pkg.Installer'
