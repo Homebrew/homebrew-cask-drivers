bc2ff783216e0a6fa1ef3f1f4233f0df1c3e84a4
Vítor Galvão <info@vitorgalvao.com>
2018-12-24 16:06:18 +0000

    Update elgato-video-capture.rb

--- Casks/elgato-video-capture.rb
+++ Casks/elgato-video-capture.rb
@@ -3,8 +3,9 @@ cask 'elgato-video-capture' do
   sha256 'dbb6b64143cf5954bdef1b9543b627bf482fe857cbeece0738bb3ca029fb47b9'
 
   url "https://edge.elgato.com/video-capture/macos/#{version.major_minor}/final/Elgato%20Video%20Capture%20#{version.major_minor}%20(#{version.patch}).dmg"
+  appcast 'https://www.elgato.com/en/video-capture/support'
   name 'Elgato Video Capture'
-  homepage 'https://www.elgato.com/en/video-capture/support'
+  homepage 'https://www.elgato.com/en/video-capture/'
 
   depends_on macos: '>= :el_capitan'
 
