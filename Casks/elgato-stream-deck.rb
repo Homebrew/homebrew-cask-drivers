895eabaacb37ab977179f174fc35d2f5ec03f7f8
Yvo Mulder <ytbmulder@gmail.com>
2018-12-25 22:30:34 +0100

    Add elgato-stream-deck 3.3.1 appcast (#694)

--- Casks/elgato-stream-deck.rb
+++ Casks/elgato-stream-deck.rb
@@ -3,6 +3,7 @@ cask 'elgato-stream-deck' do
   sha256 'c70930e88ea4f8079327e585577cafc7ec194d0f446cf9801ea906b6883272f3'
 
   url "https://edge.elgato.com/egc/macos/sd/Stream_Deck_#{version}.pkg"
+  appcast 'https://gaming.help.elgato.com/customer/en/portal/articles/2793637-elgato-stream-deck-software-release-notes'
   name 'Elgato Stream Deck'
   homepage 'https://www.elgato.com/en/gaming/stream-deck'
 
