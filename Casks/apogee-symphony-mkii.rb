2dd144a2d39871fd82624d8b45ac7982d48a7828
Yvo Mulder <ytbmulder@gmail.com>
2018-12-29 22:57:18 +0100

    Add apogee-symphony-mkii 3.1 appcast (#699)

--- Casks/apogee-symphony-mkii.rb
+++ Casks/apogee-symphony-mkii.rb
@@ -8,6 +8,7 @@ cask 'apogee-symphony-mkii' do
   end
 
   url "https://www.apogeedigital.com/drivers/SymphonyIOMkII_Thunderbolt_Release.#{version}.dmg"
+  appcast 'http://www.apogeedigital.com/support/register/symphony-io-mk-ii'
   name 'Apogee Symphony I/O MkII'
   homepage 'http://www.apogeedigital.com/support/symphony-io-mk-ii'
 
