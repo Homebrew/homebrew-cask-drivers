fdb5e6b29e1efed1f0fbcd776b523b89bfc1720e
Yvo Mulder <ytbmulder@gmail.com>
2018-12-30 15:44:47 +0100

    Fix nvidia-cuda 9.0.176 url (#701)

--- Casks/nvidia-cuda.rb
+++ Casks/nvidia-cuda.rb
@@ -2,12 +2,14 @@ cask 'nvidia-cuda' do
   if MacOS.version <= :sierra
     version '9.0.176'
     sha256 '8fad950098337d2611d64617ca9f62c319d97c5e882b8368ed196e994bdaf225'
+    url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac-dmg"
   else
     version '10.0.130'
     sha256 '4f76261ed46d0d08a597117b8cacba58824b8bb1e1d852745658ac873aae5c8e'
+    url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac"
   end
 
-  url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac"
+  appcast 'https://developer.nvidia.com/cuda-toolkit-archive'
   name 'Nvidia CUDA'
   homepage 'https://developer.nvidia.com/cuda-zone'
 
