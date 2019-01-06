c53e51562937d7b5acbd31d4d7209fe736ef8fc4
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-01-06 11:04:07 -0500

    Update logitech-options to 7.10.5

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -1,6 +1,6 @@
 cask 'logitech-options' do
-  version '6.92.231'
-  sha256 '6812ff10690c08c5b4b884bf7545f75bf4327943027b16611c6c965a430234d2'
+  version '7.10.5'
+  sha256 '23b90c5f6c232fa80b5e48e85815eb94ad873c92285f6b24066eeb28251b6660'
 
   url "https://www.logitech.com/pub/techsupport/options/Options_#{version}.zip"
   name 'Logitech Options'
