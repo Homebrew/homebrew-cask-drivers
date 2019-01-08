0500ff69ba8c0638409b385e312be6888735dc4f
Pete Winterscheidt <pete@petrocco.co>
2019-01-08 11:50:30 -0700

    Separated Ricoh Vol 3 and 4

--- Casks/ricoh-ps-printers-vol4-exp-driver.rb
+++ Casks/ricoh-ps-printers-vol4-exp-driver.rb
@@ -1,10 +1,10 @@
-cask 'ricoh-ps-printers-exp-driver' do
+cask 'ricoh-ps-printers-vol4-exp-driver' do
   version '3.4.0.0'
   sha256 'ffd01b48ef1d3e0b86894eda316068b799178969075929544c7a832d50f9bf9a'
 
   url "http://support.ricoh.com/bb/pub_e/dr_ut_e/0001310/0001310123/V#{version.delete('.')}/Ricoh_PS_Printers_Vol4_EXP_LIO_#{version}.dmg"
-  name 'Ricoh PS printers EXP driver'
-  homepage 'http://support.ricoh.com/bb/html/dr_ut_e/apc/model/mpc2011/mpc2011.htm'
+  name 'Ricoh PS printers Vol 4 EXP driver'
+  homepage 'http://support.ricoh.com/bb/html/dr_ut_e/rc3/model/mpc3004ex/mpc3004ex.htm'
 
   pkg 'Ricoh_PS_Printers_Vol4_EXP_LIO_Driver.pkg'
 
