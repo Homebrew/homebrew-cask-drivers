2540e4bd442c0e51617bb546ade565cf9a0e89ef
Pete Winterscheidt <pete@petrocco.co>
2019-01-08 11:18:13 -0700

    Cleanup cask token and name

--- Casks/ricoh-ps-printers-exp-driver.rb
+++ Casks/ricoh-ps-printers-exp-driver.rb
@@ -1,9 +1,9 @@
-cask 'ricoh-ppd' do
+cask 'ricoh-ps-printers-exp-driver' do
   version '3.4.0.0'
   sha256 'ffd01b48ef1d3e0b86894eda316068b799178969075929544c7a832d50f9bf9a'
 
   url "http://support.ricoh.com/bb/pub_e/dr_ut_e/0001310/0001310123/V#{version.delete('.')}/Ricoh_PS_Printers_Vol4_EXP_LIO_#{version}.dmg"
-  name 'Ricoh PPD Installer'
+  name 'Ricoh PS printers EXP driver'
   homepage 'http://support.ricoh.com/bb/html/dr_ut_e/apc/model/mpc2011/mpc2011.htm'
 
   pkg 'Ricoh_PS_Printers_Vol4_EXP_LIO_Driver.pkg'
