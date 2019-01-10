77a9a22a89f1162285b05385a07967c6a8c40040
Vítor Galvão <info@vitorgalvao.com>
2019-01-10 11:52:52 +0000

    Update roland-quad-capture-usb-driver.rb

--- Casks/roland-quad-capture-usb-driver.rb
+++ Casks/roland-quad-capture-usb-driver.rb
@@ -52,7 +52,7 @@ cask 'roland-quad-capture-usb-driver' do
                '/Library/Preferences/jp_co_roland_RDUSB0000Dev.pref.plist',
                '/Library/Preferences/jp_co_roland_RDUSB012FDev.pref.plist',
                '~/Library/Preferences/jp.co.roland.RDUSB012F.cpl.plist',
-               '~/Library/Saved Application State/jp.co.roland.RDUSB012F.Uninstaller.savedState'
+               '~/Library/Saved Application State/jp.co.roland.RDUSB012F.Uninstaller.savedState',
              ]
 
   caveats do
