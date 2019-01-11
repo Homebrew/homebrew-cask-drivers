e4243dd99a9d23e1fdda3ab837ad5ffb81d5d73d
Yvo Mulder <ytbmulder@gmail.com>
2019-01-11 22:59:21 +0100

    Update logitech-unifying 1.3.375 version and zap (#712)
    * Update logitech-unifying 1.3.375 version and zap
    
    * Update versions

--- Casks/logitech-unifying.rb
+++ Casks/logitech-unifying.rb
@@ -1,10 +1,23 @@
 cask 'logitech-unifying' do
-  version '1.3.375'
-  sha256 '915116757a33d3d9c5be706d12718824b4082b3e495f2a77a3ffc70db5956666'
+  if MacOS.version <= :yosemite
+    version '1.2.359'
+    sha256 'e6fd9c1b536033f3346b32c391bd58587ea9f549cab7839cf8a1dbc62a739825'
+  else
+    version '1.3.375'
+    sha256 '915116757a33d3d9c5be706d12718824b4082b3e495f2a77a3ffc70db5956666'
+  end
 
   url "https://www.logitech.com/pub/controldevices/unifying/unifying#{version}_mac.zip"
+  appcast 'https://support.logitech.com/en_us/software/unifying'
   name 'Logitech Unifying Software'
   homepage 'https://www.logitech.com/en-us/promotions/6072'
+
+  depends_on macos: [
+                      :yosemite,
+                      :el_capitan,
+                      :sierra,
+                      :high_sierra,
+                    ]
 
   pkg 'Unifying Installer.app/Contents/Resources/Logitech Unifying Signed.mpkg'
 
@@ -14,4 +27,11 @@ cask 'logitech-unifying' do
                      ],
             delete:  '/Applications/Utilities/Logitech Unifying Software.app',
             quit:    'com.logitech.unifying.assistant'
+
+  zap trash: [
+               '/Library/Application Support/Logitech.localized',
+               '~/Library/Caches/com.Logitech.Updater',
+               '~/Library/Preferences/com.Logitech.Updater.plist',
+               '~/Library/Preferences/com.logitech.unifying.assistant.plist',
+             ]
 end
