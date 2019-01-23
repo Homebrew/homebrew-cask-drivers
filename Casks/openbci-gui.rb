8f64d2ba0012b11e17e276ea89ac6aaa47736aac
suschizu <brigitte@corecode.io>
2019-01-23 02:43:56 +0100

    Update openbci-gui to 4.0.3 (#746)

--- Casks/openbci-gui.rb
+++ Casks/openbci-gui.rb
@@ -1,6 +1,6 @@
 cask 'openbci-gui' do
-  version '4.0.2'
-  sha256 '12c336479cd751e712b9aac886dfa524c0a31b9ab1429de54c6fc2b9a2e0c65f'
+  version '4.0.3'
+  sha256 'f0b4f0ea2f39718b64e5dfea95e4b565aceeaf23a93780f5b16e8a116f754d3a'
 
   # github.com/OpenBCI/OpenBCI_GUI was verified as official when first introduced to the cask
   url "https://github.com/OpenBCI/OpenBCI_GUI/releases/download/v#{version}/application.macosx.zip"
