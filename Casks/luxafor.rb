78e9e83b18649fd9219be326a286197d68cfec41
brigitte zmoelnig <brigitte@corecode.io>
2019-02-05 16:34:21 +0100

    Update luxafor to 1.2.30

--- Casks/luxafor.rb
+++ Casks/luxafor.rb
@@ -1,8 +1,8 @@
 cask 'luxafor' do
-  version '1.2.28'
-  sha256 'a56c05f415b076e4eebca9f12769bc28dd2a15287fdb95409906e2ca3981e87b'
+  version '1.2.30'
+  sha256 '5cbe9a29e225f8f1074595d556d8a9bf4106bc167c0322bdb83a1ad73af5b928'
 
-  url "https://luxafor.com/uploads/luxafor_mac_v#{version}.zip"
+  url "https://luxafor.com/wp-content/uploads/2019/01/luxafor_mac_v#{version}.zip"
   name 'Luxafor'
   homepage 'https://luxafor.com/'
 
