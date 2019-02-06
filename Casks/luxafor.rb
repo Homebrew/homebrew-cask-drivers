c95293879b4f8958d444cad8a01c5743aaf9ef0c
Vítor Galvão <info@vitorgalvao.com>
2019-02-06 16:44:43 +0000

    Update luxafor.rb

--- Casks/luxafor.rb
+++ Casks/luxafor.rb
@@ -1,8 +1,8 @@
 cask 'luxafor' do
-  version '1.2.30'
+  version '1.2.30,2019.01'
   sha256 '5cbe9a29e225f8f1074595d556d8a9bf4106bc167c0322bdb83a1ad73af5b928'
 
-  url "https://luxafor.com/wp-content/uploads/2019/01/luxafor_mac_v#{version}.zip"
+  url "https://luxafor.com/wp-content/uploads/#{version.after_comma.dots_to_slashes}/luxafor_mac_v#{version.before_comma}.zip"
   name 'Luxafor'
   homepage 'https://luxafor.com/'
 
