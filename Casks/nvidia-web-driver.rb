74cb0f41247425eeb26210ddcad2496c3593ac47
Yvo Mulder <ytbmulder@gmail.com>
2019-02-08 17:18:08 +0100

    Update nvidia-web-driver to 387.10.10.10.40.118 (#775)

--- Casks/nvidia-web-driver.rb
+++ Casks/nvidia-web-driver.rb
@@ -6,11 +6,11 @@ cask 'nvidia-web-driver' do
     version '346.03.15f16'
     sha256 'f0c1a23a262ba6db35f1d7a0da39e7b7648805d63d65be20af33e582cc7050bc'
   elsif MacOS.version <= :sierra
-    version '378.05.05.25f11'
-    sha256 '7230bf6ef1681b34d7acabeea54979139a2421f47fd28100f104923eaab9370e'
+    version '378.05.05.25f13'
+    sha256 'f5849297ee8a4a754f26d998db83878d4c02324db00ec28ab38da1d847d7e5c1'
   else
-    version '387.10.10.10.40.113'
-    sha256 'a5c5a5db6c7d7561494378d978537c9b05bf8c13b686636d2621f15b5ada6971'
+    version '387.10.10.10.40.118'
+    sha256 '1a03defb969b0240d4ef4b35071520d884e5903e56843150f0b0433c58cc7bcc'
   end
 
   url "https://images.nvidia.com/mac/pkg/#{version.major}/WebDriver-#{version}.pkg"
