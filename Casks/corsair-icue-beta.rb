5aa27dd8b293809cecc25f493562ad53374c0f58
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-02-12 19:50:34 +0100

    Add corsair-icue-beta 3.12.118

---
+++ Casks/corsair-icue-beta.rb
@@ -0,0 +1,42 @@
+cask 'corsair-icue-beta' do
+  version '3.12.118'
+  sha256 '3cc37a8c0695196d7a4817ef078cf29396c6d90315f56b8a76e654ed56788d0f'
+
+  url "http://downloads.corsair.com/Files/CUE/iCUE-#{version}-release.dmg"
+  appcast 'http://forum.corsair.com/v3/showthread.php?p=982647#post982647'
+  name 'Corsair iCUE'
+  homepage 'https://www.corsair.com/us/en/icue'
+
+  depends_on macos: '>= :high_sierra'
+
+  pkg "iCUE.pkg"
+
+  uninstall launchctl: 'com.corsair.cue.3.launchHelper',
+            quit:      [
+                         'com.corsair.cue.3.*',
+                         'org.qt-project.*',
+                       ],
+            script:    [
+                         executable: "#{appdir}/iCUE.app/Contents/Helpers/iCUEUninstaller.app/Contents/Scripts/uninstall.scpt",
+                         sudo:       true,
+                       ],
+            pkgutil:   [
+                         'com.corsair.CorsairAudio',
+                         'com.corsair.cue.3',
+                       ],
+            delete:    [
+                         '/Library/Audio/Plug-Ins/HAL/CorsairAudio.plugin',
+                         '/Library/LaunchAgents/iCUELaunchAgent.plist',
+                       ],
+            rmdir:     '/Applications/Corsair'
+
+  zap trash: [
+               '~/.config/com.corsair',
+               '~/Library/Application Support/Corsair',
+               '~/Library/Caches/Corsair',
+               '~/Library/Caches/iCUEUninstaller',
+               '~/Library/Preferences/com.corsair.cue.3.plist',
+               '~/Library/Saved Application State/com.corsair.cue.3.cue_unistaller.savedState',
+               '~/Library/Saved Application State/com.corsair.cue.3.savedState',
+             ]
+end
