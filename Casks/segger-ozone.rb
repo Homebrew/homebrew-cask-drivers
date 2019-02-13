f3b10d2d2464ee2f3e0de60bee08383d4c3d5bfc
Kirk Klobe <kirk@habitaware.com>
2019-02-13 06:34:56 -0600

    Update segger-ozone to 2.60o

--- Casks/segger-ozone.rb
+++ Casks/segger-ozone.rb
@@ -1,6 +1,6 @@
 cask 'segger-ozone' do
-  version '2.60n'
-  sha256 'ca3337744a12d49fbb59c3a590e28eb0cdcdb54ffbba7102f62a69c6a0ca7528'
+  version '2.60o'
+  sha256 'a33e83c6d6a241c685ae2afe97ee9db911beea380b3b43ceca3579f553fd4b5a'
 
   url "https://www.segger.com/downloads/jlink/Ozone_MacOSX_V#{version.no_dots}_Universal.pkg"
   name 'Ozone'
