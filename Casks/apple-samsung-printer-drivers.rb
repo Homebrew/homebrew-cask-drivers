76dd56773b0e2674864af30d0ce5e888f53781ae
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-02-14 16:51:13 +0100

    Update apple-samsung-printer-drivers 2.6 appcast, depends_on, uninstall and zap

--- Casks/apple-samsung-printer-drivers.rb
+++ Casks/apple-samsung-printer-drivers.rb
@@ -3,11 +3,17 @@ cask 'apple-samsung-printer-drivers' do
   sha256 'ecf283ff40df816e84d3a6148676114d2b4fe3f4074feb995c2c7e0be8be4964'
 
   url "https://support.apple.com/downloads/DL905/en_US/SamsungPrinterDrivers#{version}.dmg"
+  appcast 'https://support.apple.com/downloads/samsung'
   name 'Samsung Printer Drivers'
   homepage 'https://support.apple.com/kb/dl905'
+
+  depends_on macos: '<= :mavericks'
 
   pkg 'SamsungPrinterDrivers.pkg'
 
   uninstall quit:    'com.samsung.imagecapture.scanner.app',
-            pkgutil: 'com.apple.pkg.SamsungPrinterDrivers'
+            pkgutil: 'com.apple.pkg.SamsungPrinterDrivers',
+            rmdir:   '/Library/Printers/Samsung'
+
+  zap trash: '~/Library/Preferences/com.samsung.imagecapture.scanner.app.plist'
 end
