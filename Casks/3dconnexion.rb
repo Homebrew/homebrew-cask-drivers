4137f7953136fd26ed8d3fd809d32dfc38849f73
Yvo Mulder <ytbmulder@gmail.com>
2019-02-17 00:53:44 +0100

    Update 3dconnexion to 10.6.1 and add appcast, uninstall, zap and caveats (#721)
    * Add appcast, uninstall, zap and caveats
    
    * Update uninstall
    
    * Update uninstall and zap
    
    * Cleanup uninstall
    
    * Fix 'fail to unload kext' during
    
    * Update 3dconnexion to 10.6.1
    
    * Add depends_on macos
    
    * Replace /Applications with #{appdir}
    
    * Revert #{appdir} to /Applications based on cask language reference
    
    * Fix double delete and update quit
    
    * Remove `kext`.

--- Casks/3dconnexion.rb
+++ Casks/3dconnexion.rb
@@ -1,12 +1,43 @@
 cask '3dconnexion' do
-  version '10-6-0_r2876,dac9287b-1f2a-859f-1428-5bd9fb06fe4a'
-  sha256 '8060b6fbad9a3f1fe4e3c693e708f875c5cce3159948423ce414bda56434c9b3'
+  version '10-6-1_r2897,a8111ac7-d575-9772-1dc6-5c37a3a6c64c'
+  sha256 '3b037438bf220ea30d2d9b4bdaf9f512e3d3a2e2f205841f85ef9a1a81b54d91'
 
   url "https://www.3dconnexion.com/index.php?eID=sdl&ext=tx_iccsoftware&oid=#{version.after_comma}&filename=3DxWareMac_v#{version}.dmg"
+  appcast 'https://www.3dconnexion.com/service/drivers.html'
   name '3Dconnexion'
-  homepage 'https://www.3dconnexion.com/service/drivers.html'
+  homepage 'https://www.3dconnexion.com/'
+
+  depends_on macos: '>= :yosemite'
 
   pkg 'Install 3Dconnexion software.pkg'
 
-  uninstall pkgutil: 'com.3dconnexion.*'
+  uninstall pkgutil:   'com.3dconnexion.*',
+            launchctl: 'com.3dconnexion.nlserverIPalias',
+            quit:      [
+                         'com.3Dconnexion.3DxUpdater',
+                         'com.3dconnexion.*',
+                       ],
+            script:    [
+                         executable: "#{appdir}/3Dconnexion/Uninstall 3Dconnexion Driver.app/Contents/Resources/rm3dcx",
+                         sudo:       true,
+                       ],
+            delete:    [
+                         '/Applications/3Dconnexion',
+                         '/Library/Extensions/3Dconnexion.kext',
+                         '/Library/Frameworks/3DconnexionClient.framework',
+                       ]
+
+  zap trash: [
+               '/Library/Application Support/3Dconnexion',
+               '/Library/LaunchDaemons/com.3dconnexion.nlserverIPalias.plist',
+               '/Library/PreferencePanes/3Dconnexion.prefPane',
+               '~/Library/Logs/3Dconnexion',
+               '~/Library/Preferences/3Dconnexion',
+               '~/Library/Preferences/com.3dconnexion.*.plist',
+               '~/Library/Saved Application State/com.3dconnexion.*.savedState',
+             ]
+
+  caveats do
+    reboot
+  end
 end
