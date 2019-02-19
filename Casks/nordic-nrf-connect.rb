3e4cb8b4af81d74a8de021f94d4698d71e42d637
Kirk Klobe <kklobe@gmail.com>
2019-02-19 10:24:28 -0600

    Update nordic-nrf-connect to 2.6.2 (#799)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/nordic-nrf-connect.rb
+++ Casks/nordic-nrf-connect.rb
@@ -1,6 +1,6 @@
 cask 'nordic-nrf-connect' do
-  version '2.6.1'
-  sha256 '77b9224b906c034bff61ceaf7943c6d7091ba41961cfc26ae50c1d3b04a31cd4'
+  version '2.6.2'
+  sha256 '937c19f280b4c7744d364ca6ac36b15f2f67dd578c95a1d8477c324987d1d2a0'
 
   # github.com/NordicSemiconductor/pc-nrfconnect-core/ was verified as official when first introduced to the cask
   url "https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases/download/v#{version}/nrfconnect-#{version}.dmg"
