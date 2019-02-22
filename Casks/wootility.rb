3f9a3ae443009380c747e5fc6c35861381eb62db
mik-at <mik@zeux.at>
2019-02-22 19:08:49 +0100

    Update wootility to 3.1.2 (#804)

--- Casks/wootility.rb
+++ Casks/wootility.rb
@@ -1,6 +1,6 @@
 cask 'wootility' do
-  version '3.0.3'
-  sha256 '2f8329a77a445d143b705808b3fd9d7e0118de6d5159a0170d4611f81f118525'
+  version '3.1.2'
+  sha256 '82141b7e7fbd35c634f91ae462d677834d337a4709d0b3528e0854a604d8dda5'
 
   # s3.eu-west-2.amazonaws.com/wooting-update was verified as official when first introduced to the cask
   url "https://s3.eu-west-2.amazonaws.com/wooting-update/wootility-mac-latest/wootility-#{version}.dmg"
