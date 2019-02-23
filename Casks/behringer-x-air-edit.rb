da77a83ffb188dc9e73b70ccd8cab6a81722cdef
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-02-23 17:35:22 +0100

    Add behringer-x-air-edit 1.5 uninstall and zap

--- Casks/behringer-x-air-edit.rb
+++ Casks/behringer-x-air-edit.rb
@@ -3,8 +3,17 @@ cask 'behringer-x-air-edit' do
   sha256 '4db3fb2204829384f59aae00b19f49e14f0151c9eb113776f0d455350868751b'
 
   url "https://downloads.music-group.com/software/behringer/XAIR/X-AIR-Edit_MAC_V#{version}.zip"
-  name 'X-Air-Edit'
+  name 'Behringer X Air Edit'
   homepage 'https://www.music-group.com/brand/c/Behringer/downloads?active=Downloads'
 
-  app 'X-Air-Edit.app'
+  app 'X-AIR-Edit.app'
+
+  uninstall quit:   'com.music-group.X-AIR-Edit',
+            delete: '/Library/Logs/DiagnosticReports/X-AIR-Edit_*.wakeups_resource.diag'
+
+  zap trash: [
+               '~/Library/.X-AIR-Edit',
+               '~/Library/Application Support/CrashReporter/X-AIR-Edit_*.plist',
+               '~/Library/Saved Application State/com.music-group.X-AIR-Edit.savedState',
+             ]
 end
