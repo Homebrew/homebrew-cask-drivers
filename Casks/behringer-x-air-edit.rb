674310a691f104b3df91d5e141e408aacb325570
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-02-23 17:36:46 +0100

    Fix name

--- Casks/behringer-x-air-edit.rb
+++ Casks/behringer-x-air-edit.rb
@@ -3,7 +3,7 @@ cask 'behringer-x-air-edit' do
   sha256 '4db3fb2204829384f59aae00b19f49e14f0151c9eb113776f0d455350868751b'
 
   url "https://downloads.music-group.com/software/behringer/XAIR/X-AIR-Edit_MAC_V#{version}.zip"
-  name 'Behringer X Air Edit'
+  name 'Behringer X-Air-Edit'
   homepage 'https://www.music-group.com/brand/c/Behringer/downloads?active=Downloads'
 
   app 'X-AIR-Edit.app'
