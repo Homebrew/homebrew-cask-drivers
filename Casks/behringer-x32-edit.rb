bd4bd4a41d0f7b2e16bb4b6dcb95f365c38fc6b2
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-03-02 21:15:55 +0100

    Add behringer-x32-edit 3.2 uninstall and zap

--- Casks/behringer-x32-edit.rb
+++ Casks/behringer-x32-edit.rb
@@ -4,8 +4,20 @@ cask 'behringer-x32-edit' do
   sha256 '2dee264d0df054e4f9b689af99046b7b28f5b8519cf1fba8df103ca30edcdc0a'
 
   url "https://downloads.music-group.com/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
-  name 'X32-Edit'
-  homepage 'https://www.music-group.com/Categories/Behringer/Mixers/Digital-Mixers/X32/p/P0ASF'
+  name 'Behringer X32-Edit'
+  homepage 'https://www.musictribe.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF'
 
   app 'X32-Edit.app'
+
+  uninstall quit:   'com.music-group.X32-Edit',
+            delete: '/Library/Logs/DiagnosticReports/X32-Edit_*.wakeups_resource.diag'
+
+  zap trash: [
+              '~/Library/Application Support/CrashReporter/X32-Edit_*.plist',
+              '~/Library/Saved Application State/com.music-group.X32-Edit.savedState',
+             ]
+
+  caveats do
+    license 'https://www.musictribe.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/downloads?active=Downloads'
+  end
 end
