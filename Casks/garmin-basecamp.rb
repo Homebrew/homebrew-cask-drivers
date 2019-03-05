48712c163000fdee5d7250d4f7f3b8678a9d3e46
brigitte zmoelnig <brigitte@corecode.io>
2019-03-05 20:22:40 +0100

    Update garmin-basecamp to 4.8.3

--- Casks/garmin-basecamp.rb
+++ Casks/garmin-basecamp.rb
@@ -1,6 +1,6 @@
 cask 'garmin-basecamp' do
-  version '4.7.0'
-  sha256 '43d2fcd3571fff70eeca2734c56e0aae0f982bc1a7019588bd55b883cc34b16c'
+  version '4.8.3'
+  sha256 'abbd7ac8bf9ac6dd976f04637e1cd916795bb838a34e60115f3b5fb3c72095fc'
 
   url "https://download.garmin.com/software/BaseCampforMac_#{version.no_dots}.dmg"
   name 'Garmin BaseCamp'
