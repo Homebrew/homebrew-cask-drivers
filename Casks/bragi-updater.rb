8512befeda57488bd49d33c9291edfc7df15e5a9
CoreCode <core-code@users.noreply.github.com>
2019-03-10 16:41:40 -0700

    Update bragi-updater.rb (#830)

--- Casks/bragi-updater.rb
+++ Casks/bragi-updater.rb
@@ -3,7 +3,7 @@ cask 'bragi-updater' do
   sha256 'ab3e12163bf4d73c641c1b249bc7a290d8eeb2034465d243b1e194f4e2f76f94'
 
   url "http://update.bragi.com/bin/Bragi%20Updater-#{version}.dmg"
-  appcast 'http://update.bragi.com/'
+  appcast 'http://update.bragi.com/scripts/scripts.36513c90.js'
   name 'Bragi Updater'
   homepage 'http://update.bragi.com/'
 
