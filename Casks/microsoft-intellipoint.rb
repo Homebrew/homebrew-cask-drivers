45f2fd0573f6d4d1f2cf8dfba9205f85bc9c126e
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-03-11 20:14:51 +0100

    Update microsoft-intellipoint 8.2 uninstall

--- Casks/microsoft-intellipoint.rb
+++ Casks/microsoft-intellipoint.rb
@@ -3,15 +3,18 @@ cask 'microsoft-intellipoint' do
   sha256 '1a4612d3237084d0502d947793c097575febc95442130e2384c8fbf4b52cac9e'
 
   url 'https://download.microsoft.com/download/B/1/0/B109F931-70E2-425F-8681-EAAB75845AB8/Microsoft-Mouse_d305.dmg'
+  appcast 'https://www.microsoft.com/accessories/en-gb/d/intellimouse'
   name 'Microsoft IntelliPoint'
   homepage 'https://www.microsoft.com/accessories/mice'
 
   pkg 'Microsoft Mouse Installer.app/Contents/Resources/Microsoft Mouse.pkg'
 
-  uninstall pkgutil: 'com.microsoft.mouse.installer',
-            kext:    [
-                       'com.microsoft.driver.MicrosoftMouse',
-                       'com.microsoft.driver.MicrosoftMouseBluetooth',
-                       'com.microsoft.driver.MicrosoftMouseUSB',
-                     ]
+  uninstall kext:    [
+                         'com.microsoft.driver.MicrosoftMouse',
+                         'com.microsoft.driver.MicrosoftMouseBluetooth',
+                         'com.microsoft.driver.MicrosoftMouseUSB',
+                     ],
+            pkgutil: 'com.microsoft.mouse.installer',
+            delete:  '/Library/PreferencePanes/Microsoft Mouse.prefPane'
+
 end
