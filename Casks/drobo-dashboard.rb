69a326414e311f8f0dc51255c138705816dc9f3c
suschizu <brigitte@corecode.io>
2019-03-14 14:12:29 +0100

    Update drobo-dashboard to 3.4.2,109477 (#839)
    * Update drobo-dashboard to 3.4.2,109477
    
    * Update drobo-dashboard.rb

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -1,12 +1,12 @@
 cask 'drobo-dashboard' do
-  version '3.4.1,108281'
-  sha256 'c2b77fe5db23b4705c756a0311a41e62d537048daaa7dd45287985ee1f9aeb4b'
+  version '3.4.2,109477'
+  sha256 '97024146ac4e25c8236762089c1a860d63b3152704a7f8e00893321dea207b34'
 
   url "https://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version.before_comma}.dmg"
   name 'Drobo Dashboard'
   homepage 'https://www.drobo.com/'
 
-  pkg "Install.app/Contents/Drobo_Dashboard_Installer_#{version.before_comma}_#{version.after_comma}.pkg"
+  pkg "Install.app/Contents/Resources/Drobo_Dashboard_Installer_#{version.before_comma}_#{version.after_comma}.pkg"
 
   uninstall script:  {
                        executable: "#{staged_path}/Uninstall.app/Contents/Resources/Scripts/Drobo_Dashboard_uninstall.sh",
