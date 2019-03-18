30228018d9d6d7e0450fb2881d9ff6c01661b46b
Yvo Mulder <ytbmulder@gmail.com>
2019-03-18 13:21:52 +0100

    Update ubiquiti-unifi-controller 5.10.19 quit, zap and caveats (#845)
    * Update ubiquiti-unifi-controller 5.10.19 quit, zap and caveats
    
    * Fix style offenses
    
    * Update appcast on new domain
    
    * Add trash file

--- Casks/ubiquiti-unifi-controller.rb
+++ Casks/ubiquiti-unifi-controller.rb
@@ -2,10 +2,11 @@ cask 'ubiquiti-unifi-controller' do
   version '5.10.19'
   sha256 '2dea4802ccc5203d29956f5c970fc676afae484ba94255e61f11807422c66547'
 
+  # dl.ubnt.com was verified as official when first introduced to the cask
   url "https://dl.ubnt.com/unifi/#{version}/UniFi.pkg"
-  appcast 'https://www.ubnt.com/download/unifi/'
-  name 'UniFi Controller'
-  homepage 'https://www.ubnt.com/download/unifi/'
+  appcast 'https://www.ui.com/download/unifi'
+  name 'Ubiquiti UniFi Network Controller'
+  homepage 'https://unifi-sdn.ui.com/'
 
   conflicts_with cask: 'ubiquiti-unifi-controller-lts'
 
@@ -15,11 +16,24 @@ cask 'ubiquiti-unifi-controller' do
     set_ownership '~/Library/Application Support/UniFi'
   end
 
-  uninstall pkgutil: 'com.ubnt.UniFi',
+  uninstall quit:    [
+                       'com.oracle.java.*.jre',
+                       'com.ubnt.UniFi-Discover',
+                     ],
+            signal:  ['TERM', 'com.ubnt.UniFi'],
+            pkgutil: 'com.ubnt.UniFi',
             delete:  [
-                       '/Applications/UniFi.app',
                        '/Applications/UniFi-Discover.app',
+                       '/Applications/UniFi.app',
                      ]
 
-  zap trash: '~/Library/Application Support/UniFi'
+  zap trash: [
+               '~/Library/Application Support/UniFi',
+               '~/Library/Saved Application State/com.ubnt.UniFi-Discover.savedState',
+               '~/Library/Saved Application State/com.ubnt.UniFi.savedState',
+             ]
+
+  caveats do
+    license 'https://www.ui.com/eula/'
+  end
 end
