b9a7070cf574ce29ab15d0cd225996d2c982f7ec
Markus Reiter <me@reitermark.us>
2019-03-19 04:48:12 +0100

    Remove parentheses from version. (#850)

--- Casks/netgeargenie.rb
+++ Casks/netgeargenie.rb
@@ -1,12 +1,12 @@
 cask 'netgeargenie' do
-  version '2.4.40(2018-12-14)'
+  version '2.4.40,2018-12-14'
   sha256 'eca8915c6a792f85b00786fc303c8f91a7809ccfa2ba72212417fff9243517b1'
 
   url 'http://updates1.netgear.com/netgeargenie/mac/update/NETGEARGenieInstaller.dmg'
   name 'NETGEARGenie'
   homepage 'https://www.netgear.com/home/discover/apps/genie.aspx'
 
-  pkg "NETGEAR_Genie_Installer_#{version}.pkg"
+  pkg "NETGEAR_Genie_Installer_#{version.before_comma}(#{version.after_comma}).pkg"
 
   uninstall quit:    'com.yourcompany.NETGEARGenie',
             pkgutil: 'com.netgear.netgearGenie.NETGEARGenie.pkg'
