2a0a2eec48fbd1717be7ce974994cebf60a8096c
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-03-20 21:17:10 +0100

    Add openbci-gui 4.1.0 uninstall and trash

--- Casks/openbci-gui.rb
+++ Casks/openbci-gui.rb
@@ -1,6 +1,6 @@
 cask 'openbci-gui' do
   version '4.1.0'
-  sha256 '9142823e81fefb67f514468c5cad35e47fc38d815dc94d2b81e889a814f89763'
+  sha256 'ff9dd9fda3f6f05289ccba5c028dfa819c47c46317fa0288abf1543207f0334c'
 
   # github.com/OpenBCI/OpenBCI_GUI was verified as official when first introduced to the cask
   url "https://github.com/OpenBCI/OpenBCI_GUI/releases/download/v#{version}/application.macosx.zip"
@@ -9,4 +9,22 @@ cask 'openbci-gui' do
   homepage 'https://openbci.com/'
 
   app 'application.macosx/OpenBCI_GUI.app'
+
+  uninstall quit:   [
+                      'OpenBCI_GUI',
+                      'com.github.Squirrel',
+                      'com.github.electron.framework',
+                      'com.openbci.hub.*',
+                      'org.mantle.Mantle',
+                      'org.reactivecocoa.ReactiveCocoa',
+                    ]
+
+  zap trash: [
+               '~/Library/Application Support/OpenBCIHub',
+               '~/Library/Logs/OpenBCIHub',
+               '~/Library/Preferences/com.openbci.hub.plist',
+               '~/Library/Saved Application State/OpenBCI_GUI.savedState',
+               '~/Library/Saved Application State/com.openbci.hub.savedState',
+             ]
+
 end
