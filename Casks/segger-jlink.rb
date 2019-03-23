0eb89e659e4296b76bad8f4348ce76469aa1f783
Kirk Klobe <kklobe@gmail.com>
2019-03-23 06:34:44 -0500

    Update segger-jlink to 6.44c (#858)

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,15 +1,15 @@
 cask 'segger-jlink' do
-  version '6.44b'
-  sha256 '37472ffab2728858d52cceef9ea684fc1131328ccd64f37b26a97b5af3be392a'
+  version '6.44c'
+  sha256 'de9c855a4d69a93cefad67515fd60204b75780ba480502dfb8f0e69b8705394e'
 
-  url "https://www.segger.com/downloads/flasher/JLink_MacOSX_V#{version.no_dots}.pkg",
+  url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}.pkg",
       using: :post,
       data:  {
                'accept_license_agreement' => 'accepted',
                'non_emb_ctr'              => 'confirmed',
              }
   name 'Segger J-Link Command Line Tools'
-  homepage 'https://www.segger.com/downloads/flasher'
+  homepage 'https://www.segger.com/downloads/jlink'
 
   pkg "JLink_MacOSX_V#{version.no_dots}.pkg"
 
