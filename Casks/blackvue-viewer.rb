3030b8974fc41cde39d34cae225dd905a86e5ba3
Vítor Galvão <info@vitorgalvao.com>
2019-03-26 19:21:54 +0000

    Update blackvue-viewer.rb

--- Casks/blackvue-viewer.rb
+++ Casks/blackvue-viewer.rb
@@ -3,7 +3,7 @@ cask 'blackvue-viewer' do
   sha256 '4f74a31e058fd55c635424606c698dfa2c91e52575ced58085e17be5e61b094f'
 
   url "https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=#{version.after_comma}"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=74331'
+  appcast 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/'
   name 'BlackVue Viewer'
   homepage 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/'
 
