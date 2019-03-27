c64d42c2e0577c548d84f48107332e1df5bb3855
Kirk Klobe <kirk@habitaware.com>
2019-03-27 13:42:29 -0400

    Update segger-jlink from 6.44c to 6.44d

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,6 +1,6 @@
 cask 'segger-jlink' do
-  version '6.44c'
-  sha256 'de9c855a4d69a93cefad67515fd60204b75780ba480502dfb8f0e69b8705394e'
+  version '6.44d'
+  sha256 '6f679e3a88007083e73c338701056e48d7cb205726078973f49b3d02330c31aa'
 
   url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}.pkg",
       using: :post,
