263ca50825a9d7c841413811ba6b36590389b0a5
Julian Mayer <julian@corecode.io>
2019-03-28 01:19:06 +0100

    Update luxafor to 1.2.30,2019.01

--- Casks/luxafor.rb
+++ Casks/luxafor.rb
@@ -3,6 +3,7 @@ cask 'luxafor' do
   sha256 '5cbe9a29e225f8f1074595d556d8a9bf4106bc167c0322bdb83a1ad73af5b928'
 
   url "https://luxafor.com/wp-content/uploads/#{version.after_comma.dots_to_slashes}/luxafor_mac_v#{version.before_comma}.zip"
+  appcast 'https://luxafor.com/download/'
   name 'Luxafor'
   homepage 'https://luxafor.com/'
 
