3a80e1f31e25b3bcbd706ec1abfd3e49f9765a59
CoreCode <core-code@users.noreply.github.com>
2019-03-28 14:43:18 +0100

    Update synology-photo-station-uploader to 1.4.4-091 (#879)
    * Update synology-photo-station-uploader to 1.4.4-091
    
    * Update synology-photo-station-uploader.rb

--- Casks/synology-photo-station-uploader.rb
+++ Casks/synology-photo-station-uploader.rb
@@ -3,13 +3,16 @@ cask 'synology-photo-station-uploader' do
   sha256 'f25e009170387720291570d3acfef048d9ee05477c5a0b207df965ddf924d4cb'
 
   url "https://global.download.synology.com/download/Tools/PhotoStationUploader/#{version}/Mac/PhotoStationUploader-#{version.sub(%r{.*-}, '')}-Mac-Installer.dmg"
+  appcast 'https://archive.synology.com/download/Tools/PhotoStationUploader/'
   name 'Synology Photo Station Uploader'
   homepage 'https://www.synology.com/'
 
   pkg "PhotoStationUploader-#{version.sub(%r{.*-}, '')}-Mac-Installer.pkg"
 
   uninstall pkgutil:   'com.synology.photostationuploader.installer',
+            quit:      'com.synology.PhotoStationUploader',
             launchctl: [
+                         'com.synology.PhotoUploaderFinderSync',
                          'com.synology.PhotoUploaderShellApp',
                          'com.synology.SynoSIMBL_RefreshFinder',
                        ]
