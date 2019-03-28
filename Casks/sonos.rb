4d79ee88027878b444de51421c9e5ef5111c4293
Julian Mayer <julian@corecode.io>
2019-03-28 01:06:59 +0100

    Update sonos to 10.0

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -3,6 +3,7 @@ cask 'sonos' do
   sha256 '5ffefc590221a1e3f2e9a0d818b30842113eca2f53bed6a1d6cd04cb61d2a43d'
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac'
   name 'Sonos'
   homepage 'https://www.sonos.com/'
 
