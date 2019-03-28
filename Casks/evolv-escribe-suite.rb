17f20ed2c7e4246f977f61ffce35842f76cd8c78
Ph43n0m <bks42@gmx.net>
2019-03-28 19:49:56 +0100

    Update evolv-escribe-suite.rb

--- Casks/evolv-escribe-suite.rb
+++ Casks/evolv-escribe-suite.rb
@@ -1,6 +1,6 @@
 cask 'evolv-escribe-suite' do
   version '2_SP19'
-  sha256 '184a1e8d5ec462ad7b602347f182035826a0d5fecde51c54494d57ebfe80c3e0'
+  sha256 'e2544857312c5ddbffce0c66c88e86aa424f5c779123064e0283ac634c101ed4'
 
   url "https://downloads.evolvapor.com/SetupEScribe#{version}_INT.pkg"
   name 'EScribe Suite'
