86a629171d06677665cfc9ed901ffcf9344e3773
Julian Mayer <julian@corecode.io>
2019-03-28 01:14:10 +0100

    Update wootility to 3.1.3

--- Casks/wootility.rb
+++ Casks/wootility.rb
@@ -4,6 +4,7 @@ cask 'wootility' do
 
   # s3.eu-west-2.amazonaws.com/wooting-update was verified as official when first introduced to the cask
   url "https://s3.eu-west-2.amazonaws.com/wooting-update/wootility-mac-latest/wootility-#{version}.dmg"
+  appcast 'https://wooting.io/wootility'
   name 'Wootility'
   homepage 'https://wooting.nl/wootility/'
 
