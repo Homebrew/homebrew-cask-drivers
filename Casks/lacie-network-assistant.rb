044c12fbbb59aec3abb00ffe30621db35350a585
Julian Mayer <julian@corecode.io>
2019-03-28 01:17:52 +0100

    Update lacie-network-assistant to 1.5.17

--- Casks/lacie-network-assistant.rb
+++ Casks/lacie-network-assistant.rb
@@ -3,8 +3,9 @@ cask 'lacie-network-assistant' do
   sha256 'fce25a342d9b54fb438ece6a17e3ed1391f1b41a5e9fd4e9459d0ca0fe828df9'
 
   url "https://www.lacie.com/files/lacie-content/download/drivers/LaCie%20Network%20Assistant-#{version}.dmg"
+  appcast 'https://www.lacie.com/gb/en/support/network-storage/cloudbox/'
   name 'LaCie Network Assistant'
-  homepage 'https://www.lacie.com/ch/fr/support/network-storage/cloudbox/'
+  homepage 'https://www.lacie.com/gb/en/support/network-storage/cloudbox/'
 
   app 'LaCie Network Assistant.app'
 end
