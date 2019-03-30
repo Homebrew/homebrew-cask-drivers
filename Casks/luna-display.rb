537867252d064550b3b05bc6fd071f04e69997bb
CoreCode <core-code@users.noreply.github.com>
2019-03-30 11:55:20 +0100

    Update luna-display from 3.0 to 3.0.1 (#887)

--- Casks/luna-display.rb
+++ Casks/luna-display.rb
@@ -1,6 +1,6 @@
 cask 'luna-display' do
-  version '3.0'
-  sha256 '4892613350cd6f62562c82bb496877d1881d0963d3b8b3a5083783346b9cbdb7'
+  version '3.0.1'
+  sha256 'a1b68d69947c8417c8e287cce26daf56ab75cb44cc4bb48f42f51aab5b12ba5f'
 
   # s3.amazonaws.com/s3.lunadisplay was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaDisplay-#{version}.zip"
