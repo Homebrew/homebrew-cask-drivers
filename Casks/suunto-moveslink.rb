6dcad17d447d721009cb21b826901d61226fa04a
Julian Mayer <julian@corecode.io>
2019-04-01 17:43:00 +0200

    Update suunto-moveslink

--- Casks/suunto-moveslink.rb
+++ Casks/suunto-moveslink.rb
@@ -3,6 +3,7 @@ cask 'suunto-moveslink' do
   sha256 '5897fe23239a4400f9f23929f2eadcc424bdc5f3f3b3da74a1c17b8ec778997c'
 
   url "https://content.static.movescount.com/downloads/Moveslink_setup_#{version.dots_to_underscores}.dmg"
+  appcast 'http://www.movescount.com/connect/moveslink?os=mac'
   name 'Suunto Moveslink'
   homepage 'https://www.movescount.com/connect/moveslink?os=mac'
 
