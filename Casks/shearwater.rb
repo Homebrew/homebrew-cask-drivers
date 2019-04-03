43c14b16f4bafe1d63bc0fa8967a455a707c1d85
CoreCode <core-code@users.noreply.github.com>
2019-04-03 16:21:44 +0200

    Update shearwater (#890)

--- Casks/shearwater.rb
+++ Casks/shearwater.rb
@@ -3,6 +3,7 @@ cask 'shearwater' do
   sha256 '0ccb805ae6cece4ea5eb69b5d32afc49360569e93d2def1691da6f06236d655d'
 
   url "https://www.shearwater.com/wp-content/downloads/ShearwaterDesktopInstaller_#{version}.dmg"
+  appcast 'https://www.shearwater.com/downloads/shearwater-desktop-download/'
   name 'Shearwater Desktop'
   homepage 'https://www.shearwater.com/'
 
