a83d58991855d7a27ac7553cfeea1ddc55257112
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2019-04-08 00:38:00 +0300

    Fix silicon-labs-vcp-driver sha256 (#904)

--- Casks/silicon-labs-vcp-driver.rb
+++ Casks/silicon-labs-vcp-driver.rb
@@ -1,6 +1,6 @@
 cask 'silicon-labs-vcp-driver' do
   version '5.0.10'
-  sha256 '5ce0f339eb094fa52dfb42b97d7441cdb6883026589ecdec2bd8f311dee34b99'
+  sha256 '375823a96b2cdac4c8bb195cc85d149e7e5a6f8ef572c097ea691796da6edaf4'
 
   url 'https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver.zip'
   appcast 'https://www.silabs.com/documents/public/release-notes/Mac_OSX_VCP_Driver_Release_Notes.txt'
