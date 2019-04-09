f471531eb399c66f54b1ab498a7dd0bd5bda5808
Julian Mayer <julian@corecode.io>
2019-04-09 16:50:44 +0200

    Update wootility

--- Casks/wootility.rb
+++ Casks/wootility.rb
@@ -1,6 +1,6 @@
 cask 'wootility' do
   version '3.1.3'
-  sha256 '9f00c1ce9e7bad0c2dfe22046ebbd401488e057bdb66af45a676ee5030847633'
+  sha256 '86c7e0945fb7110728ee85d188d61963f0e8c9e3068a490d97ff24a091e41669'
 
   # s3.eu-west-2.amazonaws.com/wooting-update was verified as official when first introduced to the cask
   url "https://s3.eu-west-2.amazonaws.com/wooting-update/wootility-mac-latest/wootility-#{version}.dmg"
