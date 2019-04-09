80be2132eb35dd848ef8a650287f698b8dfacd1d
Work <brigitte@corecode.io>
2019-04-09 14:31:45 +0200

    Update synology-cloud-station-backup from 4.3.0-4435 to 4.3.1-4437

--- Casks/synology-cloud-station-backup.rb
+++ Casks/synology-cloud-station-backup.rb
@@ -1,6 +1,6 @@
 cask 'synology-cloud-station-backup' do
-  version '4.3.0-4435'
-  sha256 'f0c83e70578fd209ad46aef2c14767b78b14fb0d62c23daf5f655802fa9c578a'
+  version '4.3.1-4437'
+  sha256 'e1faf8be16ab3e39cf0efe086f5e3b746285b4aec970c1d8c25502cf820ca6ca'
 
   url "https://global.download.synology.com/download/Tools/CloudStationBackup/#{version}/Mac/Installer/synology-cloud-station-backup-#{version.sub(%r{.*-}, '')}.dmg"
   appcast 'https://archive.synology.com/download/Tools/CloudStationBackup/'
