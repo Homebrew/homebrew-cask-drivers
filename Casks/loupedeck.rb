66990f8cad89ef995b407b6c1384e82e8d7b2a32
Vítor Galvão <info@vitorgalvao.com>
2019-04-17 15:51:10 +0100

    Update loupedeck.rb

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -3,11 +3,11 @@ cask 'loupedeck' do
   sha256 'ed7317f5fd58e7496c529448d4076f87a847c97bd6716368eb8ebb44cb9a4879'
 
   # s3.eu-north-1.amazonaws.com/loupedeck-software-release was verified as official when first introduced to the cask
-  url "https://s3.eu-north-1.amazonaws.com/loupedeck-software-release/software_release_version_#{version.major_minor.dots_to_underscores}/mac_#{version.major_minor.dots_to_underscores}/Loupedeck_#{version.major_minor.dots_to_underscores}.dmg"
+  url "https://s3.eu-north-1.amazonaws.com/loupedeck-software-release/software_release_version_#{version.dots_to_underscores}/mac_#{version.dots_to_underscores}/Loupedeck_#{version.dots_to_underscores}.dmg"
   name 'Loupdeck'
   homepage 'https://loupedeck.com/'
 
-  pkg "Loupedeck Installer #{version.major_minor}.pkg"
+  pkg "Loupedeck Installer #{version}.pkg"
 
   uninstall signal:  [
                        ['TERM', 'com.loupedeck.Loupedeck2'],
