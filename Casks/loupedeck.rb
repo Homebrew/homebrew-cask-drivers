9edeb3017de358e5c97495df2c4bab3e078d49b4
Vítor Galvão <info@vitorgalvao.com>
2019-04-17 15:49:41 +0100

    Update loupedeck.rb

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -2,7 +2,7 @@ cask 'loupedeck' do
   version '2.6'
   sha256 'ed7317f5fd58e7496c529448d4076f87a847c97bd6716368eb8ebb44cb9a4879'
 
-  # s3.eu-north-1.amazonaws.com was verified as official when first introduced to the cask
+  # s3.eu-north-1.amazonaws.com/loupedeck-software-release was verified as official when first introduced to the cask
   url "https://s3.eu-north-1.amazonaws.com/loupedeck-software-release/software_release_version_#{version.major_minor.dots_to_underscores}/mac_#{version.major_minor.dots_to_underscores}/Loupedeck_#{version.major_minor.dots_to_underscores}.dmg"
   name 'Loupdeck'
   homepage 'https://loupedeck.com/'
