c89533f6bc04c32b6c07ebf0cd745e9b108f2a64
brigitte zmoelnig <brigitte@corecode.io>
2019-04-17 20:13:53 +0200

    Update wootility from 3.2.5 to 3.2.6

--- Casks/wootility.rb
+++ Casks/wootility.rb
@@ -1,6 +1,6 @@
 cask 'wootility' do
-  version '3.2.5'
-  sha256 'a283251399882018237841462cdd0af50b69cacb3c1712bde40a2da5af3c1673'
+  version '3.2.6'
+  sha256 'b5929c7de16f771d5a073c7fc215767644bc271b323291937f351963601d8f3a'
 
   # s3.eu-west-2.amazonaws.com/wooting-update was verified as official when first introduced to the cask
   url "https://s3.eu-west-2.amazonaws.com/wooting-update/wootility-mac-latest/wootility-#{version}.dmg"
