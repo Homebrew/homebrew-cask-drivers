d74d06485fd28ea76a99114e00dfc0a1ba942b8f
CoreCode <core-code@users.noreply.github.com>
2019-05-04 16:58:15 +0200

    Update reolink-client.rb

--- Casks/reolink-client.rb
+++ Casks/reolink-client.rb
@@ -1,5 +1,5 @@
 cask 'reolink-client' do
-  version '3.5.1.53'
+  version '3.5.6.55'
   sha256 '928b4eb3f381e431dee7fc16665a389d5e4b47dc80dc5f6f1e423f44f2730702'
 
   url 'https://cdn.reolink.com/files/client/Mac-ReolinkClient.zip'
