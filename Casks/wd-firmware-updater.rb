b14d0e55fcc59a848c8aca6e1c197cc57eeb32db
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-05-09 18:39:23 +0200

    Update wd-firmware-update from latest to 4.0.0.13

--- Casks/wd-firmware-updater.rb
+++ Casks/wd-firmware-updater.rb
@@ -1,10 +1,13 @@
 cask 'wd-firmware-updater' do
-  version :latest
-  sha256 :no_check
+  version '4.0.0.13'
+  sha256 '8fd449b79366bc6670b550cd4fd2ca5cc896b90ba29e54a8ce0f86d96ee6fbf2'
 
   url 'http://download.wdc.com/fwupdater/Mac/WDFirmwareUpdater.zip'
-  name 'WDFirmwareUpdater'
-  homepage 'https://www.wdc.com/'
+  appcast 'https://support.wdc.com/downloads.aspx?lang=en'
+  name 'WD Firmware Updater'
+  homepage 'https://support.wdc.com/downloads.aspx?lang=en'
 
   app 'WD Firmware Updater.app'
+
+  zap trash: '~/Library/Caches/WD Firmware Updater'
 end
