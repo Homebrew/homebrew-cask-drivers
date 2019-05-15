290dc0313034026ea8fe68e1d01ee8581df6938f
Andrew Dunning <adunning@users.noreply.github.com>
2019-05-15 14:47:09 -0400

    ricoh-ps-printers-vol4-exp-driver 3.6.0.0 (#948)
    * ricoh-ps-printers-vol4-exp-driver 3.6.0.0
    
    * Update ricoh-ps-printers-vol4-exp-driver.rb
    
    * Update ricoh-ps-printers-vol4-exp-driver.rb

--- Casks/ricoh-ps-printers-vol4-exp-driver.rb
+++ Casks/ricoh-ps-printers-vol4-exp-driver.rb
@@ -1,6 +1,6 @@
 cask 'ricoh-ps-printers-vol4-exp-driver' do
-  version '3.4.0.0'
-  sha256 'ffd01b48ef1d3e0b86894eda316068b799178969075929544c7a832d50f9bf9a'
+  version '3.6.0.0'
+  sha256 '269d997c6efad95453314517034d4d5339091cc33f42b1ee2ace2795b89e76df'
 
   url "http://support.ricoh.com/bb/pub_e/dr_ut_e/0001310/0001310123/V#{version.delete('.')}/Ricoh_PS_Printers_Vol4_EXP_LIO_#{version}.dmg"
   name 'Ricoh PS printers Vol 4 EXP driver'
@@ -20,5 +20,6 @@ cask 'ricoh-ps-printers-vol4-exp-driver' do
                        'ricoh.copycontrol.pkg',
                        'ricoh.cupsfilter.pkg',
                        'ricoh.joblog.pkg',
+                       'ricoh.userAuthentication.pkg',
                      ]
 end
