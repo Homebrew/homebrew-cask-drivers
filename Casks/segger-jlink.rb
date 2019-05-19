27168bdfd61bea64b91db14065807233a1350874
Kirk Klobe <kklobe@gmail.com>
2019-05-19 07:19:04 -0500

    Update segger-jlink from 6.44h to 6.44i (#963)

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,12 +1,12 @@
 cask 'segger-jlink' do
-  version '6.44h'
-  sha256 '9fd8f216020d91ce41dfa093295681f81662bbf6b28711e60a70d89ceaa81d5c'
+  version '6.44i'
+  sha256 'f04fe7da71a774b67cadb7a383025036f218def8066b197a395a817beac81c45'
 
   url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}.pkg",
       using: :post,
       data:  {
                'accept_license_agreement' => 'accepted',
-               'non_eu_denied_list'       => 'confirmed',
+               'non_emb_ctr'              => 'confirmed',
                'submit'                   => 'Download software',
              }
   name 'Segger J-Link Command Line Tools'
