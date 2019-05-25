8a2c57a34ff49eec62facb9a2a0f9ec11a44d478
Joe George <joe@externl.com>
2019-05-25 09:27:00 -0400

    caldigit-thunderbolt-charging: fix url (#954)

--- Casks/caldigit-thunderbolt-charging.rb
+++ Casks/caldigit-thunderbolt-charging.rb
@@ -2,9 +2,9 @@ cask 'caldigit-thunderbolt-charging' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.caldigit.com/Support/TS3/CalDigit-Thunderbolt-Station-Charging-Support.zip'
+  url 'https://downloads.caldigit.com/CalDigit-Thunderbolt-Station-Mac-Drivers.zip'
   name 'CalDigit Thunderbolt Station USB Charging & SuperDrive Support Driver'
-  homepage 'http://www.caldigit.com/support.asp'
+  homepage 'https://www.caldigit.com/'
 
   pkg 'CalDigit Thunderbolt Station Charging Support.pkg'
 
