4ebe030ab0b195acf3efb1b661161e435b0dc7d7
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-06-02 15:30:24 +0200

    Fix style offenses

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -15,13 +15,13 @@ cask 'segger-jlink' do
   pkg "JLink_MacOSX_V#{version.no_dots}.pkg"
 
   uninstall quit:   [
-                       'com.segger.JFlashLite.*',
-                       'com.segger.JLinkGDBServer.*',
-                       'com.segger.JLinkLicenseManager.*',
-                       'com.segger.JLinkRegistration.*',
-                       'com.segger.JLinkRemoteServer.*',
-                       'com.segger.JLinkRTTViewer.*',
-                     ],
+                      'com.segger.JFlashLite.*',
+                      'com.segger.JLinkGDBServer.*',
+                      'com.segger.JLinkLicenseManager.*',
+                      'com.segger.JLinkRegistration.*',
+                      'com.segger.JLinkRemoteServer.*',
+                      'com.segger.JLinkRTTViewer.*',
+                    ],
             pkgutil: 'com.segger.pkg.JLink'
 
   zap trash: [
