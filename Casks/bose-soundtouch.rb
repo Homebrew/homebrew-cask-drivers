6af53ced738ba92823a5079c5ce3d1f79d3ef0c8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2019-06-10 12:35:56 +0100

    Update bose-soundtouch from 21.0.3-2635-5a2f205,mr4_2018_63f87764 to 21.0.3-2635-5a2f205,st1_2019_95571ec9 (#997)

--- Casks/bose-soundtouch.rb
+++ Casks/bose-soundtouch.rb
@@ -1,5 +1,5 @@
 cask 'bose-soundtouch' do
-  version '21.0.3-2635-5a2f205,mr4_2018_63f87764'
+  version '21.0.3-2635-5a2f205,st1_2019_95571ec9'
   sha256 '967073b8d66effc2afdaf6fc157542812c07acc8e697534485bbefd6115858d3'
 
   # bose.com was verified as official when first introduced to the cask
