a4a50102a8faa26e15a4ddc1fd1a2e4e9cb6cd28
CoreCode <core-code@users.noreply.github.com>
2019-06-10 19:33:05 +0200

    Update elgato-video-capture (#998)

--- Casks/elgato-video-capture.rb
+++ Casks/elgato-video-capture.rb
@@ -3,7 +3,7 @@ cask 'elgato-video-capture' do
   sha256 'dbb6b64143cf5954bdef1b9543b627bf482fe857cbeece0738bb3ca029fb47b9'
 
   url "https://edge.elgato.com/video-capture/macos/#{version.major_minor}/final/Elgato%20Video%20Capture%20#{version.major_minor}%20(#{version.patch}).dmg"
-  appcast 'https://rink.hockeyapp.net/api/2/apps/651790b07f55248c266474ee243a733a'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/651790b07f55248c266474ee243a733a.rss'
   name 'Elgato Video Capture'
   homepage 'https://www.elgato.com/en/video-capture/'
 
