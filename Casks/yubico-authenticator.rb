e1f30ee8a939972afbd3425ae9dc4292a9fddb4e
Dan Smith <dan@bytesandbolts.com>
2019-06-11 14:57:28 +0100

    Update yubico-authenticator from 4.3.6 to 4.3.6b (#1000)

--- Casks/yubico-authenticator.rb
+++ Casks/yubico-authenticator.rb
@@ -1,6 +1,6 @@
 cask 'yubico-authenticator' do
-  version '4.3.6'
-  sha256 'dcd92bb5dbd014cd96225bb6d7088b968697850033189ac977ed581afa95116b'
+  version '4.3.6b'
+  sha256 '4fff8785ab9422249c5514950cc0b6cdb32975fc3aafef3f6cba6eaeb5c9082d'
 
   url "https://developers.yubico.com/yubioath-desktop/Releases/yubioath-desktop-#{version}-mac.pkg"
   appcast 'https://developers.yubico.com/yubioath-desktop/Release_Notes.html'
