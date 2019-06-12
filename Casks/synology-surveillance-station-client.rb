7a40a41ff18dfcddf6b95d555fca3e5f9338db8f
CoreCode <core-code@users.noreply.github.com>
2019-06-13 01:49:54 +0200

    Update synology-surveillance-station-client from 1.2.4-0642 to 1.2.5-0659 (#1005)
    * Update synology-surveillance-station-client from 1.2.4-0642 to 1.2.5-0659
    
    * Update synology-surveillance-station-client.rb
    
    * Update synology-surveillance-station-client.rb

--- Casks/synology-surveillance-station-client.rb
+++ Casks/synology-surveillance-station-client.rb
@@ -1,16 +1,16 @@
 cask 'synology-surveillance-station-client' do
-  version '1.2.4-0642'
-  sha256 'ad4a2a95ea68df329f3b2809f169a35d70d0b5429367ccfa8495339abb5f4109'
+  version '1.2.5-0659'
+  sha256 'ca4a8f089e4df91899405a613d3b8b0655628e510ee6f16282ce3dd834f5dc80'
 
   url "https://global.download.synology.com/download/Tools/SurveillanceStationClient/#{version}/Mac/Synology%20Surveillance%20Station%20Client-#{version}.dmg"
-  appcast 'https://www.synology.com/releaseNote/SurveillanceStationClient'
+  appcast 'https://archive.synology.com/download/Tools/SurveillanceStationClient/'
   name 'Synology Surveillance Station Client'
   homepage 'https://www.synology.com/surveillance/'
 
-  pkg 'Install Surveillance Station Client.pkg'
+  pkg 'Install Synology Surveillance Station Client.pkg'
 
   uninstall launchctl: 'com.synology.svsclient-SurveillanceStationClient',
-            delete:    '/Applications/Surveillance Station Client.app',
+            delete:    '/Applications/Synology Surveillance Station Client.app',
             quit:      'com.synology.svsclient-SurveillanceStationClient',
             pkgutil:   [
                          'com.synology.svsclient-Live-View',
