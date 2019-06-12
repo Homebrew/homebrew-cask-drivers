b84f14b0a17c6fb44815bf72f57e7c0036a799b2
Julian Mayer <julian@corecode.io>
2019-06-12 21:26:07 +0200

    Update yubico-yubikey-manager

--- Casks/yubico-yubikey-manager.rb
+++ Casks/yubico-yubikey-manager.rb
@@ -3,7 +3,7 @@ cask 'yubico-yubikey-manager' do
   sha256 '40e11dfe770f0bf6e3abc2ef7556463f5c6613eb11113c2a180c7ca66fd60583'
 
   url "https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-#{version}-mac.pkg"
-  appcast 'https://developers.yubico.com/yubikey-manager-qt/Release_Notes.html'
+  appcast 'https://github.com/Yubico/yubikey-manager-qt/releases.atom'
   name 'Yubikey Manager'
   homepage 'https://developers.yubico.com/yubikey-manager-qt/'
 
