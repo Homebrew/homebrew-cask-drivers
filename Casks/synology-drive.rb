a7c82a80f09ed570c2daff0128d70472d7cd55fa
CoreCode <core-code@users.noreply.github.com>
2019-06-13 03:16:39 +0200

    Update synology-drive from 1.1.4-10580 to 2.0.0,11044 (#1004)
    * Update synology-drive from 1.1.4-10580 to 2.0.0,11044
    
    * Update synology-drive.rb
    
    * Update synology-drive.rb
    
    * Update synology-drive.rb
    
    * Update synology-drive.rb

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -1,17 +1,21 @@
 cask 'synology-drive' do
-  version '1.1.4-10580'
-  sha256 '6be8292b697f2fa7eaa3b3aab09cbb66ee7f73ca18b99cd20865bc32834b09af'
+  version '2.0.0,11044'
+  sha256 '39148b4d4d589bd7d92fdf79fd011a85e98c6b4cedcf30c4e1b308d8c4e3287f'
 
-  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version}/Mac/Installer/synology-drive-#{version.sub(%r{.*-}, '')}.dmg"
-  appcast 'https://www.synology.com/en-global/releaseNote/SynologyDriveClient'
+  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
+  appcast 'https://archive.synology.com/download/Tools/SynologyDriveClient/'
   name 'Synology Drive'
   homepage 'https://www.synology.com/'
 
   auto_updates true
 
-  pkg 'Install Synology Drive.pkg'
+  pkg 'Install Synology Drive Client.pkg'
 
-  uninstall quit:      'io.com.synology.CloudStationUI',
+  uninstall quit:      [
+                         'io.com.synology.CloudStationUI',
+                         'com.synology.CloudStationUI',
+                         'com.synology.CloudStation',
+                       ],
             pkgutil:   'com.synology.CloudStation',
             launchctl: 'com.synology.Synology Cloud Station'
 end
