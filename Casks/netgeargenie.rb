7187708c1c6d67fcc06817c6cad617fb449684ea
Work <brigitte@corecode.io>
2019-06-15 14:03:58 +0200

    Update netgeargenie from 2.4.40,2018-12-14 to 2.4.42

--- Casks/netgeargenie.rb
+++ Casks/netgeargenie.rb
@@ -1,12 +1,12 @@
 cask 'netgeargenie' do
-  version '2.4.40,2018-12-14'
-  sha256 'eca8915c6a792f85b00786fc303c8f91a7809ccfa2ba72212417fff9243517b1'
+  version '2.4.42'
+  sha256 'c11c659e550cec0e4bdabfc03d9fed49ab9a92be14ee40e47119e291ba260699'
 
   url 'http://updates1.netgear.com/netgeargenie/mac/update/NETGEARGenieInstaller.dmg'
   name 'NETGEARGenie'
   homepage 'https://www.netgear.com/home/discover/apps/genie.aspx'
 
-  pkg "NETGEAR_Genie_Installer_#{version.before_comma}(#{version.after_comma}).pkg"
+  pkg "NETGEAR_Genie_Installer_#{version}.pkg"
 
   uninstall quit:    'com.yourcompany.NETGEARGenie',
             pkgutil: 'com.netgear.netgearGenie.NETGEARGenie.pkg'
