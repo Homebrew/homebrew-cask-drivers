393e96709a376a5638547f31ac7b70eda5190a05
JasperV <1297464+JasperV@users.noreply.github.com>
2019-06-18 13:49:05 +0200

    Update style in GARMIN Fresh Cask

--- Casks/fresh.rb
+++ Casks/fresh.rb
@@ -2,7 +2,6 @@ cask 'fresh' do
   version :latest
   sha256 :no_check
 
-  # navistatic.garmin.com was verified as official when first introduced to the cask
   url 'https://navistatic.garmin.com/u/garmin_fresh/mac/GARMIN_Fresh_setup.pkg'
   name 'GARMIN Fresh'
   homepage 'https://volkswagen.garmin.com/volkswagen/site/fresh'
@@ -10,8 +9,7 @@ cask 'fresh' do
   pkg 'GARMIN_Fresh_setup.pkg'
 
   uninstall delete:   '/Applications/Fresh.app',
-            pkgutil:
-                      [
+            pkgutil:  [
                         'com.garmin.fresh',
                         'com.navigon.fresh',
                       ]
