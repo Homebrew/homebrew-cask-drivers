69fc269fec60e18af08bc48e3d7b1bfd23c96af4
JasperV <1297464+JasperV@users.noreply.github.com>
2019-06-18 15:29:07 +0200

    Add GARMIN Fresh Latest (#1021)
    * Add GARMIN Fresh Latest
    
    * Update style in GARMIN Fresh Cask
    
    * Update and rename fresh.rb to garmin-fresh.rb

---
+++ Casks/garmin-fresh.rb
@@ -0,0 +1,16 @@
+cask 'garmin-fresh' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://navistatic.garmin.com/u/garmin_fresh/mac/GARMIN_Fresh_setup.pkg'
+  name 'GARMIN Fresh'
+  homepage 'https://volkswagen.garmin.com/volkswagen/site/fresh'
+
+  pkg 'GARMIN_Fresh_setup.pkg'
+
+  uninstall delete:  '/Applications/Fresh.app',
+            pkgutil: [
+                       'com.garmin.fresh',
+                       'com.navigon.fresh',
+                     ]
+end
