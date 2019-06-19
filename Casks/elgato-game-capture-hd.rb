34ffd5bb17ce73caa43bc76b762c5725560cecfa
Work <brigitte@corecode.io>
2019-06-19 23:09:59 +0200

    Update elgato-game-capture-hd from 2.11.3,1422 to 2.11.5,1431

--- Casks/elgato-game-capture-hd.rb
+++ Casks/elgato-game-capture-hd.rb
@@ -6,8 +6,8 @@ cask 'elgato-game-capture-hd' do
     version '2.9.2,1327'
     sha256 '9bcf01399719755034c964549a6a3af38932e7eaf03febc8b3742306505ca8a9'
   else
-    version '2.11.3,1422'
-    sha256 'd4d5c20269823b7e643260191a11cdf69394cdb4812251f6e3f9cd52fb4da5ff'
+    version '2.11.5,1431'
+    sha256 '5605fa3f6ac9ec49036e8d60ec67c407fc2bcb1bad2fa7259aef99b79290a04e'
   end
 
   url "https://gc-updates.elgato.com/mac/download.php?build=#{version.after_comma}"
