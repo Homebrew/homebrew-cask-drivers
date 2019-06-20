7095de4cd0c2aed8efa4fb5b8369b85c4b1dabb3
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-06-20 16:14:02 +0200

    Add garmin-fresh 3.6.0 zap

--- Casks/garmin-fresh.rb
+++ Casks/garmin-fresh.rb
@@ -1,6 +1,6 @@
 cask 'garmin-fresh' do
-  version :latest
-  sha256 :no_check
+  version '3.6.0'
+  sha256 '2eb02831657e7e606f900d0d76cbb7063763670b73d62e512f389b1890c6dc5a'
 
   url 'https://navistatic.garmin.com/u/garmin_fresh/mac/GARMIN_Fresh_setup.pkg'
   name 'GARMIN Fresh'
@@ -8,9 +8,14 @@ cask 'garmin-fresh' do
 
   pkg 'GARMIN_Fresh_setup.pkg'
 
-  uninstall delete:  '/Applications/Fresh.app',
+  uninstall quit:    'com.garmin.fresh',
             pkgutil: [
                        'com.garmin.fresh',
                        'com.navigon.fresh',
                      ]
+
+  zap trash: [
+               '~/Library/Fresh',
+               '~/Library/Saved Application State/com.garmin.fresh.savedState'
+             ]
 end
