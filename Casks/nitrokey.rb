31a0b7d8408ae473ad31ae63f6858b069e4b8569
Szczepan Zalega <szszszsz@users.noreply.github.com>
2019-06-25 16:24:37 +0200

    Update nitrokey.rb
    Update for Nitrokey App v1.4 release
    
    Not tested

--- Casks/nitrokey.rb
+++ Casks/nitrokey.rb
@@ -1,6 +1,6 @@
 cask 'nitrokey' do
-  version '1.3.2'
-  sha256 'd6276f6e632625f90a3081251b44636bc0ff4a17e245f080fbd3d661c0eadb85'
+  version '1.4'
+  sha256 '80f6c95f5fa8320e4e323e1f4c11ac428ad4b731ae186891a4a66c7c056485f0'
 
   # github.com/Nitrokey/nitrokey-app was verified as official when first introduced to the cask
   url "https://github.com/Nitrokey/nitrokey-app/releases/download/v#{version}/Nitrokey.App.dmg"
