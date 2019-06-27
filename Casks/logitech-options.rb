9d9d66d51539a2c47fe9d4786132abbd82ddde4c
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-06-27 13:23:06 +0200

    Update logitech-options 7.12.82 uninstall and zap

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -3,22 +3,39 @@ cask 'logitech-options' do
   sha256 '46d82403ff962bef5ceaa531c6914c990d0f4e1cecbc307c43a766eb8d756a88'
 
   url "https://www.logitech.com/pub/techsupport/options/Options_#{version}.zip"
+  appcast 'https://support.logitech.com/software/options'
   name 'Logitech Options'
-  homepage 'https://support.logitech.com/en_us/software/options'
+  homepage 'https://support.logitech.com/software/options'
 
   auto_updates true
   depends_on macos: '>= :sierra'
 
   pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
 
-  uninstall script:    {
+  uninstall launchctl: 'com.logitech.manager.daemon',
+            quit:      [
+                         'com.logitech.Logi-Options',
+                         'com.logitech.manager.daemon',
+                         'com.logitech.manager.uninstaller',
+                       ],
+            script:    [
                          executable: '/Applications/Utilities/LogiMgr Uninstaller.app/Contents/Resources/Uninstaller',
-                       },
+                       ],
             pkgutil:   [
                          'com.logitech.manager.pkg',
                          'com.Logitech.signedKext.pkg',
                        ],
-            launchctl: 'com.logitech.manager.daemon'
+            delete:    '/Library/Application Support/Logitech.localized'
+
+  zap trash: [
+               '~/Library/Application Support/Logitech/Logitech Options',
+               '~/Library/Caches/com.logitech.Logi-Options',
+               '~/Library/Preferences/com.logitech.Logi-Options.plist',
+               '~/Library/Preferences/com.logitech.manager.daemon.plist',
+               '~/Library/Preferences/com.logitech.manager.setting.ffff.plist',
+               '~/Library/Saved Application State/com.logitech.manager.uninstaller.savedState',
+             ],
+      rmdir: '~/Library/Application Support/Logitech'
 
   caveats do
     reboot
