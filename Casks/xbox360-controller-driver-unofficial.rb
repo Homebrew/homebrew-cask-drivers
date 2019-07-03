ed84a3d2920d5993ef8548dde1a5f083e32b1ed8
Yvo Mulder <ytbmulder@gmail.com>
2019-07-03 15:30:13 +0200

    Update xbox360-controller-driver-unofficial 0.16.11 uninstall (#1052)
    * Update xbox360-controller-driver-unofficial 0.16.11 uninstall
    
    * Update xbox360-controller-driver-unofficial.rb

--- Casks/xbox360-controller-driver-unofficial.rb
+++ Casks/xbox360-controller-driver-unofficial.rb
@@ -7,16 +7,19 @@ cask 'xbox360-controller-driver-unofficial' do
   name 'TattieBogle Xbox 360 Controller Driver (with improvements)'
   homepage 'https://github.com/360Controller/360Controller'
 
+  depends_on macos: '>= :el_capitan'
+
   pkg 'Install360Controller.pkg'
 
-  uninstall pkgutil:   'com.mice.pkg.Xbox360controller',
-            launchctl: 'com.mice.360Daemon',
+  uninstall launchctl: 'com.mice.360Daemon',
+            quit:      'com.mice.-60Daemon', # '-60' is not a typo.
             kext:      [
                          'com.mice.Xbox360ControllerForceFeedback',
                          'com.mice.driver.Xbox360Controller',
                          'com.mice.driver.Wireless360Controller',
                          'com.mice.driver.WirelessGamingReceiver',
                        ],
+            pkgutil:   'com.mice.pkg.Xbox360controller',
             # Symlink to kext in /Library/Extensions is not removed
             # during :pkgutil phase of uninstall, so we delete it here.
             delete:    '/System/Library/Extensions/360Controller.kext'
