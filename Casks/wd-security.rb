e4d1c1c669cf533e43d8a3af6ba3d9ed79d11461
CoreCode <core-code@users.noreply.github.com>
2019-07-03 15:16:54 +0200

    Update wd-security (#1053)
    * Update wd-security
    
    * Update wd-security.rb

--- Casks/wd-security.rb
+++ Casks/wd-security.rb
@@ -2,10 +2,10 @@ cask 'wd-security' do
   version '2.1.1.61'
   sha256 'b6744537b81a4bfb27c23413e457ddf74864220734a59a0df5ee0e48d90de8f5'
 
-  url "http://downloads.wdc.com/wdapp/WD_Security_Standalone_Installer_Mac_#{version.dots_to_underscores}.zip"
-  appcast 'https://support.wdc.com/downloads.aspx?lang=en'
+  url "https://downloads.wdc.com/wdapp/WD_Security_Standalone_Installer_Mac_#{version.dots_to_underscores}.zip"
+  appcast 'https://support.wdc.com/downloads.aspx?p=158'
   name 'WD Security'
-  homepage 'https://support.wdc.com/downloads.aspx?lang=en'
+  homepage 'https://support.wdc.com/downloads.aspx?p=158'
 
   installer manual: 'WD Security Installer.app'
 
