832403f45db42b877e25f1b58dc21f54bee31964
Yvo Mulder <ytbmulder@gmail.com>
2019-07-04 13:16:18 +0200

    Update logitech-options 7.12.82 uninstall and zap (#1045)
    * Update logitech-options 7.12.82 uninstall and zap
    
    * Remove appcast

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -4,21 +4,37 @@ cask 'logitech-options' do
 
   url "https://www.logitech.com/pub/techsupport/options/Options_#{version}.zip"
   name 'Logitech Options'
-  homepage 'https://support.logitech.com/en_us/software/options'
+  homepage 'https://support.logitech.com/software/options'
 
   auto_updates true
   depends_on macos: '>= :sierra'
 
   pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
 
-  uninstall script:    {
+  uninstall launchctl: 'com.logitech.manager.daemon',
+            quit:      [
+                         'com.logitech.Logi-Options',
+                         'com.logitech.manager.daemon',
+                         'com.logitech.manager.uninstaller',
+                       ],
+            script:    [
                          executable: '/Applications/Utilities/LogiMgr Uninstaller.app/Contents/Resources/Uninstaller',
-                       },
+                       ],
             pkgutil:   [
                          'com.logitech.manager.pkg',
                          'com.Logitech.signedKext.pkg',
                        ],
-            launchctl: 'com.logitech.manager.daemon'
+            delete:    '/Library/Application Support/Logitech.localized'
+
+  zap trash: [
+               '~/Library/Application Support/Logitech/Logitech Options',
+               '~/Library/Caches/com.logitech.Logi-Options',
+               '~/Library/Preferences/com.logitech.Logi-Options.plist',
+               '~/Library/Preferences/com.logitech.manager.daemon.plist',
+               '~/Library/Preferences/com.logitech.manager.setting.ffff.plist',
+               '~/Library/Saved Application State/com.logitech.manager.uninstaller.savedState',
+             ],
+      rmdir: '~/Library/Application Support/Logitech'
 
   caveats do
     reboot
