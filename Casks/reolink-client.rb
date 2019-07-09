17b506275247baa5e4d5c3dd1011ad17ac074c0d
Julian Mayer <julian@corecode.io>
2019-07-09 19:27:23 +0200

    Update reolink-client

--- Casks/reolink-client.rb
+++ Casks/reolink-client.rb
@@ -3,7 +3,7 @@ cask 'reolink-client' do
   sha256 '928b4eb3f381e431dee7fc16665a389d5e4b47dc80dc5f6f1e423f44f2730702'
 
   url 'https://cdn.reolink.com/files/client/Mac-ReolinkClient.zip'
-  appcast 'https://reolink.com/software-and-manual/'
+  appcast 'https://cdn.reolink.com/wp-content/uploads/2018/02/shipping.js'
   name 'Reolink Client'
   homepage 'https://reolink.com/software-and-manual/'
 
