8694c4ce42286140f307564d2fa7dfecf1be511b
Yvo Mulder <ytbmulder@gmail.com>
2019-07-12 14:52:17 +0200

    Add suunto-moveslink 1.0.66 zap (#1083)
    * Add suunto-moveslink 1.0.66 zap
    
    * Update suunto-moveslink.rb

--- Casks/suunto-moveslink.rb
+++ Casks/suunto-moveslink.rb
@@ -3,13 +3,20 @@ cask 'suunto-moveslink' do
   sha256 '5897fe23239a4400f9f23929f2eadcc424bdc5f3f3b3da74a1c17b8ec778997c'
 
   url "https://content.static.movescount.com/downloads/Moveslink_setup_#{version.dots_to_underscores}.dmg"
-  appcast 'https://www.movescount.com/connect/moveslink?os=mac'
+  appcast 'https://www.movescount.com/connect/moveslink?os=mac',
+          configuration: version.dots_to_underscores
   name 'Suunto Moveslink'
   homepage 'https://www.movescount.com/connect/moveslink?os=mac'
 
   pkg 'Moveslink_setup.pkg'
 
-  uninstall login_item: 'Moveslink',
-            pkgutil:    'com.suunto.moveslink.moveslink.pkg',
-            quit:       'com.yourcompany.Moveslink'
+  uninstall quit:       'com.yourcompany.Moveslink',
+            login_item: 'Moveslink',
+            pkgutil:    'com.suunto.moveslink.moveslink.pkg'
+
+  zap trash: [
+               '~/Library/Application Support/Suunto/Moveslink',
+               '~/Library/Saved Application State/com.yourcompany.Moveslink.savedState',
+             ],
+      rmdir: '~/Library/Application Support/Suunto'
 end
