6f732e4b2148386456f19df3a8a5d850709a8507
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-07-14 20:15:14 +0200

    Add sony-xperia-companion latest zap

--- Casks/sony-xperia-companion.rb
+++ Casks/sony-xperia-companion.rb
@@ -6,7 +6,20 @@ cask 'sony-xperia-companion' do
   name 'Xperia™ Companion'
   homepage 'https://support.sonymobile.com/xperia-companion/'
 
+  depends_on macos: '>= :el_capitan'
+
   app 'Xperia Companion.app'
 
-  zap trash: '~/Library/Application Support/com.sonymobile.Xperia-Companion'
+  uninstall quit: 'com.sonymobile.Xperia-Companion*'
+
+  zap trash: [
+               '~/Library/Application Support/com.sonymobile.Xperia-Companion',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.sonymobile.Xperia-Companion',
+               '~/Library/Caches/com.sonymobile.Xperia-Companion',
+               '~/Library/Caches/com.sonymobile.Xperia-Companion-Agent',
+               '~/Library/Preferences/com.sonymobile.Xperia-Companion-Agent.plist',
+               '~/Library/Preferences/com.sonymobile.Xperia-Companion-Helper.plist',
+               '~/Library/Preferences/com.sonymobile.Xperia-Companion.plist',
+               '~/Library/Saved Application State/com.sonymobile.Xperia-Companion.savedState',
+             ]
 end
