5e4cb1b792b80117ce2e467d2401e6d06ece1da1
dezmob <sebdominguez@me.com>
2019-07-15 19:23:36 +0200

    Fix checksum in silicon-labs-vcp-driver and change pkg to installer

--- Casks/silicon-labs-vcp-driver.rb
+++ Casks/silicon-labs-vcp-driver.rb
@@ -3,11 +3,10 @@ cask 'silicon-labs-vcp-driver' do
     version '4.11.3'
     pkg 'Legacy MacVCP Driver/Silicon Labs VCP Driver.pkg'
   else
-    version '5.1.0'
-    pkg 'Silicon Labs VCP Driver.pkg'
+    version '5.2.0'
   end
 
-  sha256 '278ad8f1444a55ce7e86229f8ce808403fe556cf44921c1419bb96cacaf377f1'
+  sha256 'a3feeef0088362710e66d1eaa48113bfb2286242fdbae584cb7dcdd19117a6a2'
 
   url 'https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver.zip'
   appcast 'https://www.silabs.com/documents/public/release-notes/Mac_OSX_VCP_Driver_Release_Notes.txt'
@@ -16,6 +15,12 @@ cask 'silicon-labs-vcp-driver' do
   homepage 'https://www.silabs.com/products/development-tools/software/usb-to-uart-bridge-vcp-drivers'
 
   container nested: 'Mac_OSX_VCP_Driver/SiLabsUSBDriverDisk.dmg'
+
+  installer script: {
+                      executable: "#{staged_path}/Install CP210x VCP Driver.app/Contents/MacOS/Install CP210x VCP Driver",
+                      args:       ['-install'],
+                      sudo:       true,
+                    }
 
   uninstall kext:    'com.silabs.driver.CP210xVCPDriver',
             script:  [
