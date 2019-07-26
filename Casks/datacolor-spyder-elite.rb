4df843193dab55b475ae74214d76db02cac9935c
Yvo Mulder <ytbmulder@gmail.com>
2019-07-26 13:01:55 +0200

    Add datacolor-spyder-elite 5.3 zap (#1115)
    * Add datacolor-spyder-elite 5.3 zap
    
    * Update homepage to https

--- Casks/datacolor-spyder-elite.rb
+++ Casks/datacolor-spyder-elite.rb
@@ -5,13 +5,25 @@ cask 'datacolor-spyder-elite' do
   # d3d9ci7ypuovlo.cloudfront.net/spyder was verified as official when first introduced to the cask
   url "https://d3d9ci7ypuovlo.cloudfront.net/spyder#{version.major}/Spyder#{version.major}Elite_#{version}_OSX_Installer.zip"
   name 'Spyder Elite'
-  homepage "http://www.datacolor.com/photography-design/product-overview/spyder#{version.major}-family/#spyder#{version.major}elite"
+  homepage "https://www.datacolor.com/photography-design/product-overview/spyder#{version.major}-family/#spyder#{version.major}elite"
 
   auto_updates true
 
   installer manual: "Spyder#{version.major}Elite_#{version}_OSX_Installer.app"
 
-  uninstall delete: "/Applications/Datacolor/Spyder#{version.major}Elite",
-            signal: ['TERM', "com.datacolor.spyder#{version.major}utility"],
+  uninstall signal: [
+                      ['TERM', "com.datacolor.spyder#{version.major}elite"],
+                      ['TERM', "com.datacolor.spyder#{version.major}utility"],
+                    ],
+            delete: "/Applications/Datacolor/Spyder#{version.major}Elite",
             rmdir:  '/Applications/Datacolor'
+
+  zap trash: [
+               '~/Library/Application Support/MindVision/Spyder5Elite_Installer.xtm',
+               '~/Library/Caches/com.datacolor.spyder5elite',
+               '~/Library/Preferences/Datacolor/Spyder5Elite',
+               '~/Library/Preferences/Datacolor/SpyderUtility',
+               '~/Library/Saved Application State/com.datacolor.spyder5elite.savedState',
+             ],
+      rmdir: '~/Library/Preferences/Datacolor'
 end
