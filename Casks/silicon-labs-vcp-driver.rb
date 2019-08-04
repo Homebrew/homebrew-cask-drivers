671192d1bd01ee5f054672b865ae3531614c36eb
Sébastien Dominguez <52044363+dezmob@users.noreply.github.com>
2019-08-04 22:51:59 +0200

    Fix checksum in silicon-labs-vcp-driver and change pkg to installer (#1095)
    * Fix checksum in silicon-labs-vcp-driver and change pkg to installer
    
    * Update silicon-labs-vcp-driver.rb
    
    * Updated silicon-labs-vcp-driver uninstall stanzas
    
    * Fix  style in silicon-labs-vcp-driver

--- Casks/silicon-labs-vcp-driver.rb
+++ Casks/silicon-labs-vcp-driver.rb
@@ -1,13 +1,6 @@
 cask 'silicon-labs-vcp-driver' do
-  if MacOS.version <= :yosemite
-    version '4.11.3'
-    pkg 'Legacy MacVCP Driver/Silicon Labs VCP Driver.pkg'
-  else
-    version '5.1.0'
-    pkg 'Silicon Labs VCP Driver.pkg'
-  end
-
-  sha256 '278ad8f1444a55ce7e86229f8ce808403fe556cf44921c1419bb96cacaf377f1'
+  version '5.2.0'
+  sha256 'a3feeef0088362710e66d1eaa48113bfb2286242fdbae584cb7dcdd19117a6a2'
 
   url 'https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver.zip'
   appcast 'https://www.silabs.com/documents/public/release-notes/Mac_OSX_VCP_Driver_Release_Notes.txt'
@@ -17,10 +10,14 @@ cask 'silicon-labs-vcp-driver' do
 
   container nested: 'Mac_OSX_VCP_Driver/SiLabsUSBDriverDisk.dmg'
 
-  uninstall kext:    'com.silabs.driver.CP210xVCPDriver',
-            script:  [
-                       executable: "#{staged_path}/uninstaller.sh",
-                       sudo:       true,
-                     ],
-            pkgutil: 'com.silabs.driver.CP210xVCPDriver'
+  installer script: {
+                      executable: "#{staged_path}/Install CP210x VCP Driver.app/Contents/MacOS/Install CP210x VCP Driver",
+                      args:       ['-install'],
+                      sudo:       true,
+                    }
+
+  uninstall script: {
+                      executable: "#{staged_path}/uninstaller.sh",
+                      sudo:       true,
+                    }
 end
