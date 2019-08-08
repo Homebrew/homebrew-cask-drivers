4d0f667fefb8623f0dc1e2606d7e692fc10049ad
CoreCode <core-code@users.noreply.github.com>
2019-08-08 13:20:10 +0200

    Update plantronics-hub homepage stanza (#1132)
    * Update plantronics-hub.rb
    
    * Update plantronics-hub.rb

--- Casks/plantronics-hub.rb
+++ Casks/plantronics-hub.rb
@@ -4,7 +4,7 @@ cask 'plantronics-hub' do
 
   url 'https://www.plantronics.com/content/dam/plantronics/software/PlantronicsHubInstaller.dmg'
   name 'Plantronics Hub'
-  homepage 'https://www.plantronics.com/au/product/plantronics-hub-desktop'
+  homepage 'https://www.plantronics.com/us/en/support/downloads-apps/hub-desktop'
 
   depends_on macos: '>= :yosemite'
 
@@ -15,7 +15,8 @@ cask 'plantronics-hub' do
                          'com.plantronics.plantronicsSoftware.preflight.pkg',
                          'Plantronics-Inc..Plantronics-Hub',
                        ],
-            launchctl: 'com.PlantronicsUpdateService'
+            launchctl: 'com.PlantronicsUpdateService',
+            quit:      'Plantronics-Inc..Plantronics-Hub'
 
   zap trash: [
                '~/Library/Application Support/Plantronics',
