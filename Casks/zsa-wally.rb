63ae4ff7581596bfd546f343d6b04758ff6dbcf9
Florian Didron <fdidron@users.noreply.github.com>
2019-08-21 13:59:59 +0000

    Add wally v1.1.1 (#1153)
    Wally is an open source application allowing users to flash new firmwares to their ZSA keyboards.

---
+++ Casks/zsa-wally.rb
@@ -0,0 +1,12 @@
+cask 'zsa-wally' do
+  version '1.1.1'
+  sha256 'd06c1270e9f369b3176d66b94a30b4d7f31c2a4c1c261926c7448ce3f270cf77'
+
+  # github.com/zsa/wally was verified as official when first introduced to the cask
+  url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
+  appcast 'https://github.com/zsa/wally/releases.atom'
+  name 'Wally'
+  homepage 'https://ergodox-ez.com/pages/wally'
+
+  app 'Wally.app'
+end
