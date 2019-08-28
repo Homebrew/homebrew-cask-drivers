6b42d097175efa6f1aa49f5478790e15f59cf470
CoreCode <core-code@users.noreply.github.com>
2019-08-28 19:19:20 +0200

    Update segger-systemview.rb

--- Casks/segger-systemview.rb
+++ Casks/segger-systemview.rb
@@ -3,6 +3,8 @@ cask 'segger-systemview' do
   sha256 '652dcde70b4d6699d4a82aff9bcc1960e7053b18346299a150b835127549da02'
 
   url "https://www.segger.com/downloads/jlink/SystemView_MacOSX_V#{version.no_dots}.pkg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.segger.com/downloads/jlink/systemview_mac_pkg',
+          configuration: version.no_dots
   name 'SystemView'
   homepage 'https://www.segger.com/products/development-tools/systemview'
 
