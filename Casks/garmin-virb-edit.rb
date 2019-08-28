6a11727fecacde7e5738fb7b88e73095f10441f1
CoreCode <core-code@users.noreply.github.com>
2019-08-28 19:33:03 +0200

    Update garmin-virb-edit.rb

--- Casks/garmin-virb-edit.rb
+++ Casks/garmin-virb-edit.rb
@@ -3,6 +3,7 @@ cask 'garmin-virb-edit' do
   sha256 'b58d481a30f24c7a2b4b753020ebe25250debd6bcaccceaa59684a0799e004d2'
 
   url "https://download.garmin.com/software/VIRBEditforMac_#{version.no_dots}.dmg"
+  appcast 'https://www8.garmin.com/support/download_details.jsp?id=6593'
   name 'Garmin VIRB Edit'
   homepage 'https://buy.garmin.com/en-US/US/p/573412'
 
