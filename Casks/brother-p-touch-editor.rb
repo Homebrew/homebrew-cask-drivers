145ce0da58881006dccbdc60bae0cf805a18acf1
Viktor Szakats <vszakats@users.noreply.github.com>
2019-08-30 18:21:04 +0200

    brother-p-touch-editor 5.2.7 (#1174)

--- Casks/brother-p-touch-editor.rb
+++ Casks/brother-p-touch-editor.rb
@@ -1,20 +1,13 @@
 cask 'brother-p-touch-editor' do
-  version '5.1.110'
-  sha256 '237ef13b68a96d686e99e78214a75ce3aec5bbb7b4073f49c34cd494057c0081'
+  version '5.2.7'
+  sha256 '6d14b1ee9b041eae0b07242780af7eb3b5b638fbeff0311053e624310a372afb'
 
   # download.brother.com was verified as official when first introduced to the cask
-  url "https://download.brother.com/pub/com/ptouch-su/editor/pem#{version.no_dots}x14us.dmg"
+  url "https://download.brother.com/welcome/dlfp100701/pem#{version.no_dots}x14all.dmg"
   name 'Brother P-Touch Editor'
   homepage 'https://www.brother.co.jp/eng/dev/print/other_editor/'
 
   pkg "BrotherPtEdit#{version.major}.pkg"
 
-  postflight do
-    File.unlink("#{ENV['HOME']}/Desktop/P-touch Editor.app")
-  end
-
-  uninstall pkgutil: [
-                       "com.brother.P-touchEditor#{version.major}.pkg",
-                       'com.brother.brotherptdriver.BrotherFonts_Common.pkg',
-                     ]
+  uninstall pkgutil: "com.Brother.Brotherdriver.BrotherPtEdit#{version.major}"
 end
