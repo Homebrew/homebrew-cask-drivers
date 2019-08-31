2a70427bcdba1add182a668e5923004083de1b18
CoreCode <core-code@users.noreply.github.com>
2019-08-31 02:21:54 +0200

    Update focusrite-control from 2.4.2 to 3.2.1_0 (#1172)
    * Update focusrite-control from 2.4.2 to 3.2.1_0
    
    * Update focusrite-control.rb

--- Casks/focusrite-control.rb
+++ Casks/focusrite-control.rb
@@ -1,16 +1,18 @@
 cask 'focusrite-control' do
-  version '2.4.2'
-  sha256 '6947d13533d591db0a8902ddee0f42e49ba73bf6819982a066be775bbcd54e08'
+  version '3.2.1_0'
+  sha256 '14c0ea0d2b27a34efd348509b704cc13d183f7ba7f427aea476eb372c05787bb'
 
-  url "https://customer.focusrite.com/sites/customer/files/downloads/Focusrite%20Control%20-%20#{version}.dmg"
+  url "https://customer.focusrite.com/sites/customer/files/downloads/Focusrite%20Control-#{version}.dmg"
+  appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software'
   name 'Focusrite Control'
   homepage 'https://us.focusrite.com/downloads?product=Scarlett+18i8'
 
   pkg 'Focusrite Control.pkg'
 
-  uninstall pkgutil: [
-                       'com.focusrite.FocusriteControl',
-                       'com.focusrite.pkg.FocusritePCIe.audio_driver',
-                       'com.focusrite.pkg.FocusritePCIe.midi_driver',
-                     ]
+  uninstall pkgutil:   [
+                         'com.focusrite.FocusriteControl',
+                         'com.focusrite.pkg.FocusritePCIe.audio_driver',
+                         'com.focusrite.pkg.FocusritePCIe.midi_driver',
+                       ],
+            launchctl: 'com.focusrite.ControlServer'
 end
