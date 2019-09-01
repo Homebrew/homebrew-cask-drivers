090ad681565aa608093997c837bead987c5ca9c9
Vítor Galvão <info@vitorgalvao.com>
2019-09-01 10:38:01 +0100

    Update canon-captureontouch-utility.rb

--- Casks/canon-captureontouch-utility.rb
+++ Casks/canon-captureontouch-utility.rb
@@ -1,9 +1,9 @@
 cask 'canon-captureontouch-utility' do
-  version '4.4.19'
+  version '4.4.19.0222'
   sha256 'ba196b001ff61899e3ae01ccc5e1a2878d9cf8444d5088f3ac6bc3d75b517c91'
 
-  # gdlp01.c-wss.com/gds/0/0200005510/03/CaptureOnTouchV.4.4.19.0222forMac.dmg was verified as official when first introduced to the cask
-  url 'http://gdlp01.c-wss.com/gds/0/0200005510/03/CaptureOnTouchV.4.4.19.0222forMac.dmg'
+  # gdlp01.c-wss.com/gds/0/0200005510/03 was verified as official when first introduced to the cask
+  url "http://gdlp01.c-wss.com/gds/0/0200005510/03/CaptureOnTouchV.#{version}forMac.dmg"
   name 'Canon CaptureOnTouch Utility'
   homepage 'https://www.canon.se/support/consumer_products/products/scanners/others/imageformula_p-215ii.html?type=download'
 
