964f30a38719ded9d94be74347ee1e7fda000cb0
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2019-09-03 19:29:52 +0200

    Add Canon Imageformula driver 1.6.18 (#1177)

---
+++ Casks/canon-imageformula-driver.rb
@@ -0,0 +1,27 @@
+cask 'canon-imageformula-driver' do
+  version '1.6.18.1009'
+  sha256 'c4bd8d730bc0066f47e9a5651a0051c5d8402e438f1899d2c7152376ad61ed05'
+
+  # gdlp01.c-wss.com/gds/1/0100009941/02 was verified as official when first introduced to the cask
+  url "http://gdlp01.c-wss.com/gds/1/0100009941/02/P-215IIDriverV.#{version}forMac.dmg"
+  appcast 'https://my.canon/en/support/0100992103/2'
+  name 'Canon ImageFormula Twain Driver'
+  homepage 'https://my.canon/en/support/0100992103/2'
+
+  depends_on macos: [
+                      :sierra,
+                      :high_sierra,
+                      :mojave,
+                    ]
+
+  pkg 'P215II Installer.pkg'
+
+  uninstall quit:    [
+                       'com.canonElectronics.Installer.scanserver.pkg',
+                       'com.canonElectronics.scanserver',
+                     ],
+            pkgutil: [
+                       'com.canonElectronics.Installer.P215II Driver.pkg',
+                       'com.canonElectronics.Installer.scanserver.pkg',
+                     ]
+end
