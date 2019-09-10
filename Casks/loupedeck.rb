78dcde51552ab0a32679de70609252bb4a7dae8d
Gaëtan P <29287203+woolfyx@users.noreply.github.com>
2019-09-10 16:14:31 +0200

    Update Loupedeck to v2.7.2 (#1192)
    * Update Loupedeck driver in v2.7.2
    
    URL changed for v2.7.2
    
    * Update Loupedeck to v2.7.2
    
    * Update Loupedeck to v2.7.2
    
    * Update Loupedeck to v2.7.2

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -1,15 +1,15 @@
 cask 'loupedeck' do
-  version '2.7.0'
-  sha256 'e6f9909ef78ae6c9d24ebd2875d5cb3b46358bcc37043bb1267abf819af20305'
+  version '2.7.2'
+  sha256 '91e87c9271dbf4a54d6cd533105b09e9de24839de5bfed17a6f6baa48699907e'
 
   # loupedeck-software-release.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://loupedeck-software-release.s3.amazonaws.com/Software_Release_Version_#{version.dots_to_underscores}/MacOs_#{version.dots_to_underscores}/Loupedeck.dmg"
+  url "https://loupedeck-software-release.s3.amazonaws.com/Software_Release_Mac_#{version.dots_to_underscores}/Loupedeck_#{version.dots_to_underscores}.dmg"
   name 'Loupdeck'
   homepage 'https://loupedeck.com/'
 
   depends_on macos: '>= :sierra'
 
-  pkg "Loupedeck Installer #{version}.pkg"
+  pkg "LoupedeckInstaller #{version}.pkg"
 
   uninstall signal:  [
                        ['TERM', 'com.loupedeck.Loupedeck2'],
