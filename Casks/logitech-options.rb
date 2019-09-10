2666281b04936cc1fe50e1d3de066c134b9633b1
suschizu <brigitte@corecode.io>
2019-09-10 16:14:37 +0200

    Update logitech-options.rb (#1190)

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -12,7 +12,7 @@ cask 'logitech-options' do
   homepage 'https://support.logitech.com/software/options'
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :high_sierra'
 
   pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
 
