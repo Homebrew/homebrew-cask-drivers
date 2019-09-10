cd398c2cf9bcf22cda05a67ee72e86b565cbb59a
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-10 11:34:50 -0700

    Update sonos from 10.3 to 10.4 (#1194)

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,6 +1,6 @@
 cask 'sonos' do
-  version '10.3'
-  sha256 '8626e9a71ffa49719f18fec28171b52604d41342232bd47b96d783c714718265'
+  version '10.4'
+  sha256 '139c3c978dce9dc43648847bbce44c9781ba939a6c28fc39bca99e97a0c3da81'
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac',
