9e6bb88e247cd4ce4583923b956fd7c87df40542
Markus Reiter <me@reitermark.us>
2019-10-02 01:01:36 +0200

    Fix URL for `konica-minolta-bizhub-c220-c280-c360-driver`. (#1218)

--- Casks/konica-minolta-bizhub-c220-c280-c360-driver.rb
+++ Casks/konica-minolta-bizhub-c220-c280-c360-driver.rb
@@ -1,9 +1,8 @@
 cask 'konica-minolta-bizhub-c220-c280-c360-driver' do
-  version '3.11.0,201606.27043313'
+  version '3.11.0,9417996e51b61744d50204a9201cfa60,40940,1558521685'
   sha256 '236d1fd8acf2888ac48668eebf02aa8de567d0f481142e2ac9d4d7b2a9100587'
 
-  # konicaminolta.com was verified as official when first introduced to the cask
-  url "https://o.cses.konicaminolta.com/file/Default.aspx?FilePath=DL/#{version.after_comma.major}/#{version.after_comma.minor}/BHC360PSMacOS109_#{version.before_comma.no_dots}MU.dmg"
+  url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=#{version.after_comma.before_comma}&tx_kmanacondaimport_downloadproxy[documentId]=#{version.after_comma.after_comma.before_comma}&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=#{version.after_comma.after_comma.after_comma}"
   name 'Konica Minolta Bizhub C220/C280/C360 PostScript Printer Driver'
   homepage 'https://www.konicaminolta.eu/en/business-solutions/support/download-center.html'
 
