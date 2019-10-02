a99e7c72af8aea6fdcb14dc23346f655ab8864b1
brigitte zmoelnig <brigitte@corecode.io>
2019-10-02 16:32:32 +0200

    Update synology-drive from 2.0.0-11050 to 2.0.1-11061

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -1,6 +1,6 @@
 cask 'synology-drive' do
-  version '2.0.0-11050'
-  sha256 '6b4228ce50b0151bb636653b42f02198ecab471de75defa19925d90abe9063c0'
+  version '2.0.1-11061'
+  sha256 '99d1dd5d6fd36c18e75fce0b038d3d2c3c5ad2132f95ca835450d052ab841634'
 
   url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version}/Mac/Installer/synology-drive-client-#{version.sub(%r{.*-}, '')}.dmg"
   appcast 'https://archive.synology.com/download/Tools/SynologyDriveClient/'
