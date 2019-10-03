9e2899f5252a339fffc84afc314943a6bb8f7942
suschizu <brigitte@corecode.io>
2019-10-03 16:25:45 +0200

    Update synology-drive from 2.0.0-11050 to 2.0.1-11061 (#1224)
    * Update synology-drive from 2.0.0-11050 to 2.0.1-11061
    
    * Update synology-drive.rb
    
    * Update synology-drive.rb

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -1,6 +1,6 @@
 cask 'synology-drive' do
-  version '2.0.0-11050'
-  sha256 '6b4228ce50b0151bb636653b42f02198ecab471de75defa19925d90abe9063c0'
+  version '2.0.1-11061'
+  sha256 '99d1dd5d6fd36c18e75fce0b038d3d2c3c5ad2132f95ca835450d052ab841634'
 
   url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version}/Mac/Installer/synology-drive-client-#{version.sub(%r{.*-}, '')}.dmg"
   appcast 'https://archive.synology.com/download/Tools/SynologyDriveClient/'
@@ -11,7 +11,10 @@ cask 'synology-drive' do
 
   pkg 'Install Synology Drive Client.pkg'
 
-  uninstall quit:      'io.com.synology.CloudStationUI',
+  uninstall quit:      [
+                         'io.com.synology.CloudStationUI',
+                         'com.synology.CloudStationUI',
+                       ],
             pkgutil:   'com.synology.CloudStation',
             launchctl: 'com.synology.Synology Cloud Station'
 end
