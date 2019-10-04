75792a5e323ea0d001a6aec0c4af5afcb0d50fca
Yohei Murayama <muracchi@gmail.com>
2019-10-04 09:19:24 +0900

    Update silicon-labs-vcp-driver from 5.2.1 to 5.2.2

--- Casks/silicon-labs-vcp-driver.rb
+++ Casks/silicon-labs-vcp-driver.rb
@@ -1,6 +1,6 @@
 cask 'silicon-labs-vcp-driver' do
-  version '5.2.1'
-  sha256 '53f73af0866acd61a8b09d74db497694a3059458bd8be67c4546c2b0ea169681'
+  version '5.2.2'
+  sha256 '281409987896c78e053eb6f0e10644f718f14cc0e2fa628678519f9144abf476'
 
   url 'https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver.zip'
   appcast 'https://www.silabs.com/documents/public/release-notes/Mac_OSX_VCP_Driver_Release_Notes.txt'
