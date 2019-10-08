f20de4ac678baccb251a657c04ea6174411580fe
Vítor Galvão <info@vitorgalvao.com>
2019-10-08 22:54:56 +0100

    Update canon-ufrii-lt-printer-driver.rb

--- Casks/canon-ufrii-lt-printer-driver.rb
+++ Casks/canon-ufrii-lt-printer-driver.rb
@@ -1,9 +1,9 @@
 cask 'canon-ufrii-lt-printer-driver' do
-  version '1.5.4.00'
+  version '1.5.4'
   sha256 '1d6c19b2234d4c42c731c4356fe89cc269929d5c0e9f97b06fdcc1454c5ba4c8'
 
   # gdlp01.c-wss.com/gds/1/0100010451/03 was verified as official when first introduced to the cask
-  url 'http://gdlp01.c-wss.com/gds/1/0100010451/03/mac-UFRIILT-CARPS2LBP-v154-00.dmg'
+  url "http://gdlp01.c-wss.com/gds/1/0100010451/03/mac-UFRIILT-CARPS2LBP-v#{version.no_dots}-00.dmg"
   appcast 'https://my.canon/en/support/0101045101/3'
   name 'Canon UFRII LT Printer Driver'
   homepage 'https://my.canon/en/support/0101045101/3'
@@ -12,9 +12,7 @@ cask 'canon-ufrii-lt-printer-driver' do
 
   pkg 'UFRII_LT_CARPS2_Installer.pkg'
 
-  uninstall launchctl: [
-                         'jp.co.canon.CUPSSFPPrinter.BackGrounder',
-                       ],
+  uninstall launchctl: 'jp.co.canon.CUPSSFPPrinter.BackGrounder'
             pkgutil:   [
                          'jp.co.canon.CUPSSFPPrinter.cnaccm.CommonDAT.pkg',
                          'jp.co.canon.CUPSSFPPrinter.profiles.CommonICC.pkg',
