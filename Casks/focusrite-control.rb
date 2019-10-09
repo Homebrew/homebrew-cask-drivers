e2405cc032d307881e4e3c294fe22236c98bfa1f
Work <brigitte@corecode.io>
2019-10-09 15:52:43 +0200

    Update focusrite-control from 3.4.0 to 3.4.4

--- Casks/focusrite-control.rb
+++ Casks/focusrite-control.rb
@@ -1,6 +1,6 @@
 cask 'focusrite-control' do
-  version '3.4.0'
-  sha256 '4b9dc150e594c3fcd593b9a8da677f5aa72f89ae6d1eeb0fc32c4ce3e1f3b38a'
+  version '3.4.4'
+  sha256 'a6004585646b6c2a9c43b87be2690fcfa46b71783d46308572c7ace67f43023e'
 
   url "https://customer.focusrite.com/sites/customer/files/downloads/Focusrite%20Control%20-%20#{version}.dmg"
   appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software'
