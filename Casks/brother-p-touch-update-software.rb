7fd6f079ff4b28804e1cd9c48ab6dfd67705110b
suschizu <brigitte@corecode.io>
2019-10-10 15:22:11 +0200

    Update brother-p-touch-update-software.rb

--- Casks/brother-p-touch-update-software.rb
+++ Casks/brother-p-touch-update-software.rb
@@ -1,8 +1,9 @@
 cask 'brother-p-touch-update-software' do
-  version '1.5.0'
-  sha256 '37ddaf7bec64b1d51608aff15d8bc6b72cf9bcfc205cb7f90e55cba8a2b5ac0f'
+  version '1.5.1,14:dlfp100785'
+  sha256 'bda125817c4713757568bb66d3bf741d1d82214fed4099bf9eec4f1f1a1c7226'
 
-  url "https://download.brother.com/welcome/dlfp100565/pum#{version.no_dots}x13all.dmg"
+  url "https://download.brother.com/welcome/#{version.after_colon}/pum#{version.before_comma.no_dots}ax#{version.after_comma.before_colon}all.dmg
+"
   name 'Brother P-touch Update Software'
   homepage 'https://support.brother.com/g/b/downloadhowto.aspx?c=us&lang=en&prod=lpql720nweus&os=10017&dlid=dlfp100352_000&flang=178&type3=10183'
 
