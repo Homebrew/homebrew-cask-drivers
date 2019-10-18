17d7ed8f03d29ed8f4ded209d666c92107cf138a
CoreCode <core-code@users.noreply.github.com>
2019-10-18 13:49:36 +0200

    Update logitech-unifying (#1261)
    * Update logitech-unifying
    
    * Update logitech-unifying.rb

--- Casks/logitech-unifying.rb
+++ Casks/logitech-unifying.rb
@@ -4,11 +4,11 @@ cask 'logitech-unifying' do
     sha256 'e6fd9c1b536033f3346b32c391bd58587ea9f549cab7839cf8a1dbc62a739825'
   else
     version '1.3.375'
-    sha256 '915116757a33d3d9c5be706d12718824b4082b3e495f2a77a3ffc70db5956666'
+    sha256 '82fe3df612e775ec8789fa821f4f62d4b3a55278276d03474580fee668ee50b7'
   end
 
   url "https://www.logitech.com/pub/controldevices/unifying/unifying#{version}_mac.zip"
-  appcast 'https://support.logitech.com/en_us/software/unifying'
+  appcast 'https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,websoftware=ec9eb8f1-8e0b-11e9-a62b-5b664cf4d3da'
   name 'Logitech Unifying Software'
   homepage 'https://www.logitech.com/en-us/promotions/6072'
 
