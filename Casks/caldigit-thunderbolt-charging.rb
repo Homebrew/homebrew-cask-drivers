e0d39ee5666d5fce323900b0317401e1e67dbaf6
Karl Q <kquinsland@users.noreply.github.com>
2019-11-07 13:03:10 -0800

    Update caldigit-thunderbolt-charging.rb
    Taken feedback from CI:
    
    - `audit for caldigit-thunderbolt-charging: failed; you should use sha256 :no_check when version is :latest`
    
    - `Error: Some packages are still installed, add them to uninstall pkgutil: com.CalDigit.ThunderboltStation3ChargingSupport.pkg`

--- Casks/caldigit-thunderbolt-charging.rb
+++ Casks/caldigit-thunderbolt-charging.rb
@@ -1,6 +1,6 @@
 cask 'caldigit-thunderbolt-charging' do
   version :latest
-  sha256 '81a8581e265754649f362aadd880d276ad5dd0233695e49121d9900a85d31d4c'
+  sha256 :no_check
 
   url 'https://downloads.caldigit.com/CalDigit-Thunderbolt-Station-Mac-Drivers.zip'
   name 'CalDigit Thunderbolt Station USB Charging & SuperDrive Support Driver'
@@ -12,7 +12,7 @@ cask 'caldigit-thunderbolt-charging' do
                        'com.CalDigit.ThunderboltStationChargingSupport',
                        'com.CalDigit.driver.HDPro',
                      ],
-            pkgutil: 'com.CalDigit.CalDigitThunderboltStationChargingSupport.caldigitThunderboltStationsChargingSupport.CalDigitThunderboltStationChargingSupport.pkg',
+            pkgutil: 'com.CalDigit.CalDigitThunderboltStationChargingSupport.caldigitThunderboltStationsChargingSupport.CalDigitThunderboltStationChargingSupport.pkg', 'com.CalDigit.ThunderboltStation3ChargingSupport.pkg'
             delete:  [
                        '/Library/Extensions/CalDigitHDProDrv.kext',
                        '/Library/Extensions/CalDigitThunderboltStationChargingSupport.kext',
