ec98f87b6cd00382e3c18ef54503efbb60cd6d8d
Karl Q <kquinsland@users.noreply.github.com>
2019-11-07 12:53:11 -0800

    Update caldigit-thunderbolt-charging.rb
    Caldigit has changed the `pkg` name in their new release which covers osx Catalina.
    
    I have fixed the `pkg` name and added the sha256 hash:
    
    ```
    shasum -a 256 CalDigit-Thunderbolt-Station-Mac-Drivers.zip
    81a8581e265754649f362aadd880d276ad5dd0233695e49121d9900a85d31d4c  CalDigit-Thunderbolt-Station-Mac-Drivers.zip
    ```

--- Casks/caldigit-thunderbolt-charging.rb
+++ Casks/caldigit-thunderbolt-charging.rb
@@ -1,12 +1,12 @@
 cask 'caldigit-thunderbolt-charging' do
   version :latest
-  sha256 :no_check
+  sha256 '81a8581e265754649f362aadd880d276ad5dd0233695e49121d9900a85d31d4c'
 
   url 'https://downloads.caldigit.com/CalDigit-Thunderbolt-Station-Mac-Drivers.zip'
   name 'CalDigit Thunderbolt Station USB Charging & SuperDrive Support Driver'
   homepage 'https://www.caldigit.com/'
 
-  pkg 'CalDigit Thunderbolt Station Charging Support.pkg'
+  pkg 'CalDigit-Thunderbolt-Station-Mac-Drivers.pkg'
 
   uninstall kext:    [
                        'com.CalDigit.ThunderboltStationChargingSupport',
