0c516ac98c87b9cb2df57beb592b532941de780b
Yishen Miao <mys721tx@gmail.com>
2019-11-12 12:06:32 -0800

    Updating Focusrite Control to 3.4.6.1695 (#1303)
    Updates Focusrite Control to 3.4.6.1695. The download link includes a
    patch number.

--- Casks/focusrite-control.rb
+++ Casks/focusrite-control.rb
@@ -1,9 +1,10 @@
 cask 'focusrite-control' do
-  version '3.4.4'
-  sha256 'a6004585646b6c2a9c43b87be2690fcfa46b71783d46308572c7ace67f43023e'
+  version '3.4.6.1695'
+  sha256 'c9466eef24db21ca64c7b4cdd76ffbe9ecd6cb68c878b23832efc01ea36be614'
 
   url "https://customer.focusrite.com/sites/customer/files/downloads/Focusrite%20Control%20-%20#{version}.dmg"
-  appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software'
+  appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software',
+          configuration: version.major_minor_patch
   name 'Focusrite Control'
   homepage 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software'
 
