9bbffcffc2de84caa1856554fc12bcd732f53851
Vítor Galvão <info@vitorgalvao.com>
2019-11-12 18:26:52 +0000

    Update focusrite-clarett-thunderbolt.rb

--- Casks/focusrite-clarett-thunderbolt.rb
+++ Casks/focusrite-clarett-thunderbolt.rb
@@ -3,7 +3,8 @@ cask 'focusrite-clarett-thunderbolt' do
   sha256 '2a56dff7bc57a430084575fde1f28a4981b16525950a6b30130eca154f9dcfe7'
 
   url "https://customer.focusrite.com/sites/customer/files/downloads/ClarettThunderboltDriver-#{version.major_minor_patch}.dmg"
-  appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=545&download_type=software'
+  appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=545&download_type=software',
+          configuration: version.major_minor_patch
   name 'Focusrite Clarett Thunderbolt'
   homepage 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=545'
 
