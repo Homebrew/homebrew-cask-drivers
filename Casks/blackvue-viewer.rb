a1bd0c6a8eb566987f34398cf4bc7cd3bc9dbbdf
Work <brigitte@corecode.io>
2019-11-13 16:08:28 +0100

    Update blackvue-viewer from 1.35,74331 to 1.36

--- Casks/blackvue-viewer.rb
+++ Casks/blackvue-viewer.rb
@@ -1,8 +1,8 @@
 cask 'blackvue-viewer' do
-  version '1.35,74331'
-  sha256 '64b069f7d4ad2e3c70d5f721b4ddfd5f7902b458f352786c82f751056ac370a6'
+  version '1.36'
+  sha256 '66fd0f0929fbb3719b59783e445690a9c8ea60ae950aa68e48c249fa6feb1da0'
 
-  url "https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=#{version.after_comma}"
+  url 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=74331'
   appcast 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/'
   name 'BlackVue Viewer'
   homepage 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/'
