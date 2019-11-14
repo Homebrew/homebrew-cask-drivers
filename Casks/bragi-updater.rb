361fe00f1d19a433416122c6e7b6cdf13f08f692
Viktor Szakats <commit@vsz.me>
2019-11-14 15:02:13 +0000

    secure URLs

--- Casks/bragi-updater.rb
+++ Casks/bragi-updater.rb
@@ -2,10 +2,10 @@ cask 'bragi-updater' do
   version '1.1.4'
   sha256 'ab3e12163bf4d73c641c1b249bc7a290d8eeb2034465d243b1e194f4e2f76f94'
 
-  url "http://update.bragi.com/bin/Bragi%20Updater-#{version}.dmg"
-  appcast 'http://update.bragi.com/scripts/scripts.36513c90.js'
+  url "https://update.bragi.com/bin/Bragi%20Updater-#{version}.dmg"
+  appcast 'https://update.bragi.com/scripts/scripts.36513c90.js'
   name 'Bragi Updater'
-  homepage 'http://update.bragi.com/'
+  homepage 'https://update.bragi.com/'
 
   app 'Bragi Updater.app'
 
