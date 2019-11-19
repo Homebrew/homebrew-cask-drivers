c1c6c2030aa0988bbd08ec2b4d43fcb951e4de25
Matt Coster <matthew@thecosters.co.uk>
2019-11-19 01:10:44 +0000

    Update corsair-icue from 3.20.80 to 3.22.74 (#1316)
    * Update corsair-icue from 3.20.80 to 3.22.74
    
    * iCUEUninstaller.app moved out of iCUE.app
    
    * Add missing uninstall entries from CI error report
    
    * Update corsair-icue.rb

--- Casks/corsair-icue.rb
+++ Casks/corsair-icue.rb
@@ -1,6 +1,6 @@
 cask 'corsair-icue' do
-  version '3.20.80'
-  sha256 '769d81554bbffdaefef7df677035d06bc8da8c15e16661e24c83580a99d03454'
+  version '3.22.74'
+  sha256 '27bbf2c00dbc51e2ce4c6860563def491fc790eff17c0b8934a8b16db476fd00'
 
   url "https://downloads.corsair.com/Files/CUE/iCUE-#{version}-release.dmg"
   appcast 'https://forum.corsair.com/v3/showthread.php?t=182942&page=999999999'
@@ -11,19 +11,22 @@ cask 'corsair-icue' do
 
   pkg 'iCUE.pkg'
 
-  uninstall launchctl: 'com.corsair.cue.3.launchHelper',
+  uninstall launchctl: [
+                         'com.corsair.AudioConfigService.System',
+                         'com.corsair.cue.3.launchHelper',
+                       ],
             quit:      [
-                         'com.corsair.cue.3.*',
+                         'com.corsair.cue.*',
                          'org.qt-project.*',
                        ],
             script:    [
                          executable: '/usr/bin/osascript',
-                         args:       ["#{appdir}/Corsair/iCUE.app/Contents/Helpers/iCUEUninstaller.app/Contents/Scripts/uninstall.scpt"],
+                         args:       ["#{appdir}/Corsair/iCUEUninstaller.app/Contents/Scripts/uninstall.scpt"],
                          sudo:       true,
                        ],
             pkgutil:   [
                          'com.corsair.CorsairAudio',
-                         'com.corsair.cue.3',
+                         'com.corsair.cue.*',
                        ],
             delete:    [
                          '/Library/Audio/Plug-Ins/HAL/CorsairAudio.plugin',
