4dc76eecdd121d2a46c2d8bd9bdbfc1b1a52135d
inailuig <inailuig@users.noreply.github.com>
2019-12-05 04:35:11 +0100

    update asix-ax88179 to 2.15.0 for macOS >= Catalina (#1329)
    * update asix-ax88179 to 2.15.0 for macOS >= Catalina
    
    * asix-ax88179 macos version dependence reverse conditionals
    
    * Update asix-ax88179.rb

--- Casks/asix-ax88179.rb
+++ Casks/asix-ax88179.rb
@@ -1,12 +1,22 @@
 cask 'asix-ax88179' do
-  version '2.14.0'
-  sha256 '4330ccf68f8879e7a2f6ada12a5bcdb192900e69e11b0d1e0558c3fbd331d674'
+  if MacOS.version <= :mojave
+    version '2.14.0'
+    sha256 '4330ccf68f8879e7a2f6ada12a5bcdb192900e69e11b0d1e0558c3fbd331d674'
 
-  url "https://www.asix.com.tw/FrootAttach/driver/AX88179_178A_macintosh_Driver_Installer_v#{version}.zip"
+    url "https://www.asix.com.tw/FrootAttach/driver/AX88179_178A_macintosh_Driver_Installer_v#{version}.zip"
+
+    container nested: "AX88179_178A_macintosh_Driver_Installer_v#{version}/AX88179_178A.dmg"
+  else
+    version '2.15.0'
+    sha256 '17ae7031bd8e9ffb5cfea9ee17dcd4e32b38297a6241a5c9c2ac0589fe5dfcca'
+
+    url "https://www.asix.com.tw/FrootAttach/driver/AX88179_178A_macOS_10.15_above_Driver_Installer_v#{version}.zip"
+
+    container nested: "AX88179_178A_macOS_10.15_above_Driver_Installer_v#{version}/AX88179_178A.dmg"
+  end
+
   name 'AX88179'
   homepage 'https://www.asix.com.tw/download.php?sub=driverdetail&PItemID=131'
-
-  container nested: "AX88179_178A_macintosh_Driver_Installer_v#{version}/AX88179_178A.dmg"
 
   pkg "AX88179_178A_v#{version}.pkg"
 
