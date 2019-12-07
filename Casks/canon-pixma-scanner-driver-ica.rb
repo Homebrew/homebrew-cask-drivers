c432128433499cd17dba93663364df7f862568c0
Vítor Galvão <info@vitorgalvao.com>
2019-12-07 22:10:19 +0000

    Revert premature new version method changes

--- Casks/canon-pixma-scanner-driver-ica.rb
+++ Casks/canon-pixma-scanner-driver-ica.rb
@@ -1,9 +1,9 @@
 cask 'canon-pixma-scanner-driver-ica' do
-  version '4.3.4'
+  version '4.3.4a'
   sha256 '3433cd6dbdc3e4bb84c4c6bacadc6bb2c8630fc83e3fc49ce1dbd84c91b5765c'
 
   # gdlp01.c-wss.com/gds was verified as official when first introduced to the cask
-  url "https://gdlp01.c-wss.com/gds/5/0100007645/04/misd-mac-ijscanner16f-#{version.dots_to_underscores}-ea21_3.dmg"
+  url "https://gdlp01.c-wss.com/gds/5/0100007645/04/misd-mac-ijscanner16f-#{version.dots_to_underscores.delete('^0-9_')}-ea21_3.dmg"
   name 'Canon PIXMA ICA Scanner Driver'
   homepage 'https://www.usa.canon.com/internet/portal/us/home/support/details/printers/support-inkjet-printer/mg-series/pixma-mg6220'
 
