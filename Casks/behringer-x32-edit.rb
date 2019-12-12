56d1828e73d5b80fdfaeb2543f7403ce4fba8f87
Vítor Galvão <info@vitorgalvao.com>
2019-12-12 19:36:07 +0000

    behringer-x32-edit.rb: update links (#1355)

--- Casks/behringer-x32-edit.rb
+++ Casks/behringer-x32-edit.rb
@@ -3,11 +3,11 @@ cask 'behringer-x32-edit' do
   version '4.1'
   sha256 '439f1d3d789958f4e44b53116d247363adf56aa62941dfadc0b0a6b4e458203d'
 
-  # downloads.music-group.com was verified as official when first introduced to the cask
-  url "https://downloads.music-group.com/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
+  # downloads.musictribe.com was verified as official when first introduced to the cask
+  url "https://downloads.musictribe.com/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
   appcast 'https://www.behringer.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/Downloads'
   name 'Behringer X32-Edit'
-  homepage 'https://www.musictribe.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF'
+  homepage 'https://www.behringer.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/'
 
   app 'X32-Edit.app'
 
@@ -18,8 +18,4 @@ cask 'behringer-x32-edit' do
                '~/Library/Application Support/CrashReporter/X32-Edit_*.plist',
                '~/Library/Saved Application State/com.music-group.X32-Edit.savedState',
              ]
-
-  caveats do
-    license 'https://www.musictribe.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/downloads?active=Downloads'
-  end
 end
