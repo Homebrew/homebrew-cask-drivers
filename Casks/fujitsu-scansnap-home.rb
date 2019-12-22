27b059418e5e3e740b98b4d5ec3bbd77d00eff7a
suschizu <brigitte@corecode.io>
2019-12-22 19:09:14 +0100

    Update fujitsu-scansnap-home.rb

--- Casks/fujitsu-scansnap-home.rb
+++ Casks/fujitsu-scansnap-home.rb
@@ -3,7 +3,7 @@ cask 'fujitsu-scansnap-home' do
   sha256 '46d1ef801f233725111994b5d7b904003bb28932c49bdcb90a9eca200d29951c'
 
   # origin.pfultd.com was verified as official when first introduced to the cask
-  url "https://origin.pfultd.com/downloads/ss/sshinst/m-#{version.no_dots}/MacSSHDownloadInstaller_#{version.dots_to_underscores}.dmg"
+  url "https://origin.pfultd.com/downloads/ss/sshinst/m-#{version.no_dots}/MacSSHOfflineInstaller_#{version.dots_to_underscores}.dmg"
   name 'ScanSnap Home'
   homepage 'https://www.fujitsu.com/global/products/computing/peripheral/scanners/scansnap/software/sshome/index.html'
 
