a823673953cac5264f0dde8a57d2b1bb3e209e1d
Sergey Parhomenko <sparhomenko@users.noreply.github.com>
2019-12-25 01:45:26 +0100

    Update logitech-unifying (#1382)

--- Casks/logitech-unifying.rb
+++ Casks/logitech-unifying.rb
@@ -17,6 +17,7 @@ cask 'logitech-unifying' do
                       :el_capitan,
                       :sierra,
                       :high_sierra,
+                      :catalina,
                     ]
 
   pkg 'Unifying Installer.app/Contents/Resources/Logitech Unifying Signed.mpkg'
