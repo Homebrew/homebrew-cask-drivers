21420f5ca3eb4d81789a6c65796079786f4e9127
suschizu <brigitte@corecode.io>
2019-12-27 19:41:30 +0100

    Update segger-systemview from 2.52d to 3.10 (#1376)

--- Casks/segger-systemview.rb
+++ Casks/segger-systemview.rb
@@ -1,14 +1,14 @@
 cask 'segger-systemview' do
-  version '2.52d'
-  sha256 '652dcde70b4d6699d4a82aff9bcc1960e7053b18346299a150b835127549da02'
+  version '3.10'
+  sha256 '6673e97b79f4a8adc717c8b967b53b8fd618c03b875d266368fcdc80e07e8d5e'
 
-  url "https://www.segger.com/downloads/jlink/SystemView_MacOSX_V#{version.no_dots}.pkg"
+  url 'https://www.segger.com/downloads/systemview/systemview_mac_pkg'
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.segger.com/downloads/jlink/systemview_mac_pkg',
           configuration: version.no_dots
   name 'SystemView'
   homepage 'https://www.segger.com/products/development-tools/systemview'
 
-  pkg "SystemView_MacOSX_V#{version.no_dots}.pkg"
+  pkg "SystemView_V#{version.no_dots}_MacOSX.pkg"
 
   uninstall pkgutil: 'com.segger.pkg.SystemView'
 end
