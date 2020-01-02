37f51e722f382f0ff53394296004c4627af120ef
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-01 18:37:27 -0800

    Update logitech-control-center from 3.9.10 to 3.9.11 (#1389)
    * Update logitech-control-center from 3.9.10 to 3.9.11
    
    * Update logitech-control-center.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/logitech-control-center.rb
+++ Casks/logitech-control-center.rb
@@ -1,8 +1,9 @@
 cask 'logitech-control-center' do
-  version '3.9.10'
-  sha256 'f2ee5d6a01009acc5b7da310695e1b9c0a0702814adc4e46e09023598e25721e'
+  version '3.9.11'
+  sha256 '18de9f8ae8461df57f6cc9e9f1f443674891ca94d71cb7d7dc0f4c94c32fb2c9'
 
   url "https://www.logitech.com/pub/techsupport/mouse/mac/lcc#{version}.zip"
+  appcast 'https://support.logi.com/api/v2/help_center/en-us/articles.json'
   name 'Logitech Control Center'
   homepage 'https://support.logitech.com/en_us/product/3129'
 
