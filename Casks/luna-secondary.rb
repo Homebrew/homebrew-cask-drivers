cc5171502ffce3ee50fb96c6c2f9052ea35e2287
Vítor Galvão <info@vitorgalvao.com>
2020-01-02 22:10:59 +0000

    Update luna-secondary.rb

--- Casks/luna-secondary.rb
+++ Casks/luna-secondary.rb
@@ -2,7 +2,7 @@ cask 'luna-secondary' do
   version '4.0.3'
   sha256 '126cd64265c58c08a2097bd5ccd58376e84f943986023ad21fa17bf7a105fdce'
 
-  # s3.amazonaws.com/s3.lunadisplay was verified as official when first introduced to the cask
+  # s3.amazonaws.com/s3.lunadisplay.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaSecondary-#{version}.dmg"
   appcast 'https://s3.lunadisplay.com/downloads/luna-secondary-sparkle.xml'
   name 'Luna Secondary'
