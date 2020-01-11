dcbe40af33677f9babf4d79c99635c117f7e9c42
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-01-11 17:28:52 +0100

    Add BLOCKS Code v0.2.1

---
+++ Casks/blocks-code.rb
@@ -0,0 +1,13 @@
+cask 'blocks-code' do
+  version '0.2.1'
+  sha256 'af9c68850f9a5f27e9eeed21e4372c2b8948213104ed9f96de07dab6315d90aa'
+
+  # assets.roli.com was verified as official when first introduced to the cask, ROLI is the company owning JUCE.
+  url "https://assets.roli.com/blocks/BLOCKS+Code/#{version}/BLOCKSCodeInstallerOSX_v#{version}.mpkg"
+  name 'BLOCKS Code'
+  homepage 'https://juce.com/blocks/code'
+
+  pkg "BLOCKSCodeInstallerOSX_v#{version}.mpkg"
+
+  uninstall pkgutil: 'com.roli.blocks.code'
+end
