2e67043c2a4219fd23749a10ab94146750bbaf91
Vítor Galvão <info@vitorgalvao.com>
2020-01-11 20:31:55 +0000

    Update blocks-code.rb

--- Casks/blocks-code.rb
+++ Casks/blocks-code.rb
@@ -2,8 +2,9 @@ cask 'blocks-code' do
   version '0.2.1'
   sha256 'af9c68850f9a5f27e9eeed21e4372c2b8948213104ed9f96de07dab6315d90aa'
 
-  # assets.roli.com was verified as official when first introduced to the cask, ROLI is the company owning JUCE.
+  # assets.roli.com/blocks was verified as official when first introduced to the cask, ROLI is the company owning JUCE.
   url "https://assets.roli.com/blocks/BLOCKS+Code/#{version}/BLOCKSCodeInstallerOSX_v#{version}.mpkg"
+  appcast 'https://juce.com/blocks/code'
   name 'BLOCKS Code'
   homepage 'https://juce.com/blocks/code'
 
