c375a1886a3ee75baa6a9a52cd8d6b2bdb0b9683
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-01-12 15:35:26 +0100

    Add Akai Professional LPD8 Editor v2.0.2

---
+++ Casks/akai-professional-lpd8-editor.rb
@@ -0,0 +1,10 @@
+cask 'akai-professional-lpd8-editor' do
+  version '814'
+  sha256 'a94cd6dcac129f2c49f06b9fc48f6d7e94a2973c58022c4f654aabb0780c4edb'
+
+  url "https://www.akaipro.com/amfile/file/download/file/#{version}/product/40/"
+  name 'Akai Professional LPD8 Editor'
+  homepage 'https://www.akaipro.com/lpd8-lpd8'
+
+  app 'Akai Professional LPD8 Editor.app'
+end
