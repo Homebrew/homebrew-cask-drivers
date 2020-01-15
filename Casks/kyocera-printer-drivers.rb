6515fae33b7ac002658e7182580863373f9789cd
suschizu <brigitte@corecode.io>
2020-01-15 19:27:38 +0100

    Update kyocera-printer-drivers from 5.0,2019.11.21 to 5.0a,2019.11.27 (#1410)
    * Update kyocera-printer-drivers from 5.0,2019.11.21 to 5.0a,2019.11.27
    
    * Update kyocera-printer-drivers.rb

--- Casks/kyocera-printer-drivers.rb
+++ Casks/kyocera-printer-drivers.rb
@@ -1,14 +1,15 @@
 cask 'kyocera-printer-drivers' do
-  version '5.0,2019.11.21'
-  sha256 'a0a9ebbeb577a32c389f20bcd11af2b8094d04fa8acbe8cc73de444b102d0a4e'
+  version '5.0a,2019.11.27'
+  sha256 '022c1e9a710d157b4e68e4c0d5b519ca31280eb47c43296cdae6e92da864b41d'
 
-  url "https://www.kyoceradocumentsolutions.eu/content/download-center/eu/drivers/all/MacPhase#{version.before_comma.no_dots}_#{version.after_comma.dots_to_underscores}_zip.download.zip"
+  # kyostatics.net/dlc/eu/driver/all/ was verified as official when first introduced to the cask
+  url "https://cdn.kyostatics.net/dlc/eu/driver/all/kyocera_os_x_10_6.-downloadcenteritem-Single-File.downloadcenteritem.tmp/MacPhase#{version.before_comma.no_dots}_#{version.after_comma.dots_to_underscores}.zip"
   appcast 'https://dlc.kyoceradocumentsolutions.eu/index/service/dlc.false._.TASKALFA5053CI._.EN.html'
   name 'Kyocera Mac Driver'
   homepage 'https://dlc.kyoceradocumentsolutions.eu/index/service/dlc.false._.TASKALFA5053CI._.EN.html#MAC'
 
   depends_on macos: '>= :mavericks'
-  container nested: "Mac50_#{version.after_comma}-KDC/Kyocera OS X 10.9+ Web build #{version.after_comma}.dmg"
+  container nested: "Mac#{version.before_comma.no_dots}_#{version.after_comma}-KDC/Kyocera OS X 10.9+ Web build #{version.after_comma}.dmg"
 
   pkg "Kyocera OS X 10.9+ Web build #{version.after_comma}.pkg"
 
