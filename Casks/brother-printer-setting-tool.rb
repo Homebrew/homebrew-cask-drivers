f4979a40ecd4cf54b16a34ececad57149206e8ee
Puttin Wong <yzwang.nj@gmail.com>
2020-01-16 09:42:04 +0800

    update brother-printer-setting-tool for simplicity

--- Casks/brother-printer-setting-tool.rb
+++ Casks/brother-printer-setting-tool.rb
@@ -1,13 +1,14 @@
 cask 'brother-printer-setting-tool' do
   if MacOS.version <= :mojave
-    version '1.1.4,13:dlfp100591'
+    version '1.1.4'
     sha256 '67ed22d37f234067e61dca40bb801ff6480fe6be09dbeaaeeddfd2786f824de0'
+    url "https://download.brother.com/welcome/dlfp100591/pstm#{version.no_dots}x13all.dmg"
   else
-    version '1.1.4a,13:dlfp100791'
+    version '1.1.4a'
     sha256 '813feb597f8d750f96bbf9f8fbd49ed40f6c5e49513658d656fc8619c9dd20d3'
+    url "https://download.brother.com/welcome/dlfp100791/pstm#{version.no_dots}x13all.dmg"
   end
 
-  url "https://download.brother.com/welcome/#{version.after_colon}/pstm#{version.before_comma.no_dots}x#{version.after_comma.before_colon}all.dmg"
   name 'Brother Printer Setting Tool'
   homepage 'https://support.brother.com/'
 
