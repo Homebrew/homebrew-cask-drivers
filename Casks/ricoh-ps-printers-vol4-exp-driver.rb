6e308a9696d5d37c8ea132209663b99ce7a928f3
Peter Tripp <petertripp@gmail.com>
2020-01-21 12:56:43 -0500

    ricoh-ps-printers-vol4-exp-driver 3.9.0.0

--- Casks/ricoh-ps-printers-vol4-exp-driver.rb
+++ Casks/ricoh-ps-printers-vol4-exp-driver.rb
@@ -1,8 +1,8 @@
 cask 'ricoh-ps-printers-vol4-exp-driver' do
-  version '3.6.0.0'
-  sha256 '269d997c6efad95453314517034d4d5339091cc33f42b1ee2ace2795b89e76df'
+  version '3.9.0.0'
+  sha256 '8a5219f3915a55de66ee04e489ecf54e9c2f370b16d0083b5b7451f27504f8b9'
 
-  url "http://support.ricoh.com/bb/pub_e/dr_ut_e/0001310/0001310123/V#{version.delete('.')}/Ricoh_PS_Printers_Vol4_EXP_LIO_#{version}.dmg"
+  url "http://support.ricoh.com/bb/pub_e/dr_ut_e/0001316/0001316696/V#{version.delete('.')}/Ricoh_PS_Printers_Vol4_EXP_LIO_#{version}.dmg"
   name 'Ricoh PS printers Vol 4 EXP driver'
   homepage 'http://support.ricoh.com/bb/html/dr_ut_e/rc3/model/mpc3004ex/mpc3004ex.htm'
 
