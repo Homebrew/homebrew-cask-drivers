cd936d39ad736ca2c7e7444ca4e00db2a42a20e6
Randall <ran.dall@icloud.com>
2020-01-22 19:10:32 -0800

    Update uhk-agent from 1.3.0 to 1.3.2

--- Casks/uhk-agent.rb
+++ Casks/uhk-agent.rb
@@ -1,6 +1,6 @@
 cask 'uhk-agent' do
-  version '1.3.0'
-  sha256 '4557c716837f646e30e4777eac4f5e87653b3d537820769d2751f2d28b77187d'
+  version '1.3.2'
+  sha256 'c9ac4865f4812fceb54b15c7a58e2d4460dac8b5e8240f7f54e7576faa27d367'
 
   url "https://github.com/UltimateHackingKeyboard/agent/releases/download/v#{version}/UHK.Agent-#{version}-mac.#{version}.dmg"
   appcast 'https://github.com/UltimateHackingKeyboard/agent/releases.atom'
