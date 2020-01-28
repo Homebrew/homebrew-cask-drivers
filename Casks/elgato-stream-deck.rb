3b21b8a13ac04a655af613c9e45d0fbed9bc030e
suschizu <brigitte@corecode.io>
2020-01-28 15:51:33 +0100

    Update elgato-stream-deck.rb

--- Casks/elgato-stream-deck.rb
+++ Casks/elgato-stream-deck.rb
@@ -3,8 +3,7 @@ cask 'elgato-stream-deck' do
   sha256 '698fc1e5dd44d4d50d382718bbc6e015336ae9c62c1d061cd313266f1d8f4d93'
 
   url "https://edge.elgato.com/egc/macos/sd/Stream_Deck_#{version}.pkg"
-  appcast 'https://help.elgato.com/hc/en-us/articles/360028242631-Elgato-Stream-Deck-Software-Release-Notes',
-          configuration: version.major_minor
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://gc-updates.elgato.com/mac/sd-update/final/download-website.php'
   name 'Elgato Stream Deck'
   homepage 'https://www.elgato.com/en/gaming/stream-deck'
 
