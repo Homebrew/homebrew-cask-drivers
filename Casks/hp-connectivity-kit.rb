e75b928f414259cf340e53108b72ffc3c3c402e0
Mirco Macrelli <mail@mircomacrelli.net>
2020-02-02 08:51:17 +0100

    update hp-connectivity-kit to version 20200116

--- Casks/hp-connectivity-kit.rb
+++ Casks/hp-connectivity-kit.rb
@@ -1,6 +1,6 @@
 cask 'hp-connectivity-kit' do
-  version '20181016'
-  sha256 '0ab7e168e9b3106f04f93f67b6e02e4844743e765b7c26ec1e471e96dae48566'
+  version '20200116'
+  sha256 'af5f9835cd1d559afb8fd33cd59a48f1c8f1819f2b32a137582d29b85d4f4c42'
 
   url "https://ftp.hp.com/pub/calculators/Prime/HP_Prime_Connectivity_Kit_#{version}.dmg"
   name 'HP Connectivity Kit'
