3aaff516f34b7cfd5d0b69c1c1d1d5f6ef4d8488
Markus Reiter <me@reitermark.us>
2020-02-05 22:30:08 +0100

    Fix `depends_on` for `konica-minolta-bizhub-c650i-c360i-c4050i-c4000i-c3320i-driver`. (#1445)

--- Casks/konica-minolta-bizhub-c650i-c360i-c4050i-c4000i-c3320i-driver.rb
+++ Casks/konica-minolta-bizhub-c650i-c360i-c4050i-c4000i-c3320i-driver.rb
@@ -6,7 +6,7 @@ cask 'konica-minolta-bizhub-c650i-c360i-c4050i-c4000i-c3320i-driver' do
   name 'Konica Minolta Bizhub C650i/C360i/C4050i/C4000i/C3320i PostScript Printer Driver'
   homepage 'https://www.konicaminolta.eu/eu-en/support/download-centre'
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: '>= :yosemite'
 
   pkg 'OS_10_10_x/A4/C650i_C360i_C4050i_C4000i_C3320i.pkg'
 
