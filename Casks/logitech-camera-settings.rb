3de35b4b6751dcc45def94509d0b871861b4907e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-08 17:18:27 -0800

    Update logitech-camera-settings from 2.6.2 to 3.0.12 (#1451)

--- Casks/logitech-camera-settings.rb
+++ Casks/logitech-camera-settings.rb
@@ -1,6 +1,6 @@
 cask 'logitech-camera-settings' do
-  version '2.6.2'
-  sha256 '1895fc8abbce644d6abb68fc64435ae9bb5bb722b80e3f47754d934c79878211'
+  version '3.0.12'
+  sha256 'b56b15524436a3d6a62d38d3c738e5798d2fec74a3d0e5040cb664da5880ca10'
 
   # https://support.logi.com was verified as official when first introduced to the cask
   url "https://download01.logi.com/web/ftp/pub/techsupport/cameras/Webcams/LogiCameraSettings_#{version}.pkg"
