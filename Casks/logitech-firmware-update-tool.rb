d11576bcb7b397684e61b6d2f19155c79d5c2f83
Peter Tripp <petertripp@gmail.com>
2020-02-18 11:32:10 -0500

    Remove appcast because it require JS to work.

--- Casks/logitech-firmware-update-tool.rb
+++ Casks/logitech-firmware-update-tool.rb
@@ -3,7 +3,6 @@ cask 'logitech-firmware-update-tool' do
   sha256 '045d1ca198cc54ced6c43d34155325596ca22c0d08a4f49e786f4e775bdc1b08'
 
   url 'https://download01.logi.com/web/ftp/pub/techsupport/keyboards/FirmwareUpdateToolInstaller.zip'
-  appcast 'https://support.logi.com/hc/en-us/articles/360035037273'
   name 'Logitech Firmware Update Tool'
   homepage 'https://support.logi.com/hc/en-us/articles/360035037273'
 
