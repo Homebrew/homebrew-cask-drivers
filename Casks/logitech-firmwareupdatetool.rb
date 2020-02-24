126ace171caf1657a865f11432b4afa066f039c0
Vítor Galvão <info@vitorgalvao.com>
2020-02-24 03:40:16 +0000

    Update logitech-firmwareupdatetool.rb

--- Casks/logitech-firmwareupdatetool.rb
+++ Casks/logitech-firmwareupdatetool.rb
@@ -3,7 +3,7 @@ cask 'logitech-firmwareupdatetool' do
   sha256 '045d1ca198cc54ced6c43d34155325596ca22c0d08a4f49e786f4e775bdc1b08'
 
   url 'https://download01.logi.com/web/ftp/pub/techsupport/keyboards/FirmwareUpdateToolInstaller.zip'
-  appcast 'https://support.logi.com/hc/en-us/articles/360035037273'
+  appcast 'https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,websoftware=07d54a46-c89d-11e9-9981-4556732441db'
   name 'Logitech Firmware Update Tool'
   homepage 'https://support.logi.com/hc/en-us/articles/360035037273'
 
