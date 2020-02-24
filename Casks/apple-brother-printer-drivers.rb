9b707bc13f3fc5d9b75ebf0e85f3c1d798a049f7
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-02-24 03:46:11 +0000

    Update apple-brother-printer-drivers (#1460)
    * Update apple-brother-printer-drivers
    
    * Update apple-brother-printer-drivers.rb
    
    * Update apple-brother-printer-drivers.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/apple-brother-printer-drivers.rb
+++ Casks/apple-brother-printer-drivers.rb
@@ -1,9 +1,10 @@
 cask 'apple-brother-printer-drivers' do
   version '4.1.1'
-  sha256 '4f12496a351ecf2da717239c4c85a311401ddd465a036f4c175cfcc74abf58f6'
+  sha256 '74dd579e43fb5b410d447e399bf33654adaefc01c968f8b61605ff48a41d9c2c'
 
-  url "https://support.apple.com/downloads/DL1927/en_US/brotherprinterdrivers#{version}.dmg"
-  appcast 'https://support.apple.com/downloads/brother'
+  # updates.cdn-apple.com was verified as official when first introduced to the cask
+  url 'https://updates.cdn-apple.com/2019/cert/041-88742-20191011-c679d07b-a355-4dfa-bae5-70d692b3d0b2/BrotherPrinterDrivers.dmg'
+  appcast 'https://support.apple.com/kb/DL1927'
   name 'Brother Printer Drivers'
   homepage 'https://support.apple.com/kb/DL1927'
 
