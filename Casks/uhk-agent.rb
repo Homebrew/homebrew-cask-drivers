914cba1a921a0318f5745e374c3ef71509b6fd82
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-24 15:18:15 -0800

    Update uhk-agent from 1.4.1 to 1.4.2 (#1467)

--- Casks/uhk-agent.rb
+++ Casks/uhk-agent.rb
@@ -1,6 +1,6 @@
 cask 'uhk-agent' do
-  version '1.4.1'
-  sha256 'd195026cd591c5aec58a1871971286d3fc7b071f729d070abad50e57e1c1d90a'
+  version '1.4.2'
+  sha256 'fcc70ffb34845df9f0f231721205aebc86abf67f111e95c47a9a6980633eaebc'
 
   url "https://github.com/UltimateHackingKeyboard/agent/releases/download/v#{version}/UHK.Agent-#{version}-mac.#{version}.dmg"
   appcast 'https://github.com/UltimateHackingKeyboard/agent/releases.atom'
