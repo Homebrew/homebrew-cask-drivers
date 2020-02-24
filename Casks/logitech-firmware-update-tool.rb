d52cad4d68f7e1c01b55f7daa5288f44853c95c6
Vítor Galvão <info@vitorgalvao.com>
2020-02-24 03:26:47 +0000

    Update logitech-firmware-update-tool.rb

--- Casks/logitech-firmware-update-tool.rb
+++ Casks/logitech-firmware-update-tool.rb
@@ -1,5 +1,5 @@
 cask 'logitech-firmwareupdatetool' do
-  version '1.0.69'
+  version '1.2.109'
   sha256 '045d1ca198cc54ced6c43d34155325596ca22c0d08a4f49e786f4e775bdc1b08'
 
   url 'https://download01.logi.com/web/ftp/pub/techsupport/keyboards/FirmwareUpdateToolInstaller.zip'
