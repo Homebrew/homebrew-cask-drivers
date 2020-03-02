1a0d602b5101c8e035265e8519a84f9dba36bc37
Work <brigitte@corecode.io>
2020-03-02 13:26:29 +0100

    Update lg-onscreen-control from 3.69,zBJAUHZ4JjNzO5UHojPoA to 3.76,QMvateEiepmqkHo3sfooXg

--- Casks/lg-onscreen-control.rb
+++ Casks/lg-onscreen-control.rb
@@ -1,6 +1,6 @@
 cask 'lg-onscreen-control' do
-  version '3.69,zBJAUHZ4JjNzO5UHojPoA'
-  sha256 '4ceaf3da93df82c5fa0062016f4a772eadf5c0468e65ea6bee95911e2b8414bd'
+  version '3.76,QMvateEiepmqkHo3sfooXg'
+  sha256 'b194059a03aea4bd3bacb55c319f237a2ddb0f5ab266df1775ada6f74743d065'
 
   # lge.com was verified as official when first introduced to the cask
   url "http://gscs-b2c.lge.com/downloadFile?fileId=#{version.after_comma}"
