1453fd2d99c08b0d00b2bcb73095be0f84c66556
Vítor Galvão <info@vitorgalvao.com>
2020-03-13 03:12:26 +0000

    fujitsu-scansnap-manager.rb: update homepage

--- Casks/fujitsu-scansnap-manager.rb
+++ Casks/fujitsu-scansnap-manager.rb
@@ -4,7 +4,7 @@ cask 'fujitsu-scansnap-manager' do
 
   url "https://www.fujitsu.com/downloads/IMAGE/driver/ss/mgr/m-s1300i/MacS1300iManagerV#{version.no_dots}WW1.dmg"
   name 'ScanSnap Manager for Mac'
-  homepage 'https://www.fujitsu.com/global/support/products/computing/peripheral/scanners/scansnap/software/s1300im-setup.html'
+  homepage 'http://scansnap.fujitsu.com/'
 
   pkg 'ScanSnap Manager.pkg'
 
