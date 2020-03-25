dd732e11e6e94ce20d3fae67a4c18eea44eb3ee9
suschizu <brigitte@corecode.io>
2020-03-25 13:52:58 +0100

    Update nordic-nrf-connect.rb

--- Casks/nordic-nrf-connect.rb
+++ Casks/nordic-nrf-connect.rb
@@ -6,7 +6,7 @@ cask 'nordic-nrf-connect' do
   url "https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases/download/v#{version}/nrfconnect-#{version}.dmg"
   appcast 'https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases.atom'
   name 'nRF Connect'
-  homepage 'https://www.nordicsemi.com/eng/Products/Bluetooth-low-energy/nRF-Connect-for-desktop/'
+  homepage 'https://www.nordicsemi.com/Software-and-tools/Development-Tools/nRF-Connect-for-desktop/'
 
   app 'nRF Connect.app'
 end
