ffb1f62e7c284718abe78f3f641a9e1d69a21deb
John Stader <Johnfstader@gmail.com>
2020-03-26 18:46:46 -0400

    acs-acr39u-smartcard-driver version upgrade
    The driver version was updated to 1.1.8. Additionally the second subfolder of the driver url was changed from 10954 to 11878. I can't ascertain why that change was made or find a trend.

--- Casks/acs-acr39u-smartcard-driver.rb
+++ Casks/acs-acr39u-smartcard-driver.rb
@@ -1,8 +1,8 @@
 cask 'acs-acr39u-smartcard-driver' do
-  version '1.1.6.3'
-  sha256 'b3b8e9a3e4393953d2e1671b5baef6953d531599b5977b8a7d2b314d18b2af4a'
-
-  url "https://www.acs.com.hk/download-driver-unified/10954/ACS-Unified-INST-MacOSX-#{version.no_dots}-P.zip"
+  version '1.1.8'
+  sha256 'f1234d75178d892a133a410355a5a990cf75d2f33eba25d575943d4df632f3a4'
+  
+  url "https://www.acs.com.hk/download-driver-unified/11878/ACS-Unified-INST-MacOSX-#{version.no_dots}-P.zip"
   appcast 'https://www.acs.com.hk/en/driver/302/acr39u-smart-card-reader/'
   name 'ACS Unified Installer'
   homepage 'https://www.acs.com.hk/en/driver/302/acr39u-smart-card-reader/'
