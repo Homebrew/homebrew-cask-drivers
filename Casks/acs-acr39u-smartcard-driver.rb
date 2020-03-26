c48031f0c4ec68c6fe3ed7548dd59c9b5a314413
John Stader <Johnfstader@gmail.com>
2020-03-26 18:59:40 -0400

    Update acs-acr39u-smartcard-driver.rb

--- Casks/acs-acr39u-smartcard-driver.rb
+++ Casks/acs-acr39u-smartcard-driver.rb
@@ -1,7 +1,7 @@
 cask 'acs-acr39u-smartcard-driver' do
   version '1.1.8'
   sha256 '52653d4e06cf22c9d750632b237fa42abbf2ae7ea452102150d72b00e7d0329b'
-  
+
   url "https://www.acs.com.hk/download-driver-unified/11878/ACS-Unified-INST-MacOSX-#{version.no_dots}-P.zip"
   appcast 'https://www.acs.com.hk/en/driver/302/acr39u-smart-card-reader/'
   name 'ACS Unified Installer'
