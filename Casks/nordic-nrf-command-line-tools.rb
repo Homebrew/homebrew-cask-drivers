f70561b04fa95c04b68105798e6340fc855ef3a6
Kirk Klobe <kklobe@gmail.com>
2020-03-27 10:18:42 -0500

    Update nordic-nrf-command-line-tools from 10.5.0 to 10.6.0 (#1511)
    Co-authored-by: Kirk Klobe <kirk@habitaware.com>

--- Casks/nordic-nrf-command-line-tools.rb
+++ Casks/nordic-nrf-command-line-tools.rb
@@ -1,6 +1,6 @@
 cask 'nordic-nrf-command-line-tools' do
-  version '10.5.0'
-  sha256 '5683ad7684cf71868232008e707cf447bf22aea150068117e0b77076ec3e0fd8'
+  version '10.6.0'
+  sha256 'e3dafe8731f8d1b267fd373303bc0178ab88be7a6345b710e3cf2a4326cda682'
 
   url "https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-#{version.major}-x-x/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX.tar"
   name 'nRF Command Line Tools'
