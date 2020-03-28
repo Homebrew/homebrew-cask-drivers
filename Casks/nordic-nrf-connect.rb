aaa5961924a22cd7f8b9a113a0b1e80a725eb40b
suschizu <brigitte@corecode.io>
2020-03-28 22:13:33 +0100

    Update nordic-nrf-connect from 3.3.0 to 3.3.1 (#1503)
    * Update nordic-nrf-connect from 3.3.0 to 3.3.1
    
    * Update nordic-nrf-connect.rb

--- Casks/nordic-nrf-connect.rb
+++ Casks/nordic-nrf-connect.rb
@@ -1,12 +1,12 @@
 cask 'nordic-nrf-connect' do
-  version '3.3.0'
-  sha256 '001f6901d89b7b136d1acf26acbc1e9eddf3b860c3941d9b856b13a50008ce6d'
+  version '3.3.1'
+  sha256 '02c354805e59b2794f9b6c0d2609b572531d1edb7ce2c9cc43b98825326bf560'
 
   # github.com/NordicSemiconductor/pc-nrfconnect-core/ was verified as official when first introduced to the cask
   url "https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases/download/v#{version}/nrfconnect-#{version}.dmg"
   appcast 'https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases.atom'
   name 'nRF Connect'
-  homepage 'https://www.nordicsemi.com/eng/Products/Bluetooth-low-energy/nRF-Connect-for-desktop/'
+  homepage 'https://www.nordicsemi.com/Software-and-tools/Development-Tools/nRF-Connect-for-desktop/'
 
   app 'nRF Connect.app'
 end
