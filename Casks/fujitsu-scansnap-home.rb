37507e5ccd68ac9dd1d2a5b47016546e7334fcff
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2020-04-03 22:04:40 -0400

    Update fujitsu-scansnap-home from 1.6.0 to 1.7.0

--- Casks/fujitsu-scansnap-home.rb
+++ Casks/fujitsu-scansnap-home.rb
@@ -1,6 +1,6 @@
 cask 'fujitsu-scansnap-home' do
-  version '1.6.0'
-  sha256 'c0d4cfb208ceb0e2601f650073cf85883bd7ed3a31a6a225180c087d441a3163'
+  version '1.7.0'
+  sha256 '2ec66c35b17145e8aa2ab13e06c5b56d75ecc13c6e83f1e140878d89d8d362de'
 
   # origin.pfultd.com was verified as official when first introduced to the cask
   url "https://origin.pfultd.com/downloads/ss/sshinst/m-#{version.no_dots}/MacSSHOfflineInstaller_#{version.dots_to_underscores}.dmg"
