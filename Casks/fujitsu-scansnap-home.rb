4b2e016a89a0cce050f912c7830a8acc4321a255
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2020-04-04 08:27:40 -0400

    Revert "Update fujitsu-scansnap-home from 1.6.0 to 1.7.0 (#1498)" (#1520)
    * Revert "Update fujitsu-scansnap-home from 1.6.0 to 1.7.0 (#1498)"
    
    This reverts commit c6fa4fcab030d7f9a7c32966ea541b6b19976edb.
    
    * Update fujitsu-scansnap-home from 1.6.0 to 1.7.0

--- Casks/fujitsu-scansnap-home.rb
+++ Casks/fujitsu-scansnap-home.rb
@@ -1,15 +1,16 @@
 cask 'fujitsu-scansnap-home' do
   version '1.7.0'
-  sha256 '8995fb4c000deaed2cdcf1dce347bdd95a79f275dd3a60695e72771efd417c63'
+  sha256 '2ec66c35b17145e8aa2ab13e06c5b56d75ecc13c6e83f1e140878d89d8d362de'
 
   # origin.pfultd.com was verified as official when first introduced to the cask
-  url "https://origin.pfultd.com/downloads/ss/sshinst/m-#{version.no_dots}/MacSSHDownloadInstaller_#{version.dots_to_underscores}.dmg"
+  url "https://origin.pfultd.com/downloads/ss/sshinst/m-#{version.no_dots}/MacSSHOfflineInstaller_#{version.dots_to_underscores}.dmg"
   name 'ScanSnap Home'
   homepage 'https://www.fujitsu.com/global/products/computing/peripheral/scanners/scansnap/software/sshome/index.html'
 
   depends_on macos: '>= :sierra'
+  container nested: "Download/MacSSHomeInstaller_#{version.dots_to_underscores}.dmg"
 
-  installer manual: 'SSHDownloadInstaller.app'
+  pkg 'ScanSnap Home.pkg'
 
   uninstall launchctl: [
                          'com.fujitsu.pfu.SshRegister',
