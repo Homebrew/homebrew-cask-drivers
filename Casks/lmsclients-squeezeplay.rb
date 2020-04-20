1ed489bb77cfc73843b0bb71e2def2e637d189c0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:25:38 +0100

    Add trailing slash to verified comment

--- Casks/lmsclients-squeezeplay.rb
+++ Casks/lmsclients-squeezeplay.rb
@@ -2,7 +2,7 @@ cask 'lmsclients-squeezeplay' do
   version '7.8.0r1227'
   sha256 '2802555f092a616d1843e5812abb052d4d5ef8cb918657bf3867bd86ff5c654c'
 
-  # downloads.sourceforge.net/lmsclients was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/lmsclients/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/lmsclients/SqueezePlay-x86_64-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/lmsclients/rss?path=/squeezeplay/osx'
   name 'Logitech LMS SqueezePlay Client'
