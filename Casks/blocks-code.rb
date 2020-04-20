8cc26e209f11aea26b9e85fed1be9533cfa2553d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:25:44 +0100

    Add trailing slash to verified comment

--- Casks/blocks-code.rb
+++ Casks/blocks-code.rb
@@ -2,7 +2,7 @@ cask 'blocks-code' do
   version '0.2.3'
   sha256 '9d8d03f8f004bf5c5474e67065128d3c15806ed9594202596dbe1c094276943b'
 
-  # assets.roli.com/blocks was verified as official when first introduced to the cask, ROLI is the company owning JUCE.
+  # assets.roli.com/blocks/ was verified as official when first introduced to the cask
   url "https://assets.roli.com/blocks/BLOCKS+Code/#{version}/BLOCKSCodeInstallerOSX_v#{version}.mpkg"
   appcast 'https://juce.com/blocks/code'
   name 'BLOCKS Code'
