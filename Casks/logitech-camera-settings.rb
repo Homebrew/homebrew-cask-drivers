1a942608ae7b44f77f1d8d3084369db36c89c0b0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:32:09 +0100

    logitech-camera-settings.rb: remove verified comment

--- Casks/logitech-camera-settings.rb
+++ Casks/logitech-camera-settings.rb
@@ -2,7 +2,6 @@ cask 'logitech-camera-settings' do
   version '3.0.12'
   sha256 'b56b15524436a3d6a62d38d3c738e5798d2fec74a3d0e5040cb664da5880ca10'
 
-  # https://support.logi.com/ was verified as official when first introduced to the cask
   url "https://download01.logi.com/web/ftp/pub/techsupport/cameras/Webcams/LogiCameraSettings_#{version}.pkg"
   name 'Logitech Camera Settings'
   homepage 'https://support.logi.com/hc/en-us/articles/360024692954--Downloads-HD-Webcam-C270'
