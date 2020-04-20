94633e9cd1f38fb79fba8597bf9a1d4f855c10a8
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:26:12 +0100

    Add trailing slash to verified comment

--- Casks/zsa-wally.rb
+++ Casks/zsa-wally.rb
@@ -2,7 +2,7 @@ cask 'zsa-wally' do
   version '1.1.3'
   sha256 '3e6327f23743e3163a7bed4130f73830ecaeea400098003354ec8c63e0010095'
 
-  # github.com/zsa/wally was verified as official when first introduced to the cask
+  # github.com/zsa/wally/ was verified as official when first introduced to the cask
   url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
   appcast 'https://github.com/zsa/wally/releases.atom'
   name 'Wally'
