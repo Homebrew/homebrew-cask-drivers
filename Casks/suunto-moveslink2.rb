081d4ed577b7dbadce7de51c4b358ff3f25c1ccc
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:25:02 +0100

    Add trailing slash to verified comment

--- Casks/suunto-moveslink2.rb
+++ Casks/suunto-moveslink2.rb
@@ -3,7 +3,7 @@ cask 'suunto-moveslink2' do
   version '1.4.4'
   sha256 '474ec979b4fb9b6c2319a61162e95ca895eb9949b3f2a72a7365561bcf651a8c'
 
-  # d1c229iib3zm7m.cloudfront.net was verified as official when first introduced to the cask
+  # d1c229iib3zm7m.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1c229iib3zm7m.cloudfront.net/mac/Moveslink2_#{version.dots_to_underscores}.dmg"
   appcast 'https://d1c229iib3zm7m.cloudfront.net/mac/appcast.xml'
   name 'Suunto Moveslink2'
