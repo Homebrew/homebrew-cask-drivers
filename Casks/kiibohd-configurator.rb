826b2e81d722808ab343d8dc3171136ee370decb
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:24:23 +0100

    Add trailing slash to verified comment

--- Casks/kiibohd-configurator.rb
+++ Casks/kiibohd-configurator.rb
@@ -2,7 +2,7 @@ cask 'kiibohd-configurator' do
   version '1.0.2'
   sha256 'b3eba8a4e3c44b68f5a8ab9cf14ea4531f4cda7d06b6ec84ba934950e0e7579a'
 
-  # github.com/kiibohd/configurator was verified as official when first introduced to the cask
+  # github.com/kiibohd/configurator/ was verified as official when first introduced to the cask
   url "https://github.com/kiibohd/configurator/releases/download/v#{version}/kiibohd-configurator-#{version}-mac.dmg"
   appcast 'https://github.com/kiibohd/configurator/releases.atom'
   name 'Kiibohd Configurator'
