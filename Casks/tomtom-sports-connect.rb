334d16a032467288be4ff2302794b743400484b1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:24:40 +0100

    Add trailing slash to verified comment

--- Casks/tomtom-sports-connect.rb
+++ Casks/tomtom-sports-connect.rb
@@ -2,7 +2,7 @@ cask 'tomtom-sports-connect' do
   version :latest
   sha256 :no_check
 
-  # sports.tomtom-static.com was verified as official when first introduced to the cask
+  # sports.tomtom-static.com/ was verified as official when first introduced to the cask
   url 'https://sports.tomtom-static.com/downloads/desktop/mysportsconnect/latest/TomTomSportsConnectInstaller.pkg'
   name 'TomTom MySports Connect'
   homepage 'https://www.tomtom.com/mysports/getstarted'
