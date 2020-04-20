a0424890a832cd10fe27f3ce0db50afde5e9b4b8
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:25:41 +0100

    Add trailing slash to verified comment

--- Casks/qmk-toolbox.rb
+++ Casks/qmk-toolbox.rb
@@ -2,7 +2,7 @@ cask 'qmk-toolbox' do
   version '0.0.17'
   sha256 '645ff73223ddbb35b82b39f4a6c3fb40f20c2bdbda505ce4fb1a780507334f30'
 
-  # github.com/qmk/qmk_toolbox was verified as official when first introduced to the cask
+  # github.com/qmk/qmk_toolbox/ was verified as official when first introduced to the cask
   url "https://github.com/qmk/qmk_toolbox/releases/download/#{version}/QMK.Toolbox.app.zip"
   appcast 'https://github.com/qmk/qmk_toolbox/releases.atom'
   name 'QMK Toolbox'
