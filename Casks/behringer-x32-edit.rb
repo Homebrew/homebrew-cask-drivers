de62499ab0c8725bf8ae167d64c535af5be5bb8e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:25:55 +0100

    Add trailing slash to verified comment

--- Casks/behringer-x32-edit.rb
+++ Casks/behringer-x32-edit.rb
@@ -3,7 +3,7 @@ cask 'behringer-x32-edit' do
   version '4.1'
   sha256 'a4eb8aa7c45a1266f928d3b2ab44ef274e1750e91754fe6eada122ccd0f5f093'
 
-  # downloads.musictribe.com was verified as official when first introduced to the cask
+  # downloads.musictribe.com/ was verified as official when first introduced to the cask
   url "https://downloads.musictribe.com/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
   appcast 'https://www.behringer.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/Downloads'
   name 'Behringer X32-Edit'
