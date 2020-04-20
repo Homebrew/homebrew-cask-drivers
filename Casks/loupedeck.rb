ddadac9b1dc2038603e91fdee701120dd3ba1587
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 01:25:25 +0100

    Add trailing slash to verified comment

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -2,7 +2,7 @@ cask 'loupedeck' do
   version '3.2.1'
   sha256 '3a9d3486c0f8c3a8b8e26a04f4cb1de90228eb31eac882d98d90796a1289387a'
 
-  # loupedeck-software-release.s3.amazonaws.com was verified as official when first introduced to the cask
+  # loupedeck-software-release.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://loupedeck-software-release.s3.amazonaws.com/Loupedeck_Software_v#{version.dots_to_underscores}/Loupedeck_Software_v#{version.dots_to_underscores}.dmg"
   name 'Loupdeck'
   homepage 'https://loupedeck.com/'
