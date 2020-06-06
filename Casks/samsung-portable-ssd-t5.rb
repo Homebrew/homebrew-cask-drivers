3e51e6f7d9002bd53e9a85b1343acd0a71020b92
Kevin Deldycke <kevin@deldycke.com>
2020-06-06 23:30:05 +0200

    All content of the uninstall script has been translated to cask uninstall directive.

--- Casks/samsung-portable-ssd-t5.rb
+++ Casks/samsung-portable-ssd-t5.rb
@@ -11,46 +11,38 @@ cask 'samsung-portable-ssd-t5' do
 
   pkg 'SamsungPortableSSD_Setup_Mac.pkg'
 
-  uninstall early_script: {
-                            executable: '/Applications/SamsungPortableSSD.app/Contents/Resources/CleanupPSSD_Mac.sh',
-                            sudo:       false,
-                          },
-            script:       {
-                            executable: '/Applications/SamsungPortableSSD.app/Contents/Resources/CleanupPSSDAdmin_Mac.sh',
-                            sudo:       true,
-                          },
-            quit:         [
-                            'com.samsung.portablessd.software',
-                            'Samsung.T3-Log-In-Activator-for-Mac-app',
-                            '/Applications/SamsungPortableSSD.app',
-                          ],
-            launchctl:    'com.samsung.portablessd.mon',
-            kext:         'com.samsung.portablessd.driver',
-            pkgutil:      [
-                            'com.samsung.portablessd.driverpkg',
-                            'com.samsung.portablessd.driverXpkg',
-                            'com.samsung.portablessd.softwarepkg',
-                            'com.samsung.portablessd.t3SecurityEnablerForMac',
-                            'com.samsung.portablessd.t3SecurityEnablerForMac.postflight',
-                            'com.samsung.portablessd.samsungPortableSsdDriver.postflight',
-                            'com.samsung.portablessd.samsungPortableSsdDriver.preflight',
-                            'com.samsung.portablessd.samsungPortableSsdSoftware.preflight',
-                          ],
-            delete:       [
-                            # /Library/Extensions/SamsungPortableSSDDriverX.kext
-                            # /Library/Extensions/SamsungPortableSSDDriver.kext
-                            '/Library/Extensions/SamsungPortableSSDDriver*.kext',
-                            # /Library/StagedExtensions/Library/Extensions/SamsungPortableSSDDriverX.kext
-                            # /Library/StagedExtensions/Library/Extensions/SamsungPortableSSDDriver.kext
-                            '/Library/StagedExtensions/Library/Extensions/SamsungPortableSSDDriver*.kext',
-                            # /private/var/db/receipts/com.samsung.portablessd.softwarepkg.plist
-                            # /private/var/db/receipts/com.samsung.portablessd.softwarepkg.bom
-                            # /private/var/db/receipts/com.samsung.portablessd.driverpkg.plist
-                            # /private/var/db/receipts/com.samsung.portablessd.driverXpkg.plist
-                            # /private/var/db/receipts/com.samsung.portablessd.driverpkg.bom
-                            # /private/var/db/receipts/com.samsung.portablessd.driverXpkg.bom
-                            '/private/var/db/receipts/com.samsung.portablessd.*.*',
-                          ]
+  uninstall quit:      [
+                         'com.samsung.portablessd.software',
+                         'Samsung.T3-Log-In-Activator-for-Mac-app',
+                         '/Applications/SamsungPortableSSD.app',
+                       ],
+            launchctl: 'com.samsung.portablessd.mon',
+            kext:      'com.samsung.portablessd.driver',
+            pkgutil:   [
+                         'com.samsung.portablessd.driverpkg',
+                         'com.samsung.portablessd.driverXpkg',
+                         'com.samsung.portablessd.softwarepkg',
+                         'com.samsung.portablessd.t3SecurityEnablerForMac',
+                         'com.samsung.portablessd.t3SecurityEnablerForMac.postflight',
+                         'com.samsung.portablessd.samsungPortableSsdDriver.postflight',
+                         'com.samsung.portablessd.samsungPortableSsdDriver.preflight',
+                         'com.samsung.portablessd.samsungPortableSsdSoftware.preflight',
+                       ],
+            delete:    [
+                         # /Library/Extensions/SamsungPortableSSDDriverX.kext
+                         # /Library/Extensions/SamsungPortableSSDDriver.kext
+                         '/Library/Extensions/SamsungPortableSSDDriver*.kext',
+                         # /Library/StagedExtensions/Library/Extensions/SamsungPortableSSDDriverX.kext
+                         # /Library/StagedExtensions/Library/Extensions/SamsungPortableSSDDriver.kext
+                         '/Library/StagedExtensions/Library/Extensions/SamsungPortableSSDDriver*.kext',
+                         # /private/var/db/receipts/com.samsung.portablessd.softwarepkg.plist
+                         # /private/var/db/receipts/com.samsung.portablessd.softwarepkg.bom
+                         # /private/var/db/receipts/com.samsung.portablessd.driverpkg.plist
+                         # /private/var/db/receipts/com.samsung.portablessd.driverXpkg.plist
+                         # /private/var/db/receipts/com.samsung.portablessd.driverpkg.bom
+                         # /private/var/db/receipts/com.samsung.portablessd.driverXpkg.bom
+                         '/private/var/db/receipts/com.samsung.portablessd.*.*',
+                       ]
 
   zap trash: [
                '~/Desktop/SamsungPortableSSD.app',
