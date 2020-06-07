da84cdd633fcc1b500684451f4b5ce0c16993116
suschizu <brigitte@corecode.io>
2020-06-07 13:04:39 +0200

    Update focusrite-control.rb

--- Casks/focusrite-control.rb
+++ Casks/focusrite-control.rb
@@ -2,7 +2,7 @@ cask 'focusrite-control' do
   version '3.6.0.1822'
   sha256 '17b41f26fcf6d2fa173fee49be58307139d16f8f5399c33d679da907b3b924a6'
 
-  url "https://customer.focusrite.com/sites/customer/files/downloads/Focusrite%20Control%20-%20#{version}.dmg"
+  url "https://fael-downloads-prod.focusrite.com/customer/prod/s3fs-public/downloads/Focusrite%20Control%20-%20#{version}.dmg"
   appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software',
           configuration: version.major_minor_patch
   name 'Focusrite Control'
