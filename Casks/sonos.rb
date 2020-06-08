f52ad9881f51a550230a050269ebe036650cb65a
Max <msal.coding@gmail.com>
2020-06-08 21:07:37 +0200

    fix sonos 12.0 appcast

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -3,7 +3,7 @@ cask 'sonos' do
   sha256 '7dcab807a75f9faef5941f96eff3e823771187e9f6fea3892d2674f192c0a02f'
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac',
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac2',
           configuration: version.no_dots
   name 'Sonos'
   homepage 'https://www.sonos.com/'
