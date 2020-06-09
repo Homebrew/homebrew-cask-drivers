275c4a6c6421794c0d5c726baa3d84abda6dfa59
sal0max <msal.coding@gmail.com>
2020-06-09 15:06:54 +0200

    Update sonos from 11.1 to 12.0 (#1604)
    * Update sonos from 11.1 to 12.0
    
    * fix sonos 12.0 appcast

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,9 +1,9 @@
 cask 'sonos' do
-  version '11.1'
-  sha256 'f93830f89073598eccf92c7c3d9e064330252e8e7152cdcb75094a4890ec13e8'
+  version '12.0'
+  sha256 '7dcab807a75f9faef5941f96eff3e823771187e9f6fea3892d2674f192c0a02f'
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac',
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac2',
           configuration: version.no_dots
   name 'Sonos'
   homepage 'https://www.sonos.com/'
