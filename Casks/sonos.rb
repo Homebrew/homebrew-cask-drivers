d03326576d8b2f33ad084277f8794bfabfedcb70
Randall <ran.dall@icloud.com>
2020-06-08 17:34:14 -0700

    Update sonos from 11.1 to 11.2

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,6 +1,6 @@
 cask 'sonos' do
-  version '11.1'
-  sha256 'f93830f89073598eccf92c7c3d9e064330252e8e7152cdcb75094a4890ec13e8'
+  version '11.2'
+  sha256 '1a98300bd16bb2f727eb1921db80777cc23178a483d56b44a9bb5d95d9ea6602'
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac',
