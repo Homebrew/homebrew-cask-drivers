fd150b3223d2404bb3b0da69f6c105928545d4c6
suschizu <brigitte@corecode.io>
2020-06-10 20:16:05 +0200

    Update loupedeck from 3.2.1 to 3.2.5 (#1607)
    * Update loupedeck from 3.2.1 to 3.2.5
    
    * Update loupedeck.rb

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -1,15 +1,15 @@
 cask 'loupedeck' do
-  version '3.2.1'
-  sha256 '3a9d3486c0f8c3a8b8e26a04f4cb1de90228eb31eac882d98d90796a1289387a'
+  version '3.2.5'
+  sha256 '3c877d67521da04c7356e4a6a178932ecb3b9beb8b2b938e1f7cf0d32658c5b7'
 
   # loupedeck-software-release.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://loupedeck-software-release.s3.amazonaws.com/Loupedeck_Software_v#{version.dots_to_underscores}/Loupedeck_Software_v#{version.dots_to_underscores}.dmg"
+  url "https://loupedeck-software-release.s3.amazonaws.com/Software+Release+#{version}/MacOS/Loupedeck_#{version.dots_to_underscores}.dmg"
   name 'Loupdeck'
   homepage 'https://loupedeck.com/'
 
   depends_on macos: '>= :sierra'
 
-  pkg "Loupedeck Installer #{version}.pkg"
+  pkg "LoupedeckInstaller #{version}.pkg"
 
   uninstall signal:  [
                        ['TERM', 'com.loupedeck.Loupedeck2'],
