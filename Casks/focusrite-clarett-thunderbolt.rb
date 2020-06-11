1db218700dc7e0fd6373afa42c95078ee95d0c17
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:21 +0100

    focusrite-clarett-thunderbolt: change appcast configuratio to must_contain

--- Casks/focusrite-clarett-thunderbolt.rb
+++ Casks/focusrite-clarett-thunderbolt.rb
@@ -4,7 +4,7 @@ cask 'focusrite-clarett-thunderbolt' do
 
   url "https://customer.focusrite.com/sites/customer/files/downloads/ClarettThunderboltDriver-#{version.major_minor_patch}.dmg"
   appcast 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=545&download_type=software',
-          configuration: version.major_minor_patch
+          must_contain: version.major_minor_patch
   name 'Focusrite Clarett Thunderbolt'
   homepage 'https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=545'
 
