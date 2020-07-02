c5b1fc62c5a22fc660e6d8f45ab46a2ec160f7c8
suschizu <brigitte@corecode.io>
2020-07-02 14:13:01 +0200

    Update sound-blaster-play3.rb

--- Casks/sound-blaster-play3.rb
+++ Casks/sound-blaster-play3.rb
@@ -1,9 +1,17 @@
 cask 'sound-blaster-play3' do
   # note: "3" is not a version number, but an intrinsic part of the product name
-  version '2.01.02'
-  sha256 '3dbbdc627f5ee72f87403dc471e6718c258594e0fb0b208d900a5f174ce02915'
+  if MacOS.version <= :mojave
+    version '2.00.03'
+    sha256 '0550328b70fc214fcffc418fba3e5140c853e827bc92227a73864d62ba5e8415'
 
-  url "https://download.creative.com/manualdn/Drivers/100289/MqrOwTo9a7/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg"
+    url "https://files.creative.com/manualdn/Drivers/AVP/13795/0x3F213863/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg.zip"
+
+  else
+    version '2.01.02'
+    sha256 '3dbbdc627f5ee72f87403dc471e6718c258594e0fb0b208d900a5f174ce02915'
+
+    url "https://download.creative.com/manualdn/Drivers/100289/MqrOwTo9a7/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg"
+  end
   name 'Sound Blaster Play! 3 Control Panel'
   homepage 'https://us.creative.com/p/sound-cards/sound-blaster-play-3'
 
