9f6a2f8ffe67bee7272f3f70db4a1ef4e16a2727
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-03 19:51:49 +0200

    Update blackvue-viewer from 3.00 to 3.01

--- Casks/blackvue-viewer.rb
+++ Casks/blackvue-viewer.rb
@@ -1,6 +1,6 @@
 cask 'blackvue-viewer' do
-  version '3.00'
-  sha256 'd0a45307b6726c6ff5809d72d8b707ed838a86004d856ec6d36c4f1cb0a740a4'
+  version '3.01'
+  sha256 '5b021d113c5666c0536853f697a6267210b2b2c0309f88edc3bbd548ce08be8e'
 
   url 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=74331'
   appcast 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/'
