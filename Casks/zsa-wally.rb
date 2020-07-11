2e1a4a2aafcccdd23f83ad30eb970e24796166cd
CoreCode <core-code@users.noreply.github.com>
2020-07-11 21:18:31 +0200

    Update zsa-wally (#1651)

--- Casks/zsa-wally.rb
+++ Casks/zsa-wally.rb
@@ -1,6 +1,6 @@
 cask 'zsa-wally' do
   version '1.1.3'
-  sha256 '3e6327f23743e3163a7bed4130f73830ecaeea400098003354ec8c63e0010095'
+  sha256 '9dc788e16b0dd3336748867c1b5b1c06f2c55010d5b6a037cd5cbf22c67b61ef'
 
   # github.com/zsa/wally/ was verified as official when first introduced to the cask
   url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
