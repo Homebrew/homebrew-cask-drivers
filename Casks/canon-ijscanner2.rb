88f49d3fc778a315e35cde575d370ac04dba804c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:51 +0100

    canon-ijscanner2.rb: fix for new style

--- Casks/canon-ijscanner2.rb
+++ Casks/canon-ijscanner2.rb
@@ -1,13 +1,13 @@
-cask 'canon-ijscanner2' do
-  version '4.1.4a,21_3'
-  sha256 '8aabe1d9d5d48950c84aa1bdd24eb3f7a4fcd463d51889065e963564b0e91848'
+cask "canon-ijscanner2" do
+  version "4.1.4a,21_3"
+  sha256 "8aabe1d9d5d48950c84aa1bdd24eb3f7a4fcd463d51889065e963564b0e91848"
 
   # gdlp01.c-wss.com/gds/ was verified as official when first introduced to the cask
-  url "https://gdlp01.c-wss.com/gds/9/0100006579/03/misd-mac-ijscanner2-#{version.before_comma.dots_to_underscores.delete_suffix('a')}-ea#{version.after_comma}.dmg"
-  name 'Canon Scanner ICA Driver for CanoScan 5600F, 9000F, LiDE 100, LiDE 110, LiDE 200, LiDE 210, LiDE 700F'
-  homepage 'https://www.usa.canon.com/internet/portal/us/home/support/details/scanners/film-negative-scanner/canoscan-5600f?tab=drivers_downloads&subtab=downloads-drivers'
+  url "https://gdlp01.c-wss.com/gds/9/0100006579/03/misd-mac-ijscanner2-#{version.before_comma.dots_to_underscores.delete_suffix("a")}-ea#{version.after_comma}.dmg"
+  name "Canon Scanner ICA Driver for CanoScan 5600F, 9000F, LiDE 100, LiDE 110, LiDE 200, LiDE 210, LiDE 700F"
+  homepage "https://www.usa.canon.com/internet/portal/us/home/support/details/scanners/film-negative-scanner/canoscan-5600f?tab=drivers_downloads&subtab=downloads-drivers"
 
-  pkg "Canon IJScanner2_#{format('%<major>02d%<minor>02d%<patch>02d', major: version.major, minor: version.minor, patch: version.patch.to_i)}.pkg"
+  pkg "Canon IJScanner2_#{format("%<major>02d%<minor>02d%<patch>02d", major: version.major, minor: version.minor, patch: version.patch.to_i)}.pkg"
 
-  uninstall pkgutil: "jp.co.canon.pkg.canonijscanner2.#{format('%<major>02d%<minor>02d%<patch>02d', major: version.major, minor: version.minor, patch: version.patch.to_i)}"
+  uninstall pkgutil: "jp.co.canon.pkg.canonijscanner2.#{format("%<major>02d%<minor>02d%<patch>02d", major: version.major, minor: version.minor, patch: version.patch.to_i)}"
 end
