7286543b2947a3137d583c3e54e62410cb7ee06c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:56 +0100

    qnap-external-raid-manager.rb: fix for new style

--- Casks/qnap-external-raid-manager.rb
+++ Casks/qnap-external-raid-manager.rb
@@ -1,17 +1,17 @@
-cask 'qnap-external-raid-manager' do
-  version '1.2.5.0514'
-  sha256 'e39acc998a1cc1be1cd3b4ecc53a6a85f09543c7f2c9f983a62fb03874299f1c'
+cask "qnap-external-raid-manager" do
+  version "1.2.5.0514"
+  sha256 "e39acc998a1cc1be1cd3b4ecc53a6a85f09543c7f2c9f983a62fb03874299f1c"
 
   url "https://download.qnap.com/Storage/Utility/QNAPExternalRAIDManagerMac-#{version}.dmg"
-  name 'QNAP External Raid Manager'
-  homepage 'https://www.qnap.com/en/utilities/essentials'
+  name "QNAP External Raid Manager"
+  homepage "https://www.qnap.com/en/utilities/essentials"
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: ">= :high_sierra"
 
-  pkg 'QNAP External RAID Manager.pkg'
+  pkg "QNAP External RAID Manager.pkg"
 
-  uninstall quit:    'com.qnap.raidmanager',
-            pkgutil: 'com.qnap.raidmanager',
-            delete:  '/Library/Application Support/QNAP/QNAP External RAID Manager',
-            rmdir:   '/Library/Application Support/QNAP'
+  uninstall quit:    "com.qnap.raidmanager",
+            pkgutil: "com.qnap.raidmanager",
+            delete:  "/Library/Application Support/QNAP/QNAP External RAID Manager",
+            rmdir:   "/Library/Application Support/QNAP"
 end
