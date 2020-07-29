2d66eae5e043d6c2a41add5a92832451874954db
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:09 +0100

    caldigit-docking-utility.rb: fix for new style

--- Casks/caldigit-docking-utility.rb
+++ Casks/caldigit-docking-utility.rb
@@ -1,13 +1,13 @@
-cask 'caldigit-docking-utility' do
-  version '1.9.13'
-  sha256 'f68dc8443156a5b42543afc622b2c8ba80894ee224f332b2d1d6452780cb3a7c'
+cask "caldigit-docking-utility" do
+  version "1.9.13"
+  sha256 "f68dc8443156a5b42543afc622b2c8ba80894ee224f332b2d1d6452780cb3a7c"
 
-  url 'https://downloads.caldigit.com/CalDigit-Docking-Station-Utility.zip'
-  name 'CalDigit Thunderbolt Docking Station Utility'
-  homepage 'https://www.caldigit.com/'
+  url "https://downloads.caldigit.com/CalDigit-Docking-Station-Utility.zip"
+  name "CalDigit Thunderbolt Docking Station Utility"
+  homepage "https://www.caldigit.com/"
 
   pkg "CalDigit Docking Station Utility v#{version}.pkg"
 
-  uninstall signal:  ['TERM', 'CalDigit.CalDigit-Docking-Station-Utility'],
-            pkgutil: 'com.CalDigit.CDSU.pkg'
+  uninstall signal:  ["TERM", "CalDigit.CalDigit-Docking-Station-Utility"],
+            pkgutil: "com.CalDigit.CDSU.pkg"
 end
