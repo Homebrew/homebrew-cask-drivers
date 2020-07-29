17d5d6741eaa0d062fcec1c441e80c065077a8f2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:57 +0100

    tomtom-sports-connect.rb: fix for new style

--- Casks/tomtom-sports-connect.rb
+++ Casks/tomtom-sports-connect.rb
@@ -1,24 +1,24 @@
-cask 'tomtom-sports-connect' do
+cask "tomtom-sports-connect" do
   version :latest
   sha256 :no_check
 
   # sports.tomtom-static.com/ was verified as official when first introduced to the cask
-  url 'https://sports.tomtom-static.com/downloads/desktop/mysportsconnect/latest/TomTomSportsConnectInstaller.pkg'
-  name 'TomTom MySports Connect'
-  homepage 'https://www.tomtom.com/mysports/getstarted'
+  url "https://sports.tomtom-static.com/downloads/desktop/mysportsconnect/latest/TomTomSportsConnectInstaller.pkg"
+  name "TomTom MySports Connect"
+  homepage "https://www.tomtom.com/mysports/getstarted"
 
-  pkg 'TomTomSportsConnectInstaller.pkg'
+  pkg "TomTomSportsConnectInstaller.pkg"
 
-  uninstall quit:       'com.tomtom.mysportsconnect',
-            login_item: 'TomTom Sports Connect Launcher',
+  uninstall quit:       "com.tomtom.mysportsconnect",
+            login_item: "TomTom Sports Connect Launcher",
             pkgutil:    [
-                          'com.tomtom.tomtomfa.pkg',
-                          'com.tomtom.tomtomfa.temp.pkg',
-                        ]
+              "com.tomtom.tomtomfa.pkg",
+              "com.tomtom.tomtomfa.temp.pkg",
+            ]
 
   zap trash: [
-               '~/Library/Application Support/TomTom',
-               '~/Library/Caches/TomTom',
-               '~/Library/Saved Application State/com.tomtom.mysportsconnect.savedState',
-             ]
+    "~/Library/Application Support/TomTom",
+    "~/Library/Caches/TomTom",
+    "~/Library/Saved Application State/com.tomtom.mysportsconnect.savedState",
+  ]
 end
