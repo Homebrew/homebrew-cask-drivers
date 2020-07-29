c77ccabbb7c5d45d7d49d1076c30ca0d04d4d9a1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:57 +0100

    steelseries-engine.rb: fix for new style

--- Casks/steelseries-engine.rb
+++ Casks/steelseries-engine.rb
@@ -1,39 +1,39 @@
-cask 'steelseries-engine' do
-  version '3.18.0'
-  sha256 'cc6d09e44e536976d472572ab5800fb06c2571b208b346ee47b4ef7df5edf465'
+cask "steelseries-engine" do
+  version "3.18.0"
+  sha256 "cc6d09e44e536976d472572ab5800fb06c2571b208b346ee47b4ef7df5edf465"
 
   # steelseriescdn.com/ was verified as official when first introduced to the cask
   url "https://downloads.steelseriescdn.com/drivers/engine/SteelSeriesEngine#{version}.pkg"
-  appcast 'https://steelseries.com/engine'
+  appcast "https://steelseries.com/engine"
   name "SteelSeries Engine #{version.major}"
-  homepage 'https://steelseries.com/engine'
+  homepage "https://steelseries.com/engine"
 
   auto_updates
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
   pkg "SteelSeriesEngine#{version}.pkg"
 
-  uninstall launchctl: 'com.steelseries.SSENext',
+  uninstall launchctl: "com.steelseries.SSENext",
             quit:      [
-                         "com.steelseries.SteelSeries-Engine-#{version.major}",
-                         'com.steelseries.ssenext.client.*',
-                         'com.steelseries.ssenext.uninstaller',
-                       ],
-            kext:      'com.steelseries.ssenext.driver',
+              "com.steelseries.SteelSeries-Engine-#{version.major}",
+              "com.steelseries.ssenext.client.*",
+              "com.steelseries.ssenext.uninstaller",
+            ],
+            kext:      "com.steelseries.ssenext.driver",
             script:    [
-                         executable: "/Applications/SteelSeries Engine #{version.major}/SteelSeries Engine #{version.major} Uninstaller.app/Contents/Resources/Uninstall.sh",
-                         sudo:       true,
-                       ],
-            pkgutil:   'com.steelseries.*',
+              { executable: "/Applications/SteelSeries Engine #{version.major}/SteelSeries Engine #{version.major} Uninstaller.app/Contents/Resources/Uninstall.sh",
+                sudo:       true },
+            ],
+            pkgutil:   "com.steelseries.*",
             delete:    "/Library/Application Support/SteelSeries Engine #{version.major}"
 
   zap trash: [
-               "~/Library/Application Support/steelseries-engine-#{version.major}-client",
-               "~/Library/Caches/com.steelseries.SteelSeries-Engine-#{version.major}",
-               "~/Library/Logs/SteelSeries Engine #{version.major} Client",
-               "~/Library/Preferences/com.steelseries.SteelSeries-Engine-#{version.major}.plist",
-               '~/Library/Preferences/com.steelseries.ssenext.client.helper.plist',
-               '~/Library/Preferences/com.steelseries.ssenext.client.plist',
-               '~/Library/Saved Application State/com.steelseries.ssenext.client.savedState',
-             ]
+    "~/Library/Application Support/steelseries-engine-#{version.major}-client",
+    "~/Library/Caches/com.steelseries.SteelSeries-Engine-#{version.major}",
+    "~/Library/Logs/SteelSeries Engine #{version.major} Client",
+    "~/Library/Preferences/com.steelseries.SteelSeries-Engine-#{version.major}.plist",
+    "~/Library/Preferences/com.steelseries.ssenext.client.helper.plist",
+    "~/Library/Preferences/com.steelseries.ssenext.client.plist",
+    "~/Library/Saved Application State/com.steelseries.ssenext.client.savedState",
+  ]
 end
