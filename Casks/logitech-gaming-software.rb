d8bba0363bed3626ec4582e77bec96812bd30330
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:09 +0100

    logitech-gaming-software.rb: fix for new style

--- Casks/logitech-gaming-software.rb
+++ Casks/logitech-gaming-software.rb
@@ -1,19 +1,19 @@
-cask 'logitech-gaming-software' do
-  version '9.02.22'
-  sha256 'c57c5d89015db3a572e009d4fbff47b49b61f4421bc5673ac261912ec47244aa'
+cask "logitech-gaming-software" do
+  version "9.02.22"
+  sha256 "c57c5d89015db3a572e009d4fbff47b49b61f4421bc5673ac261912ec47244aa"
 
   url "https://download01.logitech.com/web/ftp/pub/techsupport/gaming/LogitechSetup_#{version}.zip"
-  name 'Logitech Gaming Software'
-  homepage 'https://support.logitech.com/en_us/downloads'
+  name "Logitech Gaming Software"
+  homepage "https://support.logitech.com/en_us/downloads"
 
-  pkg 'LogitechGamingInstaller.app/Contents/Resources/LogiGamingSetup.mpkg'
+  pkg "LogitechGamingInstaller.app/Contents/Resources/LogiGamingSetup.mpkg"
 
-  uninstall script:    '/Applications/Logitech/Uninstaller.app/Contents/Resources/UninstallScript.sh',
-            launchctl: 'com.logitech.logiaudiod',
+  uninstall script:    "/Applications/Logitech/Uninstaller.app/Contents/Resources/UninstallScript.sh",
+            launchctl: "com.logitech.logiaudiod",
             pkgutil:   [
-                         'com.logitech.gaming',
-                         'com.logitech.lcdmanager',
-                       ]
+              "com.logitech.gaming",
+              "com.logitech.lcdmanager",
+            ]
 
   caveats do
     reboot
