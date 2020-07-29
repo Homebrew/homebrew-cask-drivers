cfc16f489c84800aa9926c91ec074b9ccb61f557
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:49 +0100

    garmin-express.rb: fix for new style

--- Casks/garmin-express.rb
+++ Casks/garmin-express.rb
@@ -1,12 +1,12 @@
-cask 'garmin-express' do
+cask "garmin-express" do
   version :latest
   sha256 :no_check
 
-  url 'https://download.garmin.com/omt/express/GarminExpress.dmg'
-  name 'Garmin Express'
-  homepage 'https://www.garmin.com/en-US/software/express'
+  url "https://download.garmin.com/omt/express/GarminExpress.dmg"
+  name "Garmin Express"
+  homepage "https://www.garmin.com/en-US/software/express"
 
-  pkg 'Install Garmin Express.pkg'
+  pkg "Install Garmin Express.pkg"
 
-  uninstall pkgutil: 'com.garmin.renu.client'
+  uninstall pkgutil: "com.garmin.renu.client"
 end
