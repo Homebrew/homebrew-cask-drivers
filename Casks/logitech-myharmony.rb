ddd18f5ca60a7ddda2c6f49b99db5ae678440f39
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:12 +0100

    logitech-myharmony.rb: fix for new style

--- Casks/logitech-myharmony.rb
+++ Casks/logitech-myharmony.rb
@@ -1,15 +1,15 @@
-cask 'logitech-myharmony' do
-  version '1.0.0.97'
-  sha256 'c5950b7c0461b5713f8896892291689e227046f7a4fc56fec790850f4b938fe0'
+cask "logitech-myharmony" do
+  version "1.0.0.97"
+  sha256 "c5950b7c0461b5713f8896892291689e227046f7a4fc56fec790850f4b938fe0"
 
   url "https://app.myharmony.com/prod/mac/#{version.major_minor}/MyHarmony-App.dmg"
-  appcast 'https://app.myharmony.com/prod/mac/harmonycast.xml'
-  name 'MyHarmony'
-  homepage 'https://setup.myharmony.com/'
+  appcast "https://app.myharmony.com/prod/mac/harmonycast.xml"
+  name "MyHarmony"
+  homepage "https://setup.myharmony.com/"
 
-  pkg 'MyHarmonySetup.pkg'
+  pkg "MyHarmonySetup.pkg"
 
-  uninstall quit:    'org.logitech.MyHarmony',
-            pkgutil: 'MyHarmony.pkg',
-            rmdir:   '/Applications/MyHarmony.app'
+  uninstall quit:    "org.logitech.MyHarmony",
+            pkgutil: "MyHarmony.pkg",
+            rmdir:   "/Applications/MyHarmony.app"
 end
