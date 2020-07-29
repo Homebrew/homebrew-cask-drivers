8f3c91803b4224aa0c25666927e0c8701184bcbb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:56 +0100

    garmin-fresh.rb: fix for new style

--- Casks/garmin-fresh.rb
+++ Casks/garmin-fresh.rb
@@ -1,21 +1,21 @@
-cask 'garmin-fresh' do
+cask "garmin-fresh" do
   version :latest
   sha256 :no_check
 
-  url 'https://navistatic.garmin.com/u/garmin_fresh/mac/GARMIN_Fresh_setup.pkg'
-  name 'GARMIN Fresh'
-  homepage 'https://volkswagen.garmin.com/volkswagen/site/fresh'
+  url "https://navistatic.garmin.com/u/garmin_fresh/mac/GARMIN_Fresh_setup.pkg"
+  name "GARMIN Fresh"
+  homepage "https://volkswagen.garmin.com/volkswagen/site/fresh"
 
-  pkg 'GARMIN_Fresh_setup.pkg'
+  pkg "GARMIN_Fresh_setup.pkg"
 
-  uninstall quit:    'com.garmin.fresh',
+  uninstall quit:    "com.garmin.fresh",
             pkgutil: [
-                       'com.garmin.fresh',
-                       'com.navigon.fresh',
-                     ]
+              "com.garmin.fresh",
+              "com.navigon.fresh",
+            ]
 
   zap trash: [
-               '~/Library/Fresh',
-               '~/Library/Saved Application State/com.garmin.fresh.savedState',
-             ]
+    "~/Library/Fresh",
+    "~/Library/Saved Application State/com.garmin.fresh.savedState",
+  ]
 end
