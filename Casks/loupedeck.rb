fd7c181df5457451b502f68425df70e0da9d34fc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:21 +0100

    loupedeck.rb: fix for new style

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -1,35 +1,35 @@
-cask 'loupedeck' do
-  version '3.2.5'
-  sha256 '3c877d67521da04c7356e4a6a178932ecb3b9beb8b2b938e1f7cf0d32658c5b7'
+cask "loupedeck" do
+  version "3.2.5"
+  sha256 "3c877d67521da04c7356e4a6a178932ecb3b9beb8b2b938e1f7cf0d32658c5b7"
 
   # loupedeck-software-release.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://loupedeck-software-release.s3.amazonaws.com/Software+Release+#{version}/MacOS/Loupedeck_#{version.dots_to_underscores}.dmg"
-  name 'Loupdeck'
-  homepage 'https://loupedeck.com/'
+  name "Loupdeck"
+  homepage "https://loupedeck.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   pkg "LoupedeckInstaller #{version}.pkg"
 
   uninstall signal:  [
-                       ['TERM', 'com.loupedeck.Loupedeck2'],
-                       ['QUIT', 'com.loupedeck.Loupedeck2'],
-                       ['INT', 'com.loupedeck.Loupedeck2'],
-                       ['HUP', 'com.loupedeck.Loupedeck2'],
-                       ['KILL', 'com.loupedeck.Loupedeck2'],
-                     ],
+    ["TERM", "com.loupedeck.Loupedeck2"],
+    ["QUIT", "com.loupedeck.Loupedeck2"],
+    ["INT", "com.loupedeck.Loupedeck2"],
+    ["HUP", "com.loupedeck.Loupedeck2"],
+    ["KILL", "com.loupedeck.Loupedeck2"],
+  ],
             pkgutil: [
-                       'com.loupedeck.PluginPackageInstaller',
-                       'com.loupedeck.ImageLibraryInstaller',
-                       'com.loupedeck.LoupedeckPackageInstaller',
-                     ],
+              "com.loupedeck.PluginPackageInstaller",
+              "com.loupedeck.ImageLibraryInstaller",
+              "com.loupedeck.LoupedeckPackageInstaller",
+            ],
             delete:  [
-                       '~/Library/Application Support/Capture One/KeyboardShortcuts/Loupedeck_beta.plist',
-                       '~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Loke Roos',
-                       '~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Karo Holmberg',
-                       '~/Library/Application Support/Adobe/Lightroom/Export Presets/Loupedeck Exports',
-                       '~/Library/Application Support/Adobe/Lightroom/Modules/loupedeck2.lrplugin',
-                       '~/Library/Application Support/LoupedeckConfig',
-                       '~/Library/Logs/LoupedeckConfig',
-                     ]
+              "~/Library/Application Support/Capture One/KeyboardShortcuts/Loupedeck_beta.plist",
+              "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Loke Roos",
+              "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Karo Holmberg",
+              "~/Library/Application Support/Adobe/Lightroom/Export Presets/Loupedeck Exports",
+              "~/Library/Application Support/Adobe/Lightroom/Modules/loupedeck2.lrplugin",
+              "~/Library/Application Support/LoupedeckConfig",
+              "~/Library/Logs/LoupedeckConfig",
+            ]
 end
