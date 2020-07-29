1e96d84174dc5db01af00c3c3f040b6101052529
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:12 +0100

    hp-easy-start.rb: fix for new style

--- Casks/hp-easy-start.rb
+++ Casks/hp-easy-start.rb
@@ -1,17 +1,17 @@
-cask 'hp-easy-start' do
+cask "hp-easy-start" do
   version :latest
   sha256 :no_check
 
-  url 'https://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Applications/HP_Easy_Start.app.zip'
-  name 'HP Easy Start'
-  homepage 'https://support.hp.com/'
+  url "https://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Applications/HP_Easy_Start.app.zip"
+  name "HP Easy Start"
+  homepage "https://support.hp.com/"
 
-  app 'HP Easy Start.app'
+  app "HP Easy Start.app"
 
   zap trash: [
-               '~/Library/Caches/com.hp.hp-easy-start',
-               '~/Library/Cookies/com.hp.hp-easy-start.binarycookies',
-               '~/Library/Logs/HP Easy Start.log',
-               '~/Library/Preferences/com.hp.hp-easy-start.plist',
-             ]
+    "~/Library/Caches/com.hp.hp-easy-start",
+    "~/Library/Cookies/com.hp.hp-easy-start.binarycookies",
+    "~/Library/Logs/HP Easy Start.log",
+    "~/Library/Preferences/com.hp.hp-easy-start.plist",
+  ]
 end
