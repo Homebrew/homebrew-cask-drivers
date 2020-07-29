28bb38f2984d53cfd113ca039891e2cf8eb89ea5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:06 +0100

    hdhomerun.rb: fix for new style

--- Casks/hdhomerun.rb
+++ Casks/hdhomerun.rb
@@ -1,19 +1,19 @@
-cask 'hdhomerun' do
-  version '20200521'
-  sha256 'b195cea70c305ad0827acce8708fda4f40c698ed167db088aae6a2d8b80aaf39'
+cask "hdhomerun" do
+  version "20200521"
+  sha256 "b195cea70c305ad0827acce8708fda4f40c698ed167db088aae6a2d8b80aaf39"
 
   url "https://download.silicondust.com/hdhomerun/hdhomerun_mac_#{version}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.silicondust.com/hdhomerun/hdhomerun_mac.dmg'
-  name 'HDHomeRun'
-  homepage 'https://www.silicondust.com/support/downloads/'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.silicondust.com/hdhomerun/hdhomerun_mac.dmg"
+  name "HDHomeRun"
+  homepage "https://www.silicondust.com/support/downloads/"
 
-  pkg 'HDHomeRun Installer.pkg'
+  pkg "HDHomeRun Installer.pkg"
 
-  uninstall pkgutil: 'com.silicondust.*hdhomerun'
+  uninstall pkgutil: "com.silicondust.*hdhomerun"
 
   zap trash: [
-               '~/Library/Caches/com.silicondust.hdhomerun',
-               '~/Library/Saved Application State/com.silicondust.hdhomerun.savedState',
-               '~/Library/WebKit/com.silicondust.hdhomerun',
-             ]
+    "~/Library/Caches/com.silicondust.hdhomerun",
+    "~/Library/Saved Application State/com.silicondust.hdhomerun.savedState",
+    "~/Library/WebKit/com.silicondust.hdhomerun",
+  ]
 end
