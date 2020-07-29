610936777aeaa4051b4d6f0eb9246f33b00f0281
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:57 +0100

    ubiquiti-unifi-controller.rb: fix for new style

--- Casks/ubiquiti-unifi-controller.rb
+++ Casks/ubiquiti-unifi-controller.rb
@@ -1,41 +1,41 @@
-cask 'ubiquiti-unifi-controller' do
-  version '5.13.32'
-  sha256 'ec43ebcd570b6d473c1e8b93d583a508f1f9bbad97940a6da0750e6509b13ae6'
+cask "ubiquiti-unifi-controller" do
+  version "5.13.32"
+  sha256 "ec43ebcd570b6d473c1e8b93d583a508f1f9bbad97940a6da0750e6509b13ae6"
 
   # dl.ubnt.com/ was verified as official when first introduced to the cask
   url "https://dl.ubnt.com/unifi/#{version}/UniFi.pkg"
-  appcast 'https://www.ui.com/download/unifi',
+  appcast "https://www.ui.com/download/unifi",
           must_contain: :no_check
-  name 'Ubiquiti UniFi Network Controller'
-  homepage 'https://unifi-sdn.ui.com/'
+  name "Ubiquiti UniFi Network Controller"
+  homepage "https://unifi-sdn.ui.com/"
 
-  conflicts_with cask: 'ubiquiti-unifi-controller-lts'
+  conflicts_with cask: "ubiquiti-unifi-controller-lts"
 
-  pkg 'UniFi.pkg'
+  pkg "UniFi.pkg"
 
   postflight do
-    set_ownership '~/Library/Application Support/UniFi'
+    set_ownership "~/Library/Application Support/UniFi"
   end
 
   uninstall quit:    [
-                       'com.oracle.java.*.jre',
-                       'com.ubnt.UniFi-Discover',
-                     ],
-            signal:  ['TERM', 'com.ubnt.UniFi'],
-            pkgutil: 'com.ubnt.UniFi',
+    "com.oracle.java.*.jre",
+    "com.ubnt.UniFi-Discover",
+  ],
+            signal:  ["TERM", "com.ubnt.UniFi"],
+            pkgutil: "com.ubnt.UniFi",
             delete:  [
-                       '/Applications/UniFi-Discover.app',
-                       '/Applications/UniFi.app',
-                     ]
+              "/Applications/UniFi-Discover.app",
+              "/Applications/UniFi.app",
+            ]
 
   zap trash: [
-               '~/Library/Application Support/UniFi',
-               '~/Library/Saved Application State/com.ubnt.UniFi-Discover.savedState',
-               '~/Library/Saved Application State/com.ubnt.UniFi.savedState',
-             ]
+    "~/Library/Application Support/UniFi",
+    "~/Library/Saved Application State/com.ubnt.UniFi-Discover.savedState",
+    "~/Library/Saved Application State/com.ubnt.UniFi.savedState",
+  ]
 
   caveats do
-    depends_on_java '8'
-    license 'https://www.ui.com/eula/'
+    depends_on_java "8"
+    license "https://www.ui.com/eula/"
   end
 end
