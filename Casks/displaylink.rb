b24e927f8b8a68aa37362d05dcf22e38995145bf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:27 +0100

    displaylink.rb: fix for new style

--- Casks/displaylink.rb
+++ Casks/displaylink.rb
@@ -1,48 +1,48 @@
-cask 'displaylink' do
+cask "displaylink" do
   if MacOS.version <= :yosemite
-    version '3.1.1,1252'
-    sha256 'cc4e73357841463509f1430520eec04792cbfbf9a2a564ea17d5cc78774372d0'
+    version "3.1.1,1252"
+    sha256 "cc4e73357841463509f1430520eec04792cbfbf9a2a564ea17d5cc78774372d0"
   elsif MacOS.version <= :high_sierra
-    version '4.3.1,1251'
-    sha256 'd5cd6787d6c4ca6a2425984bcbab607e618e9803335455e24196e14e35657b97'
+    version "4.3.1,1251"
+    sha256 "d5cd6787d6c4ca6a2425984bcbab607e618e9803335455e24196e14e35657b97"
   elsif MacOS.version <= :mojave
-    version '5.2.4,1581'
-    sha256 '5e5680f8497b77b7a8a0e4920a17791b2b388dc11046ef58070967ed524204cd'
+    version "5.2.4,1581"
+    sha256 "5e5680f8497b77b7a8a0e4920a17791b2b388dc11046ef58070967ed524204cd"
   else
-    version '5.2.4,1581'
-    sha256 '5e5680f8497b77b7a8a0e4920a17791b2b388dc11046ef58070967ed524204cd'
+    version "5.2.4,1581"
+    sha256 "5e5680f8497b77b7a8a0e4920a17791b2b388dc11046ef58070967ed524204cd"
   end
 
   url "https://www.displaylink.com/downloads/file?id=#{version.after_comma}",
       data:  {
-               'fileId'        => version.after_comma,
-               'accept_submit' => 'Accept',
-             },
+        "fileId"        => version.after_comma,
+        "accept_submit" => "Accept",
+      },
       using: :post
-  name 'DisplayLink USB Graphics Software'
-  homepage 'https://www.displaylink.com/'
+  name "DisplayLink USB Graphics Software"
+  homepage "https://www.displaylink.com/"
 
-  pkg 'DisplayLink Software Installer.pkg'
+  pkg "DisplayLink Software Installer.pkg"
 
-  uninstall pkgutil:   'com.displaylink.*',
+  uninstall pkgutil:   "com.displaylink.*",
             # 'kextunload -b com.displaylink.driver.DisplayLinkDriver' causes kernel panic
             # kext:      [
             #              'com.displaylink.driver.DisplayLinkDriver',
             #              'com.displaylink.dlusbncm'
             #            ],
             launchctl: [
-                         '73YQY62QM3.com.displaylink.DisplayLinkAPServer',
-                         'com.displaylink.useragent-prelogin',
-                         'com.displaylink.useragent',
-                         'com.displaylink.displaylinkmanager',
-                       ],
-            quit:      'DisplayLinkUserAgent',
+              "73YQY62QM3.com.displaylink.DisplayLinkAPServer",
+              "com.displaylink.useragent-prelogin",
+              "com.displaylink.useragent",
+              "com.displaylink.displaylinkmanager",
+            ],
+            quit:      "DisplayLinkUserAgent",
             delete:    [
-                         '/Applications/DisplayLink',
-                         '/Library/LaunchAgents/com.displaylink.useragent-prelogin.plist',
-                         '/Library/LaunchAgents/com.displaylink.useragent.plist',
-                         '/Library/LaunchDaemons/com.displaylink.displaylinkmanager.plist',
-                       ]
+              "/Applications/DisplayLink",
+              "/Library/LaunchAgents/com.displaylink.useragent-prelogin.plist",
+              "/Library/LaunchAgents/com.displaylink.useragent.plist",
+              "/Library/LaunchDaemons/com.displaylink.displaylinkmanager.plist",
+            ]
 
   caveats do
     reboot
