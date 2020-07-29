efc1486fabd1ae27cc4c8e4430dac06c782301cb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:56 +0100

    nitrokey.rb: fix for new style

--- Casks/nitrokey.rb
+++ Casks/nitrokey.rb
@@ -1,19 +1,19 @@
-cask 'nitrokey' do
-  version '1.4'
-  sha256 '80f6c95f5fa8320e4e323e1f4c11ac428ad4b731ae186891a4a66c7c056485f0'
+cask "nitrokey" do
+  version "1.4"
+  sha256 "80f6c95f5fa8320e4e323e1f4c11ac428ad4b731ae186891a4a66c7c056485f0"
 
   # github.com/Nitrokey/nitrokey-app/ was verified as official when first introduced to the cask
   url "https://github.com/Nitrokey/nitrokey-app/releases/download/v#{version}/Nitrokey-App.dmg"
-  appcast 'https://github.com/Nitrokey/nitrokey-app/releases.atom'
-  name 'Nitrokey App'
-  homepage 'https://www.nitrokey.com/download/macos'
+  appcast "https://github.com/Nitrokey/nitrokey-app/releases.atom"
+  name "Nitrokey App"
+  homepage "https://www.nitrokey.com/download/macos"
 
   app "Nitrokey App v#{version}.app"
 
-  uninstall quit: 'com.nitrokey.nitrokey-app'
+  uninstall quit: "com.nitrokey.nitrokey-app"
 
   zap trash: [
-               '~/Library/Preferences/com.nitrokey.Nitrokey App.plist',
-               '~/Library/Saved Application State/com.nitrokey.nitrokey-app.savedState',
-             ]
+    "~/Library/Preferences/com.nitrokey.Nitrokey App.plist",
+    "~/Library/Saved Application State/com.nitrokey.nitrokey-app.savedState",
+  ]
 end
