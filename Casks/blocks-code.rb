dcc9f6cdae9d61947091ab183001eead5d91a2be
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:30:55 +0100

    blocks-code.rb: fix for new style

--- Casks/blocks-code.rb
+++ Casks/blocks-code.rb
@@ -1,14 +1,14 @@
-cask 'blocks-code' do
-  version '0.2.3'
-  sha256 '9d8d03f8f004bf5c5474e67065128d3c15806ed9594202596dbe1c094276943b'
+cask "blocks-code" do
+  version "0.2.3"
+  sha256 "9d8d03f8f004bf5c5474e67065128d3c15806ed9594202596dbe1c094276943b"
 
   # assets.roli.com/blocks/ was verified as official when first introduced to the cask
   url "https://assets.roli.com/blocks/BLOCKS+Code/#{version}/BLOCKSCodeInstallerOSX_v#{version}.mpkg"
-  appcast 'https://juce.com/blocks/code'
-  name 'BLOCKS Code'
-  homepage 'https://juce.com/blocks/code'
+  appcast "https://juce.com/blocks/code"
+  name "BLOCKS Code"
+  homepage "https://juce.com/blocks/code"
 
   pkg "BLOCKSCodeInstallerOSX_v#{version}.mpkg"
 
-  uninstall pkgutil: 'com.roli.blocks.code'
+  uninstall pkgutil: "com.roli.blocks.code"
 end
