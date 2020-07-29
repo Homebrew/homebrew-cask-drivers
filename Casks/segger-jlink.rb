284cf61b5ab280aef4bb08d223676545aaa0f3e3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:57 +0100

    segger-jlink.rb: fix for new style

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,39 +1,39 @@
-cask 'segger-jlink' do
-  version '6.82b'
-  sha256 '89ffcbba309fe426a4e346008b1d0172743751944df35eef6870630fc9c4d16c'
+cask "segger-jlink" do
+  version "6.82b"
+  sha256 "89ffcbba309fe426a4e346008b1d0172743751944df35eef6870630fc9c4d16c"
 
   url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}.pkg",
       using: :post,
       data:  {
-               'accept_license_agreement' => 'accepted',
-               'non_emb_ctr'              => 'confirmed',
-               'submit'                   => 'Download software',
-             }
-  name 'Segger J-Link Command Line Tools'
-  homepage 'https://www.segger.com/downloads/jlink'
+        "accept_license_agreement" => "accepted",
+        "non_emb_ctr"              => "confirmed",
+        "submit"                   => "Download software",
+      }
+  name "Segger J-Link Command Line Tools"
+  homepage "https://www.segger.com/downloads/jlink"
 
   pkg "JLink_MacOSX_V#{version.no_dots}.pkg"
 
   uninstall quit:    [
-                       'com.segger.JFlashLite.*',
-                       'com.segger.JLinkGDBServer.*',
-                       'com.segger.JLinkLicenseManager.*',
-                       'com.segger.JLinkRegistration.*',
-                       'com.segger.JLinkRemoteServer.*',
-                       'com.segger.JLinkRTTViewer.*',
-                     ],
-            pkgutil: 'com.segger.pkg.JLink'
+    "com.segger.JFlashLite.*",
+    "com.segger.JLinkGDBServer.*",
+    "com.segger.JLinkLicenseManager.*",
+    "com.segger.JLinkRegistration.*",
+    "com.segger.JLinkRemoteServer.*",
+    "com.segger.JLinkRTTViewer.*",
+  ],
+            pkgutil: "com.segger.pkg.JLink"
 
   zap trash: [
-               '~/.SEGGER',
-               '~/Library/Application Support/SEGGER',
-               '~/Library/Saved Application State/com.segger.JFlashLite.*savedState',
-               '~/Library/Saved Application State/com.segger.JLinkGDBServer.*savedState',
-               '~/Library/Saved Application State/com.segger.JLinkLicenseManager.*savedState',
-               '~/Library/Saved Application State/com.segger.JLinkRTTViewer.*savedState',
-               '~/Library/Saved Application State/com.segger.JLinkRegistration.*savedState',
-               '~/Library/Saved Application State/com.segger.JLinkRemoteServer.*savedState',
-             ]
+    "~/.SEGGER",
+    "~/Library/Application Support/SEGGER",
+    "~/Library/Saved Application State/com.segger.JFlashLite.*savedState",
+    "~/Library/Saved Application State/com.segger.JLinkGDBServer.*savedState",
+    "~/Library/Saved Application State/com.segger.JLinkLicenseManager.*savedState",
+    "~/Library/Saved Application State/com.segger.JLinkRTTViewer.*savedState",
+    "~/Library/Saved Application State/com.segger.JLinkRegistration.*savedState",
+    "~/Library/Saved Application State/com.segger.JLinkRemoteServer.*savedState",
+  ]
 
   caveats do
     license @cask.url.to_s
