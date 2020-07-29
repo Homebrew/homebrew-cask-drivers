320776bac0ceba27c0166bd3f97fbac77d7b99bb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:56 +0100

    razer-synapse.rb: fix for new style

--- Casks/razer-synapse.rb
+++ Casks/razer-synapse.rb
@@ -1,32 +1,32 @@
-cask 'razer-synapse' do
-  version '1.87.1'
-  sha256 'a00fbbf1b4bb71717c1b552caf4465e16c715bc18a7e7160b4d046ba0ab5e0f4'
+cask "razer-synapse" do
+  version "1.87.1"
+  sha256 "a00fbbf1b4bb71717c1b552caf4465e16c715bc18a7e7160b4d046ba0ab5e0f4"
 
   # dl.razerzone.com/ was verified as official when first introduced to the cask
   url "https://dl.razerzone.com/drivers/Synapse2/mac/Razer_Synapse_Mac_Driver_v#{version.major_minor}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://rzr.to/synapse-mac-download',
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://rzr.to/synapse-mac-download",
           must_contain: version.major_minor
-  name 'Razer Synapse'
-  homepage 'https://www.razer.com/synapse-2'
+  name "Razer Synapse"
+  homepage "https://www.razer.com/synapse-2"
 
-  pkg 'Razer Synapse.pkg'
+  pkg "Razer Synapse.pkg"
 
-  uninstall delete:    '/Applications/Razer Synapse.app',
+  uninstall delete:    "/Applications/Razer Synapse.app",
             launchctl: [
-                         'com.razer.rzupdater',
-                         'com.razerzone.rzdeviceengine',
-                       ],
-            pkgutil:   'com.razerzone.*',
+              "com.razer.rzupdater",
+              "com.razerzone.rzdeviceengine",
+            ],
+            pkgutil:   "com.razerzone.*",
             quit:      [
-                         'com.razerzone.RzUpdater',
-                         'com.razerzone.rzdeviceengine',
-                       ]
+              "com.razerzone.RzUpdater",
+              "com.razerzone.rzdeviceengine",
+            ]
 
   zap trash: [
-               '/Library/Application Support/Razer',
-               '~/Library/Preferences/com.razer.*',
-               '~/Library/Preferences/com.razerzone.*',
-             ]
+    "/Library/Application Support/Razer",
+    "~/Library/Preferences/com.razer.*",
+    "~/Library/Preferences/com.razerzone.*",
+  ]
 
   caveats do
     reboot
