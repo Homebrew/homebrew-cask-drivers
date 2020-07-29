beaea1c7a473822613ceb04d55333e88fe90b7b2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:47 +0100

    elgato-stream-deck.rb: fix for new style

--- Casks/elgato-stream-deck.rb
+++ Casks/elgato-stream-deck.rb
@@ -1,24 +1,24 @@
-cask 'elgato-stream-deck' do
-  version '4.8.1.13027'
-  sha256 '4920fc48d4e9d4073e66bbe62b8c1709fe573f683aeb1ecf79dc81129e714ed9'
+cask "elgato-stream-deck" do
+  version "4.8.1.13027"
+  sha256 "4920fc48d4e9d4073e66bbe62b8c1709fe573f683aeb1ecf79dc81129e714ed9"
 
   url "https://edge.elgato.com/egc/macos/sd/Stream_Deck_#{version}.pkg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://gc-updates.elgato.com/mac/sd-update/final/download-website.php'
-  name 'Elgato Stream Deck'
-  homepage 'https://www.elgato.com/en/gaming/stream-deck'
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://gc-updates.elgato.com/mac/sd-update/final/download-website.php"
+  name "Elgato Stream Deck"
+  homepage "https://www.elgato.com/en/gaming/stream-deck"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   pkg "Stream_Deck_#{version}.pkg"
 
-  uninstall launchctl: 'com.elgato.StreamDeck',
-            quit:      'com.elgato.StreamDeck',
-            pkgutil:   'com.elgato.StreamDeck'
+  uninstall launchctl: "com.elgato.StreamDeck",
+            quit:      "com.elgato.StreamDeck",
+            pkgutil:   "com.elgato.StreamDeck"
 
   zap trash: [
-               '~/Library/Application Support/com.elgato.StreamDeck',
-               '~/Library/Caches/com.elgato.StreamDeck',
-               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.elgato.StreamDeck',
-               '~/Library/Preferences/com.elgato.StreamDeck.plist',
-             ]
+    "~/Library/Application Support/com.elgato.StreamDeck",
+    "~/Library/Caches/com.elgato.StreamDeck",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.elgato.StreamDeck",
+    "~/Library/Preferences/com.elgato.StreamDeck.plist",
+  ]
 end
