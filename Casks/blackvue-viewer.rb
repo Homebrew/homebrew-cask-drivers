fbd4175a2d4a992ba7b44d098defc30d26979008
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:30:52 +0100

    blackvue-viewer.rb: fix for new style

--- Casks/blackvue-viewer.rb
+++ Casks/blackvue-viewer.rb
@@ -1,13 +1,13 @@
-cask 'blackvue-viewer' do
-  version '3.01'
-  sha256 '5b021d113c5666c0536853f697a6267210b2b2c0309f88edc3bbd548ce08be8e'
+cask "blackvue-viewer" do
+  version "3.01"
+  sha256 "5b021d113c5666c0536853f697a6267210b2b2c0309f88edc3bbd548ce08be8e"
 
-  url 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=74331'
-  appcast 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/'
-  name 'BlackVue Viewer'
-  homepage 'https://www.blackvue.com/download/blackvue-mac-viewer-cloud/'
+  url "https://www.blackvue.com/download/blackvue-mac-viewer-cloud/?wpdmdl=74331"
+  appcast "https://www.blackvue.com/download/blackvue-mac-viewer-cloud/"
+  name "BlackVue Viewer"
+  homepage "https://www.blackvue.com/download/blackvue-mac-viewer-cloud/"
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: ">= :yosemite"
 
-  app 'BlackVue Viewer.app'
+  app "BlackVue Viewer.app"
 end
