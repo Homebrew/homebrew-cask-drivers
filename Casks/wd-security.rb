de45a117549bace3fada30a347dbd125407f6a09
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:39:57 +0100

    wd-security.rb: fix for new style

--- Casks/wd-security.rb
+++ Casks/wd-security.rb
@@ -1,30 +1,30 @@
-cask 'wd-security' do
-  version '2.1.1.75'
-  sha256 'f62f06d5193c210e37a62cc23a22c0e7c2a673f6fe8e7bae0f85aa748d84f521'
+cask "wd-security" do
+  version "2.1.1.75"
+  sha256 "f62f06d5193c210e37a62cc23a22c0e7c2a673f6fe8e7bae0f85aa748d84f521"
 
   url "https://downloads.wdc.com/wdapp/WD_Security_Standalone_Installer_Mac_#{version.dots_to_underscores}.zip"
-  appcast 'https://support.wdc.com/downloads.aspx?p=158'
-  name 'WD Security'
-  homepage 'https://support.wdc.com/downloads.aspx?p=158'
+  appcast "https://support.wdc.com/downloads.aspx?p=158"
+  name "WD Security"
+  homepage "https://support.wdc.com/downloads.aspx?p=158"
 
-  installer manual: 'WD Security Installer.app'
+  installer manual: "WD Security Installer.app"
 
-  uninstall launchctl: 'com.wdc.WDPrivilegedHelper',
+  uninstall launchctl: "com.wdc.WDPrivilegedHelper",
             quit:      [
-                         'com.wdc.branded.security',
-                         'com.westerndigital.WDSecurityHelper',
-                         'com.westerndigital.WDSecurityInstaller',
-                       ],
+              "com.wdc.branded.security",
+              "com.westerndigital.WDSecurityHelper",
+              "com.westerndigital.WDSecurityInstaller",
+            ],
             script:    [
-                         executable: '/Applications/WD Security Uninstaller.app/Contents/MacOS/WD Security Installer',
-                         sudo:       true,
-                       ]
+              { executable: "/Applications/WD Security Uninstaller.app/Contents/MacOS/WD Security Installer",
+                sudo:       true },
+            ]
 
   zap trash: [
-               '/Library/LaunchDaemons/com.wdc.WDPrivilegedHelper.plist',
-               '~/Library/Caches/com.apple.helpd/Generated/com.wdc.branded.security.help',
-               '~/Library/Caches/com.wdc.branded.security',
-               '~/Library/Caches/com.westerndigital.WDSecurityInstaller',
-               '~/Library/Preferences/com.wdc.branded.security.plist',
-             ]
+    "/Library/LaunchDaemons/com.wdc.WDPrivilegedHelper.plist",
+    "~/Library/Caches/com.apple.helpd/Generated/com.wdc.branded.security.help",
+    "~/Library/Caches/com.wdc.branded.security",
+    "~/Library/Caches/com.westerndigital.WDSecurityInstaller",
+    "~/Library/Preferences/com.wdc.branded.security.plist",
+  ]
 end
