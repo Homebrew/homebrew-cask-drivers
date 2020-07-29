4d66d9d1092020499f1bebbb67fb178ab1ab9f7f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:03 +0100

    gutenprint.rb: fix for new style

--- Casks/gutenprint.rb
+++ Casks/gutenprint.rb
@@ -1,22 +1,22 @@
-cask 'gutenprint' do
-  version '5.3.3'
-  sha256 '6dd810482845604a1e17fc3aef48a03d178798c14fe6f106a443f5af14022519'
+cask "gutenprint" do
+  version "5.3.3"
+  sha256 "6dd810482845604a1e17fc3aef48a03d178798c14fe6f106a443f5af14022519"
 
   url "https://downloads.sourceforge.net/gimp-print/gutenprint-#{version.major_minor}/#{version}/gutenprint-#{version}.dmg"
-  appcast 'https://sourceforge.net/projects/gimp-print/rss'
-  name 'Gutenprint'
-  homepage 'http://gimp-print.sourceforge.net/'
+  appcast "https://sourceforge.net/projects/gimp-print/rss"
+  name "Gutenprint"
+  homepage "http://gimp-print.sourceforge.net/"
 
   pkg "gutenprint-#{version}.pkg"
 
   uninstall script:  [
-                       executable: "#{staged_path}/uninstall-gutenprint.command",
-                       sudo:       true,
-                     ],
-            pkgutil: 'org.gutenprint.printer-driver',
+    { executable: "#{staged_path}/uninstall-gutenprint.command",
+      sudo:       true },
+  ],
+            pkgutil: "org.gutenprint.printer-driver",
             delete:  [
-                       '/usr/libexec/cups/backend/gutenprint*',
-                       '/usr/libexec/cups/driver/gutenprint*',
-                       '/usr/libexec/cups/filter/rastertogutenprint*',
-                     ]
+              "/usr/libexec/cups/backend/gutenprint*",
+              "/usr/libexec/cups/driver/gutenprint*",
+              "/usr/libexec/cups/filter/rastertogutenprint*",
+            ]
 end
