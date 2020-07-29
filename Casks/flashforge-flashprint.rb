d01bd68c9ce7d08df384cbc5c6173d4c22f404d3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:07 +0100

    flashforge-flashprint.rb: fix for new style

--- Casks/flashforge-flashprint.rb
+++ Casks/flashforge-flashprint.rb
@@ -1,16 +1,16 @@
-cask 'flashforge-flashprint' do
-  version '4.3.1,34c1b1fed28d05ff7090df40ff149107'
-  sha256 '7cae3e8bdae9e9370ef86603d79f761a9a2272db81935cc2a75ba67670e954ff'
+cask "flashforge-flashprint" do
+  version "4.3.1,34c1b1fed28d05ff7090df40ff149107"
+  sha256 "7cae3e8bdae9e9370ef86603d79f761a9a2272db81935cc2a75ba67670e954ff"
 
   url "https://en.fss.flashforge.com/10000/software/#{version.after_comma}.zip"
-  name 'FlashPrint'
-  homepage 'https://www.flashforge.com/software/detail/Flashprint?id=40'
+  name "FlashPrint"
+  homepage "https://www.flashforge.com/software/detail/Flashprint?id=40"
 
   auto_updates true
 
-  pkg 'FlashPrint.pkg'
+  pkg "FlashPrint.pkg"
 
-  uninstall pkgutil: 'com.sz3dp.pkg.FlashPrint'
+  uninstall pkgutil: "com.sz3dp.pkg.FlashPrint"
 
-  zap trash: '~/Library/Preferences/FlashForge.FlashPrint.plist'
+  zap trash: "~/Library/Preferences/FlashForge.FlashPrint.plist"
 end
