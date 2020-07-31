105d4de6f583c2f8058a0eca44ee02e43ba1ea11
Florian Didron <0x6664@hey.com>
2020-07-31 20:56:47 +0900

    fix: wrong shasum

--- Casks/zsa-wally.rb
+++ Casks/zsa-wally.rb
@@ -1,6 +1,6 @@
 cask "zsa-wally" do
   version "2.0.0"
-  sha256 "e29fa65666e859de3bc7f0ebce5ef92012f568116721b5d321c12fede5684287"
+  sha256 "b07a27738d9a462a32167c4d237fb3eb62a9221cc72686243eb13727cecbd1e2"
 
   # github.com/zsa/wally/ was verified as official when first introduced to the cask
   url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
