54c9fe75c3bc7579703422f7d0bb02401e8da419
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-31 10:29:24 +0200

    Update zsa-wally from 1.1.3 to 2.0.0

--- Casks/zsa-wally.rb
+++ Casks/zsa-wally.rb
@@ -1,6 +1,6 @@
 cask "zsa-wally" do
-  version "1.1.3"
-  sha256 "9dc788e16b0dd3336748867c1b5b1c06f2c55010d5b6a037cd5cbf22c67b61ef"
+  version "2.0.0"
+  sha256 "e29fa65666e859de3bc7f0ebce5ef92012f568116721b5d321c12fede5684287"
 
   # github.com/zsa/wally/ was verified as official when first introduced to the cask
   url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
