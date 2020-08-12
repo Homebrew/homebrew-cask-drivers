e391ca419338cee02285d6f86b914a895583c0c7
miccal <miccal.matthews@gmail.com>
2020-08-12 12:35:16 +0800

    Update behringer-x32-edit

--- Casks/behringer-x32-edit.rb
+++ Casks/behringer-x32-edit.rb
@@ -3,8 +3,8 @@ cask "behringer-x32-edit" do
   version "4.1"
   sha256 "a4eb8aa7c45a1266f928d3b2ab44ef274e1750e91754fe6eada122ccd0f5f093"
 
-  # downloads.musictribe.com/ was verified as official when first introduced to the cask
-  url "https://downloads.musictribe.com/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
+  # mediadl.musictribe.com/download/software/behringer/ was verified as official when first introduced to the cask
+  url "https://mediadl.musictribe.com/download/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
   appcast "https://www.behringer.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/Downloads"
   name "Behringer X32-Edit"
   homepage "https://www.behringer.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/"
