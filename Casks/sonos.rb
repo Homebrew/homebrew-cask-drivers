672bd7ca5b95ef10beffded957f8727cf6fa759c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-26 00:55:58 +0000

    Update sonos from 12.0.3 to 12.0.5 (#1712)

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,6 +1,6 @@
 cask "sonos" do
-  version "12.0.3"
-  sha256 "ab90f8d536bee88debd300d3009d3c947bbddc567aed68099b339eee260ed8ca"
+  version "12.0.5"
+  sha256 "88fb66d14e281a4767122898dcd2501f70ca557d2e6cce23fca18bbe828c5e4e"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac2",
