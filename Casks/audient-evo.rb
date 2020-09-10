139300ac7e2aae0ae3264e1d40773c7b1bdc8316
Colin Rymer <colin.rymer@gmail.com>
2020-09-10 11:57:06 -0400

    Add audient-evo 4.1.4 (#1750)
    * Add audient-evo 4.1.4
    
    * Update audient-evo.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/audient-evo.rb
@@ -0,0 +1,15 @@
+cask "audient-evo" do
+  version "4.1.4"
+  sha256 "894a6fdc75a05adc68911809dfb0d216d55d00ac511993a57beba44a6368b3dd"
+
+  # d9w4fhj63j193.cloudfront.net/EVO/EVO%208/Drivers/ was verified as official when first introduced to the cask
+  url "https://d9w4fhj63j193.cloudfront.net/EVO/EVO%208/Drivers/EVO%20v#{version}.dmg"
+  appcast "https://evo.audio/products/evo-4/downloads/"
+  name "Audient Evo"
+  desc "Control and Loop-back Mixer"
+  homepage "https://evo.audio/products/evo-4/downloads/"
+
+  app "EVO.app"
+
+  zap trash: "~/Library/Application Support/Audient"
+end
