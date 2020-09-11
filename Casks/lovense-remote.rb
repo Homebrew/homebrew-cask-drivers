f5b3858cede7f4ac3e09308082d63d6bd96d4cc5
Kristian Matthews <kristian.matthews@me.com>
2020-09-11 02:57:33 +0100

    Add Lovense_Remote.app vv1.3.9 (#1745)

---
+++ Casks/lovense-remote.rb
@@ -0,0 +1,12 @@
+cask "lovense-remote" do
+  version "1.3.9"
+  sha256 "a21b7e5a3f6e68ee7dd940bb6b8bbf75246d4e4392045672cbdf04aeb006d3c0"
+
+  url "https://lovense.com/files/apps/remote/remote.dmg"
+  appcast "https://lovense.com/sextoys/download"
+  name "Lovense Remote"
+  desc "Remote for Lovense products"
+  homepage "https://lovense.com/"
+
+  app "Lovense_Remote.app"
+end
