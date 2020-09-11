a58c2a7640e0e8d5ff4cabf7c67e0d5357fc94ae
miccal <miccal.matthews@gmail.com>
2020-09-11 08:51:36 +0800

    Update bose-updater from 7.0.0.4571 to 7.0.4.4795

--- Casks/bose-updater.rb
+++ Casks/bose-updater.rb
@@ -1,6 +1,6 @@
 cask "bose-updater" do
-  version "7.0.0.4571"
-  sha256 "56017f787fe309d5c8492d4978c9376aff5db78f2f39a7d5b48b936b647d7ce5"
+  version "7.0.4.4795"
+  sha256 "66d8b9a96dbbc8a6dd4d256c679df1f1af4f58b32ff025ad8248c352fdbb6c0b"
 
   url "https://downloads.bose.com/ced/boseupdater/mac/BoseUpdater_#{version}.dmg"
   appcast "https://btu.bose.com/data/MUV.xml"
