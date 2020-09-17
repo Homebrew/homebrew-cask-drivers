e6dda48d6202b2c82610180557a672bc08b873ae
suschizu <brigitte@corecode.io>
2020-09-17 17:29:33 +0200

    Update wd-security from 2.1.1.75 to 2.1.1.81 (#1769)

--- Casks/wd-security.rb
+++ Casks/wd-security.rb
@@ -1,6 +1,6 @@
 cask "wd-security" do
-  version "2.1.1.75"
-  sha256 "f62f06d5193c210e37a62cc23a22c0e7c2a673f6fe8e7bae0f85aa748d84f521"
+  version "2.1.1.81"
+  sha256 "d365a95e9dcad28b037fb1dbc79b3182a140f280f0d4a68c4dd00c5254e53af6"
 
   url "https://downloads.wdc.com/wdapp/WD_Security_Standalone_Installer_Mac_#{version.dots_to_underscores}.zip"
   appcast "https://support.wdc.com/downloads.aspx?p=158"
