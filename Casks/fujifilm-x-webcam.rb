da8f002e7dd83a6fc36823ad060c7309c93a2c48
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-23 06:24:28 +0800

    Update fujifilm-x-webcam.rb

--- Casks/fujifilm-x-webcam.rb
+++ Casks/fujifilm-x-webcam.rb
@@ -1,6 +1,7 @@
 cask "fujifilm-x-webcam" do
   version "2.0.0,9cy17ibx"
   sha256 "50f49b733835b8e152e8629574bd992b9e71cdee62db9a1c5ca1e6873e7c1a63"
+
   url "https://dl.fujifilm-x.com/support/software/x-webcam-mac#{version.before_comma.no_dots}-#{version.after_comma}/XWebcamIns#{version.before_comma.no_dots}.pkg"
   appcast "https://fujifilm-x.com/global/support/download/software/x-webcam/",
           must_contain: version.before_comma.no_dots
