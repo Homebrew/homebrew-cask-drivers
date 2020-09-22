87733ba850cd16b757765a087a972404bf75034b
Brian Lesperance <openbl@users.noreply.github.com>
2020-09-22 15:23:44 -0500

    Update fujifilm-x-webcam to 2.0.0,9cy17ibx

--- Casks/fujifilm-x-webcam.rb
+++ Casks/fujifilm-x-webcam.rb
@@ -1,10 +1,11 @@
 cask "fujifilm-x-webcam" do
-  version "1.0.0,gg7gh9hh"
-  sha256 "233f42bf871c1533e166dc3ebc94e4f49baa580853a7b9b3bfe5281c2cdddd5a"
+  version "2.0.0,9cy17ibx"
+  sha256 "50f49b733835b8e152e8629574bd992b9e71cdee62db9a1c5ca1e6873e7c1a63"
 
   url "https://dl.fujifilm-x.com/support/software/x-webcam-mac#{version.before_comma.no_dots}-#{version.after_comma}/XWebcamIns#{version.before_comma.no_dots}.pkg"
   appcast "https://fujifilm-x.com/global/support/download/software/x-webcam/"
   name "FUJIFILM X Webcam"
+  desc "FUJIFILM X Webcam offers a solution that can turn X Series and GFX System digital cameras into webcams"
   homepage "https://fujifilm-x.com/support/download/software/x-webcam/"
 
   depends_on macos: ">= :sierra"
