7fe83aef24ab27ce360fd0a7f50850e6f3fe3d2b
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-23 06:09:30 +0800

    Update fujifilm-x-webcam.rb

--- Casks/fujifilm-x-webcam.rb
+++ Casks/fujifilm-x-webcam.rb
@@ -1,9 +1,9 @@
 cask "fujifilm-x-webcam" do
   version "2.0.0,9cy17ibx"
   sha256 "50f49b733835b8e152e8629574bd992b9e71cdee62db9a1c5ca1e6873e7c1a63"
-
   url "https://dl.fujifilm-x.com/support/software/x-webcam-mac#{version.before_comma.no_dots}-#{version.after_comma}/XWebcamIns#{version.before_comma.no_dots}.pkg"
-  appcast "https://fujifilm-x.com/global/support/download/software/x-webcam/"
+  appcast "https://fujifilm-x.com/global/support/download/software/x-webcam/",
+          must_contain: version.before_comma.no_dots
   name "FUJIFILM X Webcam"
   desc "Solution that turns X Series and GFX System digital cameras into webcams"
   homepage "https://fujifilm-x.com/support/download/software/x-webcam/"
@@ -12,5 +12,8 @@ cask "fujifilm-x-webcam" do
 
   pkg "XWebcamIns#{version.before_comma.no_dots}.pkg"
 
-  uninstall pkgutil: "com.fujifilm.XWebcam"
+  uninstall pkgutil: [
+    "com.fujifilm.XWebcam",
+    "com.fujifilm.XWebcam.Settings",
+  ]
 end
