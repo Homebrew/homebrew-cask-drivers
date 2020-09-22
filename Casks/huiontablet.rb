bb5a1e21487a67523a33e8af79c2778725f59189
Andrew Zhou <andrewfzhou@gmail.com>
2020-09-22 14:30:32 -0500

    huion-tablet: Add Cask (#1772)
    * huion-tablet: Add Cask
    
    * Update and rename huion-tablet.rb to huiontablet.rb
    
    * huiontablet: Add zap
    
    * Update huiontablet.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/huiontablet.rb
@@ -0,0 +1,15 @@
+cask "huiontablet" do
+  version "14.4.5.200730"
+  sha256 "0fb410e205849dc277cef76c63eacc8cacb33faf3fec96a66247ce7a913a504a"
+
+  url "https://driver.huion.com/driver/Mac/HuionTablet_MacDriver_v#{version.major_minor_patch}.zip"
+  name "Huion Tablet"
+  desc "Driver for Huion Tablets"
+  homepage "https://huion.com/"
+
+  container nested: "HuionTablet_MacDriver_v#{version.major_minor_patch}.dmg"
+
+  app "HuionTablet.app"
+
+  zap trash: "~/Library/Saved Application State/com.huion.HuionTablet.savedState"
+end
