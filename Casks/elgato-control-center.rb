8ac401cd4062cb601b6f9c249ff2384e233305fa
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-23 18:50:59 +0000

    Update elgato-control-center from 1.1.2,10315 to 1.1.3,10337 (#1787)

--- Casks/elgato-control-center.rb
+++ Casks/elgato-control-center.rb
@@ -1,6 +1,6 @@
 cask "elgato-control-center" do
-  version "1.1.2,10315"
-  sha256 "c8dc50483b295f334a9b9aedf2b16367c3b7586ed9d270b6d663867e76806073"
+  version "1.1.3,10337"
+  sha256 "cb8978a4951ab137eb7a7e7e055700c343379b4efa8d0ae3fc714fe6fc8303cb"
 
   url "https://edge.elgato.com/egc/macos/eccm/#{version.before_comma}/Control_Center_#{version.before_comma}.#{version.after_comma}.zip"
   appcast "https://gc-updates.elgato.com/mac/control-center-update/feed.xml"
