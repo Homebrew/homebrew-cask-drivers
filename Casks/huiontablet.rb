1c0ad562b3dbfffcb5ff6bd4f51f12b134b9c5a8
CoreCode <core-code@users.noreply.github.com>
2020-09-25 02:30:31 +0200

    Update huiontablet from 14.4.5.200730 to 14.4.5 (#1794)

--- Casks/huiontablet.rb
+++ Casks/huiontablet.rb
@@ -1,13 +1,13 @@
 cask "huiontablet" do
-  version "14.4.5.200730"
+  version "14.4.5"
   sha256 "0fb410e205849dc277cef76c63eacc8cacb33faf3fec96a66247ce7a913a504a"
 
-  url "https://driver.huion.com/driver/Mac/HuionTablet_MacDriver_v#{version.major_minor_patch}.zip"
+  url "https://driver.huion.com/driver/Mac/HuionTablet_MacDriver_v#{version}.zip"
   name "Huion Tablet"
   desc "Driver for Huion Tablets"
   homepage "https://huion.com/"
 
-  container nested: "HuionTablet_MacDriver_v#{version.major_minor_patch}.dmg"
+  container nested: "HuionTablet_MacDriver_v#{version}.dmg"
 
   app "HuionTablet.app"
 
