136dc3daa70a283e5681c8aaf9482f5fc34f6d69
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-28 00:38:15 +0000

    Update blue-sherpa from 20191122 to 20200810 (#1774)

--- Casks/blue-sherpa.rb
+++ Casks/blue-sherpa.rb
@@ -1,13 +1,16 @@
 cask "blue-sherpa" do
-  version "20191122"
-  sha256 "7d89e64d9bf3175a07357d7903b50392d155dbd454e1dba784d3be8a27aaa14f"
+  version "20200810"
+  sha256 "f1287b3b32007620f36bc1d49bc1f8cfb7347e228e34fa3b07904b186fbfcf64"
 
   url "https://software.bluemic.com/blue/BlueSherpa-#{version}.pkg"
   appcast "https://www.bluemic.com/en-us/products/sherpa/"
   name "Blue Sherpa"
+  desc "Companion app for Blue USB microphones"
   homepage "https://www.bluemic.com/en-us/products/sherpa/"
 
-  pkg "BlueSherpa-#{version}.pkg"
+  # pkg cannot be installed automatically
+  installer manual: "BlueSherpa-#{version}.pkg"
 
-  uninstall pkgutil: "com.bluemicrophones.Blue"
+  uninstall pkgutil: "com.bluemicrophones.Blue",
+            delete:  "/Applications/Blue.app"
 end
