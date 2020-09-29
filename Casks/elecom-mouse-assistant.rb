6ea36985267a752e9fd2e212c5d3ec69ab6aecc2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-29 22:20:49 +0000

    Update elecom-mouse-assistant from 5.2.0.000 to 5.2.1.000 (#1802)

--- Casks/elecom-mouse-assistant.rb
+++ Casks/elecom-mouse-assistant.rb
@@ -1,14 +1,12 @@
 cask "elecom-mouse-assistant" do
-  version "5.2.0.000"
-  sha256 "160b2118b9c3d5c107054ae58ac9b286671990dae1d094672a653a3bf1b93c02"
+  version "5.2.1.000"
+  sha256 "ccaa064398abdcaf23ed9a6d34b862253bd9f9d60075180bd8afdd9cd98d81bc"
 
-  # dl.elecom.co.jp was verified as official when first introduced to the cask
   url "https://dl.elecom.co.jp/support/download/peripheral/mouse/assistant/mac/ELECOM_Mouse_Installer_#{version}.zip"
-  appcast "https://www.elecom.co.jp.e.gj.hp.transer.com/global/download-list/utility/mouse_assistant/mac/"
+  appcast "https://www.elecom.co.jp/global/download-list/utility/mouse_assistant/mac/"
   name "ELECOM Mouse Assistant"
-  homepage "https://www.elecom.co.jp.e.gj.hp.transer.com/global/download-list/utility/mouse_assistant/"
-
-  container nested: "ELECOM_Mouse_Installer_#{version}.dmg"
+  desc "Software to more effectively use an ELECOM mouse"
+  homepage "https://www.elecom.co.jp/global/download-list/utility/mouse_assistant/"
 
   pkg "ELECOM_Mouse_Installer_#{version}.pkg"
 
