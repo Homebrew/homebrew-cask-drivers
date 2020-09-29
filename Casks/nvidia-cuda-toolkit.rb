e6c6ea221e877d32565398ddad0c010e8915921c
m416kar98k <70918897+m416kar98k@users.noreply.github.com>
2020-09-29 00:13:13 -0700

    Update and rename nvidia-cuda to nvidia-cuda-toolkit (#1800)

--- Casks/nvidia-cuda-toolkit.rb
+++ Casks/nvidia-cuda-toolkit.rb
@@ -1,17 +1,17 @@
-cask "nvidia-cuda" do
+cask "nvidia-cuda-toolkit" do
   if MacOS.version <= :sierra
     version "9.0.176"
     sha256 "8fad950098337d2611d64617ca9f62c319d97c5e882b8368ed196e994bdaf225"
     url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac-dmg"
   else
-    version "10.1.105"
-    sha256 "1d3355fa48b5763737f1c97a6436c774eda24ae91435e8ecc22428d23a01374a"
-    url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac.dmg"
+    version "10.1.243"
+    sha256 "432a2f07a793f21320edc5d10e7f68a8e4e89465c31e1696290bdb0ca7c8c997"
+    url "https://developer.download.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac.dmg"
   end
 
-  appcast "https://developer.nvidia.com/cuda-toolkit-archive"
-  name "Nvidia CUDA"
-  homepage "https://developer.nvidia.com/cuda-zone"
+  name "Nvidia CUDA Toolkit"
+  desc "Tools to create high performance GPU-accelerated applications"
+  homepage "https://developer.nvidia.com/cuda-toolkit"
 
   # Unusual case: The software will stop working, or is dangerous to run, on the next macOS release.
   depends_on macos: [
