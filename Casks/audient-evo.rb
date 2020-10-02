398144d84890b0c93a81b5822337bc98b541ff18
Brigitte Zmoelnig <brigitte@corecode.io>
2020-10-02 17:39:47 +0200

    Update audient-evo from 4.1.4 to 4.1.6rc3

--- Casks/audient-evo.rb
+++ Casks/audient-evo.rb
@@ -1,6 +1,6 @@
 cask "audient-evo" do
-  version "4.1.4"
-  sha256 "894a6fdc75a05adc68911809dfb0d216d55d00ac511993a57beba44a6368b3dd"
+  version "4.1.6rc3"
+  sha256 "fff915e097ee464ac945794bb762a4a38f72caff6627d7eab2e500b24c029010"
 
   # d9w4fhj63j193.cloudfront.net/EVO/EVO%208/Drivers/ was verified as official when first introduced to the cask
   url "https://d9w4fhj63j193.cloudfront.net/EVO/EVO%208/Drivers/EVO%20v#{version}.dmg"
