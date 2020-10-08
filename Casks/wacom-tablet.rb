c432d7371266c40edc7bb0fd13ba01b3463a6cfc
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-08 07:12:14 +0000

    Update wacom-tablet from 6.3.40-2 to 6.3.41-2 (#1810)

--- Casks/wacom-tablet.rb
+++ Casks/wacom-tablet.rb
@@ -1,37 +1,36 @@
 cask "wacom-tablet" do
-  version "6.3.40-2"
-  sha256 "4d8a1594ee2cb24af2ec1dcf381a68147be9c8fbed6a46e9f29494800eb7773a"
+  version "6.3.41-2"
+  sha256 "a0eaf9294d5202ce309f783c763692674b05d85a8fd5e0cccee531b6ec396622"
 
   url "https://cdn.wacom.com/u/productsupport/drivers/mac/professional/WacomTablet_#{version}.dmg"
   appcast "https://www.wacom.com/en-de/support/product-support/drivers"
-  name "Wacom Intuos 4/5/Pro Tablet"
+  name "Wacom Tablet"
+  desc "Resources for Wacom tablets"
   homepage "https://www.wacom.com/en-us/support/product-support/drivers"
-
-  conflicts_with cask: "wacom-intuos-3-tablet"
 
   pkg "Install Wacom Tablet.pkg"
 
-  uninstall launchctl: [
-    "com.wacom.wacomtablet",
-    "com.wacom.UpdateHelper",
-    "com.wacom.displayhelper",
-    "com.wacom.DataStoreMgr",
-    "com.wacom.DisplayMgr",
-  ],
+  uninstall pkgutil:   "com.wacom.TabletInstaller",
             quit:      [
               "com.wacom.DisplayMgr",
               "com.wacom.RemoveWacomTablet",
               "com.wacom.TabletDriver",
               "com.wacom.Wacom-Desktop-Center",
+              "com.wacom.wacomtablet",
               "com.wacom.WacomTouchDriver",
+            ],
+            launchctl: [
+              "com.wacom.DataStoreMgr",
+              "com.wacom.displayhelper",
+              "com.wacom.IOManager",
+              "com.wacom.UpdateHelper",
               "com.wacom.wacomtablet",
             ],
             kext:      [
               "com.wacom.kext.ftdi",
               "com.wacom.kext.wacomtablet",
             ],
-            pkgutil:   "com.wacom.TabletInstaller",
-            delete:    "/Applications/Wacom Tablet.localized"
+            delete:    "/Applications/Wacom Tablet.localized/"
 
   zap trash: [
     "~/Library/Application Scripts/com.wacom.DataStoreMgr",
