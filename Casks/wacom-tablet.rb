78b2bdf9dc29506246e1edfc5fa4297ec30be942
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-08 14:52:44 +0800

    Update wacom-tablet.rb

--- Casks/wacom-tablet.rb
+++ Casks/wacom-tablet.rb
@@ -4,33 +4,33 @@ cask "wacom-tablet" do
 
   url "https://cdn.wacom.com/u/productsupport/drivers/mac/professional/WacomTablet_#{version}.dmg"
   appcast "https://www.wacom.com/en-de/support/product-support/drivers"
-  name "Wacom Intuos 4/5/Pro Tablet"
-  desc "Resources for Wacom pen tablets"
+  name "Wacom Tablet"
+  desc "Resources for Wacom tablets"
   homepage "https://www.wacom.com/en-us/support/product-support/drivers"
 
   pkg "Install Wacom Tablet.pkg"
 
-  uninstall launchctl: [
-    "com.wacom.wacomtablet",
-    "com.wacom.UpdateHelper",
-    "com.wacom.displayhelper",
-    "com.wacom.DataStoreMgr",
-    "com.wacom.DisplayMgr",
-  ],
+  uninstall pkgutil:   "com.wacom.TabletInstaller",
             quit:      [
               "com.wacom.DisplayMgr",
               "com.wacom.RemoveWacomTablet",
               "com.wacom.TabletDriver",
               "com.wacom.Wacom-Desktop-Center",
+              "com.wacom.wacomtablet",
               "com.wacom.WacomTouchDriver",
+            ],
+            launchctl: [
+              "com.wacom.DataStoreMgr",
+              "com.wacom.displayhelper",
+              "com.wacom.IOManager",
+              "com.wacom.UpdateHelper",
               "com.wacom.wacomtablet",
             ],
             kext:      [
               "com.wacom.kext.ftdi",
               "com.wacom.kext.wacomtablet",
             ],
-            pkgutil:   "com.wacom.TabletInstaller",
-            delete:    "/Applications/Wacom Tablet.localized"
+            delete:    "/Applications/Wacom Tablet.localized/"
 
   zap trash: [
     "~/Library/Application Scripts/com.wacom.DataStoreMgr",
