74ac9ae9ba75035e4b6da5e0a17d3992c08e3d6d
suschizu <brigitte@corecode.io>
2020-10-09 14:39:54 +0200

    Update loupedeck.rb

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -1,16 +1,16 @@
 cask "loupedeck" do
-  version "4.0.1"
+  version "4.0.1.4477"
   sha256 "55c429fcafc6aba063a73fc11949774c2057e6f1620736c6bfac400754e0bb48"
 
   # loupedeck-software-release.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url "https://loupedeck-software-release.s3.amazonaws.com/Loupedeck+Software+#{version}/MacOS/LoupedeckSoftware_v#{version}.dmg"
+  url "https://loupedeck-software-release.s3.amazonaws.com/Loupedeck+Software+#{version.major_minor_patch}/MacOS/LoupedeckSoftware_v#{version.major_minor_patch}.dmg"
   name "Loupdeck"
   desc "Software for Loupedeck consoles"
   homepage "https://loupedeck.com/"
 
   depends_on macos: ">= :sierra"
 
-  pkg "Loupedeck_Installer_#{version}.pkg"
+  pkg "LoupedeckInstaller_#{version}.pkg"
 
   uninstall signal:  [
     ["TERM", "com.loupedeck.Loupedeck2"],
