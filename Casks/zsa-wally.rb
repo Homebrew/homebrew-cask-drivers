f887621063b189660f40723f412f4d636720d5e4
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-10-09 15:36:30 +0800

    Update zsa-wally from 2.0.0 to 2.1.0 (#1811)

--- Casks/zsa-wally.rb
+++ Casks/zsa-wally.rb
@@ -1,6 +1,6 @@
 cask "zsa-wally" do
-  version "2.0.0"
-  sha256 "b07a27738d9a462a32167c4d237fb3eb62a9221cc72686243eb13727cecbd1e2"
+  version "2.1.0"
+  sha256 "23f2770744800ba2af2f33caa297c6621a6610c4999ad0d3cf7673a5060c2a44"
 
   # github.com/zsa/wally/ was verified as official when first introduced to the cask
   url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
