9338e7e83698b188e94f4b90cbb85c6727b71bdc
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-10 02:49:27 +0000

    Update hdhomerun from 20200907 to 20201009 (#1814)
    * Update hdhomerun from 20200907 to 20201008
    
    * Update hdhomerun.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/hdhomerun.rb
+++ Casks/hdhomerun.rb
@@ -1,10 +1,11 @@
 cask "hdhomerun" do
-  version "20200907"
-  sha256 "f234aab75b2581b4244cd320cc9d0435797ee806d5d172a320ed663f99c6074a"
+  version "20201009"
+  sha256 "90bb2d5d9515426715d5bf751705932ed56eba4c07165718551670b103b275f3"
 
   url "https://download.silicondust.com/hdhomerun/hdhomerun_mac_#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.silicondust.com/hdhomerun/hdhomerun_mac.dmg"
   name "HDHomeRun"
+  desc "Watch, schedule and record terrestrial HDTV"
   homepage "https://www.silicondust.com/support/downloads/"
 
   pkg "HDHomeRun Installer.pkg"
