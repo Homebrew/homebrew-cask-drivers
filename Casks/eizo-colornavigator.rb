e1dbeeae23c3cd0044be049ffdb7cdbdb5a62052
nikste <44636701+MayNiklas@users.noreply.github.com>
2020-10-15 01:17:54 +0200

    Add ColorNavigator 7.app v7.11 (#1822)

---
+++ Casks/eizo-colornavigator.rb
@@ -0,0 +1,19 @@
+cask "eizo-colornavigator" do
+  version "7.11"
+  sha256 "4d0c5064bfcfe9f1387bb44792a0835a627a9f4cf49dd20daf9f64662a733af8"
+
+  url "https://www.eizoglobal.com/support/db/files/software/software/graphics/colornavigator#{version.major}/ColorNavigator#{version.no_dots}.pkg"
+  name "Eizo ColorNavigator"
+  desc "Calibrate Eizo Coloredge screens"
+  homepage "https://www.eizoglobal.com/products/coloredge/cn7/"
+
+  pkg "ColorNavigator#{version.no_dots}.pkg"
+
+  uninstall pkgutil: "com.eizo.pkg.ColorNavigator#{version.major.no_dots}",
+            quit:    "com.eizo.ColorNavigator#{version.major}",
+            delete:  "/Library/Application Support/EIZO/ColorNavigator #{version.major}",
+            rmdir:   "/Library/Application Support/EIZO"
+
+  zap trash: "~/Library/Application Support/EIZO/ColorNavigator #{version.major}",
+      rmdir: "~/Library/Application Support/EIZO"
+end
