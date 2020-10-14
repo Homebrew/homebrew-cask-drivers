62da84bab464f68c61ee7b5c5aee19ebea923e41
Anatoly Lebedev <beastea@gmail.com>
2020-10-14 02:17:37 +0200

    concept2-utility: update checksum (#1818)
    * update checksum
    
    * add desc stanza to the cask
    
    * make description much shorter
    
    * cut description, add some context
    
    * Update concept2-utility.rb
    
    * Update concept2-utility.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/concept2-utility.rb
+++ Casks/concept2-utility.rb
@@ -4,13 +4,14 @@ cask "concept2-utility" do
     sha256 "e4ebee8cde57c7ef63c3903285c3fc0ee8f87221e7c5529b9dcf97b3f9ebb57e"
   else
     version "7.10.01"
-    sha256 "2bccb53d1070cb3263bdcdb85f25923f3cf369942b99cf550e88ae4a80febe89"
+    sha256 "652727e61e0db60b94d74fbb14753872f54c896c71e5d15febdd34764378dee0"
   end
 
   url "https://software.concept2.com/utility/Concept2Utility#{version.no_dots}.dmg"
   appcast "https://www.concept2.com/service/software/concept2-utility",
           must_contain: version.no_dots
   name "Concept2 Utility"
+  desc "Utilities for the Concept2 Performance Monitor"
   homepage "https://www.concept2.com/service/software/concept2-utility"
 
   depends_on macos: ">= :yosemite"
