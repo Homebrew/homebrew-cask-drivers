a2c7a4a32a902f89d04b5760757d0dd54a8ea55d
Vítor Galvão <info@vitorgalvao.com>
2020-10-14 13:49:27 +0100

    Update eizo-colornavigator.rb

--- Casks/eizo-colornavigator.rb
+++ Casks/eizo-colornavigator.rb
@@ -2,9 +2,9 @@ cask "eizo-colornavigator" do
   version "7.11"
   sha256 "4d0c5064bfcfe9f1387bb44792a0835a627a9f4cf49dd20daf9f64662a733af8"
 
-  url "https://www.eizoglobal.com/support/db/files/software/software/graphics/colornavigator#{version.major.no_dots}/ColorNavigator#{version.no_dots}.pkg"
-  name "ColorNavigator 7"
-  desc "Used for calibrating Eizo Coloredge screens"
+  url "https://www.eizoglobal.com/support/db/files/software/software/graphics/colornavigator#{version.major}/ColorNavigator#{version.no_dots}.pkg"
+  name "Eizo ColorNavigator"
+  desc "Calibrate Eizo Coloredge screens"
   homepage "https://www.eizoglobal.com/products/coloredge/cn7/"
 
   pkg "ColorNavigator#{version.no_dots}.pkg"
@@ -12,8 +12,9 @@ cask "eizo-colornavigator" do
   uninstall pkgutil: [
     "com.eizo.pkg.ColorNavigator#{version.major.no_dots}",
   ],
+            quit:    "com.eizo.ColorNavigator#{version.major}",
             delete:  [
-              "/Library/Application\ Support/EIZO/",
-              "~/Library/Application\ Support/EIZO/",
+              "/Library/Application Support/EIZO/",
+              "~/Library/Application Support/EIZO/",
             ]
 end
