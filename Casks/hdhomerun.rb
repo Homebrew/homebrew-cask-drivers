535d8b3520085292118821285e9852f10dee4074
Randall <ran.dall@icloud.com>
2020-10-23 12:59:59 -0700

    Update hdhomerun from 20201014 to 20201022

--- Casks/hdhomerun.rb
+++ Casks/hdhomerun.rb
@@ -1,6 +1,6 @@
 cask "hdhomerun" do
-  version "20201014"
-  sha256 "9f0c6d43ba8494bfa834b567e43c962e90a81070ee8463de720abe389c173027"
+  version "20201022"
+  sha256 "738384f8a49b7c024079387f894e14e1a0a33ee02e1a9758c48bbeff3bc6abc3"
 
   url "https://download.silicondust.com/hdhomerun/hdhomerun_mac_#{version}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.silicondust.com/hdhomerun/hdhomerun_mac.dmg"
