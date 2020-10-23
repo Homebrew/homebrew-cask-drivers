5e6ac6b390cb39789beee1755f6f2272e9aa3964
Randall <17261190+ran-dall@users.noreply.github.com>
2020-10-23 01:26:33 +0000

    Update luna-secondary from 4.2.0 to 4.3.0 (#1838)
    * Update luna-secondary from 4.2.0 to 4.3.0
    
    * Update luna-secondary.rb
    
    * Update luna-secondary.rb
    
    * Update luna-secondary.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/luna-secondary.rb
+++ Casks/luna-secondary.rb
@@ -1,11 +1,12 @@
 cask "luna-secondary" do
-  version "4.2.0"
-  sha256 "b7796ae945a9fba9e4cdd69b9c2f0fba0b3ca76a17f26551724529c44cf53242"
+  version "4.3.0"
+  sha256 "619af97a113ddd9c4990df4eaaca7cd7c233e22e13db94e97812164d9086878e"
 
   # s3.amazonaws.com/s3.lunadisplay.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaSecondary-#{version}.dmg"
   appcast "https://s3.lunadisplay.com/downloads/luna-secondary-sparkle.xml"
   name "Luna Secondary"
+  desc "Turn a computer or tablet into a second display"
   homepage "https://lunadisplay.com/"
 
   app "Luna Secondary.app"
