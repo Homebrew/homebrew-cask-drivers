ab362c5215d6e90921f91b2bd21b41d95906820a
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-23 08:23:28 +0800

    Update luna-secondary.rb

--- Casks/luna-secondary.rb
+++ Casks/luna-secondary.rb
@@ -6,6 +6,7 @@ cask "luna-secondary" do
   url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaSecondary-#{version}.dmg"
   appcast "https://s3.lunadisplay.com/downloads/luna-secondary-sparkle.xml"
   name "Luna Secondary"
+  desc "Software to use a wireless second display"
   homepage "https://lunadisplay.com/"
 
   app "Luna Secondary.app"
