b6f862981a44ee8ce27a48cd4f86edc9323c898e
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-23 08:31:58 +0800

    Update luna-secondary.rb

--- Casks/luna-secondary.rb
+++ Casks/luna-secondary.rb
@@ -1,12 +1,12 @@
 cask "luna-secondary" do
   version "4.3.0"
-  sha256 "92e4368566984f18e686e90ccd3171bd05e425da439420f4642d267b2f20d711"
+  sha256 "619af97a113ddd9c4990df4eaaca7cd7c233e22e13db94e97812164d9086878e"
 
   # s3.amazonaws.com/s3.lunadisplay.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaSecondary-#{version}.dmg"
   appcast "https://s3.lunadisplay.com/downloads/luna-secondary-sparkle.xml"
   name "Luna Secondary"
-  desc "Software to use a wireless second display"
+  desc "Turn any Mac or iPad into a second display"
   homepage "https://lunadisplay.com/"
 
   app "Luna Secondary.app"
