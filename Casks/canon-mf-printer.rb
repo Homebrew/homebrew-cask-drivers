2e51ff1c0aa9b5e91d84698e23c42b50d722992e
tie624 <tie624@gmail.com>
2020-10-28 18:17:10 -0400

    add canon driver for mf printer/fax/scanner (#1851)

---
+++ Casks/canon-mf-printer.rb
@@ -0,0 +1,16 @@
+cask "canon-mf-printer" do
+  version "10.11.4"
+  sha256 "2d8919b41ab9a9d5d51d86b8dea277e9dd12e2e3e8aeb98b89e670bf65c14509"
+
+  # gdlp01.c-wss.com/gds/ was verified as official when first introduced to the cask
+  url "https://gdlp01.c-wss.com/gds/3/0100010873/01/mac-mf-v#{version.no_dots}-00.dmg"
+  name "Canon MF Printer Driver & Utilities"
+  desc "Printer driver & utilities for Canon imageCLASS MF printers"
+  homepage "https://www.usa.canon.com/internet/portal/us/home/support/drivers-downloads"
+
+  depends_on macos: ">= :yosemite"
+
+  pkg "MF_Printer_Installer.pkg"
+
+  uninstall pkgutil: "jp.co.canon.CUPSMFPrinter.*"
+end
