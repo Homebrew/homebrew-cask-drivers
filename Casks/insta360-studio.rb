cb4a6d725476278c45b63f7495b8343812d97ae0
suschizu <brigitte@corecode.io>
2020-10-30 13:03:32 +0100

    Update insta360-studio from 3.5.0,2020_20200806_222513:20200807.eea8769d2706b6acdcb86c3c26a161e5 to 3.5.4,2020_20201029_064913:20201029.749ae151b284b24ee59feeced39256b6 (#1855)
    * Update insta360-studio from 3.5.0,2020_20200806_222513:20200807.eea8769d2706b6acdcb86c3c26a161e5 to 3.5.4,2020_20201029_064913:20201029.749ae151b284b24ee59feeced39256b6
    
    * Update insta360-studio.rb
    
    * Update insta360-studio.rb
    
    * Update insta360-studio.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/insta360-studio.rb
+++ Casks/insta360-studio.rb
@@ -1,14 +1,13 @@
 cask "insta360-studio" do
-  version "3.5.0,2020_20200806_222513:20200807.eea8769d2706b6acdcb86c3c26a161e5"
-  sha256 "3ef2a47200254e08d159dd584649685dbf6a50e73105cbbe13abc48e27982017"
+  version "3.5.4,2020_20201029_064913:20201029.749ae151b284b24ee59feeced39256b6"
+  sha256 "b17676b7de5ba30630f0467c13c2bb32e9d74542a5531f8a69302d979e240e24"
 
-  url "https://res.insta360.com/static/assets/storage/#{version.after_colon.major}/#{version.after_colon.minor}/Insta360_Studio_#{version.after_comma.before_colon}_signed.pkg.zip"
+  url "https://res.insta360.com/static/assets/storage/#{version.after_colon.major}/#{version.after_colon.minor}/Insta360_Studio_#{version.after_comma.before_colon}_signed.pkg"
   name "Insta360 Studio"
+  desc "Video and photo editor"
   homepage "https://www.insta360.com/"
 
-  container type: :zip
-
-  pkg "Insta360 Studio #{version.after_comma.before_colon}_signed.pkg"
+  pkg "Insta360_Studio_#{version.after_comma.before_colon}_signed.pkg"
 
   uninstall quit:    "com.insta360.studio",
             pkgutil: [
