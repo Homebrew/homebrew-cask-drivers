8f63fcce5b3aa5f434931f0bf0b1f9fc735268ef
CoreCode <core-code@users.noreply.github.com>
2020-11-01 17:29:19 +0100

    Update loupedeck.rb

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -1,6 +1,6 @@
 cask "loupedeck" do
   version "4.1.1"
-  sha256 "75122fb6258bbcd49284a7e71bae24efa035854cfcc4a91f033a5be78f65591c"
+  sha256 "2c78d44f51cf8173fad8eb5de62b147bc54eb211dc41e6c58df5289ebbe15178"
 
   # loupedeck-software-release.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://loupedeck-software-release.s3.amazonaws.com/Software+version+#{version}/MacOS/Loupedeck-MacOS-v+#{version}.dmg"
