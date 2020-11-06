aa0e84db23beae88cc038537c71c40d850e36ac9
Randall <ran.dall@icloud.com>
2020-11-06 11:29:12 -0800

    Update drobo-dashboard from 3.5.0,113780 to 3.5.1,115249

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -1,6 +1,6 @@
 cask "drobo-dashboard" do
-  version "3.5.0,113780"
-  sha256 "2ba60ae2b1f9c382977fdc26e0424241637c519e9aa2f2aeaab2183be82099c0"
+  version "3.5.1,115249"
+  sha256 "86b87e27bb1926b3b456986ee11d8ee0d8b39a6f82fd8c01dd4e5224fbfd6998"
 
   url "https://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version.before_comma}.dmg"
   appcast "https://www.drobo.com/docs/start-drobo/"
