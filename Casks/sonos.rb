30cf5d966126658d9ca03da3f8f584c3bcd6842c
Brigitte Zmoelnig <brigitte@corecode.io>
2020-11-12 19:28:02 +0100

    Update sonos from 12.1 to 12.2

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -2,7 +2,7 @@ cask "sonos" do
   version "12.2"
   sha256 "342864b703d8f2d6c3e0601394273ab87cf9e9dcec1a77e6e78ce2e13339e182"
 
-  url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
+  url "https://update-software.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac2",
           must_contain: version.no_dots
   name "Sonos"
