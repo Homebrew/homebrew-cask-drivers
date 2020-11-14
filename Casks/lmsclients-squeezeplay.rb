58e95d2dd9f40dc81ca39745177d910d0e43970f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-14 23:17:58 +0000

    Update lmsclients-squeezeplay from 7.8.0r1227 to 7.8.0r1286 (#1889)

--- Casks/lmsclients-squeezeplay.rb
+++ Casks/lmsclients-squeezeplay.rb
@@ -1,6 +1,6 @@
 cask "lmsclients-squeezeplay" do
-  version "7.8.0r1227"
-  sha256 "2802555f092a616d1843e5812abb052d4d5ef8cb918657bf3867bd86ff5c654c"
+  version "7.8.0r1286"
+  sha256 "f0dd0590013d4472fcbd1fc745581616120957cd0f9136c6483b0b7347f61d66"
 
   # downloads.sourceforge.net/lmsclients/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/lmsclients/SqueezePlay-x86_64-#{version}.dmg"
