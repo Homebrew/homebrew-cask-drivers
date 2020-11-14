4326f6880c440d03b227fc7ce6bfcb6875ea5064
Miccal Matthews <miccal.matthews@gmail.com>
2020-11-14 11:23:56 +0800

    Update asix-ax88179.rb

--- Casks/asix-ax88179.rb
+++ Casks/asix-ax88179.rb
@@ -1,5 +1,5 @@
 cask "asix-ax88179" do
-  version "2.19.0_beta2,1.8.0"
+  version "2.19.0,1.8.0"
   sha256 "23dca0c54f2779c135dc250a937314ad0b26252c2dbfdd2eaa36bf284ba09a72"
 
   url "https://www.asix.com.tw/en/support/download/file/119"
@@ -7,7 +7,7 @@ cask "asix-ax88179" do
   desc "USB 3.0 to gigabit ethernet drivers for ASIX Electronics devices"
   homepage "https://www.asix.com.tw/en/support/download"
 
-  container nested: "AX88179_178A_macOS_10.9_to_10.15_Driver_Installer_v#{version.before_comma}/AX88179_178A_v#{version.before_comma}.dmg"
+  container nested: "AX88179_178A_macOS_10.9_to_11_Driver_Installer_v#{version.before_comma}_beta2/AX88179_178A_v#{version.before_comma}.dmg"
 
   installer manual: "AX88179_178A_v#{version.before_comma}.app"
 
