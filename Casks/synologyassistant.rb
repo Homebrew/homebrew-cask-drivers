9657fe03ad22a9eb37d837896987512cfc2e545a
suschizu <brigitte@corecode.io>
2020-11-16 09:02:45 +0100

    Update synologyassistant.rb

--- Casks/synologyassistant.rb
+++ Casks/synologyassistant.rb
@@ -3,7 +3,7 @@ cask "synologyassistant" do
   sha256 "b586a41cf390586bdc29ab4d6ee7abbca2e435c74426b194997f35553f1ef7ae"
 
   url "https://global.download.synology.com/download/Tools/Assistant/#{version}/Mac/synology-assistant-#{version}.dmg"
-  appcast "https://archive.synology.com/download/Tools/Assistant/"
+  appcast "https://www.synology.com/en-us/releaseNote/Assistant"
   name "Synology Assistant"
   homepage "https://www.synology.com/"
 
