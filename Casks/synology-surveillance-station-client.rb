765dcd95476390ba20d62ffb264f201a8f95e5d1
suschizu <brigitte@corecode.io>
2020-11-16 13:23:01 +0100

    Update appcast in synology-surveillance-station-client.rb (#1893)
    * Update synology-surveillance-station-client.rb
    
    * Update synology-surveillance-station-client.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/synology-surveillance-station-client.rb
+++ Casks/synology-surveillance-station-client.rb
@@ -3,8 +3,9 @@ cask "synology-surveillance-station-client" do
   sha256 "cda1fb645c34d84b56c821caa70c69de159218a9c3bd12c60934cf8adbf976f1"
 
   url "https://global.download.synology.com/download/Tools/SurveillanceStationClient/#{version}/Mac/Synology%20Surveillance%20Station%20Client-#{version}.dmg"
-  appcast "https://archive.synology.com/download/Tools/SurveillanceStationClient/"
+  appcast "https://www.synology.com/en-us/releaseNote/SurveillanceStationClient"
   name "Synology Surveillance Station Client"
+  desc "Desktop utility to access Surveillance Station on Synology products"
   homepage "https://www.synology.com/surveillance/"
 
   pkg "Install Synology Surveillance Station Client.pkg"
