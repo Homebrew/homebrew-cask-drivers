787bc98ca65b335de0d2736536ceb6e0f2adc27c
suschizu <brigitte@corecode.io>
2020-11-17 19:11:22 +0100

    Update brother-p-touch-update-software from 1.5.5,15:dlfp100785 to 1.5.6,110:dlfp100885 (#1898)

--- Casks/brother-p-touch-update-software.rb
+++ Casks/brother-p-touch-update-software.rb
@@ -1,6 +1,6 @@
 cask "brother-p-touch-update-software" do
-  version "1.5.5,15:dlfp100785"
-  sha256 "c6e1afdb1daf04c143532f9dab019a764a2f6e07567c1b42d61d34232b990c32"
+  version "1.5.6,110:dlfp100885"
+  sha256 "179cd72961d1f2b8fba1146a4b636ca1529e8fc8adecac0165111ac36bd85a7a"
 
   url "https://download.brother.com/welcome/#{version.after_colon}/pum#{version.before_comma.no_dots}x#{version.after_comma.before_colon}all.dmg"
   name "Brother P-touch Update Software"
