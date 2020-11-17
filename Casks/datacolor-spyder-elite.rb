12848ddbbb935a5a6cb597882811708c3a3ab8e1
suschizu <brigitte@corecode.io>
2020-11-17 03:25:37 +0100

    Update datacolor-spyder-elite from 5.3 to 5.7 (#1897)
    * Update datacolor-spyder-elite from 5.3 to 5.7
    
    * Update datacolor-spyder-elite.rb
    
    * Update datacolor-spyder-elite.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/datacolor-spyder-elite.rb
+++ Casks/datacolor-spyder-elite.rb
@@ -1,29 +1,31 @@
 cask "datacolor-spyder-elite" do
-  version "5.3"
-  sha256 "aac0c688f34380737af11da28b9c48164b15800606353cf80090ecd54d2d0ba3"
+  version "5.7"
+  sha256 "4474f20807ab97c3337a4d03526b7a348c387509fb8ff6cec97276269ecfc495"
 
-  # d3d9ci7ypuovlo.cloudfront.net/spyder was verified as official when first introduced to the cask
-  url "https://d3d9ci7ypuovlo.cloudfront.net/spyder#{version.major}/Spyder#{version.major}Elite_#{version}_OSX_Installer.zip"
+  # d3d9ci7ypuovlo.cloudfront.net/ was verified as official when first introduced to the cask
+  url "https://d3d9ci7ypuovlo.cloudfront.net/spyder#{version.major}/Spyder#{version.major}Elite_#{version}.pkg.zip"
   name "Spyder Elite"
+  desc "Monitor calibration tool"
   homepage "https://www.datacolor.com/photography-design/product-overview/spyder#{version.major}-family/#spyder#{version.major}elite"
 
   auto_updates true
 
-  installer manual: "Spyder#{version.major}Elite_#{version}_OSX_Installer.app"
+  pkg "Spyder#{version.major}Elite #{version}.pkg"
 
-  uninstall signal: [
+  uninstall signal:  [
     ["TERM", "com.datacolor.spyder#{version.major}elite"],
     ["TERM", "com.datacolor.spyder#{version.major}utility"],
   ],
-            delete: "/Applications/Datacolor/Spyder#{version.major}Elite",
-            rmdir:  "/Applications/Datacolor"
+            delete:  "/Applications/Datacolor/Spyder#{version.major}Elite",
+            pkgutil: "com.datacolor.pkg.spyder#{version.major}elite",
+            rmdir:   "/Applications/Datacolor"
 
   zap trash: [
-    "~/Library/Application Support/MindVision/Spyder5Elite_Installer.xtm",
-    "~/Library/Caches/com.datacolor.spyder5elite",
-    "~/Library/Preferences/Datacolor/Spyder5Elite",
+    "~/Library/Application Support/MindVision/Spyder#{version.major}Elite_Installer.xtm",
+    "~/Library/Caches/com.datacolor.spyder#{version.major}elite",
+    "~/Library/Preferences/Datacolor/Spyder#{version.major}Elite",
     "~/Library/Preferences/Datacolor/SpyderUtility",
-    "~/Library/Saved Application State/com.datacolor.spyder5elite.savedState",
+    "~/Library/Saved Application State/com.datacolor.spyder#{version.major}elite.savedState",
   ],
       rmdir: "~/Library/Preferences/Datacolor"
 end
