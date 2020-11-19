b4afe167711d75327d353ab6b197ab3bb08f1682
Tyler Gross <tgrosst@users.noreply.github.com>
2020-11-18 18:46:30 -0600

    Update jabra-direct from 4.0.6820 to 4.14.15119 (#1900)
    * Update jabra-direct from 4.0.6820 to 4.14.15119
    
    * Update jabra-direct.rb
    
    * Update jabra-direct.rb
    
    Co-authored-by: Tyler Gross <tgross@sbs-m168.local>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/jabra-direct.rb
+++ Casks/jabra-direct.rb
@@ -1,11 +1,12 @@
 cask "jabra-direct" do
-  version "4.0.6820"
-  sha256 "97e4a91c6bd40ae94d825868d7d27fcfdbd72cc0bd5d90b4db26709a08ed1845"
+  version "4.14.15119"
+  sha256 "f592ec4f7a963d67f5641e0ef778f0a69132c209af3212dbdb518e85f55c4183"
 
   # jabraxpressonlineprdstor.blob.core.windows.net/jdo/ was verified as official when first introduced to the cask
   url "https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.dmg"
   appcast "https://jabraexpressonlinejdo.jabra.com/jdo/jdo.json"
   name "Jabra Direct"
+  desc "Optimise and personalise your Jabra headset"
   homepage "https://www.jabra.com/software-and-services/jabra-direct"
 
   pkg "JabraDirectSetup.pkg"
@@ -18,9 +19,12 @@ cask "jabra-direct" do
     "com.jabra.softphoneService",
     "nl.superalloy.oss.terminal-notifier",
   ],
+            delete:     "/Applications/Jabra Direct.app",
             login_item: "Jabra Direct",
-            pkgutil:    "com.jabra.directonline",
-            delete:     "/Applications/Jabra Direct.app"
+            pkgutil:    [
+              "com.jabra.directonline",
+              "com.jabra.kext",
+            ]
 
   zap trash: [
     "~/Library/Application Support/Jabra",
