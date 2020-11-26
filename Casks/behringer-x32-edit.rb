a1934a9d6e3336f1c3b4c8eb78250563d595a5ae
Alec Sammon <alec.sammon@googlemail.com>
2020-11-26 22:51:58 +0000

    Fix hash and install of behringer-x32-edit (#1921)

--- Casks/behringer-x32-edit.rb
+++ Casks/behringer-x32-edit.rb
@@ -1,14 +1,15 @@
 cask "behringer-x32-edit" do
   # NOTE: "32" is not a version number, but an intrinsic part of the product name
   version "4.2"
-  sha256 "d94ff3b039f3732da7c8b2d930c9f75c97b0012ae4adaca99d456c842abde057"
+  sha256 "69297f08ff205016f4490f891a6f50b623d2a50752019459d7d91a4f809c1520"
 
   # mediadl.musictribe.com/download/software/behringer/ was verified as official when first introduced to the cask
   url "https://mediadl.musictribe.com/download/software/behringer/X32/X32-Edit_MAC_#{version}.zip"
   name "Behringer X32-Edit"
+  desc "Edit Behringer Digital Mixers via LAN/Wi-Fi"
   homepage "https://www.behringer.com/Categories/Behringer/Mixers/Digital/X32/p/P0ASF/"
 
-  app "X32-Edit.app"
+  app "X32-Edit_#{version}/X32-Edit.app"
 
   uninstall quit:   "com.music-group.X32-Edit",
             delete: "/Library/Logs/DiagnosticReports/X32-Edit_*.wakeups_resource.diag"
@@ -16,5 +17,6 @@ cask "behringer-x32-edit" do
   zap trash: [
     "~/Library/Application Support/CrashReporter/X32-Edit_*.plist",
     "~/Library/Saved Application State/com.music-group.X32-Edit.savedState",
+    "~/Library/.X32-Edit",
   ]
 end
