947a657334c49d648db9482dbfa473f2b65a6133
Randall <ran.dall@icloud.com>
2020-12-07 07:33:04 -0800

    Update nordic-nrf-connect from 3.6.0 to 3.6.1

--- Casks/nordic-nrf-connect.rb
+++ Casks/nordic-nrf-connect.rb
@@ -1,6 +1,6 @@
 cask "nordic-nrf-connect" do
-  version "3.6.0"
-  sha256 "3750c69d7f26c1aff7ef372a83a2be1159b8ab9e7896b49486ae83beacd31ace"
+  version "3.6.1"
+  sha256 "27589e37e9fa56a67301d645e1b05f57d347e78b90b9fa12d112d868546691cf"
 
   # github.com/NordicSemiconductor/pc-nrfconnect-core/ was verified as official when first introduced to the cask
   url "https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases/download/v#{version}/nrfconnect-#{version}.dmg"
