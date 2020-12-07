e8e8cb0a62daf5665fb4310346b6bb4b70f134fe
Leandro Facchinetti <git@leafac.com>
2020-12-07 03:02:32 +0000

    Add canon-eos-webcam-utility

---
+++ Casks/canon-eos-webcam-utility.rb
@@ -0,0 +1,15 @@
+cask "canon-eos-webcam-utility" do
+  version "1.0"
+  sha256 "38b232bd9e91064bb291b26e831cee088c6ede2c4e3b1bb18c08280b11108304"
+
+  url "https://downloads.canon.com/webcam/EOSWebcamUtility-MAC#{version}.pkg.zip"
+  name "Canon EOS Webcam Utility"
+  desc "Use a Canon camera as a webcam"
+  homepage "https://www.usa.canon.com/internet/portal/us/home/support/self-help-center/eos-webcam-utility/"
+
+  pkg "EOSWebcamUtility-MAC#{version}.pkg"
+
+  uninstall script: {
+    executable: "/Applications/EOS Webcam Utility/EOS Webcam Utility Uninstaller.app/Contents/MacOS/EOS Webcam Utility Uninstaller",
+  }
+end
