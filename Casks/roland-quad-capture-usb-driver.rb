3e57fc30549e221920ac31ac30dd37f13ff413b0
suschizu <brigitte@corecode.io>
2020-12-07 20:05:06 +0100

    Update roland-quad-capture-usb-driver.rb

--- Casks/roland-quad-capture-usb-driver.rb
+++ Casks/roland-quad-capture-usb-driver.rb
@@ -3,14 +3,20 @@ cask "roland-quad-capture-usb-driver" do
     version "1.5.2,10:24c21855-3c40-469c-a40c-7647a52bf022"
     sha256 "da575f4b3874e0042fddffb4462f4521019ceaa3f44942f64bce099b426cf2d1"
     url "https://static.roland.com/assets/media/tgz/quad_mx#{version.after_comma.before_colon}d#{version.before_comma.no_dots}.tgz"
+
+    pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver10#{version.after_comma.before_colon}.pkg"
   elsif MacOS.version <= :sierra
     version "1.5.3,12:199aee04-dd6b-4a9c-9945-ba9ffb295113"
     sha256 "712b27a25275d748e35c174b242f21a2967f5caca013f6cb09330b9232288770"
     url "https://static.roland.com/assets/media/tgz/quad_m#{version.after_comma.before_colon}d#{version.before_comma.no_dots}.tgz"
+
+    pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver10#{version.after_comma.before_colon}.pkg"
   elsif MacOS.version <= :mojave
     version "1.5.4,13:53d8a307-a8ae-4f9b-9a59-a1adb8c67012"
     sha256 "d3524d7844d24805c3a1c25c09dce62ab87d4c8dd6941a0b1d3653c696563117"
     url "https://static.roland.com/assets/media/tgz/quad_m#{version.after_comma.before_colon}d#{version.before_comma.no_dots}.tgz"
+
+    pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver10#{version.after_comma.before_colon}.pkg"
   else
     version "1.5.6,11"
     sha256 "0d77926e818e1da93ea2f980ef29d840015f085ec134a68a2501b1ccfd5ddfde"
@@ -23,7 +29,7 @@ cask "roland-quad-capture-usb-driver" do
 
   depends_on macos: ">= :yosemite"
 
-  pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver10#{version.after_comma.before_colon}.pkg"
+  pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver#{version.after_comma}.pkg"
 
   uninstall pkgutil:   "jp.co.roland.QuadCapture.*",
             launchctl: [
