80c32b3c6798d9f12d3b81e9ebc98f2b218ee12a
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-08 22:28:58 +0000

    'Update verified parameter – group 0'

--- Casks/canon-fax.rb
+++ Casks/canon-fax.rb
@@ -2,8 +2,8 @@ cask "canon-fax" do
   version "4.15.6"
   sha256 "14d274075bd9818525c2ceb72f64b412731a058f2832740f5dd1a33bb838da58"
 
-  # gdlp01.c-wss.com/gds/ was verified as official when first introduced to the cask
-  url "https://gdlp01.c-wss.com/gds/3/0100010683/04/mac-fax-v#{version.no_dots}-01.dmg"
+  url "https://gdlp01.c-wss.com/gds/3/0100010683/04/mac-fax-v#{version.no_dots}-01.dmg",
+      verified: "gdlp01.c-wss.com/gds/"
   name "Canon Fax Driver & Utilities"
   desc "Fax driver & utilities for Canon imageCLASS MF printers"
   homepage "https://www.usa.canon.com/internet/portal/us/home/support/drivers-downloads"
