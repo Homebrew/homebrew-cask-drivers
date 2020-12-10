47c4ff27412fb47ab74e287edc6774919a528e47
Markus Reiter <me@reitermark.us>
2020-12-10 10:15:14 +0100

    logitech-firmwareupdatetool: use `sha256 :no_check` for unversioned URL

--- Casks/logitech-firmwareupdatetool.rb
+++ Casks/logitech-firmwareupdatetool.rb
@@ -1,6 +1,6 @@
 cask "logitech-firmwareupdatetool" do
   version "2.0.47973"
-  sha256 "4dae89363248fd000800f0d1b51167dfcf064dd1ce5bc42a2e0beeab6a0668f2"
+  sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/keyboards/FirmwareUpdateToolInstaller.zip"
   appcast "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,websoftware=07d54a46-c89d-11e9-9981-4556732441db"
