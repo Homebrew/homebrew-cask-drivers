9c691e111fc66cc2af08f3be76f3aa32466cdcdc
Markus Reiter <me@reitermark.us>
2020-12-10 10:15:13 +0100

    akai-professional-lpd8-editor: use `sha256 :no_check` for unversioned URL

--- Casks/akai-professional-lpd8-editor.rb
+++ Casks/akai-professional-lpd8-editor.rb
@@ -1,6 +1,6 @@
 cask "akai-professional-lpd8-editor" do
   version "2.0.2"
-  sha256 "a94cd6dcac129f2c49f06b9fc48f6d7e94a2973c58022c4f654aabb0780c4edb"
+  sha256 :no_check
 
   url "https://www.akaipro.com/amfile/file/download/file/814/product/40/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.akaipro.com/amfile/file/download/file/814/product/40/"
