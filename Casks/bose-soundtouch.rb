7038d9860e0c002d8027902bb6a4dfee60459c01
suschizu <brigitte@corecode.io>
2020-12-10 19:45:42 +0100

    Update bose-soundtouch.rb

--- Casks/bose-soundtouch.rb
+++ Casks/bose-soundtouch.rb
@@ -1,9 +1,9 @@
 cask "bose-soundtouch" do
-  version "26.0.0-3251-ff6a93d,3_2020_a3b99494"
+  version "26.0.0-3251-ff6a93d,st3_2020_a3b99494"
   sha256 "be74aeaa1e73bd6b07d92ceb5fcff5b58c4023eb7ccdb034fea842080554aa4e"
 
-  url "https://downloads.bose.com/ced/soundtouch/st#{version.after_comma}/SoundTouch-app-installer-#{version.before_comma}.dmg"
-  appcast "https://downloads.bose.com/ced/soundtouch/soundtouch_controller_app/index.html"
+  url "https://downloads.bose.com/ced/soundtouch/#{version.after_comma}/SoundTouch-app-installer-#{version.before_comma}.dmg"
+  appcast "https://downloads.bose.com/ced/soundtouch/#{version.after_comma}/index.xml"
   name "Bose Soundtouch Controller App"
   desc "Control Bose SoundTouch systems from your computer"
   homepage "https://downloads.bose.com/ced/soundtouch/soundtouch_controller_app/index.html"
