a46437f37697d4b4816cc945f96e27f5fff7d551
Markus Reiter <me@reitermark.us>
2020-12-10 10:15:14 +0100

    lovense-remote: use `sha256 :no_check` for unversioned URL

--- Casks/lovense-remote.rb
+++ Casks/lovense-remote.rb
@@ -1,6 +1,6 @@
 cask "lovense-remote" do
   version "1.4.3"
-  sha256 "b87e24855e28080709fb640ca8c1313b0a077c17c3310b7b8a649cd9bfa67813"
+  sha256 :no_check
 
   url "https://lovense.com/files/apps/remote/remote.dmg"
   appcast "https://lovense.com/sextoys/download"
