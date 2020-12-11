647310b56c26fadaead3f43d1775fb704bf81920
suschizu <brigitte@corecode.io>
2020-12-11 21:14:31 +0100

    Update fujifilm-x-acquire from 1.16.0,r3z1pz9e to 1.18.0,f12zkf10 (#1973)

--- Casks/fujifilm-x-acquire.rb
+++ Casks/fujifilm-x-acquire.rb
@@ -1,6 +1,6 @@
 cask "fujifilm-x-acquire" do
-  version "1.16.0,r3z1pz9e"
-  sha256 "0ad0fdf27271ec4ba47f06eea00ad6dc970cf1ebbe245353c258e61475e5ac53"
+  version "1.18.0,f12zkf10"
+  sha256 "41d482cf8f0e9e096052caf8e4deca6ad5ca193093d17f5b06936623f1b74203"
 
   url "https://dl.fujifilm-x.com/support/software/x-acquire-mac#{version.before_comma.no_dots}-#{version.after_comma}/XAcquireIns#{version.before_comma.no_dots}.dmg"
   appcast "https://fujifilm-x.com/global/support/download/software/x-acquire/"
