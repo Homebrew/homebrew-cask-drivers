76858384a2d80b095e9ac59bed4633d795a2491f
Randall <ran.dall@icloud.com>
2020-12-17 10:25:26 -0800

    Update 3dconnexion from 10-6-6,r3234:360DF97D-ED08-4ccf-A55E-0BF905E58476 to 10-6-7,r3287:36E24890-6B5F-443a-8A9F-1851F9ADB985

--- Casks/3dconnexion.rb
+++ Casks/3dconnexion.rb
@@ -1,6 +1,6 @@
 cask "3dconnexion" do
-  version "10-6-6,r3234:360DF97D-ED08-4ccf-A55E-0BF905E58476"
-  sha256 "6c52a57a538ef10fda6611b186b13897aade6ff827ce3cb53ea16b6109942aec"
+  version "10-6-7,r3287:36E24890-6B5F-443a-8A9F-1851F9ADB985"
+  sha256 "4752bd4297733743fb512121116b536ffe260152f97134398d028b9936bc26f9"
 
   url "https://download.3dconnexion.com/drivers/mac/#{version.before_comma}_#{version.after_colon}/3DxWareMac_v#{version.before_comma}_#{version.after_comma.before_colon}.dmg"
   appcast "https://3dconnexion.com/us/drivers/"
