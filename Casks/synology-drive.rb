cc87300ffea8e87a69ac917c61580ddc48d0ec3b
suschizu <brigitte@corecode.io>
2020-12-18 11:10:00 +0100

    Update synology-drive from 2.0.2,11078 to 2.0.3,11102 (#1986)

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -1,8 +1,8 @@
 cask "synology-drive" do
-  version "2.0.2,11078"
-  sha256 "196865f411631103958d71e3dc0c640909e53ef5e3cb44013a22d66ae4f1ec85"
+  version "2.0.3,11102"
+  sha256 "02a9a2698639d761e1d82b49b6bdb86801e4fb129879d01e2e4f86403d012edf"
 
-  url "https://global.download.synology.com/download/Tools/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
+  url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
   appcast "https://www.synology.com/en-us/releaseNote/SynologyDriveClient"
   name "Synology Drive"
   desc "Sync and backup service to Synology NAS drives"
