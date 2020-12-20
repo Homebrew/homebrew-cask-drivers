894f2f01eb005eac426c187595555613635ba549
Yurii Kolesnykov <root@yurikoles.com>
2020-12-20 10:40:23 +0200

    sound-blaster-play3: fix case-sensitivity issue, add desc (#1987)
    * sound-blaster-play3: fix case-sensitivity issue, add desc
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    
    * Update sound-blaster-play3.rb
    
    * Update sound-blaster-play3.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/sound-blaster-play3.rb
+++ Casks/sound-blaster-play3.rb
@@ -5,16 +5,18 @@ cask "sound-blaster-play3" do
     sha256 "0550328b70fc214fcffc418fba3e5140c853e827bc92227a73864d62ba5e8415"
 
     url "https://files.creative.com/manualdn/Drivers/AVP/13795/0x3F213863/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg.zip"
+    pkg "Install.pkg"
   else
     version "2.01.02"
     sha256 "3dbbdc627f5ee72f87403dc471e6718c258594e0fb0b208d900a5f174ce02915"
 
     url "https://download.creative.com/manualdn/Drivers/100289/MqrOwTo9a7/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg"
+    pkg "INSTALL.pkg"
   end
-  name "Sound Blaster Play! 3 Control Panel"
-  homepage "https://us.creative.com/p/sound-cards/sound-blaster-play-3"
 
-  pkg "Install.pkg"
+  name "Sound Blaster PLAY! 3"
+  desc "Driver and control panel for the Sound Blaster PLAY! 3 USB DAC and Amp"
+  homepage "https://us.creative.com/p/sound-cards/sound-blaster-play-3"
 
   uninstall launchctl: "com.creative.trustudiopc",
 
