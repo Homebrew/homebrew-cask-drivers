86ffb24786deb4e2ab783cba48a187ccc1a9e110
Doug Goldstein <cardoe@cardoe.com>
2020-12-21 15:15:32 -0600

    Update canon-mf-printer from 10.11.4 to 10.11.6 (#1990)

--- Casks/canon-mf-printer.rb
+++ Casks/canon-mf-printer.rb
@@ -1,8 +1,8 @@
 cask "canon-mf-printer" do
-  version "10.11.4"
-  sha256 "2d8919b41ab9a9d5d51d86b8dea277e9dd12e2e3e8aeb98b89e670bf65c14509"
+  version "10.11.6"
+  sha256 "cded9cbfc4ad45f03258066fc96b3a7c2ee0f4e486043813b9242e3fd568c078"
 
-  url "https://gdlp01.c-wss.com/gds/3/0100010873/01/mac-mf-v#{version.no_dots}-00.dmg",
+  url "https://gdlp01.c-wss.com/gds/3/0100010873/03/mac-mf-v#{version.no_dots}-00.dmg",
       verified: "gdlp01.c-wss.com/gds/"
   name "Canon MF Printer Driver & Utilities"
   desc "Printer driver & utilities for Canon imageCLASS MF printers"
