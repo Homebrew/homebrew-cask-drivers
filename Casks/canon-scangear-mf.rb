e06866e0f4fcb9aeb5003af6f4a88320a94e197a
Steve Huff <shuff@vecna.org>
2021-01-04 18:32:57 -0500

    Update canon-scangear-mf from 2.15.4 to 2.15.6

--- Casks/canon-scangear-mf.rb
+++ Casks/canon-scangear-mf.rb
@@ -1,9 +1,8 @@
 cask "canon-scangear-mf" do
-  version "2.15.4"
-  sha256 "caee9018c9e7a41f5f45ff04a547eb4d6154e7c6a065779c6e680a4d75e17d62"
+  version "2.15.6"
+  sha256 "5efcaeccd8b1f1103d2bb966199a47fc50f0116996d987bd51178d61474c2151"
 
-  url "https://gdlp01.c-wss.com/gds/8/0100010618/04/mac-scan-v#{version.no_dots}-00.dmg",
-      verified: "gdlp01.c-wss.com/gds/"
+  url "https://gdlp01.c-wss.com/gds/8/0100010618/06/mac-scan-v#{version.no_dots}-00.dmg"
   name "Canon Scanner Driver & Utilities"
   desc "Scanner driver & utilities for Canon imageCLASS MF printers"
   homepage "https://www.usa.canon.com/internet/portal/us/home/support/drivers-downloads"
