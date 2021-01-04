ffa9f0267708dfe071755369c5a471f6f03220a2
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-05 06:53:38 +0800

    Update elgato-thunderbolt-dock.rb

--- Casks/elgato-thunderbolt-dock.rb
+++ Casks/elgato-thunderbolt-dock.rb
@@ -1,15 +1,16 @@
 cask "elgato-thunderbolt-dock" do
-  version "1.2.10,131"
-  sha256 :no_check
+  version "1.2.10.131_40"
+  sha256 "e76aff1d404451fa87d42c566475e83a09a15318cf33ad9219ed3792d5a7a098"
 
-  url "https://update.elgato.com/mac/thunderbolt-dock-update/download.php"
+  url "https://edge.elgato.com/thunderbolt-dock/Elgato_Thunderbolt_Dock_Software_#{version}.pkg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://update.elgato.com/mac/thunderbolt-dock-update/download.php"
   name "Elgato Thunderbolt Dock"
   desc "Menu bar utility for Elgato Thunderbolt docks"
   homepage "https://www.elgato.com/en/dock/thunderbolt-3"
 
   depends_on macos: ">= :el_capitan"
-  container type: :pkg
+
+  pkg "Elgato_Thunderbolt_Dock_Software_#{version}.pkg"
 
   uninstall pkgutil:    [
     "com.elgato.Elgato-Thunderbolt-Dock-Utility",
