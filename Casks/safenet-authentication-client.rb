a41265c1625c718a387e14ddaf134cfcc6c64ea9
Seeker <meaningseeking@protonmail.com>
2021-01-28 17:13:39 -0800

    safenet-authentication-client: delete redundant `verified`

--- Casks/safenet-authentication-client.rb
+++ Casks/safenet-authentication-client.rb
@@ -2,8 +2,7 @@ cask "safenet-authentication-client" do
   version "10.2.109.0"
   sha256 "d0c316379c03e17a35a53e81df641b85b38b5cf7b7a8f6b5f11789a0819f943a"
 
-  url "https://www.globalsign.com/en/safenet-drivers/USB/#{version.major_minor}/Safenet_#{version.major_minor}_Post_GA_R3.zip",
-      verified: "www.globalsign.com/en/safenet-drivers/"
+  url "https://www.globalsign.com/en/safenet-drivers/USB/#{version.major_minor}/Safenet_#{version.major_minor}_Post_GA_R3.zip"
   name "SafeNet Authentication Client"
   desc "Client for smart card readers and security tokens"
   homepage "https://support.globalsign.com/ssl/ssl-certificates-installation/safenet-drivers"
