eeab1cce53260e695a0b3298c02d9f8fe0cc1c69
sgulls <abaoagjoojin@outlook.com>
2021-02-04 18:58:16 -0500

    Add zap stanza

--- Casks/razer-macos-unofficial.rb
+++ Casks/razer-macos-unofficial.rb
@@ -3,9 +3,15 @@ cask "razer-macos-unofficial" do
   sha256 "258bab06048b5b7b9dca76ed4ba75b9a1c65b9c9b3fe07dd1a777db0b634491f"
 
   url "https://github.com/1kc/razer-macos/releases/download/v#{version}/Razer.macOS-#{version}.dmg"
+  appcast "https://github.com/1kc/razer-macos/releases.atom"  
   name "Razer macOS"
-  desc "Open source color effects manager for Razer devices on macOS"
+  desc "Open source color effects manager for Razer devices"
   homepage "https://github.com/1kc/razer-macos"
 
-  app "Razer macOS.app"
+  app "Razer macOS.app"i
+  zap trash: [
+               '~/Library/Preferences/com.electron.razer-macos.helper.Renderer.plist',
+               '~/Library/Preferences/com.electron.razer-macos.plist',
+               '~/Library/Saved Application State/com.electron.razer-macos.savedState'
+             ]
 end
