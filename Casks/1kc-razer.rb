9958262d1fab82293a1e38645b9239c1958ecf56
sgulls <47063912+sgulls@users.noreply.github.com>
2021-02-05 18:52:51 -0500

    Add Razer macOS (#2032)
    * add Razer macOS
    
    * renamed razer-macos to razer-macos-unofficial
    
    * Add zap stanza
    
    * Update razer-macos-unofficial.rb
    
    * Update razer-macos-unofficial.rb
    
    * Update and rename razer-macos-unofficial.rb to 1kc-razer.rb
    
    * Update 1kc-razer.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/1kc-razer.rb
@@ -0,0 +1,17 @@
+cask "1kc-razer" do
+  version "0.3.4"
+  sha256 "258bab06048b5b7b9dca76ed4ba75b9a1c65b9c9b3fe07dd1a777db0b634491f"
+
+  url "https://github.com/1kc/razer-macos/releases/download/v#{version}/Razer.macOS-#{version}.dmg"
+  name "Razer macOS"
+  desc "Open source color effects manager for Razer devices"
+  homepage "https://github.com/1kc/razer-macos"
+
+  app "Razer macOS.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.electron.razer-macos.helper.Renderer.plist",
+    "~/Library/Preferences/com.electron.razer-macos.plist",
+    "~/Library/Saved Application State/com.electron.razer-macos.savedState",
+  ]
+end
