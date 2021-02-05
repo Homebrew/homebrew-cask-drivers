405230fbb11f7a5162b0339dd2dbc77c5dda3fcb
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-06 07:54:11 +0800

    Update saleae-logic.rb

--- Casks/saleae-logic.rb
+++ Casks/saleae-logic.rb
@@ -2,13 +2,13 @@ cask "saleae-logic" do
   version "2.3.18"
   sha256 "d30691a301b2574ca01fdea72da6051200135c4e499cedbac40207fda5e50755"
 
-  url "https://downloads.saleae.com/logic2/Logic-macOS-#{version}-master.zip"
+  url "https://downloads.saleae.com/logic#{version.major}/Logic-macOS-#{version}-master.zip"
   appcast "https://ideas.saleae.com/f/changelog/"
   name "Saleae Logic2"
   desc "Signal analysis software for Saleae's  devices"
   homepage "https://www.saleae.com/"
 
-  app "Logic2.app"
+  app "Logic#{version.major}.app"
 
   zap trash: [
     "~/Library/Saved Application State/com.saleae.saleae.savedState",
