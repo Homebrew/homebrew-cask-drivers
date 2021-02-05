212a76bce54d137c945d55c276ff104e393ab4e3
Vítor Galvão <info@vitorgalvao.com>
2021-02-05 04:38:59 +0000

    Update and rename razer-macos-unofficial.rb to 1kc-razer.rb

--- Casks/1kc-razer.rb
+++ Casks/1kc-razer.rb
@@ -1,12 +1,16 @@
-cask "razer-macos-unofficial" do
+cask "1kc-razer" do
   version "0.3.4"
   sha256 "258bab06048b5b7b9dca76ed4ba75b9a1c65b9c9b3fe07dd1a777db0b634491f"
 
   url "https://github.com/1kc/razer-macos/releases/download/v#{version}/Razer.macOS-#{version}.dmg"
-  appcast "https://github.com/1kc/razer-macos/releases.atom"
   name "Razer macOS"
   desc "Open source color effects manager for Razer devices"
   homepage "https://github.com/1kc/razer-macos"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Razer macOS.app"
 
