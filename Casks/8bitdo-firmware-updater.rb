3b1f5172bb510f91114f1c1c485e4a3139454d4c
Loïc PÉRON <loic.peron@gmail.com>
2021-02-14 13:17:26 +0100

    Removed appcast from 8bitdo-firmware-updater as required by brew audit --cask

--- Casks/8bitdo-firmware-updater.rb
+++ Casks/8bitdo-firmware-updater.rb
@@ -3,7 +3,6 @@ cask "8bitdo-firmware-updater" do
   sha256 "181acf95fb028f3df2770812298eef2efe80965f74f635baf02ce0e8a5cf6fc2"
 
   url "http://tools.8bitdo.com/8BitdoFirmwareUpdater/8BitDoFirmwareUpdaterV#{version}.zip"
-  appcast "http://tools.8bitdo.com/8BitdoFirmwareUpdater/appcast.xml"
   name "8BitDo Firmware Updater"
   homepage "https://support.8bitdo.com/firmware-updater.html"
 
