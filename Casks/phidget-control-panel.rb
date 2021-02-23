2b4913501eb32f9711952e97084e8ee44899564f
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-23 12:27:25 +0800

    Update phidget-control-panel.rb

--- Casks/phidget-control-panel.rb
+++ Casks/phidget-control-panel.rb
@@ -1,37 +1,30 @@
 cask "phidget-control-panel" do
   version "22"
+  sha256 "9cf21d515e96413c05ac9b630a2e2cec42c30fbf798c3b54a040f4542a6a4d3b"
 
-  if MacOS.version <= :snow_leopard # 10.5 -- 10.6
-    target = "_OS_X_10_5"
-    sha256 ""
-  elsif MacOS.version <= :yosemite # 10.7 -- 10.10
-    target = "_OS_X_10_7"
-    sha256 "2381e59c226ff1ed691462d6ab8ca87cd58ebd2ee82ac9b06b814a769dbcae3d"
-  else
-    target = ""
-    sha256 "9cf21d515e96413c05ac9b630a2e2cec42c30fbf798c3b54a040f4542a6a4d3b"
-  end
-
-  url "https://www.phidgets.com/downloads/phidget#{version}/libraries/macos/Phidget#{version}#{target}.dmg"
+  url "https://www.phidgets.com/downloads/phidget#{version}/libraries/macos/Phidget#{version}.dmg"
   name "Phidget Control Panel"
   desc "Libraries and control software for the Phidgets sensing kits"
   homepage "https://www.phidgets.com/"
 
+  depends_on macos: ">= :el_capitan"
+
   pkg "Phidgets.pkg"
 
   uninstall pkgutil: [
-    "com.phidgets.phidget22java",
-    "com.phidgets.phidget22extra",
-    "com.phidgets.phidget22networkserver",
-    "com.phidgets.controlpanel22",
-    "com.phidgets.phidgetframework22",
+    "com.phidgets.phidget#{version}java",
+    "com.phidgets.phidget#{version}extra",
+    "com.phidgets.phidget#{version}networkserver",
+    "com.phidgets.controlpanel#{version}",
+    "com.phidgets.phidgetframework#{version}",
     "com.phidgets.signeddriver",
-  ], kext: [
-    "com.phidgets.driver.Phidget",
-  ]
+  ],
+            kext:    [
+              "com.phidgets.driver.Phidget",
+            ]
 
-  # A reboot _seems_ to be needed for the driver to be fully functional (device
-  # detected, but sensors unreadable otherwise)
+  # A reboot _seems_ to be needed for the driver to be fully functional
+  # (device detected, but sensors unreadable otherwise)
   caveats do
     reboot
   end
