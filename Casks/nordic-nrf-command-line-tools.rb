eb0af0d7a04a96258e9a05f60aa41f46d1a4aa87
Leo Soares <leojrfs@gmail.com>
2021-02-25 00:06:37 +0000

    Update nordic-nrf-command-line-tools from 10.8.0 to 10.12.1 (#2058)

--- Casks/nordic-nrf-command-line-tools.rb
+++ Casks/nordic-nrf-command-line-tools.rb
@@ -1,6 +1,6 @@
 cask "nordic-nrf-command-line-tools" do
-  version "10.8.0"
-  sha256 "0c6af1ce68829d1ccc7b600219b255605a0c7ca704dabc92f74a191aa16af0e4"
+  version "10.12.1"
+  sha256 "16b4f40beee75ec8d7a6b45b5c1b831a7602dc397791a7439282290d37b2a357"
 
   url "https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX.tar"
   name "nRF Command Line Tools"
