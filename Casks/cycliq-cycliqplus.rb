7bd0477180db090027ecc52ea11af4d02a5c0680
Diego <jahndiego@mailbox.org>
2021-03-01 01:32:28 +0100

    Add CycliqPlus.app v2.53 (#2060)
    * Add CycliqPlus.app v2.53
    
    * Update cycliq-cycliqplus.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/cycliq-cycliqplus.rb
@@ -0,0 +1,16 @@
+cask "cycliq-cycliqplus" do
+  version "2.53"
+  sha256 "76424d10b3e31640267c27a563c630431ad43e20e47b1baf5aebdd9dcb107f43"
+
+  url "https://cycliq.com/files/software/CycliqPlus-#{version}.dmg"
+  name "CycliqPlus"
+  desc "Footage and bike camera settings editor"
+  homepage "https://cycliq.com/"
+
+  app "CycliqPlus.app"
+
+  zap trash: [
+    "~/Library/Caches/com.cycliq.mac",
+    "~/Library/WebKit/com.cycliq.mac",
+  ]
+end
