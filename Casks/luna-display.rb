db832734b0dfb6662f4af059bd24766be983a26c
Fabian <31616408+EinJochel@users.noreply.github.com>
2021-03-03 18:24:34 +0100

    Push luna-display to version 4.4.0
    Also added a description and more items to zap

--- Casks/luna-display.rb
+++ Casks/luna-display.rb
@@ -1,11 +1,12 @@
 cask "luna-display" do
-  version "4.3.0"
-  sha256 "5c483aa7b9104f527de425b38ec2c3a786ff8f21cdbbd917c4f0f47f135e61c1"
+  version "4.4.0"
+  sha256 "ff3b201e1a0c4be8c67c59a82df666ba99ef0f8b85bb4e70ee1e64d2693f2c25"
 
   url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaDisplay-#{version}.dmg",
-      verified: "s3.amazonaws.com/s3.lunadisplay.com/"
+      verified: "s3.amazonaws.com/s3.lunadisplay.com/downloads/"
   appcast "https://s3.lunadisplay.com/downloads/sparkle.xml"
   name "Luna Display"
+  desc "Use your iPad as a wireless second display"
   homepage "https://lunadisplay.com/"
 
   depends_on macos: ">= :el_capitan"
@@ -17,6 +18,11 @@ cask "luna-display" do
   zap trash: [
     "~/Library/Caches/com.astro-hq.LunaDisplayMac",
     "~/Library/Preferences/com.astro-hq.LunaDisplayMac.plist",
+    "~/Library/Preferences/com.astro-hq.AstroLauncher.shared.plist",
+    "~/Library/Application Support/com.astro-hq.LunaDisplayMac",
+    "~/Library/Application Support/com.astro-hq.LunaDeviceSetupService",
+    "~/Library/Application Support/LunaDisplay",
     "~/Library/Saved Application State/com.astro-hq.LunaDisplayMac.savedState",
+    "~/Library/Logs/Astropad",
   ]
 end
