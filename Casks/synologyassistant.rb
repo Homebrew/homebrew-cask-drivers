edd1c69df6bc726032b62582c2d426fcfc38938d
Loïc PÉRON <loic.peron@gmail.com>
2021-03-04 12:15:40 +0100

    Add livecheck to synology-assistant

--- Casks/synologyassistant.rb
+++ Casks/synologyassistant.rb
@@ -3,10 +3,14 @@ cask "synologyassistant" do
   sha256 "1c7c913b056b855ed07bce5e6dafc23b1ea154b1a7fc06ec800affc1cc235ff5"
 
   url "https://global.download.synology.com/download/Utility/Assistant/#{version}/Mac/synology-assistant-#{version}.dmg"
-  appcast "https://www.synology.com/en-us/releaseNote/Assistant"
   name "Synology Assistant"
   desc "Tool to manage Synology NAS's across a LAN"
   homepage "https://www.synology.com/"
+
+  livecheck do
+    url "https://www.synology.com/en-us/releaseNote/Assistant"
+    regex(/Version: (\d+.\d+-\d+)/i)
+  end
 
   app "SynologyAssistant.app"
 
