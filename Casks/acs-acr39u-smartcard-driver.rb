b9486ff7c75fd59e89a0bc905464e1ca31c8021b
gsmv4 <64798417+gsmv4@users.noreply.github.com>
2021-03-05 11:49:02 +0100

    Update acs-acr39u-smartcard-driver.rb
    changed version to 1.1.8.2

--- Casks/acs-acr39u-smartcard-driver.rb
+++ Casks/acs-acr39u-smartcard-driver.rb
@@ -1,6 +1,6 @@
 cask "acs-acr39u-smartcard-driver" do
-  version "1.1.8"
-  sha256 "52653d4e06cf22c9d750632b237fa42abbf2ae7ea452102150d72b00e7d0329b"
+  version "1.1.8.2"
+  sha256 "f1234d75178d892a133a410355a5a990cf75d2f33eba25d575943d4df632f3a4"
 
   url "https://www.acs.com.hk/download-driver-unified/11878/ACS-Unified-INST-MacOSX-#{version.no_dots}-P.zip"
   appcast "https://www.acs.com.hk/en/driver/302/acr39u-smart-card-reader/"
