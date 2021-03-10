5ab86d66a58e384d99d5aad0e0122a656a68dd0f
Jason Tedor <jason@tedor.me>
2021-03-10 13:45:54 -0500

    Bump Sonos to 13.0 (#2076)

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,6 +1,6 @@
 cask "sonos" do
-  version "12.2.2"
-  sha256 "c4eeba983d7f0fa6637b14a40abe40908b7db9e3bb4ef9d7cdb99cd1f4ec769d"
+  version "13.0"
+  sha256 "f4a0112cbd5b8340ca25e6349daa8000690dd80f182702bdc399787e0e861101"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac2",
