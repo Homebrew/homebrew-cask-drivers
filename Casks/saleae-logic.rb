2e9e061c8cc94d6fc37c5e980ecd61cb54d39ff2
Alexandre Rey <alx.rey@icloud.com>
2021-03-13 16:39:17 +0100

    Update saleae-logic from 2.3.19 to 2.3.20

--- Casks/saleae-logic.rb
+++ Casks/saleae-logic.rb
@@ -1,6 +1,6 @@
 cask "saleae-logic" do
-  version "2.3.19"
-  sha256 "4521d2d7fe05f1ee3dc8d169b039db1abf5cc62a12b314ad73509ee24db71db5"
+  version "2.3.20"
+  sha256 "18f24a0fe748d9922a08f50d50a868a380b91ce9f4af511c9c73bb50b8cf4845"
 
   url "https://downloads.saleae.com/logic#{version.major}/Logic-macOS-#{version}-master.zip"
   appcast "https://ideas.saleae.com/f/changelog/"
