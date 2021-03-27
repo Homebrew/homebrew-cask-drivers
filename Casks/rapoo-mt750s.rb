2b62f7896c7785180db0f76886cd9051059e12f4
Rui Coelho <rui.coelho@miniclip.com>
2021-03-27 13:50:38 +0000

    Add rapoo mt750s driver

---
+++ Casks/rapoo-mt750s.rb
@@ -0,0 +1,20 @@
+cask "rapoo-mt750s" do
+  version "20200817"
+  sha256 "98ef8dae8c824752fceb48b3bae9b88b9d19369bce317c34046e14d97e5c43ce"
+
+  url "http://rpd.rapoo.cn/download/RapooOptions_MT750S_#{version}.pkg",
+      verified: "rpd.rapoo.cn/"
+  name "Rapoo MT750S Drivers"
+  desc "Multi-mode Wireless Laser Mouse Drivers"
+  homepage "https://www.rapoo-eu.com/support/mice-support/mt750s-support/"
+
+  pkg "RapooOptions_MT750S_#{version}.pkg"
+
+  uninstall pkgutil: [
+    "com.mygreatcompany.pkg.MT750S",
+    "com.mygreatcompany.pkg.MT750SUninstall",
+    "com.mygreatcompany.pkg.MT750SMgr",
+    "com.mygreatcompany.pkg.RapooOptionsDeamon",
+  ],
+            quit:    "com.mygreatcompany.pkg.RapooOptionsDeamon"
+end
