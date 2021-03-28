fe691ea4e9a85130b607ec28202a8d1decb503d9
zsoltion <zsoltion@users.noreply.github.com>
2021-03-28 23:38:06 +0200

    Update elgato-video-capture from 2.0.3.8714_1821 to 2.0.5.1830
    Update elgato-video-capture from 2.0.3.8714_1821 to 2.0.5.1830

--- Casks/elgato-video-capture.rb
+++ Casks/elgato-video-capture.rb
@@ -1,6 +1,6 @@
 cask "elgato-video-capture" do
-  version "2.0.3.8714_1821"
-  sha256 "ef5fd243d140ccd5dca8e3d571c18fb7ca1480a5713720af9cd826ae87ca3603"
+  version "2.0.5.1830"
+  sha256 "ef294607a2b620907cbafb61970953d1c4dbce2d7e93b51413618b196afe74ee"
 
   url "https://edge.elgato.com/video-capture/macos/#{version.major_minor_patch}/final/Video_Capture_#{version}.zip"
   appcast "https://www.elgato.com/sites/default/files/downloads.json"
