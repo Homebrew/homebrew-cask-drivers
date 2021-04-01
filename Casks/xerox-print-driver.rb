23364841dc4eef68e203e3fc4d9702b21d3cdc4a
Stephen DiIorio <11286278+StephenDiIorio@users.noreply.github.com>
2021-04-01 00:31:07 -0400

    Upgrade Xerox Printer Drivers to v5.5.9(2180) and changed http to https

--- Casks/xerox-print-driver.rb
+++ Casks/xerox-print-driver.rb
@@ -2,22 +2,23 @@ cask "xerox-print-driver" do
   if MacOS.version <= :yosemite
     version "3.123.0_1865"
     sha256 "ac9c013705742538c0faa5df2194e3a7d4fb9980dd0570e41b213ff87172ee6c"
-    url "http://download.support.xerox.com/pub/drivers/CQ8570/drivers/macosx1010/ar/XeroxPrintDriver_#{version}.dmg"
+    url "https://download.support.xerox.com/pub/drivers/CQ8570/drivers/macosx1010/ar/XeroxPrintDriver_#{version}.dmg"
   elsif MacOS.version <= :el_capitan
     version "4.17.1_1980"
     sha256 "36b1ddf1f598ceaf6f91d38b0d228be3f8f6188c251761424cbea7a869488883"
-    url "http://download.support.xerox.com/pub/drivers/CQ8570/drivers/macosx1011/pt_BR/XeroxPrintDriver_#{version}.dmg"
+    url "https://download.support.xerox.com/pub/drivers/CQ8570/drivers/macosx1011/pt_BR/XeroxPrintDriver_#{version}.dmg"
   elsif MacOS.version <= :sierra
     version "5.2.0_2115"
     sha256 "9989b6c127fca8c97b24bd86fd4d20035cd094c69e3fd41f6a243361f86483ec"
-    url "http://download.support.xerox.com/pub/drivers/CQ8570/drivers/macos1012/pt_BR/XeroxPrintDriver_#{version}.dmg"
+    url "https://download.support.xerox.com/pub/drivers/CQ8570/drivers/macos1012/pt_BR/XeroxPrintDriver_#{version}.dmg"
   else
-    version "5.4.2_2138"
-    sha256 "8b69dd93c07f8fe61a4e15bff49d3f1e5a41a669e84cd6b6f199187fa50b9eac"
-    url "http://download.support.xerox.com/pub/drivers/BALTORO_HF/drivers/macOS10_13/pt_BR/XeroxDrivers_#{version}.dmg"
+    version "5.5.9_2180"
+    sha256 "24e700fedf39391d9df3310d34dcf98cfe4b6e46acfead09903c8befd10b1345"
+    url "https://download.support.xerox.com/pub/drivers/BALTORO_HF/drivers/macOS10_13/pt_BR/XeroxDrivers_#{version}.dmg"
   end
 
   name "Xerox Print Driver"
+  desc "Drivers for Xerox printers"
   homepage "https://www.support.xerox.com/support/colorqube-8570/downloads/"
 
   if MacOS.version <= :sierra
