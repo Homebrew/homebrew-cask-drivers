6ff13284bcb968fefd0f3ce48f8050e200c8c93e
zsoltion <zsoltion@users.noreply.github.com>
2021-04-03 01:19:32 +0200

    Update yubico-yubikey-manager from 1.2.0 to 1.2.1 (#2101)

--- Casks/yubico-yubikey-manager.rb
+++ Casks/yubico-yubikey-manager.rb
@@ -1,6 +1,6 @@
 cask "yubico-yubikey-manager" do
-  version "1.2.0"
-  sha256 "64415156f04dfdc356a22c88dfad44e053bf36a431d9d47f3c7fe49eab2b95da"
+  version "1.2.1"
+  sha256 "d794f87c6e9bada9cff1dba2f7e7109496830ac7dcf290d6fb91022d3d579848"
 
   url "https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-#{version}-mac.pkg"
   appcast "https://developers.yubico.com/yubikey-manager-qt/Releases/"
