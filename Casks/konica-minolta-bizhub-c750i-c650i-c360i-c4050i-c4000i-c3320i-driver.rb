0a4e27239dd140bebcd86c622c83e2be3eab40fd
Markus Reiter <me@reitermark.us>
2021-04-03 04:44:43 +0200

    Update konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver from 2.2.3,7e6620c13efd50326af685f926806e0f:125070 to 2.2.3A,f2ecbc809ef24949980f4af4f0b66fc9:127009

--- Casks/konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver.rb
+++ Casks/konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver.rb
@@ -1,6 +1,6 @@
 cask "konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver" do
-  version "2.2.3,7e6620c13efd50326af685f926806e0f:125070"
-  sha256 "dca184d4122ef1a3af821f64731110d632be813fd2df1e875dd170dad89188b0"
+  version "2.2.3A,f2ecbc809ef24949980f4af4f0b66fc9:127009"
+  sha256 "636d2bc1443c25fa5b081b1624a9c27163172c88e4fd03fade980e9ae7f2b5d5"
 
   url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=#{version.after_comma.before_colon}&tx_kmanacondaimport_downloadproxy[documentId]=#{version.after_colon}&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
   name "Konica Minolta Bizhub C750i/C650i/C360i/C4050i/C4000i/C3320i Printer Driver"
