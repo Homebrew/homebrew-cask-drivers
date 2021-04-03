eeec02090b1f650ff6ebecd5c8a67263f021cc70
ZepDonald <marcel-github@von-au.net>
2021-04-03 16:16:56 +0200

    Update philips-hue-sync from 1.5.3.3 to 1.6.0.9

--- Casks/philips-hue-sync.rb
+++ Casks/philips-hue-sync.rb
@@ -1,5 +1,5 @@
 cask "philips-hue-sync" do
-  version "1.5.3.3"
+  version "1.6.0.9"
   sha256 :no_check
 
   url "https://flex1548-esd.flexnetoperations.com/flexnet/operations/WebContent?fileID=HueSyncMac",
