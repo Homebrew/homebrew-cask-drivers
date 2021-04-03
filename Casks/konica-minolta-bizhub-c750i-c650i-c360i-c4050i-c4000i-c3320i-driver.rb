3aecb8447cafb90f13a5c9e470a6608af6b1d421
Markus Reiter <me@reitermark.us>
2021-04-03 20:49:09 +0200

    Update konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver.rb

--- Casks/konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver.rb
+++ Casks/konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver.rb
@@ -1,5 +1,5 @@
 cask "konica-minolta-bizhub-c750i-c650i-c360i-c4050i-c4000i-c3320i-driver" do
-  version "2.2.3A,f2ecbc809ef24949980f4af4f0b66fc9:127009"
+  version "2.2.3A,d87425ee468e6d834887936aca5b096c:127009"
   sha256 "636d2bc1443c25fa5b081b1624a9c27163172c88e4fd03fade980e9ae7f2b5d5"
 
   url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=#{version.after_comma.before_colon}&tx_kmanacondaimport_downloadproxy[documentId]=#{version.after_colon}&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
