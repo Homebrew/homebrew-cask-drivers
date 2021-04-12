00efc50440fb21fc3fa81bfeb2b13f7e7fefa445
Talha Khan <talha@averred.net>
2021-04-12 12:27:18 +0800

    Update insta360-studio to 3.6.4 and add livecheck
    Update insta360-studio from 3.6.2,2021_20210310_020734:20210310.ccdb347864e767b6d9ebb50fd2c4e059 to 3.6.4,2021_20210410_171056:20210412.faaa98a3bfa1d724f7ceab610c1aea6b

--- Casks/insta360-studio.rb
+++ Casks/insta360-studio.rb
@@ -1,6 +1,6 @@
 cask "insta360-studio" do
-  version "3.6.2,2021_20210310_020734:20210310.ccdb347864e767b6d9ebb50fd2c4e059"
-  sha256 "1b173dbc63ae40840a8d6403f525103c8808d214e8ca387d4a8d7d769547f01a"
+  version "3.6.4,2021_20210410_171056:20210412.faaa98a3bfa1d724f7ceab610c1aea6b"
+  sha256 "416ffd5108c3c4a4c7f754807ee43a86884eae021f38148349df56a42332f1d8"
 
   url "https://res.insta360.com/static/assets/storage/#{version.after_colon.major}/#{version.after_colon.minor}/Insta360_Studio_#{version.after_comma.before_colon}_signed.pkg"
   name "Insta360 Studio"
@@ -8,7 +8,15 @@ cask "insta360-studio" do
   homepage "https://www.insta360.com/"
 
   livecheck do
-    skip "No version information available"
+    url "https://openapi.insta360.com/website/appDownload/getGroupApp?group=insta360-go2&X-Language=en-us"
+    strategy :page_match do |page|
+      apps = JSON.parse(page)["data"]["apps"].find { |app| app["id"] == 38 }
+      macos = apps["items"].find { |item| item["platform"] == "mac" }
+      v1 = macos["version"]
+      pattern = %r{.+/(\d+)/([[:xdigit:]]+)/Insta360[._-]Studio[._-](\d+[._-]\d+[._-]\d+)[._-]signed\.pkg}i
+      match = macos["channels"][0]["download_url"].match(pattern)
+      "#{v1},#{match[3]}:#{match[1]}.#{match[2]}"
+    end
   end
 
   pkg "Insta360_Studio_#{version.after_comma.before_colon}_signed.pkg"
