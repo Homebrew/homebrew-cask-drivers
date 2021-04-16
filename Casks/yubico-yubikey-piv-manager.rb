a94cde1372b302c963cd435e50c0294329e720d7
Michael Cho <cho-m@tuta.io>
2021-04-15 18:24:04 -0700

    yubico-yubikey-piv-manager: add livecheck

--- Casks/yubico-yubikey-piv-manager.rb
+++ Casks/yubico-yubikey-piv-manager.rb
@@ -3,9 +3,15 @@ cask "yubico-yubikey-piv-manager" do
   sha256 "579d1c92a9de556c829c05995a7f495c7f7d320fe7d9287d278688f8c04e8ce3"
 
   url "https://developers.yubico.com/yubikey-piv-manager/Releases/yubikey-piv-manager-#{version}-mac.pkg"
-  appcast "https://developers.yubico.com/yubikey-piv-manager/Release_Notes.html"
   name "YubiKey PIV Manager"
+  desc "Application for configuring a PIV-enabled YubiKey"
   homepage "https://developers.yubico.com/yubikey-piv-manager/"
+
+  livecheck do
+    url "https://developers.yubico.com/yubikey-piv-manager/Releases/"
+    strategy :page_match
+    regex(/href=.*?yubikey-piv-manager-(\d+(?:\.\d+)*[a-z]?)-mac\.pkg/i)
+  end
 
   pkg "yubikey-piv-manager-#{version}-mac.pkg"
 
