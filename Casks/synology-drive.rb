3cf3810d4e6a85669f7c279cbe1956f08857afef
Michael Cho <cho-m@tuta.io>
2021-04-17 02:36:18 -0700

    synology-drive: add livecheck (#2123)

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -3,10 +3,17 @@ cask "synology-drive" do
   sha256 "19b8f03ece1dc8443da1e6df469d2ddf8916c4d2d15d7c9a04c0e84deb93c33b"
 
   url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
-  appcast "https://www.synology.com/en-us/releaseNote/SynologyDriveClient"
   name "Synology Drive"
   desc "Sync and backup service to Synology NAS drives"
   homepage "https://www.synology.com/"
+
+  livecheck do
+    url "https://www.synology.com/en-us/releaseNote/SynologyDriveClient"
+    strategy :page_match do |page|
+      match = page.match(/Version:\s*(\d+(?:\.\d+)*)-(\d+)/i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   auto_updates true
 
