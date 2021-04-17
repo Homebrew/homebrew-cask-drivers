ba13bb26522e95044cb178ff7a6be021482c14a1
Michael Cho <cho-m@tuta.io>
2021-04-17 12:39:40 -0700

    elektron-overbridge: add livecheck

--- Casks/elektron-overbridge.rb
+++ Casks/elektron-overbridge.rb
@@ -4,9 +4,17 @@ cask "elektron-overbridge" do
 
   url "https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/#{version.after_comma}/Elektron_Overbridge_#{version.before_comma}.dmg",
       verified: "s3-eu-west-1.amazonaws.com/"
-  appcast "https://www.elektron.se/support/?connection=overbridge#resources"
   name "Overbridge"
+  desc "Integrate Elektron hardware into music software"
   homepage "https://www.elektron.se/overbridge/"
+
+  livecheck do
+    url "https://www.elektron.se/support/?connection=overbridge"
+    strategy :page_match do |page|
+      match = page.match(%r{href=.*?/([\da-f]+(?:-[\da-f]+)*)/Elektron_Overbridge_(\d+(?:\.\d+)*)\.dmg}i)
+      "#{match[2]},#{match[1]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
