b8dfef2b92c644a64a01314cec2e555a256f35e1
Michael Cho <cho-m@tuta.io>
2021-04-18 16:42:00 -0700

    obinskit: add livecheck (#2143)

--- Casks/obinskit.rb
+++ Casks/obinskit.rb
@@ -3,9 +3,14 @@ cask "obinskit" do
   sha256 "5204f9d567fbddc2778c5e7af65f0fedc1661734bca111689d8af130e134ba81"
 
   url "http://releases.obins.net/occ/darwin/x64/ObinsKit_#{version}_x64.dmg"
-  appcast "http://releases.obins.net/occ/darwin/x64/latest-mac.yml"
   name "ObinsKit"
+  desc "Software for Anne Pro and Anne Pro 2"
   homepage "http://en.obins.net/obinskit"
+
+  livecheck do
+    url "http://releases.obins.net/occ/darwin/x64/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "ObinsKit.app"
 
