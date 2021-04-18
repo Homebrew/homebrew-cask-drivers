f2b3ec5afd5d4dabe5b3316dd2b4565b42780ab1
Michael Cho <cho-m@tuta.io>
2021-04-17 19:50:55 -0700

    elecom-mouse-assistant: update from 5.2.1.002 to 5.2.2.002 and add livecheck (#2136)

--- Casks/elecom-mouse-assistant.rb
+++ Casks/elecom-mouse-assistant.rb
@@ -1,12 +1,19 @@
 cask "elecom-mouse-assistant" do
-  version "5.2.1.002"
-  sha256 "4105ba47f41fc874929e1aeb10f9a123fe24809e1502c24b08d2a801e0498b2c"
+  version "5.2.2.002"
+  sha256 "eb0e6679efc3a71ea617b2e610a06bf535eada8816ff7e274f70f66e22b65341"
 
   url "https://dl.elecom.co.jp/support/download/peripheral/mouse/assistant/mac/ELECOM_Mouse_Installer_#{version}.zip"
-  appcast "https://www.elecom.co.jp/global/download-list/utility/mouse_assistant/mac/"
   name "ELECOM Mouse Assistant"
   desc "Software to more effectively use an ELECOM mouse"
   homepage "https://www.elecom.co.jp/global/download-list/utility/mouse_assistant/"
+
+  livecheck do
+    url "https://www.elecom.co.jp/global/download-list/utility/mouse_assistant/mac/"
+    strategy :page_match
+    regex(/ELECOM_Mouse_Installer_(\d+(?:\.\d+)*)\.zip/i)
+  end
+
+  depends_on macos: ">= :el_capitan"
 
   pkg "ELECOM_Mouse_Installer_#{version}.pkg"
 
