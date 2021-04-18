2ca325b60d9aad5226fc8f21906edf795aafdc89
Michael Cho <cho-m@tuta.io>
2021-04-17 19:18:50 -0700

    concept2-utility: add livecheck and set min OS 10.13

--- Casks/concept2-utility.rb
+++ Casks/concept2-utility.rb
@@ -1,5 +1,5 @@
 cask "concept2-utility" do
-  if MacOS.version <= :el_capitan
+  if MacOS.version <= :sierra
     version "7.09.02"
     sha256 "e4ebee8cde57c7ef63c3903285c3fc0ee8f87221e7c5529b9dcf97b3f9ebb57e"
   else
@@ -8,11 +8,15 @@ cask "concept2-utility" do
   end
 
   url "https://software.concept2.com/utility/Concept2Utility#{version.no_dots}.dmg"
-  appcast "https://www.concept2.com/service/software/concept2-utility",
-          must_contain: version.no_dots
   name "Concept2 Utility"
   desc "Utilities for the Concept2 Performance Monitor"
   homepage "https://www.concept2.com/service/software/concept2-utility"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/Concept2\s+Utility\s+(\d+(?:\.\d+)*)/i)
+  end
 
   depends_on macos: ">= :yosemite"
 
