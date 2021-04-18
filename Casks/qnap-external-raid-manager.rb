79c6dadeee27aaa1cff23e29b6a0bc68e7609597
Michael Cho <cho-m@tuta.io>
2021-04-17 17:14:13 -0700

    Update qnap-external-raid-manager from 1.3.0.1127 to 1.3.3.0317 and add livecheck

--- Casks/qnap-external-raid-manager.rb
+++ Casks/qnap-external-raid-manager.rb
@@ -1,10 +1,17 @@
 cask "qnap-external-raid-manager" do
-  version "1.3.0.1127"
-  sha256 "b0d542eda74f8c8ae31bdc8db8205e015b7947c5262458114ebee1dbef24c711"
+  version "1.3.3.0317"
+  sha256 "2281b1df0dcea8ac2d53c5b367bbd9b3ff5c9efbf708e58e213dae2368080c7a"
 
   url "https://download.qnap.com/Storage/Utility/QNAPExternalRAIDManagerMac-#{version}.dmg"
   name "QNAP External Raid Manager"
+  desc "Set up, monitor, and view the status of your QNAP external RAID devices"
   homepage "https://www.qnap.com/en/utilities/essentials"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/QNAPExternalRAIDManagerMac-(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   depends_on macos: ">= :high_sierra"
 
