181f163a48d02999c6dfe2b4a5bc50d3ad2a0d66
Michael Cho <cho-m@tuta.io>
2021-04-17 19:06:53 -0700

    clavia-nord-sound-manager: add livecheck

--- Casks/clavia-nord-sound-manager.rb
+++ Casks/clavia-nord-sound-manager.rb
@@ -3,9 +3,15 @@ cask "clavia-nord-sound-manager" do
   sha256 "6a58ef7ddbc5f5c40d278bf91b63331e5ee435e6faa2ec3dfb4eff22d9bc3061"
 
   url "https://www.nordkeyboards.com/sites/default/files/files/downloads/software/nord-sound-manager/Nord%20Sound%20Manager%20v#{version}.dmg"
-  appcast "https://www.nordkeyboards.com/software-tools/nord-sound-manager/update-history"
   name "Clavia Nord Sound Manager"
+  desc "Organize and backup the memory of your Nord instrument"
   homepage "https://www.nordkeyboards.com/downloads/software-tools/nord-sound-manager"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/Nord%20Sound%20Manager%20v(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   app "Nord Sound Manager v#{version}.app"
 
