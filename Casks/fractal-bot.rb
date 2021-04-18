6d7f30b4c11303600978a720084b089f030c9a0c
Michael Cho <cho-m@tuta.io>
2021-04-17 20:50:32 -0700

    Update fractal-bot from 3.00.06 to 3.00.08 and add livecheck

--- Casks/fractal-bot.rb
+++ Casks/fractal-bot.rb
@@ -1,13 +1,17 @@
 cask "fractal-bot" do
-  version "3.00.06"
-  sha256 "32f8ad0cd6f733b176046b1256a5ccf9ce44f1fb7eea25fcb6e159882cee74ab"
+  version "3.00.08"
+  sha256 "ee70ce6afd76264e6802d564196cd7c519ff470574d9d33da3aa4ac1a9ffb33e"
 
   url "https://www.fractalaudio.com/downloads/fractal-bot/Fractal-Bot-OSX-v#{version.tr(".", "p")}.dmg"
-  appcast "https://www.fractalaudio.com/fractal-bot/",
-          must_contain: "#{version.tr(".", "p")}.dmg"
   name "Fractal Bot"
   desc "Send and receive data to and from your Fractal Audio Systems products"
   homepage "https://www.fractalaudio.com/fractal-bot/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/Version\s*(\d+(?:\.\d+)*).*?\.dmg/i)
+  end
 
   app "Fractal-Bot.app"
 
