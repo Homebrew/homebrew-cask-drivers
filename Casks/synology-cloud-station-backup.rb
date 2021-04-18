384ee5bd1c13ed05cf42f297c8d7d8791d5f7064
Michael Cho <cho-m@tuta.io>
2021-04-17 19:51:11 -0700

    synology-cloud-station-backup: add livecheck (#2149)

--- Casks/synology-cloud-station-backup.rb
+++ Casks/synology-cloud-station-backup.rb
@@ -3,10 +3,17 @@ cask "synology-cloud-station-backup" do
   sha256 "1759be61a09cacd976969f71b0c0f12f34684976de6ad98323c512aec8f3399d"
 
   url "https://global.download.synology.com/download/Tools/CloudStationBackup/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-cloud-station-backup-#{version.after_comma}.dmg"
-  appcast "https://www.synology.com/en-us/dsm/packages/CloudStation"
   name "Synology Cloud Station Backup"
   desc "Back up files to a centralized Synology NAS"
   homepage "https://www.synology.com/"
+
+  livecheck do
+    url "https://www.synology.com/en-us/releaseNote/CloudStationBackup"
+    strategy :page_match do |page|
+      match = page.match(/Version:\s*(\d+(?:\.\d+)*)-(\d+)/i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   auto_updates true
 
