0a5fa82b2f1b470b740ecde714e21f0da3a1dd29
Michael Cho <cho-m@tuta.io>
2021-04-17 23:28:18 -0700

    saleae-logic: add livecheck

--- Casks/saleae-logic.rb
+++ Casks/saleae-logic.rb
@@ -3,10 +3,16 @@ cask "saleae-logic" do
   sha256 "4014c00aded81af96cca966e5818bf766993445b4770e17548bad87e314d45a2"
 
   url "https://downloads.saleae.com/logic#{version.major}/Logic-macOS-#{version}-master.zip"
-  appcast "https://ideas.saleae.com/f/changelog/"
   name "Saleae Logic2"
   desc "Signal analysis for Saleae's devices"
   homepage "https://www.saleae.com/"
+
+  livecheck do
+    url "https://logic2api.saleae.com/download?os=osx"
+    strategy :header_match
+  end
+
+  depends_on macos: ">= :mojave"
 
   app "Logic#{version.major}.app"
 
