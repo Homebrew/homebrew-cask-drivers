9811fbe1197f91e195396a5b24b8c87d6a76f198
Michael Cho <cho-m@tuta.io>
2021-04-25 16:49:51 -0700

    wd-security: add livecheck (#2179)

--- Casks/wd-security.rb
+++ Casks/wd-security.rb
@@ -3,9 +3,17 @@ cask "wd-security" do
   sha256 "d365a95e9dcad28b037fb1dbc79b3182a140f280f0d4a68c4dd00c5254e53af6"
 
   url "https://downloads.wdc.com/wdapp/WD_Security_Standalone_Installer_Mac_#{version.dots_to_underscores}.zip"
-  appcast "https://support.wdc.com/downloads.aspx?p=158"
   name "WD Security"
+  desc "Encryption software for WD drives"
   homepage "https://support.wdc.com/downloads.aspx?p=158"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      v = page[%r{href=.*?/WD_Security_Standalone_Installer_Mac_(\d+(?:_\d+)*)\.zip}i, 1]
+      v.tr("_", ".")
+    end
+  end
 
   installer manual: "WD Security Installer.app"
 
