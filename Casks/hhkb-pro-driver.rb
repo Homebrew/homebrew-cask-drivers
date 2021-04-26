9bb709416ad7b6c3bdef361a6949a3e327a369c9
Michael Cho <cho-m@tuta.io>
2021-04-25 23:45:04 -0700

    Update hhkb-pro-driver from 5.0.1 to 5.1 and add livecheck

--- Casks/hhkb-pro-driver.rb
+++ Casks/hhkb-pro-driver.rb
@@ -1,11 +1,19 @@
 cask "hhkb-pro-driver" do
-  version "5.0.1"
-  sha256 "908a09868935ea67b527f8909c61b53b1e3e5b084cc8ac2cdc2db81436d7717f"
+  version "5.1"
+  sha256 "08518539c02e6dae7ee93f4188be9665e8c2b1e33f0fea681fa6cfc4dffc383b"
 
   url "https://www.pfu.fujitsu.com/hhkeyboard/downloads/HHKBProDriver#{version}.dmg"
   name "PFU Happy Hacking Keyboard Professional Driver"
   desc "Driver for HHKB Professional"
   homepage "https://www.pfu.fujitsu.com/hhkeyboard/macdownload.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{href=.*?/HHKBProDriver(\d+(?:\.\d+)*)\.dmg}i)
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "HHKB Pro Driver.app"
 end
