62a676463bd4cbef4f70e292e78afa0a306ed96f
Michael Cho <cho-m@tuta.io>
2021-04-27 14:43:21 -0700

    focusrite-control: add livecheck (#2182)

--- Casks/focusrite-control.rb
+++ Casks/focusrite-control.rb
@@ -3,10 +3,15 @@ cask "focusrite-control" do
   sha256 "0fed5039967b432bdfec2751c3bb92cf018a3271c1c99d0a0e555a154b1bec21"
 
   url "https://fael-downloads-prod.focusrite.com/customer/prod/s3fs-public/downloads/Focusrite%20Control%20-%20#{version}.dmg"
-  appcast "https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software",
-          must_contain: version.major_minor_patch
   name "Focusrite Control"
-  homepage "https://customer.focusrite.com/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software"
+  desc "Control your Focusrite interface"
+  homepage "https://focusrite.com/en/focusrite-control"
+
+  livecheck do
+    url "https://customer.focusrite.com/en/support/downloads?brand=Focusrite&product_by_type=1360&download_type=software"
+    strategy :page_match
+    regex(%r{href=.*?/Focusrite%20Control%20-%20(\d+(?:\.\d+)*)\.dmg}i)
+  end
 
   pkg "Focusrite Control.pkg"
 
