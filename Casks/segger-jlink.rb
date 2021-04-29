6585661d3efc0eb684146dabb215fa8d781790b1
Shahar Hadas <shaharhd@gmail.com>
2021-04-29 04:01:09 +0300

    Update segger-jlink.rb
    Fixed sha256

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -2,7 +2,7 @@ cask "segger-jlink" do
   version "7.20"
 
   if Hardware::CPU.intel?
-    sha256 "2d64bf590320c193a644ab6b15e6f673b44d7798ca178d47f6f78df88849c425"
+    sha256 "53e270e9de769a9a158e43de8d1dc2f9c5d21487ecb9a95ea2061e639717e8b7"
 
     url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}_x86_64.pkg",
         using: :post,
@@ -14,7 +14,7 @@ cask "segger-jlink" do
 
     pkg "JLink_MacOSX_V#{version.no_dots}_x86_64.pkg"
   else
-    sha256 "c3f077119e4260821f7fa99587ac1b01e1caf9c12526863a334d844e0a7672d2"
+    sha256 "a3bf9bfe49adfac6dc859c7310e504b10fa5755f58bf788497867ded40fbf142"
 
     url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}_arm64.pkg",
         using: :post,
