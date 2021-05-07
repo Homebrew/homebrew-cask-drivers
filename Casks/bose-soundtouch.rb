e652a95a46576c21392ea248b0b99c8f59d9d8ce
Sam Ford <1584702+samford@users.noreply.github.com>
2021-05-07 16:45:00 -0400

    bose-soundtouch: add livecheck (#2196)
    * bose-soundtouch: add livecheck
    
    * Update bose-soundtouch.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/bose-soundtouch.rb
+++ Casks/bose-soundtouch.rb
@@ -3,10 +3,25 @@ cask "bose-soundtouch" do
   sha256 "be74aeaa1e73bd6b07d92ceb5fcff5b58c4023eb7ccdb034fea842080554aa4e"
 
   url "https://downloads.bose.com/ced/soundtouch/#{version.after_comma}/SoundTouch-app-installer-#{version.before_comma}.dmg"
-  appcast "https://downloads.bose.com/ced/soundtouch/#{version.after_comma}/index.xml"
   name "Bose Soundtouch Controller App"
   desc "Control Bose SoundTouch systems from your computer"
   homepage "https://downloads.bose.com/ced/soundtouch/soundtouch_controller_app/index.html"
+
+  # The first-party download page uses JavaScript to render the content. The
+  # URL below redirects to the XML file containing release information that's
+  # used as the data source for the download page.
+  livecheck do
+    url "https://worldwide.bose.com/updates/soundtouch"
+    regex(%r{
+      <RELEASE [^>]*?URLPATH=["']/ced/soundtouch/([^/]+)/?["'][^>]*?>\s*
+      <IMAGE [^>]*?FILENAME=["']SoundTouch-app-installer-(\d+(?:\.\d+)+(?:-[a-z\d]+)+)\.dmg["']
+    }ix)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   auto_updates true
 
@@ -23,7 +38,10 @@ cask "bose-soundtouch" do
   },
             signal:    ["TERM", "com.Bose.SoundTouch"],
             quit:      "io.qt.SoundTouchHelper",
-            launchctl: "com.Bose.SoundTouch"
+            launchctl: [
+              "com.Bose.SoundTouch",
+              "application.com.Bose.SoundTouch",
+            ]
 
   zap trash: [
     "~/Library/Application Support/SoundTouch",
