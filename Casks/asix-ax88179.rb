81317dfcaa13da10df3496051d050d7389cf3878
Eric Park <me@ericswpark.com>
2021-05-20 10:49:46 +0900

    asix-ax88179: implement requested changes

--- Casks/asix-ax88179.rb
+++ Casks/asix-ax88179.rb
@@ -1,8 +1,14 @@
 cask "asix-ax88179" do
   if MacOS.version <= :catalina
     version "2.19.0,1109"
+    container nested: "AX88179_178A_macOS_10.9_to_10.15_Driver_Installer_v#{version.before_comma}/AX88179_178A_v#{version.before_comma}.dmg"
+    installer manual: "AX88179_178A_v#{version.before_comma}.app"
+    uninst_pkg "AX88179_178A_Uninstall_v*.pkg"
   else
     version "1.2.0,1126"
+    container nested: "ASIX_USB_Device_Installer_macOS_11.0_above_Driver_v#{version.before_comma}/ASIX_USB_Device_Installer_v#{version.before_comma}.dmg"
+    installer manual: "ASIX_USB_Device_Installer_v#{version.before_comma}.pkg"
+    uninst_pkg "ASIX_USB_Device_Uninstaller_v*.pkg"
   end
   sha256 :no_check
 
@@ -23,20 +29,8 @@ cask "asix-ax88179" do
     end
   end
 
-  if MacOS.version <= :catalina
-    container nested: "AX88179_178A_macOS_10.9_to_10.15_Driver_Installer_v#{version.before_comma}/AX88179_178A_v#{version.before_comma}.dmg"
-    installer manual: "AX88179_178A_v#{version.before_comma}.app"
-  else
-    container nested: "ASIX_USB_Device_Installer_macOS_11.0_above_Driver_v#{version.before_comma}/ASIX_USB_Device_Installer_v#{version.before_comma}.dmg"
-    installer manual: "ASIX_USB_Device_Installer_v#{version.before_comma}.pkg"
-  end
-
   uninstall_preflight do
-    if MacOS.version <= :catalina
-      staged_path.glob("AX88179_178A_Uninstall_v*.pkg").first.rename(staged_path/"AX88179_178A_Uninstall.pkg")
-    else
-      staged_path.glob("ASIX_USB_Device_Uninstaller_v*.pkg").first.rename(staged_path/"AX88179_178A_Uninstall.pkg")
-    end
+    staged_path.glob(uninst_pkg).first.rename(staged_path/"AX88179_178A_Uninstall.pkg")
 
     system_command "/usr/sbin/installer",
                    args: [
