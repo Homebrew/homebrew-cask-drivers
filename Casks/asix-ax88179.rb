910e71f5f494e24886533821b39a0c096f9362a5
Eric Park <me@ericswpark.com>
2021-05-25 00:25:20 +0900

    Update asix-ax88179 with macOS 11.0 driver (#2231)
    * Update asix-ax88179 with macOS 11.0 driver
    
    Closes #2226
    
    * asix-ax88179: implement requested changes
    
    * asix-ax88179: fix violations
    
    * asix-ax88179: set sha256 hash
    
    * Update asix-ax88179.rb
    
    * Update asix-ax88179.rb
    
    * Update asix-ax88179.rb
    
    * Update asix-ax88179.rb
    
    * Revert "asix-ax88179: set sha256 hash"
    
    This reverts commit a1e453c42c7b0c94f99e2b9fe07fd0d7dddd8f2e.
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/asix-ax88179.rb
+++ Casks/asix-ax88179.rb
@@ -1,5 +1,39 @@
 cask "asix-ax88179" do
-  version "2.19.0,1109"
+  if MacOS.version <= :catalina
+    version "2.19.0,1109"
+
+    container nested: "AX88179_178A_macOS_10.9_to_10.15_Driver_Installer_v#{version.before_comma}/AX88179_178A_v#{version.before_comma}.dmg"
+    installer manual: "AX88179_178A_v#{version.before_comma}.app"
+
+    livecheck do
+      url "https://www.asix.com.tw/en/support/download/step2/11/2/3"
+      strategy :page_match do |page|
+        page.split(/class=['"]?list__item['"]?/).map do |list_item|
+          match = list_item.match(
+            %r{data-href=.*?/download/file/(\d+).*?macOS\s*10.*?Vision\s*?(?:<br>)?\s*?(\d+(?:\.\d+)*)<}mi,
+          )
+          "#{match[2]},#{match[1]}" if match
+        end.compact
+      end
+    end
+  else
+    version "1.2.0,1126"
+
+    container nested: "ASIX_USB_Device_Installer_macOS_11.0_above_Driver_v#{version.before_comma}/ASIX_USB_Device_Installer_v#{version.before_comma}.dmg"
+    pkg "ASIX_USB_Device_Installer_v#{version.before_comma}.pkg"
+
+    livecheck do
+      url "https://www.asix.com.tw/en/support/download/step2/11/2/3"
+      strategy :page_match do |page|
+        page.split(/class=['"]?list__item['"]?/).map do |list_item|
+          match = list_item.match(
+            %r{data-href=.*?/download/file/(\d+).*?macOS\s*11.*?Vision\s*?(?:<br>)?\s*?(\d+(?:\.\d+)*)<}mi,
+          )
+          "#{match[2]},#{match[1]}" if match
+        end.compact
+      end
+    end
+  end
   sha256 :no_check
 
   url "https://www.asix.com.tw/en/support/download/file/#{version.after_comma}"
@@ -7,24 +41,12 @@ cask "asix-ax88179" do
   desc "USB 3.0 to gigabit ethernet drivers for ASIX Electronics devices"
   homepage "https://www.asix.com.tw/en/support/download"
 
-  livecheck do
-    url "https://www.asix.com.tw/en/support/download/step2/11/2/3"
-    strategy :page_match do |page|
-      page.split(/class=['"]?list__item['"]?/).map do |list_item|
-        match = list_item.match(
-          %r{data-href=.*?/download/file/(\d+).*?macOS.*?Vision\s*?(?:<br>)?\s*?(\d+(?:\.\d+)*)<}mi,
-        )
-        "#{match[2]},#{match[1]}" if match
-      end.compact
-    end
-  end
-
-  container nested: "AX88179_178A_macOS_10.9_to_10.15_Driver_Installer_v#{version.before_comma}/AX88179_178A_v#{version.before_comma}.dmg"
-
-  installer manual: "AX88179_178A_v#{version.before_comma}.app"
-
   uninstall_preflight do
-    staged_path.glob("AX88179_178A_Uninstall_v*.pkg").first.rename(staged_path/"AX88179_178A_Uninstall.pkg")
+    if MacOS.version <= :catalina
+      staged_path.glob("AX88179_178A_Uninstall_v*.pkg").first.rename(staged_path/"AX88179_178A_Uninstall.pkg")
+    else
+      staged_path.glob("ASIX_USB_Device_Uninstaller_v*.pkg").first.rename(staged_path/"AX88179_178A_Uninstall.pkg")
+    end
 
     system_command "/usr/sbin/installer",
                    args: [
