ea970e4ce6a2768351a7f9bfc5317fe25f147214
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-25 11:50:04 +0800

    Update xppen-pentablet.rb

--- Casks/xppen-pentablet.rb
+++ Casks/xppen-pentablet.rb
@@ -1,13 +1,16 @@
-# frozen_string_literal: true
-
 cask "xppen-pentablet" do
   version "3.1.1_210507"
-  sha256 "335c01a6483f259a45b330a2a6e545f77550682326bd82d65bfb0c5ea512e104"
+  sha256 :no_check
 
   url "https://www.xp-pen.com/download/file/id/1968"
   name "XP-Pen PenTablet"
   desc "Driver and configuration app for XP-Pen graphic tablets"
   homepage "https://www.xp-pen.com/"
+
+  livecheck do
+    url :url
+    strategy :header_match
+  end
 
   container nested: "XP-PENMac_#{version}/XP-PENMac_#{version}.dmg"
 
