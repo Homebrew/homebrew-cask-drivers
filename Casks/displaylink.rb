2629a34ed36714ad3a943c5f2e952fae4f984ad9
Lennart Lövstrand <lennart.git@lovstrand.com>
2021-06-02 22:35:39 +0200

    Update displaylink from 1.3.1,1713 to 1.4

--- Casks/displaylink.rb
+++ Casks/displaylink.rb
@@ -9,8 +9,8 @@ cask "displaylink" do
     version "5.2.5,1636"
     sha256 "aa061f65ffb613c5138b88051f56da12825cfe217fa6ae589f7d5125981f76b7"
   else
-    version "1.3.1,1713"
-    sha256 "d5df1c15e0c5aac8a15f7df85e4b4ffeb46f28e7cd9a768d57e300ac06b1c1cb"
+    version "1.4"
+    sha256 "88c7ffa126c4c2270792d3548df0f49211de97b3bb40d5da544aaca3b9f6ba0e"
   end
 
   url "https://www.displaylink.com/downloads/file?id=#{version.after_comma}",
