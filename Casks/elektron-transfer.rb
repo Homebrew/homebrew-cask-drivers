a4936f8968b5d059d7297d80aa95454cb0d8a676
Michael Cho <cho-m@tuta.io>
2021-06-04 11:06:25 -0700

    Update elektron-transfer.rb

--- Casks/elektron-transfer.rb
+++ Casks/elektron-transfer.rb
@@ -2,7 +2,7 @@ cask "elektron-transfer" do
   version "1.4.0.1"
   sha256 "2eb728e31f045e9f4c3c2fa3480b699379dedfae3c402d18b2b7dab2ef87a6b8"
 
-  url "https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/133f6ec3-a98c-5781-9910-39a0fce402e7/Elektron_Transfer_1.4.0.1.dmg"
+  url "https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/133f6ec3-a98c-5781-9910-39a0fce402e7/Elektron_Transfer_#{version}.dmg"
   name "Elektron Transfer"
   desc "Tool for transferring samples, presets and firmware updates"
   homepage "https://www.elektron.se/support/?connection=transfer#resources"
