165025e408bd7ecce4a9120eb3d894dd893340d9
Alexey Loginov <nvmker@gmail.com>
2021-06-05 22:26:29 +0300

    Update elektron-transfer from 1.2.2.9 to 1.4.0.1 (#2260)
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/elektron-transfer.rb
+++ Casks/elektron-transfer.rb
@@ -1,15 +1,20 @@
 cask "elektron-transfer" do
-  version "1.2.2.9"
-  sha256 "e8940345dc4212f6d77cac5b61c1dd0147ad3e9d1d7aeb21e6c344ee0b8de3e5"
+  version "1.4.0.1,133f6ec3-a98c-5781-9910-39a0fce402e7"
+  sha256 "2eb728e31f045e9f4c3c2fa3480b699379dedfae3c402d18b2b7dab2ef87a6b8"
 
-  url "https://www.elektron.se/wp-content/uploads/2018/03/Elektron_Transfer_#{version}_macOS.zip"
+  url "https://se-elektron-devops.s3.amazonaws.com/release/#{version.after_comma}/Elektron_Transfer_#{version.before_comma}.dmg",
+      verified: "se-elektron-devops.s3.amazonaws.com/release/"
   name "Elektron Transfer"
   desc "Tool for transferring samples, presets and firmware updates"
   homepage "https://www.elektron.se/support/?connection=transfer#resources"
 
   livecheck do
-    url "https://www.elektron.se/support/?connection=transfer"
-    regex(%r{href=.*?/Elektron[._-]?Transfer[._-]?v?(\d+(?:\.\d+)+)[._-]?macOS\.zip}i)
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(%r{href=.*?/(\h+(?:-\h+)*)/Elektron[._-]?Transfer[._-]?v?(\d+(?:\.\d+)+)\.dmg}i).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
   end
 
   depends_on macos: ">= :el_capitan"
