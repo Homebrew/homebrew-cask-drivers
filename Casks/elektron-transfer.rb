c611c5e2a3078c6bf10f1a692c4681fac0c98fa8
Michael Cho <cho-m@tuta.io>
2021-06-05 10:16:15 -0700

    Update elektron-transfer.rb

--- Casks/elektron-transfer.rb
+++ Casks/elektron-transfer.rb
@@ -2,14 +2,15 @@ cask "elektron-transfer" do
   version "1.4.0.1"
   sha256 "2eb728e31f045e9f4c3c2fa3480b699379dedfae3c402d18b2b7dab2ef87a6b8"
 
-  url "https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/133f6ec3-a98c-5781-9910-39a0fce402e7/Elektron_Transfer_#{version}.dmg"
+  url "https://s3-eu-west-1.amazonaws.com/se-elektron-devops/release/133f6ec3-a98c-5781-9910-39a0fce402e7/Elektron_Transfer_#{version}.dmg",
+      verified: "s3-eu-west-1.amazonaws.com/se-elektron-devops/release/"
   name "Elektron Transfer"
   desc "Tool for transferring samples, presets and firmware updates"
   homepage "https://www.elektron.se/support/?connection=transfer#resources"
 
   livecheck do
     url "https://www.elektron.se/support/?connection=transfer"
-    regex(%r{href=.*?/Elektron[._-]?Transfer[._-]?v?(\d+(?:\.\d+)+)[._-]?macOS\.zip}i)
+    regex(%r{href=.*?/Elektron[._-]?Transfer[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
   end
 
   depends_on macos: ">= :el_capitan"
