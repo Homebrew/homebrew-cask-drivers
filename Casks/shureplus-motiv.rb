bef528f436920f6ac2a680b89fca3bfcd4041697
Michael Cho <cho-m@tuta.io>
2021-06-13 11:47:27 -0700

    Update shureplus-motiv.rb

--- Casks/shureplus-motiv.rb
+++ Casks/shureplus-motiv.rb
@@ -3,10 +3,15 @@ cask "shureplus-motiv" do
   sha256 "a9c81b7123f1613f5ef5a76d49444ad1154d2a0cb77ce41a9d386999466a784d"
 
   url "https://d24z4d3zypmncx.cloudfront.net/Software/ckgifea480ba50b703lmhw1ks/ShurePlusMOTIV-mac-#{version}.zip",
-      verified: "https://d24z4d3zypmncx.cloudfront.net/Software/ckgifea480ba50b703lmhw1ks/"
+      verified: "d24z4d3zypmncx.cloudfront.net/Software/ckgifea480ba50b703lmhw1ks/"
   name "ShurePlus MOTIV"
   desc "Additional features and controls for Shure MV7 and MV88+ microphones"
   homepage "https://www.shure.com/en-US/products/software/shure_plus_motiv_desktop"
+
+  livecheck do
+    url "https://www.shure.com/en-US/support/downloads/software-firmware-archive/shure_plus_motiv_desktop"
+    regex(%r{href=.*?/ShurePlusMOTIV[._-]mac[._-]v?(\d+(?:\.\d+)+)\.(?:zip|dmg)}i)
+  end
 
   app "ShurePlus MOTIV.app"
 
