a0704723c63fc6548a4f423d382f02f240b1bacf
Michael Cho <cho-m@tuta.io>
2021-06-14 12:33:01 -0700

    Update elgato-wave-link.rb

--- Casks/elgato-wave-link.rb
+++ Casks/elgato-wave-link.rb
@@ -1,27 +1,20 @@
 cask "elgato-wave-link" do
-  version "1.2.1.2612,1.2.1"
+  version "1.2.1.2612"
   sha256 "23bd542313fdfc4b73d422a21bde0d2e012372aee34ba485bffea6faf0b2c420"
 
-  url "https://edge.elgato.com/egc/macos/wavelink/#{version.after_comma}/WaveLink_#{version.before_comma}.pkg"
+  url "https://edge.elgato.com/egc/macos/wavelink/#{version.major_minor_patch.chomp(".0")}/WaveLink_#{version}.pkg"
   name "Elgato Wave Link"
-  desc "Intuitive software custom-built for content creation"
+  desc "Software custom-built for content creation"
   homepage "https://www.elgato.com/en/wave-1"
 
   livecheck do
-    url "https://help.elgato.com/hc/en-us/articles/360043289491-Elgato-Wave-Link-Release-Notes"
-    strategy :page_match do |page|
-      page.split(/class=['"]?toggle-content['"]?/).map do |toggle_content|
-        match = toggle_content.match(
-          %r{href=.*?/(\d+(?:\.\d+)*)/WaveLink_(\d+(?:\.\d+)*)\.pkg}mi,
-        )
-        "#{match[2]},#{match[1]}" if match
-      end.compact
-    end
+    url "https://www.elgato.com/sites/default/files/downloads.json"
+    regex(%r{"downloadURL"\s*:\s*"[^"]*?/WaveLink[._-]v?(\d+(?:\.\d+)+)\.pkg"}i)
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :catalina"
 
-  pkg "WaveLink_#{version.before_comma}.pkg"
+  pkg "WaveLink_#{version}.pkg"
 
   uninstall launchctl: "com.elgato.WaveLink",
             quit:      "com.elgato.WaveLink",
