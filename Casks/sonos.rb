88398bd814ceef1b80ee762e2a36866d16148141
Bob Olde Hampsink <bob@robuust.digital>
2021-06-16 11:36:52 +0200

    Update sonos from 13.1,63.2.88230 to 13.1.2

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,6 +1,6 @@
 cask "sonos" do
-  version "13.1,63.2.88230"
-  sha256 "f5a5987f0c54bc20d204eb9f99991a0e3c1ed346ec89e25ae3d80dffae380c2a"
+  version "13.1.2"
+  sha256 "e7e06baf8254553a1a0d1e1d625b222734a2cb1b6ce2d59877f90083f18476ad"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.before_comma.no_dots}.dmg"
   name "Sonos"
