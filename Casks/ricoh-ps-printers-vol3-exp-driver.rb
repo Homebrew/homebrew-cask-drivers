9a78ba3b85870e51e12e76c8dbc40c81e4191128
Michael Cho <cho-m@tuta.io>
2021-06-15 21:31:07 -0700

    Update ricoh-ps-printers-vol3-exp-driver.rb

--- Casks/ricoh-ps-printers-vol3-exp-driver.rb
+++ Casks/ricoh-ps-printers-vol3-exp-driver.rb
@@ -1,14 +1,19 @@
 cask "ricoh-ps-printers-vol3-exp-driver" do
-  version "3.9.0.0"
+  version "3.9.0.0,0001316460"
   sha256 "2a092e94f8abb664fb9fd1512135a53d0ff44ba9d0bcea89a364aebffda936a2"
 
-  url "https://support.ricoh.com/bb/pub_e/dr_ut_e/0001316/0001316460/V#{version.delete(".")}/Ricoh_PS_Printers_Vol3_EXP_LIO_#{version}.dmg"
+  url "https://support.ricoh.com/bb/pub_e/dr_ut_e/#{version.after_comma[0..6]}/#{version.after_comma}/V#{version.before_comma.no_dots}/Ricoh_PS_Printers_Vol3_EXP_LIO_#{version.before_comma}.dmg"
   name "Ricoh PS printers Vol 3 EXP driver"
+  desc "Allow PS driver to control the print device and enable full functionality"
   homepage "https://support.ricoh.com/bb/html/dr_ut_e/apc/model/mpc2011/mpc2011.htm"
 
   livecheck do
     url "https://support.ricoh.com/bb/html/dr_ut_e/apc/model/mpc2011/mpc2011en.htm"
-    regex(/href=.*Ricoh_PS_Printers_Vol3_EXP_LIO_(\d+(?:\.\d+)+)\.dmg/i)
+    strategy :page_match do |page|
+      page.scan(%r{href=.*?/(\d+)/V\d+/Ricoh_PS_Printers_Vol3_EXP_LIO[._-]v?(\d+(?:\.\d+)+)\.dmg}i).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
   end
 
   pkg "Ricoh_PS_Printers_Vol3_EXP_LIO_Driver.pkg"
