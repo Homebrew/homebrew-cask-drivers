70b91b2fd096cb1c0157276275d25a256185071a
Michael Cho <cho-m@tuta.io>
2021-06-16 23:22:34 -0700

    Update opentx-companion from 2.3.11 to 2.3.12

--- Casks/opentx-companion.rb
+++ Casks/opentx-companion.rb
@@ -1,6 +1,6 @@
 cask "opentx-companion" do
-  version "2.3.11"
-  sha256 "67e087f19bf74ef07a1fd28d0f68b8a7bbf00fce44ff76bb65dfbd0a0b10af7d"
+  version "2.3.12"
+  sha256 "9a260ec2cb8e3a95150bb11fea7a80be172418cf56b23075bcb4ea92d8a9b96b"
 
   url "https://downloads.open-tx.org/#{version.major_minor}/release/companion/macosx/opentx-companion-#{version}.dmg"
   name "OpenTX Companion"
@@ -9,8 +9,7 @@ cask "opentx-companion" do
 
   livecheck do
     url "https://www.open-tx.org/downloads"
-    strategy :page_match
-    regex(/opentx\s(\d(?:\.\d+)*)/i)
+    regex(%r{href=.*?/opentx[._-]v?(\d+(?:\.\d+)+)}i)
   end
 
   app "OpenTX Companion #{version.major_minor}.app"
