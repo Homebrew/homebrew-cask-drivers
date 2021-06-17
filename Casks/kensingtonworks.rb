02a2c29e08b3114a4d62ef389472269693231a44
TK Lai <iam@tklai.net>
2021-06-17 20:22:17 +0800

    Add KensingtonWorks v2.2.10
    KensingtonWorks v2.2.10
    
    Kensington TrackballWorks is deprecated and introduced KensingtonWorks.
    This application supports macOS 10.12 to macOS 11 on both Intel and Apple Silicon Macs.

---
+++ Casks/kensingtonworks.rb
@@ -0,0 +1,34 @@
+cask "kensingtonworks" do
+  version "2.2.10"
+  sha256 "b3b8190703272e9cb4c1d27b165a63227a4f16a33ff5b130ab15f51a6eb95d66"
+
+  url "https://www.kensington.com/siteassets/software-support/kensingtonworks/new-kensingtonworks-download/kensingtonworks_#{version}.pkg"
+  name "KensingtonWorks"
+  desc "Software to personalise Kensingtons trackballs, mice, and presenters"
+  homepage "https://www.kensington.com/software/kensingtonworks/"
+
+  pkg "kensingtonworks_#{version}.pkg"
+
+  uninstall launchctl: "com.kensington.trackballworks",
+            quit:      [
+              "com.kensington.kensingtonworks2.app",
+              "com.kensington.kensingtonworks2.helper2",
+              "com.kensington.tbwDKDriver",
+              "com.kensington.tbwdkmanager",
+            ],
+            script:    {
+              executable: "/Applications/Utilities/KensingtonWorks Uninstaller.app/Contents/MacOS/KensingtonWorks Uninstaller",
+            },
+            pkgutil:   [
+              "com.kensington.trackballworks2.installer",
+            ],
+            delete:    [
+              "/Library/Application Support/Kensington",
+              "~/Library/Application Support/KensingtonWorks",
+              "~/Library/Logs/KensingtonWorks",
+            ]
+
+  caveats do
+    reboot
+  end
+end
