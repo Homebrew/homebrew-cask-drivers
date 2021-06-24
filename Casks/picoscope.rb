09b56e346faac4ac51c33ba8570ff4be4883912d
Constantin Dullo <constantin.dullo@me.com>
2021-06-24 16:06:29 +0200

    Update picoscope download url to the new path

--- Casks/picoscope.rb
+++ Casks/picoscope.rb
@@ -2,7 +2,7 @@ cask "picoscope" do
   version "6.14.44.5870"
   sha256 "2dd29c6baa1e93be9f9160319a27fdb4308681fa616b2be66493e6b5e439c2cd"
 
-  url "https://www.picotech.com/download/software/sr/PicoScope-#{version}.pkg"
+  url "https://www.picotech.com/download/software/beta/PicoScope-#{version}.pkg"
   name "PicoScope"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
