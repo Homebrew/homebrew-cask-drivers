82a0d1d77bf5f08e6f148f1adb3fa80d034fa8b0
flieger19 <constantin.dullo@gmail.com>
2021-06-24 16:48:41 +0200

    Pico scope (#2294)
    * Add PicoScope 6 v6.14.36.5676
    
    Test and Measurement oscilloscope software for PicoScope oscilloscops
    
    * Update picoscope download url to the new path
    
    * Update picoscope from 6.14.44.5870 to 6.14.48-4r5944
    
    * Fix picoscope style offenseFix picoscope style offenses
    
    * Update picoscope.rb
    
    Co-authored-by: Constantin Dullo <constantin.dullo@me.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/picoscope.rb
+++ Casks/picoscope.rb
@@ -1,8 +1,8 @@
 cask "picoscope" do
-  version "6.14.44.5870"
-  sha256 "2dd29c6baa1e93be9f9160319a27fdb4308681fa616b2be66493e6b5e439c2cd"
+  version "6.14.48-4r5944"
+  sha256 "176c8017125b0f6252d2b3483e15c3f159f5d9a9706e8c460bb60565a0e81ef9"
 
-  url "https://www.picotech.com/download/software/sr/PicoScope-#{version}.pkg"
+  url "https://www.picotech.com/download/software/beta/PicoScope-#{version}.pkg"
   name "PicoScope"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
@@ -10,10 +10,10 @@ cask "picoscope" do
   livecheck do
     url "https://www.picotech.com/downloads/_lightbox/"
     strategy :page_match
-    regex(%r{href=.*?/PicoScope-(\d+(?:\.\d+)*)\.pkg}i)
+    regex(%r{href=.*?/PicoScope-(\d+(?:.\d+)*)\.pkg}i)
   end
 
   pkg "PicoScope-#{version}.pkg"
 
-  uninstall pkgutil: "com.picotech.picoscope6"
+  uninstall pkgutil: "com.picotech.picoscope#{version.major}"
 end
