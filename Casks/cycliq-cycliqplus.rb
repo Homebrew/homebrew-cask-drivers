e5ab6f1cea1a89d7844d5d60dd08a3dd021b4c32
Ryoma Kai <legknmj@icloud.com>
2021-06-29 09:14:30 +0900

    Change the cycliq-cycliqplus url path (#2300)

--- Casks/cycliq-cycliqplus.rb
+++ Casks/cycliq-cycliqplus.rb
@@ -2,7 +2,7 @@ cask "cycliq-cycliqplus" do
   version "2.58"
   sha256 "23f1c90ccd83ff99b3269d709781e37b40778e6c87526dbb48ed4a86a875726f"
 
-  url "https://cycliq.com/files/software/CycliqPlus-#{version}.dmg"
+  url "https://cycliq.com/files/software/downloads/CycliqPlus-#{version}.dmg"
   name "CycliqPlus"
   desc "Footage and bike camera settings editor"
   homepage "https://cycliq.com/"
