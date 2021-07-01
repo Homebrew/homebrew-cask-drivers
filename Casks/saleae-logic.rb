70e8c9c53467ea86905b7e52f6304af0f855fd82
Michael Cho <cho-m@tuta.io>
2021-06-30 21:45:19 -0700

    Update saleae-logic from 2.3.29 to 2.3.30

--- Casks/saleae-logic.rb
+++ Casks/saleae-logic.rb
@@ -1,6 +1,6 @@
 cask "saleae-logic" do
-  version "2.3.29"
-  sha256 "3cc4963084668a67bd7089ac9bcd020e5afcfc74134a373977b4ab312173f210"
+  version "2.3.30"
+  sha256 "ac62b75bc2e450f1617a12a4abba64c5b615d3032ff49afd0230e294baa067bf"
 
   url "https://downloads.saleae.com/logic#{version.major}/Logic-macOS-#{version}-master.zip"
   name "Saleae Logic2"
