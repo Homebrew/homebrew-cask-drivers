7e2f67f4e8b2dadb29387ab1dc7145237b84c9a6
Michael Cho <cho-m@tuta.io>
2021-06-30 21:47:11 -0700

    Update synology-drive from 3.0.1,12664 to 3.0.1,12667

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -1,6 +1,6 @@
 cask "synology-drive" do
-  version "3.0.1,12664"
-  sha256 "c3df3c20b506e84656ede14a150164fcacd15570fab43c87933223ce1a2ed86e"
+  version "3.0.1,12667"
+  sha256 "2fd2ad2d5ee1de87531e016931dae14c14f44febd33992c156f14c0295dd2485"
 
   url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.before_comma}-#{version.after_comma}/Mac/Installer/synology-drive-client-#{version.after_comma}.dmg"
   name "Synology Drive"
