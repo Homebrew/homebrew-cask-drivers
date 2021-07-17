a8e8c99010a604c9b07538314ed33854d6967eed
Tristan Naumann <tristan.naumann@gmail.com>
2021-07-17 05:26:28 -0400

    Update safenet-authentication-client (#2324)
    * Update safenet-authentication-client to Big Sur
    
    Updates safenet-authentication-client cask to work with Big Sur. Fixes #2323.
    
    * add conditional statement
    
    * update uninstall stanza
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/safenet-authentication-client.rb
+++ Casks/safenet-authentication-client.rb
@@ -1,8 +1,16 @@
 cask "safenet-authentication-client" do
-  version "10.2.111.0"
-  sha256 "d0c316379c03e17a35a53e81df641b85b38b5cf7b7a8f6b5f11789a0819f943a"
+  if MacOS.version <= :catalina
+    version "10.2.109.0"
+    sha256 "d0c316379c03e17a35a53e81df641b85b38b5cf7b7a8f6b5f11789a0819f943a"
+    url "https://www.globalsign.com/en/safenet-drivers/USB/#{version.major_minor}/Safenet_#{version.major_minor}_Post_GA_R3.zip"
+    container nested: "Safenet #{version.major_minor} Post GA R3/Safenet #{version.major_minor} Post GA R3/SafeNetAuthenticationClient.#{version}.dmg"
+  else
+    version "10.2.111.0"
+    sha256 "af622151e7188d25f326420d3d210dc676002c765a670bd756fd40bd050fd0a5"
+    url "https://www.globalsign.com/en/safenet-drivers/USB/#{version.major_minor}/Safenet_#{version.major_minor}_Post_GA_(R4).zip"
+    container nested: "Safenet #{version.major_minor} Post GA (R4) 2/SafeNet Authentication Client #{version.major_minor}.pkg"
+  end
 
-  url "https://www.globalsign.com/en/safenet-drivers/USB/#{version.major_minor}/Safenet_#{version.major_minor}_Post_GA_R3.zip"
   name "SafeNet Authentication Client"
   desc "Client for smart card readers and security tokens"
   homepage "https://support.globalsign.com/ssl/ssl-certificates-installation/safenet-drivers"
@@ -11,8 +19,10 @@ cask "safenet-authentication-client" do
     skip "No version information available"
   end
 
-  depends_on macos: :catalina
-  container nested: "Safenet #{version.major_minor} Post GA R3/Safenet #{version.major_minor} Post GA R3/SafeNetAuthenticationClient.#{version}.dmg"
+  depends_on macos: [
+    :catalina,
+    :big_sur,
+  ]
 
   pkg "SafeNet Authentication Client #{version.major_minor}.pkg"
 
@@ -20,9 +30,9 @@ cask "safenet-authentication-client" do
     "com.SafeNet.SACMonitor",
     "com.SafeNet.SACSrv",
   ],
+            signal:    ["QUIT", "com.SafeNet.SACMonitor"],
             quit:	     [
               "com.gemalto.Gemalto-Smart-Card-Token.PKCS11-Token",
-              "SACMonitor",
             ],
             pkgutil:   [
               "com.safenet.safenetAuthenticationClient.eTokenConf.pkg",
