39ffabc2289aa37c8568a26cd89ce3ba1001ae2d
Bevan Kay <email@bevankay.me>
2021-07-19 00:49:50 +1000

    Update phidget-control-panel from 22,1.7.20210428 to 22,1.7.20210706 (#2328)

--- Casks/phidget-control-panel.rb
+++ Casks/phidget-control-panel.rb
@@ -1,6 +1,6 @@
 cask "phidget-control-panel" do
-  version "22,1.7.20210428"
-  sha256 "ec079434e0169b4ea950fa7c99157d95f65cfff6431dcfe2a1c63f41f807e1a4"
+  version "22,1.7.20210706"
+  sha256 "1894307fc12d64b7f2343d8a09e2471eb8e9d448164e72cf3e193e403d340522"
 
   url "https://www.phidgets.com/downloads/phidget#{version.before_comma}/libraries/macos/Phidget#{version.before_comma}/Phidget#{version.before_comma}_#{version.after_comma}.dmg"
   name "Phidget Control Panel"
