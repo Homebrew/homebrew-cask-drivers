416b3d2dc97b585a60c51af550644a2d2bd551fd
morrison12 <morrison12@users.noreply.github.com>
2021-07-18 17:25:34 -0400

    add MuteMe 0.5.0 (#2309)
    * add MuteMe 0.5.0
    
    * add zap stanza to muteme cask
    
    * correct & augment zap list for muteme cask
    
    * add trailing comma in zap stanza for muteme cask
    
    * update livecheck, update url
    
    * Update muteme.rb
    
    Co-authored-by: James Watt <james@bevjam.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/muteme.rb
@@ -0,0 +1,26 @@
+cask "muteme" do
+  version "0.5.1"
+  sha256 "c6b3c62da90275900666446f736ec7289ef03a65d164f2e0d4350b587fb99d98"
+
+  url "https://muteme.io/download/flavor/default/#{version}/osx_64/MuteMe-Client-#{version}.dmg",
+      verified: "https://muteme.io/"
+  name "muteme"
+  desc "Companion app for the MuteMe physical mute button"
+  homepage "https://muteme.com/"
+
+  livecheck do
+    url "https://downloads.muteme.com/download/latest/osx_64"
+    strategy :header_match
+  end
+
+  app "MuteMe-Client.app"
+
+  zap trash: [
+    "~/Library/Application Support/MuteMe-Client",
+    "~/Library/Caches/com.electron.muteme-client",
+    "~/Library/Caches/com.electron.muteme-client.ShipIt",
+    "~/Library/Preferences/ByHost/com.electron.muteme-client.ShipIt.*.plist",
+    "~/Library/Preferences/com.electron.muteme-client.plist",
+    "~/Library/Saved Application State/com.electron.muteme-client.savedState",
+  ]
+end
