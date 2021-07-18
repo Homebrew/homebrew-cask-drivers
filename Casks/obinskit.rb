24d7a8acfc8667807e6cb58965d3f6869db1ca82
Bevan Kay <email@bevankay.me>
2021-07-18 21:12:07 +1000

    Update obinskit.rb

--- Casks/obinskit.rb
+++ Casks/obinskit.rb
@@ -2,13 +2,13 @@ cask "obinskit" do
   version "1.1.7"
   sha256 "1092926d74f0f3735309d04d3527be94392344fab405a567f218b81cf46fa3d0"
 
-  url "http://releases.obins.net/occ/darwin/x64/ObinsKit_#{version}_x64.dmg"
+  url "https://releases.obins.net/occ/darwin/x64/ObinsKit_#{version}_x64.dmg"
   name "ObinsKit"
   desc "Software for Anne Pro and Anne Pro 2"
   homepage "http://en.obins.net/obinskit"
 
   livecheck do
-    url "http://releases.obins.net/occ/darwin/x64/latest-mac.yml"
+    url "https://releases.obins.net/occ/darwin/x64/latest-mac.yml"
     strategy :electron_builder
   end
 
