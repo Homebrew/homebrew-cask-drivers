3226e66ee284a9e3fee717a7ba894853469e20d0
Bevan Kay <email@bevankay.me>
2021-07-20 10:20:59 +1000

    add livecheck, add desc

--- Casks/nordic-nrf-command-line-tools.rb
+++ Casks/nordic-nrf-command-line-tools.rb
@@ -4,7 +4,16 @@ cask "nordic-nrf-command-line-tools" do
 
   url "https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX.tar"
   name "nRF Command Line Tools"
+  desc "Command-line tools for Nordic nRF Semiconductors"
   homepage "https://www.nordicsemi.com/Software-and-Tools/Development-Tools/nRF-Command-Line-Tools"
+
+  livecheck do
+    url "https://www.nordicsemi.com/Products/Development-tools/nRF-Command-Line-Tools/Download"
+    strategy :page_match do |page|
+      page.scan(/nRF[._-]Command[._-]Line[._-]Tools[._-]v?(\d+(?:[_.]\d+)+)[._-]OSX\.zip/i)
+          .map { |match| match[0].tr("_", ".") }
+    end
+  end
 
   depends_on cask: "segger-jlink"
   container nested: "nRF-Command-Line-Tools_#{version.dots_to_underscores}.tar"
