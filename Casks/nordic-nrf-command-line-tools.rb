da8fe0eef1438222de9b0ab430564b6b9b428c05
Shahar Hadas <shaharhd@gmail.com>
2021-07-21 04:19:52 +0300

    Update nordic-nrf-command-line-tools from 10.12.1 to 10.13.0 (#2332)
    * Update nordic-nrf-command-line-tools from 10.12.1 to 10.13.0
    
    * add livecheck, add desc
    
    Co-authored-by: Bevan Kay <email@bevankay.me>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/nordic-nrf-command-line-tools.rb
+++ Casks/nordic-nrf-command-line-tools.rb
@@ -1,14 +1,39 @@
 cask "nordic-nrf-command-line-tools" do
-  version "10.12.1"
-  sha256 "16b4f40beee75ec8d7a6b45b5c1b831a7602dc397791a7439282290d37b2a357"
+  version "10.13.0"
+  sha256 "29b8824be08acbd7215bcb5de77f156660a35c982e3e2a669e1abf3924d4d158"
 
-  url "https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX.tar"
+  url "https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX.zip"
   name "nRF Command Line Tools"
+  desc "Command-line tools for Nordic nRF Semiconductors"
   homepage "https://www.nordicsemi.com/Software-and-Tools/Development-Tools/nRF-Command-Line-Tools"
 
+  livecheck do
+    url "https://www.nordicsemi.com/Products/Development-tools/nRF-Command-Line-Tools/Download"
+    strategy :page_match do |page|
+      page.scan(/nRF[._-]Command[._-]Line[._-]Tools[._-]v?(\d+(?:[_.]\d+)+)[._-]OSX\.zip/i)
+          .map { |match| match[0].tr("_", ".") }
+    end
+  end
+
   depends_on cask: "segger-jlink"
-  container nested: "nRF-Command-Line-Tools_#{version.dots_to_underscores}.tar"
 
   binary "nrfjprog/nrfjprog"
   binary "mergehex/mergehex"
+
+  preflight do
+    system_command "/usr/bin/tar", args: [
+      "--extract",
+      "--file",
+      "#{staged_path}/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX.tar",
+      "--directory",
+      staged_path,
+    ]
+    system_command "/usr/bin/tar", args: [
+      "--extract",
+      "--file",
+      "#{staged_path}/nRF-Command-Line-Tools_#{version.dots_to_underscores}.tar",
+      "--directory",
+      staged_path,
+    ]
+  end
 end
