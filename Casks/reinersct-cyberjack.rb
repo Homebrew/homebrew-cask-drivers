c7d423398ee9fd23ae98718a2791796b8eaa5877
David Haller <davidhaller@mailbox.org>
2021-07-29 10:50:03 +0200

    Add reinersct-cyberjack version SP14

---
+++ Casks/reinersct-cyberjack.rb
@@ -0,0 +1,16 @@
+cask "reinersct-cyberjack" do
+  version "3.99.5final.SP14-arm64-signed"
+  sha256 "16c3628c7bd74617308caafc063517cfa8739d99ce0ea2159be125e1399e344c"
+
+  url "https://support.reiner-sct.de/downloads/MAC/pcsc-cyberjack_#{version}.pkg"
+  name "reinersct-cyberjack"
+  desc "Driver for smartcard readers by REINER SCT"
+  homepage "https://reiner-sct.de/"
+
+  depends_on macos: ">= :big_sur"
+  depends_on arch: :arm64
+
+  pkg "pcsc-cyberjack_#{version}.pkg"
+
+  uninstall pkgutil: "com.reiner-sct.mac.cyberjack"
+end
