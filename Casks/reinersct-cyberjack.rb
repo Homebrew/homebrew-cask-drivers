d108f3c7841843eaf4f56839bcb6dba64c9105fb
Bevan Kay <email@bevankay.me>
2021-07-31 11:13:03 +1000

    intel and arm conditional, add livecheck

--- Casks/reinersct-cyberjack.rb
+++ Casks/reinersct-cyberjack.rb
@@ -1,18 +1,32 @@
 cask "reinersct-cyberjack" do
-  version "3.99.5final.SP14"
-  sha256 "16c3628c7bd74617308caafc063517cfa8739d99ce0ea2159be125e1399e344c"
+  if Hardware::CPU.intel?
+    version "3.99.5final.SP13"
+    sha256 "25bcd5d5ff197163bba94e72cddb4b74ad8a04df8e88df01404378f80c46f9b4"
 
-  url "https://support.reiner-sct.de/downloads/MAC/pcsc-cyberjack_#{version}-arm64-signed.pkg"
+    url "https://support.reiner-sct.de/downloads/MAC/pcsc-cyberjack_#{version}-x86_64-signed.pkg"
+
+    livecheck do
+      url "https://www.reiner-sct.de/support/support-anfrage/?productGroup=77304735&product=77304820&q=driver&os=macOS"
+      regex(/href=.*?pcsc[._-]cyberjack[._-]v?(\d+(?:\.[\dA-z]+)+)[._-]x86[._-]64[._-]signed\.pkg/i)
+    end
+
+  else
+    version "3.99.5final.SP14"
+    sha256 "16c3628c7bd74617308caafc063517cfa8739d99ce0ea2159be125e1399e344c"
+
+    url "https://support.reiner-sct.de/downloads/MAC/pcsc-cyberjack_#{version}-arm64-signed.pkg"
+
+    livecheck do
+      url "https://www.reiner-sct.de/support/support-anfrage/?productGroup=77304735&product=77304820&q=driver&os=macOS"
+      regex(/href=.*?pcsc[._-]cyberjack[._-]v?(\d+(?:\.[\dA-z]+)+)[._-]arm64[._-]signed\.pkg/i)
+    end
+  end
+
   name "reinersct-cyberjack"
   desc "Driver for smartcard readers by REINER SCT"
   homepage "https://reiner-sct.de/"
 
-  livecheck do
-    skip "No version information available"
-  end
-
-  depends_on macos: ">= :big_sur"
-  depends_on arch: :arm64
+  depends_on macos: ">= :high_sierra"
 
   pkg "pcsc-cyberjack_#{version}.pkg"
 
