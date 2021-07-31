2ca9039d317ea9450dd4c20eaa02ec7a5243183e
Bevan Kay <email@bevankay.me>
2021-07-31 11:27:07 +1000

    Update reinersct-cyberjack.rb

--- Casks/reinersct-cyberjack.rb
+++ Casks/reinersct-cyberjack.rb
@@ -10,6 +10,7 @@ cask "reinersct-cyberjack" do
       regex(/href=.*?pcsc[._-]cyberjack[._-]v?(\d+(?:\.[\dA-z]+)+)[._-]x86[._-]64[._-]signed\.pkg/i)
     end
 
+    pkg "pcsc-cyberjack_#{version}-x86_64-signed.pkg"
   else
     version "3.99.5final.SP14"
     sha256 "16c3628c7bd74617308caafc063517cfa8739d99ce0ea2159be125e1399e344c"
@@ -20,6 +21,8 @@ cask "reinersct-cyberjack" do
       url "https://www.reiner-sct.de/support/support-anfrage/?productGroup=77304735&product=77304820&q=driver&os=macOS"
       regex(/href=.*?pcsc[._-]cyberjack[._-]v?(\d+(?:\.[\dA-z]+)+)[._-]arm64[._-]signed\.pkg/i)
     end
+
+    pkg "pcsc-cyberjack_#{version}-arm64-signed.pkg"
   end
 
   name "reinersct-cyberjack"
@@ -27,8 +30,6 @@ cask "reinersct-cyberjack" do
   homepage "https://reiner-sct.de/"
 
   depends_on macos: ">= :high_sierra"
-
-  pkg "pcsc-cyberjack_#{version}.pkg"
 
   uninstall pkgutil: "com.reiner-sct.mac.cyberjack"
 end
