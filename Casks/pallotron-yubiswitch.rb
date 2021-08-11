5fdb7b5926c3c77d40187706740b3fc9e5e8da91
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-11 14:46:18 +0800

    Update pallotron-yubiswitch.rb

--- Casks/pallotron-yubiswitch.rb
+++ Casks/pallotron-yubiswitch.rb
@@ -3,7 +3,6 @@ cask "pallotron-yubiswitch" do
   sha256 "691ff7017c000211587217c60a6a9f352b3cb8d9ee2ce099f01d096d794a01ff"
 
   url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}/yubiswitch_#{version}.dmg"
-  appcast "https://github.com/pallotron/yubiswitch/releases.atom"
   name "Yubiswitch"
   desc "Status bar application to enable/disable Yubikey Nano"
   homepage "https://github.com/pallotron/yubiswitch"
