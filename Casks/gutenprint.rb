e9dd3afe0676877da3ecdafb0b461346c44a3189
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-12 12:19:36 +0800

    gutenprint: add livecheck (#2369)
    * Update gutenprint.rb
    
    * Update gutenprint.rb
    
    * Update gutenprint.rb
    
    * Update gutenprint.rb
    
    * Update gutenprint.rb

--- Casks/gutenprint.rb
+++ Casks/gutenprint.rb
@@ -3,9 +3,15 @@ cask "gutenprint" do
   sha256 "6dd810482845604a1e17fc3aef48a03d178798c14fe6f106a443f5af14022519"
 
   url "https://downloads.sourceforge.net/gimp-print/gutenprint-#{version.major_minor}/#{version}/gutenprint-#{version}.dmg"
-  appcast "https://sourceforge.net/projects/gimp-print/rss"
   name "Gutenprint"
+  desc "Drivers for various printers for use with CUPS and GIMP"
   homepage "http://gimp-print.sourceforge.net/"
+
+  livecheck do
+    url "http://gimp-print.sourceforge.net/MacOSX.php"
+    strategy :page_match
+    regex(/gutenprint-(\d+(?:\.\d+)*)\.dmg/i)
+  end
 
   pkg "gutenprint-#{version}.pkg"
 
