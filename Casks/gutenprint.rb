f088a9da3f70d61b76d51ad7b88618c5da9bcb16
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-12 11:13:16 +0800

    Update gutenprint.rb

--- Casks/gutenprint.rb
+++ Casks/gutenprint.rb
@@ -3,7 +3,6 @@ cask "gutenprint" do
   sha256 "6dd810482845604a1e17fc3aef48a03d178798c14fe6f106a443f5af14022519"
 
   url "https://downloads.sourceforge.net/gimp-print/gutenprint-#{version.major_minor}/#{version}/gutenprint-#{version}.dmg"
-  appcast "https://sourceforge.net/projects/gimp-print/rss"
   name "Gutenprint"
   desc "Drivers for various printers for use with CUPS and GIMP"
   homepage "http://gimp-print.sourceforge.net/"
