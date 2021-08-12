f56e1c7a5895661193924b84cd1679bb1dcefff3
miccal <miccal.matthews@gmail.com>
2021-08-12 10:46:51 +0800

    Update focusrite-control from 3.7.2.1923 to 3.7.4.1943

--- Casks/focusrite-control.rb
+++ Casks/focusrite-control.rb
@@ -1,6 +1,6 @@
 cask "focusrite-control" do
-  version "3.7.2.1923"
-  sha256 "0fed5039967b432bdfec2751c3bb92cf018a3271c1c99d0a0e555a154b1bec21"
+  version "3.7.4.1943"
+  sha256 "969d9e78f42f9f810e40be4e2b07450e5cf6cd4267d72c5ae525899742c8baab"
 
   url "https://fael-downloads-prod.focusrite.com/customer/prod/s3fs-public/downloads/Focusrite%20Control%20-%20#{version}.dmg"
   name "Focusrite Control"
