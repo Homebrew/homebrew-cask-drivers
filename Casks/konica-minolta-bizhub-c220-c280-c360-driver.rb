f33666ab6bd90bb5372528a0a20e4129f6b42ae3
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-21 13:20:38 +0800

    Update konica-minolta-bizhub-c220-c280-c360-driver.rb

--- Casks/konica-minolta-bizhub-c220-c280-c360-driver.rb
+++ Casks/konica-minolta-bizhub-c220-c280-c360-driver.rb
@@ -3,10 +3,12 @@ cask "konica-minolta-bizhub-c220-c280-c360-driver" do
   sha256 :no_check
 
   url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=9417996e51b61744d50204a9201cfa60&tx_kmanacondaimport_downloadproxy[documentId]=40940&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=9417996e51b61744d50204a9201cfa60%26tx_kmanacondaimport_downloadproxy[documentId]=40940%26tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta%26tx_kmanacondaimport_downloadproxy[language]=EN%26type=1558521685",
-          must_contain: version.no_dots
   name "Konica Minolta Bizhub C220/C280/C360 PostScript Printer Driver"
   homepage "https://www.konicaminolta.eu/en/business-solutions/support/download-center.html"
+
+  livecheck do
+    skip "unversioned URL"
+  end
 
   pkg "bizhub_C360_109.pkg"
 
