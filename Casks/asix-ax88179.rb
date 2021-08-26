6c0d32e135116f37726157714ef0ea4eb407bbcd
Bevan Kay <email@bevankay.me>
2021-08-27 07:42:46 +1000

    Update asix-ax88179 from 1.2.0,1132 to 1.3.0,1147 (#2388)
    * Update asix-ax88179 from 1.2.0,1132 to 1.3.0,1147
    
    * Update asix-ax88179.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/asix-ax88179.rb
+++ Casks/asix-ax88179.rb
@@ -34,9 +34,9 @@ cask "asix-ax88179" do
       end
     end
   else
-    version "1.2.0,1132"
+    version "1.3.0,1147"
 
-    container nested: "ASIX_USB_Device_Installer_macOS_11.3_above_Driver_v#{version.before_comma}_20210521/ASIX_USB_Device_Installer_v#{version.before_comma}.dmg"
+    container nested: "ASIX_USB_Device_Installer_macOS_11.3_above_Driver_v#{version.before_comma}/ASIX_USB_Device_Installer_v#{version.before_comma}.dmg"
     pkg "ASIX_USB_Device_Installer_v#{version.before_comma}.pkg"
 
     livecheck do
