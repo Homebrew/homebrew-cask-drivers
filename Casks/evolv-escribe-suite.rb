2cbe44915e94825a948379bdf80adb21ccaf0fbe
miccal <miccal.matthews@gmail.com>
2021-09-01 19:09:18 +0800

    Update evolv-escribe-suite from 2_SP24 to 2_SP30

--- Casks/evolv-escribe-suite.rb
+++ Casks/evolv-escribe-suite.rb
@@ -1,6 +1,6 @@
 cask "evolv-escribe-suite" do
-  version "2_SP24"
-  sha256 "4fb85616db9d6f605fac2bc583d8ab016eb79815385a585bec252185860d1ed4"
+  version "2_SP30"
+  sha256 "2131fc9c50af27acb68c82feae54f30839da6545d6b74d6b218dea9317ca8fa4"
 
   url "https://downloads.evolvapor.com/SetupEScribe#{version}_INT.pkg"
   name "EScribe Suite"
