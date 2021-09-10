d3f71f2fd3bccc129decee6f3c08d17584e03706
Bevan Kay <email@bevankay.me>
2021-09-10 23:06:08 +1000

    Update cycliq-cycliqplus from 2.61 to 2.62

--- Casks/cycliq-cycliqplus.rb
+++ Casks/cycliq-cycliqplus.rb
@@ -1,6 +1,6 @@
 cask "cycliq-cycliqplus" do
-  version "2.61"
-  sha256 "0a01a44079e2b984524d453f5e987b0cdf48c648ca682a5f4c106a53172f1641"
+  version "2.62"
+  sha256 "d9c80bceef3153564802749e660d5fcca7a4e6de5c8186a418822d80c9986115"
 
   url "https://cycliq.com/files/software/downloads/CycliqPlus-#{version}.dmg"
   name "CycliqPlus"
