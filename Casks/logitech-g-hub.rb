a0ddd24670678a84b244e68350ea0248200cc730
Bevan Kay <email@bevankay.me>
2021-09-16 09:47:40 +1000

    fix style

--- Casks/logitech-g-hub.rb
+++ Casks/logitech-g-hub.rb
@@ -30,7 +30,7 @@ cask "logitech-g-hub" do
     args:       ["--uninstall"],
     sudo:       true,
   },
-            delete: "/Applications/lghub.app",  
+            delete: "/Applications/lghub.app",
             trash:  "/Users/Shared/LGHUB"
 
   zap trash: [
