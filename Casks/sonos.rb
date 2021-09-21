9127160773af74c2c2811ec10786ae07f1048e80
Jason <jason@family-imac.home.tedor.me>
2021-09-21 13:42:36 -0400

    Update sonos from 13.2,64.3.19080 to 13.3,65.1.21150

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,6 +1,6 @@
 cask "sonos" do
-  version "13.2,64.3.19080"
-  sha256 "68867497849ea49a0f6eaf49a53eada1ce97522a5e4e1db231fcd4858ec9bbde"
+  version "13.3,65.1.21150"
+  sha256 "1a03da1e60de06e6a7af3bb52687a22379a44b3d1c1b96766a46abab9b9fc466"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.before_comma.no_dots}.dmg"
   name "Sonos"
