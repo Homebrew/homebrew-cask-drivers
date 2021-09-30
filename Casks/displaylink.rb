0829fe5a8574a93366d8d5ca647dd4587fd34308
Bevan Kay <email@bevankay.me>
2021-10-01 09:53:29 +1000

    displaylink: version bumps and url updates (#2440)

--- Casks/displaylink.rb
+++ Casks/displaylink.rb
@@ -1,27 +1,25 @@
 cask "displaylink" do
-  if MacOS.version <= :yosemite
-    version "3.1.1,1252"
-    sha256 "cc4e73357841463509f1430520eec04792cbfbf9a2a564ea17d5cc78774372d0"
-  elsif MacOS.version <= :high_sierra
-    version "4.3.1,1251"
+  if MacOS.version <= :sierra
+    version "4.3.1,2021-02"
     sha256 "d5cd6787d6c4ca6a2425984bcbab607e618e9803335455e24196e14e35657b97"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20USB%20Graphics%20Software%20for%20Mac%20OS%20X%20and%20macOS#{version.before_comma}-EXE.dmg"
+  elsif MacOS.version <= :high_sierra
+    version "5.1.1,2021-02"
+    sha256 "1ac9093f8113af8c35d6f3ff5b1ae3f119a5aff0d5309d75c7a1742f159184b5"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.before_comma}-EXE.dmg"
   elsif MacOS.version <= :mojave
-    version "5.2.5,1636"
-    sha256 "aa061f65ffb613c5138b88051f56da12825cfe217fa6ae589f7d5125981f76b7"
+    version "5.2.6,2021-05"
+    sha256 "9f1854cd5720105d6d45c91172419c503358543e4a23d7113387aedf16a39cbb"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.before_comma}-EXE.dmg"
   else
-    version "1.4,1715"
-    sha256 "7839fe5200ecb8f83c714a3195d54dffd2eb2969ee443f2cbc386ff28aeb93fc"
+    version "1.5,2021-09"
+    sha256 "d703cc8e9093e4d163c5e612326c0907a02c6d4eec6aaca8d0727503859ef95d"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20Manager%20Graphics%20Connectivity#{version.before_comma}-EXE.pkg"
   end
 
-  url "https://www.displaylink.com/downloads/file?id=#{version.after_comma}",
-      data:  {
-        "fileId"        => version.after_comma,
-        "accept_submit" => "Accept",
-      },
-      using: :post
   name "DisplayLink USB Graphics Software"
   desc "Drivers for DisplayLink docks, adapters and monitors"
-  homepage "https://www.displaylink.com/"
+  homepage "https://www.synaptics.com/products/displaylink-graphics"
 
   livecheck do
     skip "No version information available"
@@ -30,12 +28,7 @@ cask "displaylink" do
   if MacOS.version <= :mojave
     pkg "DisplayLink Software Installer.pkg"
   else
-    container type: :pkg
-    preflight do
-      # Renames the downloaded file to the correct name
-      system("mv \"#{caskroom_path}/#{version}/file?id=#{version.after_comma}\" \"#{caskroom_path}/#{version}/DisplayLink Manager Graphics Connectivity #{version.before_comma}.pkg\"")
-    end
-    pkg "DisplayLink Manager Graphics Connectivity #{version.before_comma}.pkg"
+    pkg "DisplayLink Manager Graphics Connectivity#{version.before_comma}-EXE.pkg"
   end
 
   uninstall pkgutil:   "com.displaylink.*",
@@ -60,6 +53,6 @@ cask "displaylink" do
 
   caveats do
     reboot
-    license @cask.url.to_s
+    license "https://www.synaptics.com/products/displaylink-graphics/downloads/macos-#{version.before_comma}"
   end
 end
