1caef3919a5b60cc6ad31d0ee8699c7972532b25
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-01 11:11:30 +0800

    Update logitech-options.rb

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -1,36 +1,35 @@
 cask "logitech-options" do
-  if MacOS.version <= :sierra
-    version "7.14.77"
-    sha256 "e4df55642e04139fc93d955e949bf736196a404ed067d87f8de7eb9ac9117ece"
-  elsif MacOS.version <= :high_sierra
-    version "8.30.293"
-    sha256 "db5f2cd94960223bdf74f0db6fc009f82f80928fe2ce849202754bbdb720eb87"
-  elsif MacOS.version <= :mojave
+  if MacOS.version <= :mojave
     version "8.54.147"
     sha256 "7b7a8d7a498d868c90b4ffe7dfc50a7a39c25e1f61350702e87d4c771b3d6459"
+
+    url "https://download01.logi.com/web/ftp/pub/techsupport/options/Options_#{version}.zip",
+        verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
+
+    livecheck do
+      skip "No version information available"
+    end
   else
-    version "9.20.265"
-    sha256 "948f49141756cab91bcac6530a858b54794e7c5a8744e16a4514edb5c11dfce8"
+    version "9.40.75"
+    sha256 :no_check
+
+    url "https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.zip",
+        verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
+
+    livecheck do
+      url "https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.zip"
+      strategy :extract_plist
+    end
   end
 
-  url "https://www.logitech.com/pub/techsupport/options/Options_#{version}.zip"
   name "Logitech Options"
   desc "Software for Logitech devices"
   homepage "https://support.logitech.com/software/options"
 
-  livecheck do
-    url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,websoftware=ec86eb2b-8e0b-11e9-a62b-a944e73f7596"
-    regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
-  end
-
   auto_updates true
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :mojave"
 
-  if MacOS.version <= :high_sierra
-    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
-  else
-    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
-  end
+  pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
 
   uninstall launchctl: [
     "com.logi.bolt.app",
