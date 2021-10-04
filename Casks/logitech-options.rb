9128b437bd16dd8dcd276c076822116bd82640d4
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-04 13:47:49 +0800

    Update logitech-options.rb

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -1,5 +1,27 @@
 cask "logitech-options" do
-  if MacOS.version <= :mojave
+  if MacOS.version <= :sierra
+    version "7.14.77"
+    sha256 "e4df55642e04139fc93d955e949bf736196a404ed067d87f8de7eb9ac9117ece"
+
+    url "https://download01.logi.com/web/ftp/pub/techsupport/options/Options_#{version}.zip",
+        verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
+
+    livecheck do
+      url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.12"
+      regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
+    end
+  elsif MacOS.version <= :high_sierra
+    version "8.30.293"
+    sha256 "db5f2cd94960223bdf74f0db6fc009f82f80928fe2ce849202754bbdb720eb87"
+
+    url "https://download01.logi.com/web/ftp/pub/techsupport/options/Options_#{version}.zip",
+        verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
+
+    livecheck do
+      url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.13"
+      regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
+    end
+  elsif MacOS.version <= :mojave
     version "8.54.147"
     sha256 "7b7a8d7a498d868c90b4ffe7dfc50a7a39c25e1f61350702e87d4c771b3d6459"
 
@@ -28,9 +50,13 @@ cask "logitech-options" do
   homepage "https://support.logitech.com/software/options"
 
   auto_updates true
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :sierra"
 
-  pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
+  if MacOS.version <= :high_sierra
+    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
+  else
+    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
+  end
 
   uninstall launchctl: [
     "com.logi.bolt.app",
