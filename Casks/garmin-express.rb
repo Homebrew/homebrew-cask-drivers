bc41fc3dfa63a37c3cfdb0483ddbc9f2ad97acc1
osx user <gili.shemesh@gmail.com>
2021-10-11 11:44:10 +0300

    added zap santaza descripbed in https://support.garmin.com/en-US/?faq=6nBhfHhXik18XrRU6BfULA

--- Casks/garmin-express.rb
+++ Casks/garmin-express.rb
@@ -4,9 +4,18 @@ cask "garmin-express" do
 
   url "https://download.garmin.com/omt/express/GarminExpress.dmg"
   name "Garmin Express"
+  desc "Update maps and software, sync with Garmin Connect and register your device"
   homepage "https://www.garmin.com/en-US/software/express"
 
   pkg "Install Garmin Express.pkg"
 
   uninstall pkgutil: "com.garmin.renu.client"
+
+  zap trash: [
+    "~/Library/Preferences/com.garmin.renu*",
+    "~/Library/Application Support/Garmin/Express",
+    "~/Library/Caches/com.garmin.renu.client",
+    "~/Library/Caches/com.garmin.renu.service.crashreporter",
+    "~/Library/Caches/com.garmin.renu.service",
+  ]
 end
