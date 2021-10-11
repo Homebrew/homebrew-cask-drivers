55895cae811d73bea1dfeaa176d38f3985acd63a
shemeshg <gili.shemesh@gmail.com>
2021-10-11 13:05:08 +0300

    added zap santaza described in https://support.garmin.com/en-US/?faq… (#2460)
    * added zap santaza descripbed in https://support.garmin.com/en-US/?faq=6nBhfHhXik18XrRU6BfULA
    
    * Update garmin-express.rb
    
    * Update garmin-express.rb
    
    * add uninstall quit
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/garmin-express.rb
+++ Casks/garmin-express.rb
@@ -1,12 +1,27 @@
 cask "garmin-express" do
-  version :latest
+  version "7.9.0.0,7090000"
   sha256 :no_check
 
   url "https://download.garmin.com/omt/express/GarminExpress.dmg"
   name "Garmin Express"
+  desc "Update maps and software, sync with Garmin Connect and register your device"
   homepage "https://www.garmin.com/en-US/software/express"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   pkg "Install Garmin Express.pkg"
 
-  uninstall pkgutil: "com.garmin.renu.client"
+  uninstall quit:    "com.garmin.renu.client",
+            pkgutil: "com.garmin.renu.client"
+
+  zap trash: [
+    "~/Library/Application Support/Garmin/Express",
+    "~/Library/Caches/com.garmin.renu.client",
+    "~/Library/Caches/com.garmin.renu.service",
+    "~/Library/Caches/com.garmin.renu.service.crashreporter",
+    "~/Library/Preferences/com.garmin.renu*",
+  ]
 end
