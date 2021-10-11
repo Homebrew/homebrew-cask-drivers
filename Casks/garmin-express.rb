f669c446d4f5bde2b4c104fab870d365bce6b357
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-11 17:27:56 +0800

    Update garmin-express.rb

--- Casks/garmin-express.rb
+++ Casks/garmin-express.rb
@@ -1,5 +1,5 @@
 cask "garmin-express" do
-  version :latest
+  version "7090000"
   sha256 :no_check
 
   url "https://download.garmin.com/omt/express/GarminExpress.dmg"
@@ -7,15 +7,20 @@ cask "garmin-express" do
   desc "Update maps and software, sync with Garmin Connect and register your device"
   homepage "https://www.garmin.com/en-US/software/express"
 
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
   pkg "Install Garmin Express.pkg"
 
   uninstall pkgutil: "com.garmin.renu.client"
 
   zap trash: [
-    "~/Library/Preferences/com.garmin.renu*",
     "~/Library/Application Support/Garmin/Express",
     "~/Library/Caches/com.garmin.renu.client",
-    "~/Library/Caches/com.garmin.renu.service.crashreporter",
     "~/Library/Caches/com.garmin.renu.service",
+    "~/Library/Caches/com.garmin.renu.service.crashreporter",
+    "~/Library/Preferences/com.garmin.renu*",
   ]
 end
