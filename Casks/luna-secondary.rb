7a6c996e81d86962dedbd6ca2036b767c1e3f3a7
John Benninghoff <jbenninghoff@mac.com>
2021-10-17 18:31:44 -0500

    Update luna-secondary from 4.5.0 to 5.0.1 (#2472)

--- Casks/luna-secondary.rb
+++ Casks/luna-secondary.rb
@@ -1,22 +1,25 @@
 cask "luna-secondary" do
-  version "4.5.0"
-  sha256 "38fca2cafa45cc08d12dedf5973850b1dcf99f35ab20428927ca4fa7c6a91dda"
+  version "5.0.1,4179"
+  sha256 "5216f292e8cf13befd771ca609cb667f0bd004193826009bdeeedbd125941e79"
 
-  url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaSecondary-#{version}.dmg",
-      verified: "s3.amazonaws.com/s3.lunadisplay.com/"
+  url "https://downloads.astropad.com/luna-secondary/mac/LunaSecondary-#{version.before_comma}.#{version.after_comma}.dmg"
   name "Luna Secondary"
   desc "Turn a computer or tablet into a second display"
-  homepage "https://lunadisplay.com/"
+  homepage "https://astropad.com/product/lunadisplay/"
 
   livecheck do
-    url "https://downloads.astropad.com/luna/latest-secondary"
-    strategy :header_match
+    url "https://s3.lunadisplay.com/downloads/luna-secondary-sparkle.xml"
+    strategy :sparkle
   end
 
   app "Luna Secondary.app"
 
   zap trash: [
+    "~/Library/Application Support/astropad",
+    "~/Library/Application Support/com.astro-hq.LunaSecondaryMac",
     "~/Library/Caches/com.astro-hq.LunaSecondaryMac",
+    "~/Library/Logs/Astropad",
     "~/Library/Preferences/com.astro-hq.LunaSecondaryMac.plist",
+    "~/Library/Saved Application State/com.astro-hq.LunaSecondaryMac.savedState",
   ]
 end
