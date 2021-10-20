c2dc8272c0836d29491671af33b8acf969700556
Bevan Kay <email@bevankay.me>
2021-10-20 14:50:15 +1100

    Update qflipper from 0.4.0 to 0.5.2 (#2479)

--- Casks/qflipper.rb
+++ Casks/qflipper.rb
@@ -1,6 +1,6 @@
 cask "qflipper" do
-  version "0.4.0"
-  sha256 "0761a9c76b8858d755739b3a117a1cf6c5f112abe03409f9f490f978494787e5"
+  version "0.5.2"
+  sha256 "5a3d0ab92d3d21ceb677ccc0a08172aacae3e870a04ab8c4f16a9f971c5c4d04"
 
   url "https://update.flipperzero.one/builds/qFlipper/#{version}/qFlipper-#{version}.dmg"
   name "qFlipper"
