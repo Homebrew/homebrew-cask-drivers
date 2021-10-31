0986e7e2ac59264ee1702ce02d56994bed205d1a
Bevan Kay <email@bevankay.me>
2021-10-31 21:22:17 +1100

    insta360-studio: update version, update livecheck, update url

--- Casks/insta360-studio.rb
+++ Casks/insta360-studio.rb
@@ -1,22 +1,24 @@
 cask "insta360-studio" do
-  version "3.6.4,2021_20210412_145507:20210412.bc8d698e971ae16f2517bc3e3c581278"
-  sha256 "8369259ce47cb23e9ae40c378636fdcf2bb91e179a781e80768399613147bd26"
+  version "4.0.1,16e0e5b15f9e5855b6f88a8fdc9fb430:2021_20211019_170852"
+  sha256 "c30f695742c9fd84e2ea78642f1145b74c8b4d4c74faa0c6967678e26ff55c77"
 
-  url "https://res.insta360.com/static/assets/storage/#{version.after_colon.major}/#{version.after_colon.minor}/Insta360_Studio_#{version.after_comma.before_colon}_signed.pkg"
+  url "https://res.insta360.com/static/#{version.after_comma.before_colon}/Insta360_Studio_#{version.after_comma.after_colon}_signed.pkg"
   name "Insta360 Studio"
   desc "Video and photo editor"
   homepage "https://www.insta360.com/"
 
   livecheck do
-    url "https://openapi.insta360.com/website/appDownload/getGroupApp?group=insta360-go2&X-Language=en-us"
+    url "https://openapi.insta360.com/app/appDownload/getGroupApp?group=insta360-go2&X-Language=en-us"
     strategy :page_match do |page|
       apps = JSON.parse(page)["data"]["apps"].find { |app| app["id"] == 38 }["items"]
                  .sort_by { |k| k["version"] }.reverse
       macos = apps.find { |item| item["platform"] == "mac" }
       v = macos["version"]
-      regex = %r{/(\d+)/([[:xdigit:]]+)/Insta360[._-]Studio[._-](\d+(?:[._-]\d+)*)[._-]signed\.pkg}i
+      regex = %r{/([[:xdigit:]]+)/Insta360[._-]Studio[._-](\d+(?:[._-]\d+)*)[._-]signed\.pkg}i
       match = macos["channels"][0]["download_url"].match(regex)
-      "#{v},#{match[3]}:#{match[1]}.#{match[2]}"
+      next if match.blank? || v.blank?
+
+      "#{v},#{match[1]}:#{match[2]}"
     end
   end
 
