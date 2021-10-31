656eaf56af7178830b7e3e6d74e2f989bea29609
flieger19 <constantin.dullo@gmail.com>
2021-11-01 00:33:24 +0100

    Add macOS Big Sur version to the 3Dconnexion cask (#2505)
    * Add macOS Big Sur version to the 3Dconnexion cask
    
    * Update Casks/3dconnexion.rb
    
    * update livechecks
    
    * fix livecheck for macOS Big Sure
    
    Co-authored-by: Constantin Dullo <constantin.dullo@me.com>
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/3dconnexion.rb
+++ Casks/3dconnexion.rb
@@ -1,21 +1,34 @@
 cask "3dconnexion" do
-  version "10-6-7,r3287:36E24890-6B5F-443a-8A9F-1851F9ADB985"
-  sha256 "4752bd4297733743fb512121116b536ffe260152f97134398d028b9936bc26f9"
+  if MacOS.version <= :catalina
+    version "10-6-7,r3287:36E24890-6B5F-443a-8A9F-1851F9ADB985"
+    sha256 "4752bd4297733743fb512121116b536ffe260152f97134398d028b9936bc26f9"
 
-  url "https://download.3dconnexion.com/drivers/mac/#{version.before_comma}_#{version.after_colon}/3DxWareMac_v#{version.before_comma}_#{version.after_comma.before_colon}.dmg"
+    url "https://download.3dconnexion.com/drivers/mac/#{version.before_comma}_#{version.after_colon}/3DxWareMac_v#{version.before_comma}_#{version.after_comma.before_colon}.dmg"
+
+    livecheck do
+      url "https://3dconnexion.com/us/drivers/"
+      strategy :page_match do |page|
+        match = page.match(%r{href=.*?_([\dA-F]+(?:-[\dA-F]+)*)/3DxWareMac_v(\d+(?:-\d+)*)_(r\d+)\.dmg}i)
+        next if match.blank?
+
+        "#{match[2]},#{match[3]}:#{match[1]}"
+      end
+    end
+  else
+    version "10-7-0_r3386"
+    sha256 "bebd9c01c96bfb9411fd5b4ccd2bbba3b798cba001b0e200547d0e9c3dcbf893"
+
+    url "https://download.3dconnexion.com/drivers/technical_support/3DxWareMac_v#{version}.dmg"
+
+    livecheck do
+      url "https://3dconnexion.com/us/support/faq/beta-driver-for-macos-11-big-sur/"
+      regex(/href=.*?3DxWareMac_v(\d+(?:-\d+)*_r\d+)\.dmg/i)
+    end
+  end
+
   name "3Dconnexion"
   desc "3DxWare Driver"
   homepage "https://3dconnexion.com/"
-
-  livecheck do
-    url "https://3dconnexion.com/us/drivers/"
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?_([\dA-F]+(?:-[\dA-F]+)*)/3DxWareMac_v(\d+(?:-\d+)*)_(r\d+)\.dmg}i)
-      next if match.blank?
-
-      "#{match[2]},#{match[3]}:#{match[1]}"
-    end
-  end
 
   depends_on macos: ">= :yosemite"
 
