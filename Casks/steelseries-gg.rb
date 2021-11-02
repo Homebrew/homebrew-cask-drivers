4e1cdf0ddcb2377f1472699110c1f3debfbc908f
Alexander L <lackner.alex@gmail.com>
2021-11-02 03:07:40 +0100

    Update steelseries-gg from 10.0.0 to 11.1.0 (#2506)
    * Update steelseries-gg from 10.0.0 to 11.1.0
    
    * Update steelseries-gg.rb
    
    * Update steelseries-gg.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/steelseries-gg.rb
+++ Casks/steelseries-gg.rb
@@ -1,14 +1,15 @@
 cask "steelseries-gg" do
-  version "10.0.0"
-  sha256 "b8f4d88c93257f02de29b675d290978d8739444e7368be3a7e3053308c7dc0d4"
+  version "11.1.0"
+  sha256 "38e9fbe1e984083ca48194868e031bab7ce1a3a7e5959c51ab27ded3b5c1dbbe"
 
-  url "https://steelseries.com/gg/downloads/gg/#{version}/darwin"
+  url "https://engine.steelseriescdn.com/SteelSeriesGG#{version}.pkg",
+      verified: "engine.steelseriescdn.com/"
   name "SteelSeries GG #{version.major}"
   desc "Settings for SteelSeries peripherals and accessories"
   homepage "https://steelseries.com/gg"
 
   livecheck do
-    url "https://steelseries.com/engine/latest/darwin"
+    url "https://steelseries.com/gg/downloads/gg/latest/darwin"
     strategy :header_match
   end
 
