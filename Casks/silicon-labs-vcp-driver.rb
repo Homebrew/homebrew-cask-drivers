073c6dbd966d5b48b50979683f9460a2f478f1d5
fnmeyer <fnmeyer@gmail.com>
2021-11-06 12:36:54 -0400

    silicon-labs-vcp-driver update to 6.0.2 (#2517)
    * Silicon-labs-vcp-driver update to 6.02
    
    * Silicon-labs-vcp-driver update to 6.02 - change desc

--- Casks/silicon-labs-vcp-driver.rb
+++ Casks/silicon-labs-vcp-driver.rb
@@ -1,11 +1,11 @@
 cask "silicon-labs-vcp-driver" do
-  version "6.0.1"
+  version "6.0.2"
   sha256 :no_check
 
   url "https://www.silabs.com/documents/public/software/Mac_OSX_VCP_Driver.zip"
   appcast "https://www.silabs.com/documents/public/release-notes/Mac_OSX_VCP_Driver_Release_Notes.txt"
   name "Silicon Labs VCP Driver"
-  name "CP210x USB to UART Bridge VCP Driver"
+  desc "CP210x USB to UART Bridge VCP Driver"
   homepage "https://www.silabs.com/products/development-tools/software/usb-to-uart-bridge-vcp-drivers"
 
   container nested: "macOS_VCP_Driver/SiLabsUSBDriverDisk.dmg"
