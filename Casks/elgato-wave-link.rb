8eaab2bd99a256be1140264387abd23eea4d4df8
Bevan Kay <email@bevankay.me>
2021-11-09 15:18:12 +1100

    Update elgato-wave-link from 1.3.0.2689 to 1.3.2.2785 (#2522)

--- Casks/elgato-wave-link.rb
+++ Casks/elgato-wave-link.rb
@@ -1,6 +1,6 @@
 cask "elgato-wave-link" do
-  version "1.3.0.2689"
-  sha256 "8e8f28b2b66ff00b3f70e93716c735cc6f0564d26fafb9c3c26544dda2cd7cdd"
+  version "1.3.2.2785"
+  sha256 "f03972a22d7fdc67debaa5b6941794ded3cfff42573a4b3b3cdc9a712eafe985"
 
   url "https://edge.elgato.com/egc/macos/wavelink/#{version.major_minor_patch}/WaveLink_#{version}.pkg"
   name "Elgato Wave Link"
