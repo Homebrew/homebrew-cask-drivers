e33edb0e3e1bd30d7c19d196819d9592f01b158f
Bevan Kay <email@bevankay.me>
2021-11-10 16:00:18 +1100

    Update nordic-nrf-connect from 3.8.0 to 3.9.0

--- Casks/nordic-nrf-connect.rb
+++ Casks/nordic-nrf-connect.rb
@@ -1,6 +1,6 @@
 cask "nordic-nrf-connect" do
-  version "3.8.0"
-  sha256 "bb8d03ce5ea0fd559063fe4cd243b266519549630396830c8249c73b45fb426c"
+  version "3.9.0"
+  sha256 "c16888cbd69f668cd885af161fb5fa7b9c949e1a6c3e33249f77cb5023a1ea88"
 
   url "https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases/download/v#{version}/nrfconnect-#{version}.dmg",
       verified: "github.com/NordicSemiconductor/pc-nrfconnect-core/"
