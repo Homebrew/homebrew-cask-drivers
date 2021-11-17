cef0438a1fad8faaf958389fd69f6ce6ffb01e7d
Bevan Kay <email@bevankay.me>
2021-11-17 17:39:54 +1100

    Update logitech-g-hub from 2021.10.8013 to 2021.12.4779

--- Casks/logitech-g-hub.rb
+++ Casks/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2021.10.8013"
+  version "2021.12.4779"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
