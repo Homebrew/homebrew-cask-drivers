0c80014b3a885729ee6fcb872a87cee637c5255a
Bevan Kay <email@bevankay.me>
2021-11-18 21:22:27 +1100

    Update 1kc-razer from 0.4.5 to 0.4.6

--- Casks/1kc-razer.rb
+++ Casks/1kc-razer.rb
@@ -1,6 +1,6 @@
 cask "1kc-razer" do
-  version "0.4.5"
-  sha256 "8187f9fed1d8160f9d954294ee567af2d833325ada3218f138317b5b944f5fdd"
+  version "0.4.6"
+  sha256 "ae42ff5be42b735e284023f37c9fabba927594527bf26474cd5418cb04b599f4"
 
   url "https://github.com/1kc/razer-macos/releases/download/v#{version}/Razer.macOS-#{version}-universal.dmg"
   name "Razer macOS"
