ef3fb75f18a0e3cfef706d19deb6783a7d372cf4
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-22 14:23:05 -0800

    brother-p-touch-update-software: replace colon with comma

--- Casks/brother-p-touch-update-software.rb
+++ Casks/brother-p-touch-update-software.rb
@@ -1,8 +1,8 @@
 cask "brother-p-touch-update-software" do
-  version "1.5.6,110:dlfp100885"
+  version "1.5.6,110,dlfp100885"
   sha256 "179cd72961d1f2b8fba1146a4b636ca1529e8fc8adecac0165111ac36bd85a7a"
 
-  url "https://download.brother.com/welcome/#{version.after_colon}/pum#{version.before_comma.no_dots}x#{version.after_comma.before_colon}all.dmg"
+  url "https://download.brother.com/welcome/#{version.csv[2]}/pum#{version.csv[0].no_dots}x#{version.csv[1]}all.dmg"
   name "Brother P-touch Update Software"
   homepage "https://support.brother.com/"
 
