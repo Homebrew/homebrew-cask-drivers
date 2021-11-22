18a65438b12fc933a4cecaff13337a80d7fb34b6
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-22 14:26:39 -0800

    insta360-studio: replace colon in version string with comma

--- Casks/insta360-studio.rb
+++ Casks/insta360-studio.rb
@@ -1,8 +1,8 @@
 cask "insta360-studio" do
-  version "4.1.0,9c3c4473273192e24b4143306d29d3ea:2021_20211105_112742"
+  version "4.1.0,9c3c4473273192e24b4143306d29d3ea,2021_20211105_112742"
   sha256 "c037f5aa18c8f5ce606963be0bba6b737b20a6c9056839e8ca8dd830a8737e4e"
 
-  url "https://res.insta360.com/static/#{version.after_comma.before_colon}/Insta360_Studio_#{version.after_comma.after_colon}_signed.pkg"
+  url "https://res.insta360.com/static/#{version.csv[1]}/Insta360_Studio_#{version.csv[2]}_signed.pkg"
   name "Insta360 Studio"
   desc "Video and photo editor"
   homepage "https://www.insta360.com/"
@@ -18,11 +18,11 @@ cask "insta360-studio" do
       match = macos["channels"][0]["download_url"].match(regex)
       next if match.blank? || v.blank?
 
-      "#{v},#{match[1]}:#{match[2]}"
+      "#{v},#{match[1]},#{match[2]}"
     end
   end
 
-  pkg "Insta360_Studio_#{version.after_comma.after_colon}_signed.pkg"
+  pkg "Insta360_Studio_#{version.csv[2]}_signed.pkg"
 
   uninstall quit:    "com.insta360.studio",
             pkgutil: [
