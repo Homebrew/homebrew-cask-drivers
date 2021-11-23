57ec952f63ff28c27021e296bf3522a932f873aa
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-22 21:44:38 -0800

    Add livecheck

--- Casks/brother-p-touch-update-software.rb
+++ Casks/brother-p-touch-update-software.rb
@@ -1,10 +1,39 @@
 cask "brother-p-touch-update-software" do
-  version "1.5.6,110,dlfp100885"
+  os = {
+    big_sur:  "10064",
+    monterey: "10071",
+  }
+
+  version "1.5.6,110,100885"
   sha256 "179cd72961d1f2b8fba1146a4b636ca1529e8fc8adecac0165111ac36bd85a7a"
 
-  url "https://download.brother.com/welcome/#{version.csv[2]}/pum#{version.csv[0].no_dots}x#{version.csv[1]}all.dmg"
+  url "https://download.brother.com/welcome/dlfp#{version.csv[2]}/pum#{version.csv[0].no_dots}x#{version.csv[1]}all.dmg"
   name "Brother P-touch Update Software"
   homepage "https://support.brother.com/"
+
+  livecheck do
+    url "https://support.brother.com/g/b/downloadlist.aspx?c=nz&lang=en&prod=d800weas&os=#{os[MacOS.version.to_sym]}"
+    strategy :page_match do |page|
+      download_match = page.match(/href=["']?([^"' >]*?)["']?>P-touch Update Software \(Mac\)</i)
+      next if download_match.blank?
+
+      version_match = download_match.post_match.match(/\((\d+(?:\.\d+)+)\)/)
+      next if version_match.blank?
+
+      download_page = Homebrew::Livecheck::Strategy.page_content(
+        URI.join("https://support.brother.com/", download_match[1].sub("downloadend.aspx", "downloadhowto.aspx")),
+      )
+      next if download_page[:content].blank?
+
+      dlfp_match = download_page[:content].match(%r{href=.*?/dlfp(\d+)/pum(\d+)x(\d+)all\.dmg}i)
+      next if dlfp_match.blank?
+      next if dlfp_match[2] != version_match[1].tr(".", "")
+
+      "#{version_match[1]},#{dlfp_match[3]},#{dlfp_match[1]}"
+    end
+  end
+
+  depends_on macos: os.keys
 
   pkg "BrotherPtUpdateSoftware.pkg"
 
