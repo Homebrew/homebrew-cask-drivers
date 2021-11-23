fba146933b508f33d5b7ed8828b12469e8cf58f8
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-22 19:19:19 -0800

    apple-hewlett-packard-printer-drivers: use `version.csv` (#2547)
    * apple-hewlett-packard-printer-drivers: use `version.csv`
    
    * Update apple-hewlett-packard-printer-drivers.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/apple-hewlett-packard-printer-drivers.rb
+++ Casks/apple-hewlett-packard-printer-drivers.rb
@@ -2,7 +2,7 @@ cask "apple-hewlett-packard-printer-drivers" do
   version "5.1,2020,001-41745-20201210-DBC9B46B-88B2-4032-87D9-449AF1D20804"
   sha256 "9ee54766e32cdd3ce6a0ff019aad02400eac66fc669c991f47a6c37379cf8fac"
 
-  url "https://updates.cdn-apple.com/#{version.after_comma.before_comma}/macos/#{version.after_comma.after_comma}/HewlettPackardPrinterDrivers.dmg",
+  url "https://updates.cdn-apple.com/#{version.csv[1]}/macos/#{version.csv[2]}/HewlettPackardPrinterDrivers.dmg",
       verified: "updates.cdn-apple.com/"
   name "HP Printer Drivers"
   desc "HP printing and scanning software"
@@ -13,6 +13,8 @@ cask "apple-hewlett-packard-printer-drivers" do
     strategy :page_match do |page|
       version = page.match(/"strTitle":.*?v(\d+(?:\.\d+)+)\s*for\s*macOS/i)
       metaurl = page.match(%r{"metaUrl":.*?/(\d+)/macos/([\dA-F]+(?:-[\dA-F]+)*)/HewlettPackardPrinterDrivers\.dmg}i)
+      next if version.blank? || metaurl.blank?
+
       "#{version[1]},#{metaurl[1]},#{metaurl[2]}"
     end
   end
@@ -20,15 +22,6 @@ cask "apple-hewlett-packard-printer-drivers" do
   pkg "HewlettPackardPrinterDrivers.pkg"
 
   uninstall quit:    [
-    "com.hp.HP-Scanner",
-    "com.hp.HPAiOScan",
-    "com.hp.HPAiOTulip",
-    "com.hp.HPDOT4Scan",
-    "com.hp.HPM1210_1130.HP_LaserJet_Professional_Utility",
-    "com.hp.HPSOAPScan",
-    "com.hp.HP_LaserJet_Professional_Utility",
-    "com.hp.LEDMScan",
-    "com.hp.ScanService",
     "com.hp.aio.faxarchive",
     "com.hp.customer.uploader",
     "com.hp.devicemodel.hpdot4d",
@@ -36,11 +29,20 @@ cask "apple-hewlett-packard-printer-drivers" do
     "com.hp.dm.hpdot4d",
     "com.hp.event.status.handler.generic",
     "com.hp.events.*",
+    "com.hp.HP_LaserJet_Professional_Utility",
+    "com.hp.HP-Scanner",
+    "com.hp.HPAiOScan",
+    "com.hp.HPAiOTulip",
     "com.hp.hpalerts.plugin.*",
+    "com.hp.HPDOT4Scan",
+    "com.hp.HPM1210_1130.HP_LaserJet_Professional_Utility",
+    "com.hp.HPSOAPScan",
+    "com.hp.LEDMScan",
     "com.hp.printerutility.*",
     "com.hp.productresearch.*",
     "com.hp.scan.*",
     "com.hp.scanModule.*",
+    "com.hp.ScanService",
   ],
             signal:  ["TERM", "com.hp.printerutility"],
             kext:    "com.hp.kext.io.enabler.compound",
