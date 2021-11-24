afb97f8aa5c9aca5d9b2056b2567bd6f5c75db59
Vitaliy Babichev <bvitaliyg@gmail.com>
2021-11-24 11:11:11 +0000

    Adding ROLI Connect (#2549)
    * Added roli connect
    
    * Self-review changes
    
    * Update roli-connect.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/roli-connect.rb
@@ -0,0 +1,28 @@
+cask "roli-connect" do
+  version "1.0.18"
+  sha256 :no_check
+
+  url "https://api.roli.com/api/v1/product/com.roli.hub/latest/osx?flavor=general-edition"
+  name "ROLI Connect"
+  desc "Tools manager for ROLI products"
+  homepage "https://roli.com/"
+
+  livecheck do
+    url :url
+    strategy :header_match
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "ROLI Connect.app"
+
+  uninstall quit: "com.roli.hub"
+
+  zap trash: [
+    "~/Library/Application Support/ROLI Connect",
+    "~/Library/Logs/ROLI Connect",
+    "~/Library/Preferences/com.roli.hub.helper.plist",
+    "~/Library/Preferences/com.roli.hub.plist",
+    "~/Library/Saved Application State/com.roli.hub.savedState",
+  ]
+end
