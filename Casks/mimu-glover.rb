bbcede9f91cf59391cc90f11828640627c7bf254
Tomas Kubla <tomas@kubla.sk>
2021-11-25 12:37:35 +0000

    Add mimu-glover 1.1.0

---
+++ Casks/mimu-glover.rb
@@ -0,0 +1,22 @@
+cask "mimu-glover" do
+  version "1.1.0"
+  sha256 "1b3eaf2dffe2496d5570283d918f517b5efa3251788678e9a81c4ba280bd33bd"
+
+  url "https://static.mimugloves.com/Glover/Glover-MacOS-#{version}.zip"
+  name "Glover"
+  desc "MiMU Gloves Software (as well for Leap motion, micro:bit, iPhone, OSC device)"
+  homepage "https://mimugloves.com/glover/"
+
+  livecheck do
+    url "https://mimugloves.com/glover/"
+    strategy :page_match
+    regex(%r{https://static\.mimugloves\.com/Glover/Glover-MacOS-(\d+(?:\.\d+)*)\.zip}i)
+  end
+
+  app "Glover.app"
+
+  zap trash: [
+    "~/Library/Application Support/Glover",
+    "~/Library/Caches/Glover",
+  ]
+end
