4ae47a93035dfabf147629f6570d9cdd136b3ea1
kiblik <kiblik@gjh.sk>
2021-11-25 22:44:07 +0000

    Add Wave for work 0.3.7 (#2553)
    * Add Wave for work 0.3.7
    
    * clean livecheck
    
    * Update genki-wave-for-work.rb
    
    * move com.genkiinstruments.Wave.Work
    
    * update unintsall stanza
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

---
+++ Casks/genki-wave-for-work.rb
@@ -0,0 +1,25 @@
+cask "genki-wave-for-work" do
+  version "0.3.7"
+  sha256 :no_check
+
+  url "https://bitbucket.org/genki_instruments/software_releases/downloads/Wave_for_Work_mac.zip",
+      verified: "https://bitbucket.org/genki_instruments/software_releases/"
+  name "Wave for work"
+  desc "Wave Ring Software"
+  homepage "https://genkiinstruments.com/products/wave-for-work"
+
+  livecheck do
+    url "https://bitbucket.org/genki_instruments/software_releases/downloads/appcast_wfw.xml"
+    regex(/sparkle:shortVersionString="(\d+(?:\.\d+)+)"\s+sparkle:os="macos"/i)
+  end
+
+  pkg "Install_Wave_for_Work.pkg"
+
+  uninstall pkgutil:   "com.genkiinstruments.waveforwork",
+            launchctl: "com.genkiinstruments.Wave.Work"
+
+  zap trash: [
+    "~/Library/Preferences/com.genkiinstruments.waveforwork.plist",
+    "~/Library/Wave for Work",
+  ]
+end
