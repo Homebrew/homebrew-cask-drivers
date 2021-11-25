f44bcf09eacc8c47010cbfbe682606d4a8baaf9a
Bevan Kay <email@bevankay.me>
2021-11-25 22:09:42 +1100

    rename, update livecheck, update desc

---
+++ Casks/genkiinstruments-softwave.rb
@@ -0,0 +1,26 @@
+cask "genkiinstruments-softwave" do
+  version "0.12.1"
+  sha256 :no_check
+
+  url "https://bitbucket.org/genki_instruments/software_releases/downloads/Softwave_mac.zip",
+      verified: "https://bitbucket.org/genki_instruments/software_releases/"
+  name "Softwave"
+  desc "Wave Ring Software"
+  homepage "https://genkiinstruments.com/products/softwave"
+
+  livecheck do
+    url "https://bitbucket.org/genki_instruments/software_releases/downloads/appcast.xml"
+    regex(/shortVersionString="(\d+(?:\.\d+)*)"\s+sparkle:os="macos"/i)
+  end
+
+  pkg "Softwave.pkg"
+
+  uninstall pkgutil: [
+    "com.genkiinstruments.softwave",
+  ]
+
+  zap trash: [
+    "~/Library/Preferences/com.genkiinstruments.softwave.plist",
+    "~/Library/Softwave",
+  ]
+end
