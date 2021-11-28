8d1327156d9b1a14bff76fde224459cbd3ed5b03
Alex Oswald <oswald.al@northeastern.edu>
2021-11-28 16:21:08 -0500

    Update fujifilm-x-raw-studio from 1.12.0 to 1.14.0

--- Casks/fujifilm-x-raw-studio.rb
+++ Casks/fujifilm-x-raw-studio.rb
@@ -1,10 +1,11 @@
 cask "fujifilm-x-raw-studio" do
-  version "1.12.0,021aqi7m"
-  sha256 "3a8cdb3613d381ebb5ef8f46fa1b110242865d42d444cc2902892698fd89d735"
+  version "1.14.0,nupc0929"
+  sha256 "02a98c0ba981fbbd50839e8b4e368f66f83cfaefc446f627d35826620379554a"
 
   url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.before_comma.no_dots}-#{version.after_comma}/XRawStudio#{version.before_comma.no_dots}.dmg"
   appcast "https://fujifilm-x.com/en-us/support/download/software/x-raw-studio/"
   name "FUJIFILM X RAW STUDIO"
+  desc "Connect to Fujifilm cameras to extract and convert RAW files the way you want"
   homepage "https://fujifilm-x.com/en-us/support/download/software/x-raw-studio/"
 
   depends_on macos: ">= :sierra"
