cd60027026211e9de94c414365733c10e5ea48b8
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-29 07:41:20 +0800

    Update fujifilm-x-raw-studio.rb

--- Casks/fujifilm-x-raw-studio.rb
+++ Casks/fujifilm-x-raw-studio.rb
@@ -1,12 +1,21 @@
 cask "fujifilm-x-raw-studio" do
-  version "1.14.0,nupc0929"
+  version "1140,nupc0929"
   sha256 "02a98c0ba981fbbd50839e8b4e368f66f83cfaefc446f627d35826620379554a"
 
-  url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.before_comma.no_dots}-#{version.after_comma}/XRawStudio#{version.before_comma.no_dots}.dmg"
-  appcast "https://fujifilm-x.com/en-us/support/download/software/x-raw-studio/"
+  url "https://dl.fujifilm-x.com/support/software/x-raw-studio-mac#{version.csv.first}-#{version.csv.second}/XRawStudio#{version.csv.first}.dmg"
   name "FUJIFILM X RAW STUDIO"
   desc "Connect to Fujifilm cameras to extract and convert RAW files the way you want"
   homepage "https://fujifilm-x.com/en-us/support/download/software/x-raw-studio/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      match = page.match(%r{x[._-]raw[._-]studio[._-]macv?(\d*)[._-](.*)/XRawStudio(\d*)\.dmg}i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   depends_on macos: ">= :sierra"
 
