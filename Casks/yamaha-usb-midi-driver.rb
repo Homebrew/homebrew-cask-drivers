847b72bee923cd58962e038ce4efc79fc8a80781
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-29 08:10:41 +0800

    Update yamaha-usb-midi-driver.rb

--- Casks/yamaha-usb-midi-driver.rb
+++ Casks/yamaha-usb-midi-driver.rb
@@ -3,14 +3,14 @@ cask "yamaha-usb-midi-driver" do
   sha256 "77481dfeebe0de9df780245d365c6669f301e2cd24176d7bd705e5cdbdde95cf"
 
   url "https://usa.yamaha.com/files/download/software/8/1400138/um#{version.no_dots}mx.zip"
-  name "Yamaha USB MIDI driver"
+  name "yamaha-usb-midi-driver"
   desc "MIDI driver for Yamaha instruments"
   homepage "https://usa.yamaha.com/support/updates/usb_midi_driver_for_mac.html"
 
   livecheck do
     url :homepage
     strategy :page_match
-    regex(/USB-MIDI\sDriver\sV?(\d+(?:\.\d+)+)\sfor\sMac/i)
+    regex(%r{<h1>.*?V(\d+(?:\.\d+)*(?:-\d+)?).*?</h1>}i)
   end
 
   pkg "um#{version.no_dots}mx/Yamaha USB-MIDI Driver V#{version}.pkg"
