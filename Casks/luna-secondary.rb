a5281a8615626ce3f44d4967fc847489050a3c3c
Bevan Kay <email@bevankay.me>
2021-12-02 09:28:05 +1100

    Update luna-secondary.rb

--- Casks/luna-secondary.rb
+++ Casks/luna-secondary.rb
@@ -2,7 +2,7 @@ cask "luna-secondary" do
   version "5.1.0,4190"
   sha256 "27d17ba2da5aa68c833e8467d590252aac0daa0b2f028f47813471c08c64d474"
 
-  url "https://downloads.astropad.com/luna-secondary/mac/LunaSecondary-#{version.before_comma}.#{version.after_comma}.dmg"
+  url "https://downloads.astropad.com/luna-secondary/mac/LunaSecondary-#{version.csv.first}.#{version.csv.second}.dmg"
   name "Luna Secondary"
   desc "Turn a computer or tablet into a second display"
   homepage "https://astropad.com/product/lunadisplay/"
