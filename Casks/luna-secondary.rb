fdf08ae26df94ffa99edfd6442bcd7a0d5f55f49
John Benninghoff <jbenninghoff@mac.com>
2021-12-01 16:34:38 -0600

    Update luna-secondary from 5.0.2 to 5.1.0 (#2560)
    * Update luna-secondary from 5.0.2 to 5.1.0
    
    * Update luna-secondary.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/luna-secondary.rb
+++ Casks/luna-secondary.rb
@@ -1,8 +1,8 @@
 cask "luna-secondary" do
-  version "5.0.2,4184"
-  sha256 "2f17903ce181e7718cab87edc96cd19873650913b0501af6bd074145dde9472e"
+  version "5.1.0,4190"
+  sha256 "27d17ba2da5aa68c833e8467d590252aac0daa0b2f028f47813471c08c64d474"
 
-  url "https://downloads.astropad.com/luna-secondary/mac/LunaSecondary-#{version.before_comma}.#{version.after_comma}.dmg"
+  url "https://downloads.astropad.com/luna-secondary/mac/LunaSecondary-#{version.csv.first}.#{version.csv.second}.dmg"
   name "Luna Secondary"
   desc "Turn a computer or tablet into a second display"
   homepage "https://astropad.com/product/lunadisplay/"
