40f7d2a0d624cf7d58b3cecdae626e87ad8c9be0
Bevan Kay <email@bevankay.me>
2021-12-02 14:25:30 +1100

    Add livecheck

--- Casks/lovense-remote.rb
+++ Casks/lovense-remote.rb
@@ -3,10 +3,14 @@ cask "lovense-remote" do
   sha256 :no_check
 
   url "https://lovense.com/files/apps/remote/remote.dmg"
-  appcast "https://lovense.com/sextoys/download"
   name "Lovense Remote"
   desc "Remote for Lovense products"
   homepage "https://lovense.com/"
+
+  livecheck do
+    url "https://lovense.com/sextoys/download"
+    regex(/type=.*?Mac[-._\s]?v?[-._\s]?(\d+(?:\.\d+)+)[\s<"]/i)
+  end
 
   app "Lovense_Remote.app"
 end
