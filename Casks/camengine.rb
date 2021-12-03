6bbac3c9db05d637ed9cfe4abb232c8778146f8c
Alexander Goldstein <alexg@alexland.org>
2021-12-03 14:21:38 -0600

    camengine: removing version number hardcode

--- Casks/camengine.rb
+++ Casks/camengine.rb
@@ -1,13 +1,13 @@
 cask "camengine" do
-  version "4.0.63.2,21102201"
+  version "4.0.63.2,21102201,0.0.8000.46"
   sha256 "e2c184fcb617c664c6407525c026bc30d73b8b6cccc6d9519568a848c3adafa6"
 
-  url "https://storage.avermedia.com/web_release_www/software/AVerMedia_PW513_Firmware_0.0.8000.46_&_CamEngine_Mac_v#{version.before_comma}.zip"
+  url "https://storage.avermedia.com/web_release_www/software/AVerMedia_PW513_Firmware_#{version.csv.third}_&_CamEngine_Mac_v#{version.csv.first}.zip"
   name "camengine"
   desc "App to unleash the potential of the Live Streamer CAM"
   homepage "https://www.avermedia.com/us/product-detail/PW513"
 
-  pkg "Step2.CamEngineV4_v#{version.before_comma}_#{version.after_comma}.pkg",
+  pkg "Step2.CamEngineV4_v#{version.csv.first}_#{version.csv.second}.pkg",
       choices: [
         {
           "choiceIdentifier" => "PKG_REF_0",
