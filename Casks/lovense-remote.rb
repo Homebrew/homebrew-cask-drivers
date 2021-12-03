f01097652fa2655896fe7c089a66e3114840021a
Matt Coster <opensource@mtcoster.net>
2021-12-03 11:24:14 +0000

    Update lovense-remote from 1.4.3 to 1.5.6 (#2563)
    * Update lovense-remote from 1.4.3 to 1.5.6
    
    * Add livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/lovense-remote.rb
+++ Casks/lovense-remote.rb
@@ -1,12 +1,16 @@
 cask "lovense-remote" do
-  version "1.4.3"
+  version "1.5.6"
   sha256 :no_check
 
   url "https://lovense.com/files/apps/remote/remote.dmg"
-  appcast "https://lovense.com/sextoys/download"
   name "Lovense Remote"
   desc "Remote for Lovense products"
   homepage "https://lovense.com/"
+
+  livecheck do
+    url "https://lovense.com/sextoys/download"
+    regex(/type=.*?Mac[-._\s]?v?[-._\s]?(\d+(?:\.\d+)+)[\s<"]/i)
+  end
 
   app "Lovense_Remote.app"
 end
