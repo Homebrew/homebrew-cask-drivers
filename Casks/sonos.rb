ff1ea89da6d47cfddd845b500f9e5101198e6d04
Jason Tedor <jason@tedor.me>
2021-12-09 17:12:59 -0500

    Update Casks/sonos.rb

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,5 +1,5 @@
 cask "sonos" do
-  version "13.4.1"
+  version "13.4.1,66.4.23300"
   sha256 "f92473d3959ef36ef174a5b0e57c2883d170f5a2a904f134df783357c39fd188"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.before_comma.no_dots}.dmg"
