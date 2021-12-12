eec6ed72d89fae2130cc74c4ce0ca9ca9e8a6d75
Bevan Kay <email@bevankay.me>
2021-12-13 09:40:28 +1100

    Update hhkb-keymap-tool from 1.1.0 to 1.2.0

--- Casks/hhkb-keymap-tool.rb
+++ Casks/hhkb-keymap-tool.rb
@@ -1,6 +1,6 @@
 cask "hhkb-keymap-tool" do
-  version "1.1.0"
-  sha256 "b4544c9a0ab53f69739a056ddf6e3b7edb784889a5dcd32b42572ade985fa9db"
+  version "1.2.0"
+  sha256 "19ceae98bcf1ebbb6faaa8f8c33d396d439cedd5c4e7a49e4afc086c57e76cdb"
 
   url "https://origin.pfultd.com/downloads/hhkb/mac/HHKBkeymapTool_#{version.no_dots}ma.dmg",
       verified: "origin.pfultd.com"
