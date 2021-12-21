bb72d5f89e0de024256998106949102a389754fa
Doug Goldstein <cardoe@cardoe.com>
2021-12-21 15:21:55 -0600

    Update canon-mf-printer from 10.11.6 to 10.11.8 (#2610)
    * Update canon-mf-printer from 10.11.6 to 10.11.8
    
    * Add livecheck skip stanza
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/canon-mf-printer.rb
+++ Casks/canon-mf-printer.rb
@@ -1,12 +1,16 @@
 cask "canon-mf-printer" do
-  version "10.11.6"
-  sha256 "cded9cbfc4ad45f03258066fc96b3a7c2ee0f4e486043813b9242e3fd568c078"
+  version "10.11.8"
+  sha256 "d5f10278e0dd91a898757299adb4a566e5f14e6ad073c885da268768b7b8172e"
 
-  url "https://gdlp01.c-wss.com/gds/3/0100010873/03/mac-mf-v#{version.no_dots}-00.dmg",
+  url "https://gdlp01.c-wss.com/gds/3/0100011243/02/mac-mf-v#{version.no_dots}-00.dmg",
       verified: "gdlp01.c-wss.com/gds/"
   name "Canon MF Printer Driver & Utilities"
   desc "Printer driver & utilities for Canon imageCLASS MF printers"
   homepage "https://www.usa.canon.com/internet/portal/us/home/support/drivers-downloads"
+
+  livecheck do
+    skip "No version information available"
+  end
 
   depends_on macos: ">= :yosemite"
 
