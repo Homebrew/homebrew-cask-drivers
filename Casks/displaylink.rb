f29d8931f0e0acd51e7437f23a700333f9c4a891
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-26 11:24:27 +0800

    Update displaylink.rb

--- Casks/displaylink.rb
+++ Casks/displaylink.rb
@@ -2,19 +2,19 @@ cask "displaylink" do
   if MacOS.version <= :sierra
     version "4.3.1,2021-02"
     sha256 "d5cd6787d6c4ca6a2425984bcbab607e618e9803335455e24196e14e35657b97"
-    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20USB%20Graphics%20Software%20for%20Mac%20OS%20X%20and%20macOS#{version.before_comma}-EXE.dmg"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20Mac%20OS%20X%20and%20macOS#{version.csv.first}-EXE.dmg"
   elsif MacOS.version <= :high_sierra
     version "5.1.1,2021-02"
     sha256 "1ac9093f8113af8c35d6f3ff5b1ae3f119a5aff0d5309d75c7a1742f159184b5"
-    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.before_comma}-EXE.dmg"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.csv.first}-EXE.dmg"
   elsif MacOS.version <= :mojave
     version "5.2.6,2021-05"
     sha256 "9f1854cd5720105d6d45c91172419c503358543e4a23d7113387aedf16a39cbb"
-    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.before_comma}-EXE.dmg"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.csv.first}-EXE.dmg"
   else
     version "1.6,2021-12"
     sha256 "ea1a5f68fbc51d2604f55be03c787c0e1f341b6d50bca71d6da8066008e319f9"
-    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.after_comma}/DisplayLink%20Manager%20Graphics%20Connectivity#{version.before_comma}-EXE.pkg"
+    url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20Manager%20Graphics%20Connectivity#{version.csv.first}-EXE.pkg"
   end
 
   name "DisplayLink USB Graphics Software"
@@ -28,7 +28,7 @@ cask "displaylink" do
   if MacOS.version <= :mojave
     pkg "DisplayLink Software Installer.pkg"
   else
-    pkg "DisplayLink Manager Graphics Connectivity#{version.before_comma}-EXE.pkg"
+    pkg "DisplayLink Manager Graphics Connectivity#{version.csv.first}-EXE.pkg"
   end
 
   uninstall pkgutil:   "com.displaylink.*",
@@ -39,9 +39,9 @@ cask "displaylink" do
             #            ],
             launchctl: [
               "73YQY62QM3.com.displaylink.DisplayLinkAPServer",
+              "com.displaylink.displaylinkmanager",
               "com.displaylink.useragent-prelogin",
               "com.displaylink.useragent",
-              "com.displaylink.displaylinkmanager",
             ],
             quit:      "DisplayLinkUserAgent",
             delete:    [
@@ -53,6 +53,6 @@ cask "displaylink" do
 
   caveats do
     reboot
-    license "https://www.synaptics.com/products/displaylink-graphics/downloads/macos-#{version.before_comma}"
+    license "https://www.synaptics.com/products/displaylink-graphics/downloads/macos-#{version.csv.first}"
   end
 end
