443d5f8325e395eb5840bd10110550946dbd288c
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-03 01:45:56 +0800

    Update sony-rcs300.rb

--- Casks/sony-rcs300.rb
+++ Casks/sony-rcs300.rb
@@ -1,15 +1,15 @@
 cask "sony-rcs300" do
+  arch = Hardware::CPU.intel? ? "" : "1"
+
   version "1.0.0"
 
   if Hardware::CPU.intel?
-    suffix = ""
     sha256 "55183d65fc180544ac58ab3a6851980f63d47afdfc2b447ab9416bee7f32f99b"
   else
-    suffix = "1"
     sha256 "429f0481a2a04c2a31579ac9b26716c343633342b112130660e5e79a701c10b9"
   end
 
-  url "https://www.sony.co.jp/Products/felica/consumer/support/download/driver/usbdriver/usbdriver#{suffix}-s300-v#{version}.dmg"
+  url "https://www.sony.co.jp/Products/felica/consumer/support/download/driver/usbdriver/usbdriver#{arch}-s300-v#{version}.dmg"
   name "SONY USB Driver for RC-S300"
   desc "SONY CCID CardReader RC-S300 Driver"
   homepage "https://www.sony.co.jp/Products/felica/consumer/products/RC-S300.html"
@@ -17,7 +17,7 @@ cask "sony-rcs300" do
   livecheck do
     url "https://www.sony.co.jp/Products/felica/consumer/support/download/usbdriver.html"
     strategy :page_match
-    regex(%r{href=.*?/usbdriver#{suffix}-s300-v(\d+(?:\.\d+)*)\.dmg}i)
+    regex(%r{href=.*?/usbdriver#{arch}[._-]s300[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
   end
 
   depends_on macos: ">= :catalina"
