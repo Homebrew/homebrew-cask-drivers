078b911d70a99944d3c80b8006c9f88d38cf51c1
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 21:59:09 +0800

    Update toneprint.rb

--- Casks/toneprint.rb
+++ Casks/toneprint.rb
@@ -3,9 +3,9 @@ cask "toneprint" do
   sha256 "c85bb2875578eab40b9e091ebe07e2a85e2055af047ccf883e2aae2e0df7f6d5"
 
   url "https://mediadl.musictribe.com/download/software/tcelectronic/TonePrintEditor/TonePrint_#{version}_MacOS.zip",
-      verified: "mediadl.musictribe.com"
+      verified: "mediadl.musictribe.com/download/software/tcelectronic/TonePrintEditor/"
   name "TonePrint"
-  desc "TonePrints let you completely alter the character of your  pedal in seconds"
+  desc "Alter the character of your TonePrint pedal"
   homepage "https://www.tcelectronic.com/tonePrints.html"
 
   livecheck do
@@ -15,7 +15,7 @@ cask "toneprint" do
   app "Toneprint.app"
 
   zap trash: [
-    "~/Library/Caches/com.tcelectronic.toneprint",
     "~/Library/Application Support/TonePrint",
+    "~/Library/Caches/com.tcelectronic.toneprint",
   ]
 end
