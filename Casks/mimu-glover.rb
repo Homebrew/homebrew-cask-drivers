4d56c964e6b02dc5260629049a82641d34a53993
Bevan Kay <email@bevankay.me>
2022-01-19 09:06:34 +1100

    Update mimu-glover from 1.1.0 to 1.1.1

--- Casks/mimu-glover.rb
+++ Casks/mimu-glover.rb
@@ -1,6 +1,6 @@
 cask "mimu-glover" do
-  version "1.1.0"
-  sha256 "1b3eaf2dffe2496d5570283d918f517b5efa3251788678e9a81c4ba280bd33bd"
+  version "1.1.1"
+  sha256 "9f318e06e9b134c6c587ad1e9b4f4cb6106ebe155d6162da5401df343b48fbd5"
 
   url "https://static.mimugloves.com/Glover/Glover-MacOS-#{version}.zip"
   name "Glover"
