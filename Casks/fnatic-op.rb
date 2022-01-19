2022c6b7f3b8c9946f6157f889de94fb72ebc1a0
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-19 20:31:46 +0800

    Update fnatic-op.rb

--- Casks/fnatic-op.rb
+++ Casks/fnatic-op.rb
@@ -3,21 +3,26 @@ cask "fnatic-op" do
   sha256 :no_check
 
   url "https://op-releases.tools.fnatic.io/download/macos",
-      verified: "op-releases.tools.fnatic.io/"
+      verified: "op-releases.tools.fnatic.io/download/"
   name "Fnatic OP"
   desc "Customize your FNATICGEAR devices"
   homepage "https://fnatic.com/gear/op"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "Fnatic OP.app"
 
   uninstall quit: "com.fnatic.FnaticOP"
 
   zap trash: [
+    "~/Library/Application Support/fnaticop",
+    "~/Library/Caches/com.fnatic.FnaticOP*",
+    "~/Library/Logs/Fnatic OP",
     "~/Library/Preferences/com.fnatic.FnaticOP.helper.plist",
     "~/Library/Preferences/com.fnatic.FnaticOP.plist",
-    "~/Library/Logs/Fnatic OP",
     "~/Library/Saved Application State/com.fnatic.FnaticOP.savedState",
-    "~/Library/Caches/com.fnatic.FnaticOP*",
-    "~/Library/Application Support/fnaticop",
   ]
 end
