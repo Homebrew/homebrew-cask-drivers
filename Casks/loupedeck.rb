c157139ce274f67213083a81eed4737ec31b33e6
woolfyx <29287203+woolfyx@users.noreply.github.com>
2022-01-25 04:00:46 +0100

    Update Loupedeck to v5.0.3 (#2656)
    * Update Loupedeck to v5.0.3
    
    * Update loupedeck.rb
    
    * Update loupedeck.rb
    
    * Update loupedeck.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/loupedeck.rb
+++ Casks/loupedeck.rb
@@ -1,12 +1,17 @@
 cask "loupedeck" do
-  version "4.1.1"
-  sha256 "2c78d44f51cf8173fad8eb5de62b147bc54eb211dc41e6c58df5289ebbe15178"
+  version "5.0.3"
+  sha256 "7e7ec5d88f8bd239d7261c720c98115afc49b8c6171bb9e779ecb111ac38a9b0"
 
-  url "https://loupedeck-software-release.s3.amazonaws.com/Software+version+#{version}/MacOS/Loupedeck-MacOS-v+#{version}.dmg",
-      verified: "loupedeck-software-release.s3.amazonaws.com/"
+  url "https://f.hubspotusercontent40.net/hubfs/5145542/Knowledge%20Base/LD%20Software%20Downloads/#{version}/Loupedeck%20#{version}.dmg",
+      verified: "f.hubspotusercontent40.net/"
   name "Loupdeck"
   desc "Software for Loupedeck consoles"
   homepage "https://loupedeck.com/"
+
+  livecheck do
+    url "https://support.loupedeck.com/loupedeck-software-download"
+    regex(/Mac\sOS\s(\d+(?:\.\d+)+)/i)
+  end
 
   depends_on macos: ">= :sierra"
 
@@ -20,17 +25,22 @@ cask "loupedeck" do
     ["KILL", "com.loupedeck.Loupedeck2"],
   ],
             pkgutil: [
-              "com.loupedeck.PluginPackageInstaller",
               "com.loupedeck.ImageLibraryInstaller",
+              "com.loupedeck.LibraryInstaller",
+              "com.loupedeck.LoupedeckLibraryPackageManagerMacPackageInstaller",
               "com.loupedeck.LoupedeckPackageInstaller",
+              "com.loupedeck.MediaInstaller",
+              "com.loupedeck.PluginPackageInstaller",
             ],
-            delete:  [
-              "~/Library/Application Support/Capture One/KeyboardShortcuts/Loupedeck_beta.plist",
-              "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Loke Roos",
-              "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Karo Holmberg",
-              "~/Library/Application Support/Adobe/Lightroom/Export Presets/Loupedeck Exports",
-              "~/Library/Application Support/Adobe/Lightroom/Modules/loupedeck2.lrplugin",
-              "~/Library/Application Support/LoupedeckConfig",
-              "~/Library/Logs/LoupedeckConfig",
-            ]
+            delete:  "/Applications/Loupedeck.app"
+
+  zap trash: [
+    "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Karo Holmberg",
+    "~/Library/Application Support/Adobe/CameraRaw/Settings/Loupedeck - Loke Roos",
+    "~/Library/Application Support/Adobe/Lightroom/Export Presets/Loupedeck Exports",
+    "~/Library/Application Support/Adobe/Lightroom/Modules/loupedeck2.lrplugin",
+    "~/Library/Application Support/Capture One/KeyboardShortcuts/Loupedeck_beta.plist",
+    "~/Library/Application Support/LoupedeckConfig",
+    "~/Library/Logs/LoupedeckConfig",
+  ]
 end
