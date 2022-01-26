1d74893afa14bd56d83eebecfde6583dcb39e720
Bevan Kay <email@bevankay.me>
2022-01-26 23:03:35 +1100

    Update logitech-g-hub from 2022.1.3492 to 2022.2.659

--- Casks/logitech-g-hub.rb
+++ Casks/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2022.1.3492"
+  version "2022.2.659"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
