37beddb40714286c8392f0667131613be912ee2d
viking1304 <viking1304@gmail.com>
2022-01-27 13:23:40 +0100

    Update konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver to 11.8.0A

--- Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
+++ Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
@@ -5,10 +5,10 @@ cask "konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver" do
 
     url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=08e8308b1e134d6bb6837e3b046b126b&tx_kmanacondaimport_downloadproxy[documentId]=122431&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
   else
-    version "11.7.0A"
+    version "11.8.0A"
     sha256 :no_check
 
-    url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=cbeeb8a37c48e4fe49180b4543df08bf&tx_kmanacondaimport_downloadproxy[documentId]=127275&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
+    url "https://dl.konicaminolta.eu/en?tx_kmanacondaimport_downloadproxy[fileId]=7228bd01e7674417c6a223ce7a186487&tx_kmanacondaimport_downloadproxy[documentId]=130160&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
   end
 
   name "KONICA MINOLTA bizhub C759/C658/C368/C287/C3851 Series Printer"
