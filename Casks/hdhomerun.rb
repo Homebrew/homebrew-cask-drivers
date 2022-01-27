40197fb5f541c1b795dc6a856a9884c6c6875ac7
owine <owine@users.noreply.github.com>
2022-01-27 08:34:42 -0600

    Update hdhomerun from 20211212 to 20220126

--- Casks/hdhomerun.rb
+++ Casks/hdhomerun.rb
@@ -1,6 +1,6 @@
 cask "hdhomerun" do
-  version "20211212"
-  sha256 "9a86962ff9de7e6f0b3af4f1978e04c29e4c0dd26df4134fae98e80053eb89a0"
+  version "20220126"
+  sha256 "4525ee207802c30b2f58ea0079f41cfc896d57aad1be1ed56d155fe461fee375"
 
   url "https://download.silicondust.com/hdhomerun/hdhomerun_mac_#{version}.dmg"
   name "HDHomeRun"
