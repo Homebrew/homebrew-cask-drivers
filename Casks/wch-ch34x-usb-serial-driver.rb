d1d8130515beb985c4766db7fbd17b645e94fd90
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-30 18:25:15 +0800

    Update wch-ch34x-usb-serial-driver.rb

--- Casks/wch-ch34x-usb-serial-driver.rb
+++ Casks/wch-ch34x-usb-serial-driver.rb
@@ -4,16 +4,17 @@ cask "wch-ch34x-usb-serial-driver" do
 
   url "http://www.wch.cn/downloads/file/369.html"
   name "WCH USB serial driver for CH340/CH341/CH342/CH343/CH344/CH9101/CH9102/CH9103/CH9143"
+  desc "USB serial driver"
   homepage "http://www.wch.cn/download/CH34XSER_MAC_ZIP.html"
 
   pkg "CH34XSER_MAC/CH34xVCPDriver.pkg"
 
-  if MacOS.version <= :catalina
-    uninstall pkgutil: "cn.wch.pkg.CH34xVCPDriver",
-              kext:    ["cn.wch.CH34xVCPDriver", "cn.wch.CH34xVCPControlDriver"]
-  else
-    uninstall pkgutil: "cn.wch.pkg.CH34xVCPDriver"
-  end
+  uninstall quit:    "cn.wch.CH34xVCPDriver",
+            pkgutil: "cn.wch.pkg.CH34xVCPDriver",
+            kext:    [
+              "cn.wch.CH34xVCPDriver",
+              "cn.wch.CH34xVCPControlDriver",
+            ]
 
   caveats do
     reboot
