74dac41ba546ab4ce66c1a8af85f140d60299106
owine <owine@users.noreply.github.com>
2022-02-02 18:23:01 -0600

    Update luna-display from 5.1.2 to 5.1.3 (#2671)

--- Casks/luna-display.rb
+++ Casks/luna-display.rb
@@ -1,6 +1,6 @@
 cask "luna-display" do
-  version "5.1.2,4198"
-  sha256 "009de30ab3a9c4be80c84b8a3ec446e300d96af8d3ce77ccb97112bad6e55bb8"
+  version "5.1.3,4199"
+  sha256 "ef012e906d51fc1c01d5e2d1a91fb1e97c9f632b8038061a5bfccce1e3cc331b"
 
   url "https://downloads.astropad.com/luna/mac/LunaDisplay-#{version.csv.first}.#{version.csv.second}.dmg"
   name "Luna Display"
