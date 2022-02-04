34e88311a9498a797faddf34ff1bf8b609366e9a
Nicholas Ngai <nicholas@ngai.me>
2022-02-03 19:05:51 -0800

    Create rode-central cask v1.3.6 (#2673)
    * Create rode-central cask v1.3.6
    
    * Update rode-central.rb
    
    * Update rode-central.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/rode-central.rb
@@ -0,0 +1,20 @@
+cask "rode-central" do
+  version "1.3.6"
+  sha256 :no_check
+
+  url "https://cdn1.rode.com/rodecentral_installation_file_mac.zip"
+  name "Rode Central"
+  desc "Configure RØDE device settings, features, functions, and firmware"
+  homepage "https://rode.com/software/rode-central"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  pkg "RODE Central macOS v#{version}/RODE Central Installer.pkg"
+
+  uninstall pkgutil: "com.rodecentral.installer"
+end
