c7ba9248606b8b3fccf117048c8d4353d175ceff
John Benninghoff <jbenninghoff@mac.com>
2022-02-15 16:03:08 -0600

    Update caldigit-thunderbolt-charging (#2688)

--- Casks/caldigit-thunderbolt-charging.rb
+++ Casks/caldigit-thunderbolt-charging.rb
@@ -4,23 +4,18 @@ cask "caldigit-thunderbolt-charging" do
 
   url "https://downloads.caldigit.com/CalDigit-Thunderbolt-Station-Mac-Drivers.zip"
   name "CalDigit Thunderbolt Station USB Charging & SuperDrive Support Driver"
+  desc "Improved Apple device support"
   homepage "https://www.caldigit.com/"
 
   pkg "CalDigit-Thunderbolt-Station-Mac-Drivers.pkg"
 
-  uninstall kext:    [
-    "com.CalDigit.ThunderboltStationChargingSupport",
-    "com.CalDigit.driver.HDPro",
-  ],
+  uninstall kext:    "com.CalDigit.ThunderboltStationChargingSupport",
             pkgutil: [
               "com.CalDigit.CalDigitThunderboltStationChargingSupport.StationsChargingSupport." \
               "CalDigitThunderboltStationChargingSupport.pkg",
               "com.CalDigit.ThunderboltStation3ChargingSupport.pkg",
             ],
-            delete:  [
-              "/Library/Extensions/CalDigitHDProDrv.kext",
-              "/Library/Extensions/CalDigitThunderboltStationChargingSupport.kext",
-            ]
+            delete:  "/Library/Extensions/CalDigitThunderboltStationChargingSupport.kext"
 
   caveats do
     reboot
