0f34a073528a3d591cdde6b708b4a09fb8b66075
Malo Bourgon <mbourgon@gmail.com>
2022-02-28 11:28:50 -0800

    Upgrade OBSBOT Me Tool.app to v1.0.2.1

--- Casks/obsbot-me-tool.rb
+++ Casks/obsbot-me-tool.rb
@@ -1,9 +1,9 @@
 cask "obsbot-me-tool" do
-  version "1.0.0.12"
-  sha256 "77dbc8ba54b64112713e72155d517f3ac54cac7dd1515a90b0f13a58ebb01e3b"
+  version "1.0.2.1"
+  sha256 "8afb295a65cfd82ca4023238e3cf4d0e7ae14799c3c84afd2f8a9fafcbaa7257"
 
-  url "https://obsbot-static-resource.oss-cn-hongkong.aliyuncs.com/obsbot_me/tool/Obsbot_metool_pc_OA_E_MacOS_#{version}_release.dmg",
-      verified: "obsbot-static-resource.oss-cn-hongkong.aliyuncs.com/obsbot_me/tool/"
+  url "https://obsbot-static-resource.oss-cn-hongkong.aliyuncs.com/download/obsbot-me/metool/Obsbot_metool_pc_OA_E_MacOS_#{version}_release.dmg",
+      verified: "obsbot-static-resource.oss-cn-hongkong.aliyuncs.com/download/obsbot-me/metool/"
   name "OBSBOT Me Tool"
   desc "Configuration and firmware update utility for OBSBOT Me"
   homepage "https://www.obsbot.com/download?tab=obsbot-me"
@@ -13,7 +13,7 @@ cask "obsbot-me-tool" do
     regex(/Obsbot[._-]metool[._-]pc[._-]OA[._-]E[._-]MacOS[._-](\d+(?:\.\d+)+)[._-]release\.dmg/i)
   end
 
-  depends_on macos: "<= :big_sur"
+  depends_on macos: ">= :high_sierra"
 
   app "OBSBOT Me Tool.app"
 end
