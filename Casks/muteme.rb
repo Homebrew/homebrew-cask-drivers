8e6a596725cb5effeb7cf2dccb255fdcc9c351d7
Braxton Schafer <braxton@cmdcentral.xyz>
2022-03-01 11:21:03 -0600

    Update muteme cask to support Apple Silicon (#2705)
    * update muteme to support m1
    
    * Update muteme.rb
    
    * Update Casks/muteme.rb
    
    Co-authored-by: Rylan Polster <rslpolster@gmail.com>
    
    Co-authored-by: Braxton Schafer <braxton@epic.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Rylan Polster <rslpolster@gmail.com>

--- Casks/muteme.rb
+++ Casks/muteme.rb
@@ -1,15 +1,22 @@
 cask "muteme" do
-  version "0.8.3"
-  sha256 "a40ece78cf534775b9bc3e9b4211d7ab8c1af053c01e145dbefd5cfe4205b27d"
+  arch = Hardware::CPU.intel? ? "osx_64" : "osx_arm64"
 
-  url "https://muteme.io/download/flavor/default/#{version}/osx_64/MuteMe-Client-#{version}.dmg",
-      verified: "https://muteme.io/"
-  name "muteme"
+  version "0.9.2"
+
+  if Hardware::CPU.intel?
+    sha256 "788efd319495b9b221c5a4706d2e63c6147d807c168ec2ae7a39d9ac6d521c5d"
+  else
+    sha256 "9bc1225d65931c189ad953c90c8e67299d9d7336728312754bc430607dab7397"
+  end
+
+  url "https://muteme.io/download/flavor/default/#{version}/#{arch}/MuteMe-Client-#{version}.dmg",
+      verified: "muteme.io/download/flavor/default/"
+  name "MuteMe"
   desc "Companion app for the MuteMe physical mute button"
   homepage "https://muteme.com/"
 
   livecheck do
-    url "https://downloads.muteme.com/download/latest/osx_64"
+    url "https://downloads.muteme.com/download/latest/#{arch}"
     strategy :header_match
   end
 
