4543eaba2498f0b957dbb250213178a592a23752
Jason Tedor <jason@tedor.me>
2022-03-24 05:11:27 -1000

    Update sonos from 14.2 to 14.4

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,6 +1,6 @@
 cask "sonos" do
-  version "14.2,67.1.26143"
-  sha256 "29f08ece3abcd3bb0f0219c329270df4b56acb8629f0ba6d242e5ca9d8c0d31e"
+  version "14.4,67.1.27100"
+  sha256 "b8049c78a859f911e4d08283bd0b73e28080bbd2da82c757786a62a3fd660df6"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.csv.first.no_dots}.dmg"
   name "Sonos"
