10009dc945488d5ebd8ea6460124df1a7dfe2980
Bevan Kay <email@bevankay.me>
2022-03-24 19:27:25 +1100

    Update sony-ps-remote-play from 4.5.0 to 5.0.0

--- Casks/sony-ps-remote-play.rb
+++ Casks/sony-ps-remote-play.rb
@@ -1,5 +1,5 @@
 cask "sony-ps-remote-play" do
-  version "4.5.0"
+  version "5.0.0"
   sha256 :no_check
 
   url "https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg"
