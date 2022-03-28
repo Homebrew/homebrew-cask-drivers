98a7909cdf124a8fe959153c31de6130a631a522
Nils Streedain <tannins_berets_0@icloud.com>
2022-03-28 13:36:01 -0700

    Update logi-options-plus.rb

--- Casks/logi-options-plus.rb
+++ Casks/logi-options-plus.rb
@@ -1,5 +1,5 @@
 cask "logi-options-plus" do
-  version "0.90.1438"
+  version "0.91.3227"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
