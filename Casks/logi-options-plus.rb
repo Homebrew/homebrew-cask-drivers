2aa2c7883defda89ffe2df73068665d3071cdbd0
Nils Streedain <tannins_berets_0@icloud.com>
2022-03-28 23:26:22 -0700

    Repaired uninstallation/zap process for version 0.91

--- Casks/logi-options-plus.rb
+++ Casks/logi-options-plus.rb
@@ -14,28 +14,42 @@ cask "logi-options-plus" do
   installer script: "logioptionsplus_installer.app/Contents/MacOS/logioptionsplus_installer"
 
   uninstall launchctl: [
-    "com.logi.optionsplus",
-    "com.logi.optionsplus.updater",
-  ],
-            quit:      [
-              "com.logi.optionsplus",
-              "com.logi.optionsplus.updater",
-              "com.logi.optionsplus.agent",
-            ],
-            delete:    "/Library/Application Support/Logitech.localized"
+			   "com.logi.cp-dev-mgr",
+			   "com.logi.optionsplus",
+			   "com.logi.optionsplus.updater",
+			   "com.logi.optionsplus.agent",
+		   ],
+		   quit: [
+				"com.logi.cp-dev-mgr",
+				"com.logi.optionsplus",
+				"com.logi.optionsplus.updater",
+				"com.logi.optionsplus.agent",
+				"com.Logitech.Unifying Software.pkg",
+			],
+			pkgutil: [
+				"com.Logitech.Updater.pkg",
+				"com.Logitech.Unifying Software.pkg",
+			],
+            delete: [
+				"/Applications/logioptionsplus.app",
+				"/Library/Application Support/Logitech.localized",
+				"/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/C/com.logi.optionsplus.helper",
+				"/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/T/com.logi.optionsplus.agent.logs",
+				"/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/T/.com.logi.optionsplus.6N7HiI",
+				"/Library/LaunchAgents/com.logi.optionsplus.plist",
+				"/Library/LaunchDaemons/com.logi.optionsplus.updater.plist",
+			]
 
   zap trash: [
+	"~/Library/Application Support/Logitech",
+	"~/Library/Application Support/LogiOptionsPlus",
     "~/Library/Preferences/com.logi.optionsplus.plist",
+	"~/Library/Preferences/com.logi.cp-dev-mgr.plist",
+	"~/Library/Preferences/com.Logitech.Updater.plist",
     "~/Library/Application Support/logioptionsplus",
     "~/Library/Saved Application State/com.logi.optionsplus.savedState",
-    "/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/C/com.logi.optionsplus.helper",
-    "/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/T/com.logi.optionsplus.agent.logs",
-    "/Library/Application Support/Logitech/LogiOptionsPlus",
-    "/Library/Application Support/Logitech/Logitch Options",
-    "/Library/LaunchAgents/com.logi.optionsplus.plist",
-    "/Library/LaunchDaemons/com.logi.optionsplus.updater.plist",
-  ],
-      rmdir: "~/Library/Application Support/Logitech"
+	"/Users/Shared/LogiOptionsPlus"
+  ]
 
   caveats do
     reboot
