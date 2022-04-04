81947009355b6d50171094ce9834c15bb2638d20
Nils Streedain <tannins_berets_0@icloud.com>
2022-04-03 23:57:31 -0700

    Add Logi Options+ 0.91.3227 (#2734)
    * Create logi-options-plus.rb
    
    * Update logi-options-plus.rb
    
    * Repaired uninstallation/zap process for version 0.91
    
    * Changed to manual installation
    
    Newer versions of Logi Options+ require user interaction during the installation process, therefore a manual installation is needed instead of an automatic script.
    
    * Removed version & auto updates
    
    Logitech stopped using version numbers in URLs meaning the latest is always downloaded. Therefore, auto_updates have been disabled & the version has been replaced with :latest
    
    * Fixed auto-tab causing failed styling
    
    * Corrected uninstall uninstall stanza
    
    Removed unique files and a package required for other Logitech software from the uninstall stanza
    
    * Removed global files from uninstall process
    
    Removed global Logitech updater & application support files from uninstall process

---
+++ Casks/logi-options-plus.rb
@@ -0,0 +1,45 @@
+cask "logi-options-plus" do
+  version :latest
+  sha256 :no_check
+
+  url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
+      verified: "download01.logi.com/web/ftp/pub/techsupport/optionsplus/"
+  name "Logitech Options+"
+  desc "Software for Logitech devices"
+  homepage "https://www.logitech.com/en-us/software/logi-options-plus.html"
+
+  depends_on macos: ">= :catalina"
+
+  installer manual: "logioptionsplus_installer.app"
+
+  uninstall launchctl: [
+    "com.logi.cp-dev-mgr",
+    "com.logi.optionsplus",
+    "com.logi.optionsplus.updater",
+    "com.logi.optionsplus.agent",
+  ],
+            quit:      [
+              "com.logi.cp-dev-mgr",
+              "com.logi.optionsplus",
+              "com.logi.optionsplus.updater",
+              "com.logi.optionsplus.agent",
+            ],
+            delete:    [
+              "/Applications/logioptionsplus.app",
+              "/Library/LaunchDaemons/com.logi.optionsplus.updater.plist",
+              "/Library/LaunchAgents/com.logi.optionsplus.plist",
+            ]
+
+  zap trash: [
+    "~/Library/Application Support/LogiOptionsPlus",
+    "~/Library/Preferences/com.logi.optionsplus.plist",
+    "~/Library/Preferences/com.logi.cp-dev-mgr.plist",
+    "~/Library/Application Support/logioptionsplus",
+    "~/Library/Saved Application State/com.logi.optionsplus.savedState",
+    "/Users/Shared/LogiOptionsPlus",
+  ]
+
+  caveats do
+    reboot
+  end
+end
