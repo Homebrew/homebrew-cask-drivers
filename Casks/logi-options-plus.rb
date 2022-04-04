f8fcf9a953a84aeaf35c371390bb8cb893ab9bfc
Nils Streedain <tannins_berets_0@icloud.com>
2022-04-03 20:58:05 -0700

    Corrected uninstall uninstall stanza
    Removed unique files and a package required for other Logitech software from the uninstall stanza

--- Casks/logi-options-plus.rb
+++ Casks/logi-options-plus.rb
@@ -23,26 +23,18 @@ cask "logi-options-plus" do
               "com.logi.optionsplus",
               "com.logi.optionsplus.updater",
               "com.logi.optionsplus.agent",
-              "com.Logitech.Unifying Software.pkg",
             ],
             pkgutil:   [
               "com.Logitech.Updater.pkg",
-              "com.Logitech.Unifying Software.pkg",
             ],
             delete:    [
               "/Applications/logioptionsplus.app",
               "/Library/Application Support/Logitech.localized",
               "/Library/LaunchDaemons/com.logi.optionsplus.updater.plist",
               "/Library/LaunchAgents/com.logi.optionsplus.plist",
-              "/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/C/com.logi.optionsplus.helper",
-              "/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/T/com.logi.optionsplus.agent.logs",
-              "/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/T/com.logi.optionsplus.installer.logs",
-              "/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/T/com.logi.optionsplus.updater.logs",
-              "/private/var/folders/pm/x750ngd17hgc1wc7_09wjgdh0000gn/T/.com.logi.optionsplus.6N7HiI",
             ]
 
   zap trash: [
-    "~/Library/Application Support/Logitech",
     "~/Library/Application Support/LogiOptionsPlus",
     "~/Library/Preferences/com.logi.optionsplus.plist",
     "~/Library/Preferences/com.logi.cp-dev-mgr.plist",
