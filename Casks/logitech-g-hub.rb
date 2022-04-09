0ec313570f0b6c04a6247b4f82533a55124b1858
owine <owine@users.noreply.github.com>
2022-04-09 10:25:20 -0500

    Update logitech-g-hub from 2022.3.2423 to 2022.3.242300

--- Casks/logitech-g-hub.rb
+++ Casks/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2022.3.2423"
+  version "2022.3.242300"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
