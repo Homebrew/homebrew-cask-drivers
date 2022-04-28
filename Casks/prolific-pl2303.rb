87d1978ebe856f5ebd302a3f2beda985b68772d8
Nikolay Ermolovich <rupreht@rupreht.ru>
2022-04-28 17:20:05 +0500

    Update prolific-pl2303.rb (#2776)
    - fix protocol to use HTTPS rather than HTTP

--- Casks/prolific-pl2303.rb
+++ Casks/prolific-pl2303.rb
@@ -2,13 +2,13 @@ cask "prolific-pl2303" do
   version "2.1.0_20210311"
   sha256 "02b83dc67c06edc6908cd56dc5032689aff39666b27e8c8b9ad83d3f013cc14d"
 
-  url "http://www.prolific.com.tw/UserFiles/files/PL2303HXD_G_Mac%20Driver_v#{version.dots_to_underscores}.zip"
+  url "https://www.prolific.com.tw/UserFiles/files/PL2303HXD_G_Mac%20Driver_v#{version.dots_to_underscores}.zip"
   name "Prolific USB to Serial Cable driver"
   desc "PL2303 USB-to-serial driver"
-  homepage "http://www.prolific.com.tw/US/"
+  homepage "https://www.prolific.com.tw/US/"
 
   livecheck do
-    url "http://www.prolific.com.tw/US/ShowProduct.aspx?p_id=229&pcid=41"
+    url "https://www.prolific.com.tw/US/ShowProduct.aspx?p_id=229&pcid=41"
     regex(/PL2303HXD_G_Mac\s*Driver_v?(\d+(?:\.\d+)*_\d+)\.zip/i)
   end
 
