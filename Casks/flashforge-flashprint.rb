c6d945d770b4214e5923b65dda4ede1b417ee766
kennedyshead <magnusknutas@gmail.com>
2022-05-10 11:43:18 +0200

    Updates uninstall/sha for flashforge-flashprint

--- Casks/flashforge-flashprint.rb
+++ Casks/flashforge-flashprint.rb
@@ -1,6 +1,6 @@
 cask "flashforge-flashprint" do
   version "5.3.3,1c68fcfadc5c29627057053d5a5af558"
-  sha256 "38b11cd90400119bcd145ca5a9981dfea7ab8caabbb0fbd746783ab1821d8740"
+  sha256 "01908dc35bfb8b2fbf812caabac070dc0a788d99b1179ce2495996e818c07dbc"
 
   url "https://en.fss.flashforge.com/10000/software/#{version.csv.second}.zip"
   name "FlashPrint"
@@ -11,7 +11,7 @@ cask "flashforge-flashprint" do
 
   pkg "FlashPrint #{version.major}.pkg"
 
-  uninstall pkgutil: "com.sz3dp.pkg.FlashPrint"
+  uninstall pkgutil: "com.sz3dp.pkg.FlashPrint5"
 
   zap trash: "~/Library/Preferences/FlashForge.FlashPrint.plist"
 end
