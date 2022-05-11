06310728b32a818e59606607c8968d3b4666d8c5
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-11 11:15:27 +0800

    Update safenet-authentication-client.rb

--- Casks/safenet-authentication-client.rb
+++ Casks/safenet-authentication-client.rb
@@ -19,11 +19,7 @@ cask "safenet-authentication-client" do
     skip "No version information available"
   end
 
-  depends_on macos: [
-    :catalina,
-    :big_sur,
-    :monterey,
-  ]
+  depends_on macos: ">= :catalina"
 
   pkg "SafeNet Authentication Client #{version.major_minor}.pkg"
 
