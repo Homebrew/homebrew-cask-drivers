f37a03905dd997502d2c5602a03ee7e689be22cd
Bevan Kay <email@bevankay.me>
2022-05-12 11:34:02 +1000

    Update cycliqplus-firmware-updater from 1.06 to 1.08

--- Casks/cycliqplus-firmware-updater.rb
+++ Casks/cycliqplus-firmware-updater.rb
@@ -1,6 +1,6 @@
 cask "cycliqplus-firmware-updater" do
-  version "1.06"
-  sha256 "d9c1b5cf54a2a8ef82081c11b717c9f93445b789b8c05faff1150b8092e22510"
+  version "1.08"
+  sha256 "596af6de43bc5cb8ce9b38f3ce1e45ef0fcbb4ff9e559ddbc0dc959fb7b13d12"
 
   url "https://cycliq.com/files/software/downloads/CycliqFirmwareUpdater-#{version}.dmg"
   name "CycliqPlus Firmware Updater"
