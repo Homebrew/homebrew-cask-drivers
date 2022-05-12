64aa5c88159c30c0995301695c5b32b5e2584d26
Bevan Kay <email@bevankay.me>
2022-05-12 11:57:17 +1000

    Update picoscope from 6.14.61 to 6.14.62 (#2812)

--- Casks/picoscope.rb
+++ Casks/picoscope.rb
@@ -1,6 +1,6 @@
 cask "picoscope" do
-  version "6.14.61"
-  sha256 "62f30f18f939a71aaefa34b90f00d40afdb28ab5165d929a3fe8955292c85ffb"
+  version "6.14.62"
+  sha256 "ec54bf083e1c9b1268828ebb8c7de5f490a2403e219dd6dd7b88b27864e2f168"
 
   url "https://www.picotech.com/download/software/sr/PicoScope-#{version}.pkg"
   name "PicoScope"
