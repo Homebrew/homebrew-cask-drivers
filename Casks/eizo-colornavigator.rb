fafb499bfbd90db69ddbabc5ac7fbe573cc12cd2
Niklas <44636701+MayNiklas@users.noreply.github.com>
2022-05-17 02:56:15 +0200

    eizo-colornavigator: 7.13 -> 7.1.7 (#2823)
    * eizo-colornavigator: 7.13 -> 7.1.7
    
    * Update eizo-colornavigator.rb
    
    * Update eizo-colornavigator.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/eizo-colornavigator.rb
+++ Casks/eizo-colornavigator.rb
@@ -1,11 +1,18 @@
 cask "eizo-colornavigator" do
-  version "7.13"
-  sha256 "45d9966ac09e8b4b428fcb870ce7a991e1f4f6a38898407922bb31b3aa1f53a1"
+  version "7.1.7"
+  sha256 "656efade81522cb35a8effdaf6c93f2426db10dc4454b04eeee41c78f315a0ff"
 
   url "https://www.eizoglobal.com/support/db/files/software/software/graphics/colornavigator#{version.major}/ColorNavigator#{version.no_dots}.pkg"
   name "Eizo ColorNavigator"
   desc "Calibrate Eizo Coloredge screens"
-  homepage "https://www.eizoglobal.com/products/coloredge/cn7/"
+  homepage "https://www.eizoglobal.com/products/coloredge/cn#{version.major}/"
+
+  livecheck do
+    url "https://www.eizoglobal.com/support/db/products/download/900"
+    regex(/Ver[._-]v?(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :mojave"
 
   pkg "ColorNavigator#{version.no_dots}.pkg"
 
