4405615e9fdf9e7495a3501c4e7ae351a140c17b
Roman Zhuzha <roman.zhuzha@getbux.com>
2022-05-30 11:20:59 +0200

    Update elgato-wave-link from 1.5.0.3042_2 to 1.5.0.3042

--- Casks/elgato-wave-link.rb
+++ Casks/elgato-wave-link.rb
@@ -1,5 +1,5 @@
 cask "elgato-wave-link" do
-  version "1.5.0.3042_2"
+  version "1.5.0.3042"
   sha256 "56157301e1b419b03328fc32d8817eaf7bb10c35373db5d73c2f52fdb13dfba7"
 
   url "https://edge.elgato.com/egc/macos/wavelink/#{version.major_minor_patch}/WaveLink_#{version}.pkg"
