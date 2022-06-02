ed2420b51f7d90d9dfeb97b0e94880a747d146bd
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2022-06-01 18:19:46 -0700

    Update focusrite-control from 3.10.0.1981 to 3.11.0.1983 (#2852)
    * Update focusrite-control from 3.10.0.1981 to 3.11.0.1983
    
    * focusrite-control: Fix URL

--- Casks/focusrite-control.rb
+++ Casks/focusrite-control.rb
@@ -1,8 +1,8 @@
 cask "focusrite-control" do
-  version "3.10.0.1981"
-  sha256 "1250152259ba9836656c75b5d7ea296a092c57cef51d3f6ebddb2a89e30cc8a4"
+  version "3.11.0.1983"
+  sha256 "6563ce66aaec2cb24a31bc8bc91f0d819ecaafc515559f6a692c3c0d766c210c"
 
-  url "https://fael-downloads-prod.focusrite.com/customer/prod/s3fs-public/downloads/Focusrite%20Control%20-%20#{version}.dmg"
+  url "https://fael-downloads-prod.focusrite.com/customer/prod/downloads/Focusrite%20Control%20-%20#{version}.dmg"
   name "Focusrite Control"
   desc "Control your Focusrite interface"
   homepage "https://focusrite.com/en/focusrite-control"
