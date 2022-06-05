5ca242a67a3a9c102749390d069b9a5a2205a2b5
Andre Arko <andre@arko.net>
2022-06-04 23:36:06 -0700

    verify s3 bucket

--- Casks/dymo-label.rb
+++ Casks/dymo-label.rb
@@ -2,7 +2,8 @@ cask "dymo-label" do
   version "1.4.3.103"
   sha256 "b490b687ed24af5a198ce215d68e041b6efc5d5e2f83ced6f5e5b788b52142f1"
 
-  url "https://s3.amazonaws.com/download.dymo.com/dymo/Software/Mac/DCDMac#{version}.pkg"
+  url "https://s3.amazonaws.com/download.dymo.com/dymo/Software/Mac/DCDMac#{version}.pkg",
+    verified: "s3.amazonaws.com/download.dymo.com"
   name "Dymo Label"
   desc "Software for DYMO LabelWriters"
   homepage "https://www.dymo.com/en-US/online-support"
