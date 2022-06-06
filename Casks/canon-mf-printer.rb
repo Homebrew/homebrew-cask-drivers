76a0ced4272d879c24535bc8ed0b9a9c6e4d6680
Bo Anderson <mail@boanderson.me>
2022-06-06 05:35:27 +0100

    canon-mf-printer: correct minimum macOS

--- Casks/canon-mf-printer.rb
+++ Casks/canon-mf-printer.rb
@@ -12,7 +12,7 @@ cask "canon-mf-printer" do
     skip "No version information available"
   end
 
-  depends_on macos: ">= :yosemite"
+  depends_on macos: ">= :el_capitan"
 
   pkg "MF_Printer_Installer.pkg"
 
