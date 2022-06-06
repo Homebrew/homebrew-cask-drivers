a85a04b38171b3df353542fda4a2fa4fcf27b702
Bo Anderson <mail@boanderson.me>
2022-06-06 05:34:14 +0100

    canon-fax: remove no-op Yosemite dependency

--- Casks/canon-fax.rb
+++ Casks/canon-fax.rb
@@ -8,8 +8,6 @@ cask "canon-fax" do
   desc "Fax driver & utilities for Canon imageCLASS MF printers"
   homepage "https://www.usa.canon.com/internet/portal/us/home/support/drivers-downloads"
 
-  depends_on macos: ">= :yosemite"
-
   pkg "Canon_FAX_Installer.pkg"
 
   uninstall pkgutil: "jp.co.canon.CUPSFAX.*"
