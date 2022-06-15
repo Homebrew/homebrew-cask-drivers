0fea7466dabbc1b36c1ccec079a239b6824b6da3
Shahar Hadas <shaharhd@gmail.com>
2022-06-16 00:38:39 +0300

    Update segger-jlink.rb
    Moved to use universal version

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,15 +1,9 @@
 cask "segger-jlink" do
-  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
-
   version "7.66c"
 
-  if Hardware::CPU.intel?
-    sha256 "cd49810c97d8461121191395b8446257a3803569efad65100c884b1db1c30a93"
-  else
-    sha256 "166c0684c4ac0aeb13f3db87e049fcf6052a54960a5a1113769b2b6edc9f0b08"
-  end
+  sha256 "ff730a4e4c1a7fc1215f558b4fe3ee506eb4e5894f8b866bc128ef276084e31a"
 
-  url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}_#{arch}.pkg",
+  url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}_universal.pkg",
       using: :post,
       data:  {
         "accept_license_agreement" => "accepted",
@@ -25,7 +19,7 @@ cask "segger-jlink" do
     regex(/Version\s*V(\d+(?:\.\d+[a-z]?)*)/i)
   end
 
-  pkg "JLink_MacOSX_V#{version.no_dots}_#{arch}.pkg"
+  pkg "JLink_MacOSX_V#{version.no_dots}_universal.pkg"
 
   uninstall quit:    [
     "com.segger.JFlashLite.*",
