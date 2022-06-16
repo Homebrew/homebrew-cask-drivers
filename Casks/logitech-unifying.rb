12174265dc131a8becc46a6845ab550995fe4f06
Bo Anderson <mail@boanderson.me>
2022-06-16 03:39:10 +0100

    logitech-unifying: remove Yosemite support

--- Casks/logitech-unifying.rb
+++ Casks/logitech-unifying.rb
@@ -1,11 +1,6 @@
 cask "logitech-unifying" do
-  if MacOS.version <= :yosemite
-    version "1.2.359"
-    sha256 "e6fd9c1b536033f3346b32c391bd58587ea9f549cab7839cf8a1dbc62a739825"
-  else
-    version "1.3.375"
-    sha256 "82fe3df612e775ec8789fa821f4f62d4b3a55278276d03474580fee668ee50b7"
-  end
+  version "1.3.375"
+  sha256 "82fe3df612e775ec8789fa821f4f62d4b3a55278276d03474580fee668ee50b7"
 
   url "https://download01.logi.com/web/ftp/pub/controldevices/unifying/unifying#{version}_mac.zip"
   name "Logitech Unifying Software"
