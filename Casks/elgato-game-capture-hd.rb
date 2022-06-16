0c5ae0b9631b420bc85c253ea137603d68988892
Bo Anderson <mail@boanderson.me>
2022-06-16 03:39:10 +0100

    elgato-game-capture-hd: remove Yosemite support

--- Casks/elgato-game-capture-hd.rb
+++ Casks/elgato-game-capture-hd.rb
@@ -1,10 +1,5 @@
 cask "elgato-game-capture-hd" do
-  if MacOS.version <= :yosemite
-    version "2.0.5,983"
-    sha256 "4803bcac9069e1e63a89e9053fdf2285487acf9e608e84f7610555075489ad5a"
-
-    url "https://gc-updates.elgato.com/mac/download.php?build=#{version.csv.second}"
-  elsif MacOS.version <= :el_capitan
+  if MacOS.version <= :el_capitan
     version "2.9.2,1327"
     sha256 "9bcf01399719755034c964549a6a3af38932e7eaf03febc8b3742306505ca8a9"
 
