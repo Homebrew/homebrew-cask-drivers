cc93e6beb1abafd0bb03ea57db825725026af55c
Bo Anderson <mail@boanderson.me>
2022-06-16 03:39:10 +0100

    roland-quad-capture-usb-driver: remove Yosemite support

--- Casks/roland-quad-capture-usb-driver.rb
+++ Casks/roland-quad-capture-usb-driver.rb
@@ -1,11 +1,5 @@
 cask "roland-quad-capture-usb-driver" do
-  if MacOS.version <= :yosemite
-    version "1.5.2,10"
-    sha256 "da575f4b3874e0042fddffb4462f4521019ceaa3f44942f64bce099b426cf2d1"
-    url "https://static.roland.com/assets/media/tgz/quad_mx#{version.after_comma}d#{version.before_comma.no_dots}.tgz"
-
-    pkg "QuadCaptureUSBDriver/QuadCapture_USBDriver10#{version.after_comma}.pkg"
-  elsif MacOS.version <= :sierra
+  if MacOS.version <= :sierra
     version "1.5.3,12"
     sha256 "712b27a25275d748e35c174b242f21a2967f5caca013f6cb09330b9232288770"
     url "https://static.roland.com/assets/media/tgz/quad_m#{version.after_comma}d#{version.before_comma.no_dots}.tgz"
