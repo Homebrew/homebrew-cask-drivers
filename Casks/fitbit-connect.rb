31d9a0cb9b8842abfe870315e414db4245a74e6e
Bevan Kay <email@bevankay.me>
2022-06-27 17:26:35 +1000

    fitbit-connect: add livecheck (#2890)
    Update fitbit-connect from 2.0.2.7241-2018-07-25 to 2.0.2.7241-2018-07-25

--- Casks/fitbit-connect.rb
+++ Casks/fitbit-connect.rb
@@ -3,9 +3,14 @@ cask "fitbit-connect" do
   sha256 "464ccce6b3ea13c1836dd74e315270816fe15cb6e19276be1b189ba99ad5d346"
 
   url "https://cache.fitbit.com/FitbitConnect/FitbitConnect-v#{version}.dmg"
-  appcast "https://desktop-static.fitbit.com/content/rss/client/synclair/fitbit-connect-update-mac.xml"
   name "Fitbit Connect"
+  desc "Connect your Fitbit device"
   homepage "https://www.fitbit.com/"
+
+  livecheck do
+    url "https://www.fitbit.com/global/us/setup"
+    regex(/href=.*?FitbitConnect[._-]v?(\d+(?:[.-]\d+)+)\.dmg/i)
+  end
 
   pkg "Install Fitbit Connect.pkg"
 
