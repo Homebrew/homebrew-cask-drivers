0020bf7b06903ee998067bfad55c306a1ec1c390
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:29:45 +0100

    synology-drive: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/synology-drive.rb
+++ Casks/synology-drive.rb
@@ -20,10 +20,10 @@ cask "synology-drive" do
   pkg "Install Synology Drive Client.pkg"
 
   uninstall quit:      [
-    "io.com.synology.CloudStationUI",
-    "com.synology.CloudStation",
-    "com.synology.CloudStationUI",
-  ],
+              "io.com.synology.CloudStationUI",
+              "com.synology.CloudStation",
+              "com.synology.CloudStationUI",
+            ],
             pkgutil:   "com.synology.CloudStation",
             launchctl: "com.synology.Synology Cloud Station",
             delete:    "/Applications/Synology Drive Client.app"
