d11897b4eb2e7ad0a1bafe75101eca6c2c2ce233
Bevan Kay <email@bevankay.me>
2022-07-06 12:57:01 +1000

    Update audient-evo from 4.2.3 to 4.3.4 (#2904)

--- Casks/audient-evo.rb
+++ Casks/audient-evo.rb
@@ -1,6 +1,6 @@
 cask "audient-evo" do
-  version "4.2.3"
-  sha256 "5a5645eec7ca5b44dde2c1cbb0b3fbb2d8b0b9ab17362f63bd19b11336cb54da"
+  version "4.3.4"
+  sha256 "b369876e29f83771497f7e23800e43cfe59e2efd6437d998cac51447ace29f93"
 
   url "https://d9w4fhj63j193.cloudfront.net/EVO/EVO%20Drivers/EVO%20v#{version}.dmg",
       verified: "d9w4fhj63j193.cloudfront.net/EVO/EVO%20Drivers/"
