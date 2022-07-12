dd51f2d3277507ab7a20c0ea7bafe956e58bb641
Miccal Matthews <miccal.matthews@gmail.com>
2022-07-13 06:29:52 +0800

    Update nordic-nrf-connect.rb

--- Casks/nordic-nrf-connect.rb
+++ Casks/nordic-nrf-connect.rb
@@ -2,8 +2,8 @@ cask "nordic-nrf-connect" do
   version "3.11.1"
   sha256 "3b986650099cbc2f8030e93888811111b391d188b3183fbeac835443ec0ad91b"
 
-  url "https://github.com/NordicSemiconductor/pc-nrfconnect-core/releases/download/v#{version}/nrfconnect-#{version}.dmg",
-      verified: "github.com/NordicSemiconductor/pc-nrfconnect-core/"
+  url "https://github.com/NordicSemiconductor/pc-nrfconnect-launcher/releases/download/v#{version}/nrfconnect-#{version}.dmg",
+      verified: "github.com/NordicSemiconductor/pc-nrfconnect-launcher"
   name "nRF Connect"
   desc "Development software for Bluetooth Low Energy and cIoT"
   homepage "https://www.nordicsemi.com/Software-and-tools/Development-Tools/nRF-Connect-for-desktop/"
