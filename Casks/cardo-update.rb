379229bb41921fa94018f24db3871f8aaedec21c
Miccal Matthews <miccal.matthews@gmail.com>
2022-07-14 15:28:42 +0800

    Update cardo-update.rb

--- Casks/cardo-update.rb
+++ Casks/cardo-update.rb
@@ -1,11 +1,16 @@
 cask "cardo-update" do
-  version "3.6.0"
+  version "3.6.0.21600"
   sha256 :no_check
 
   url "https://update.cardosystems.com/cardo-app/cardo_updater_macOS_latest.pkg"
   name "Cardo Update"
-  desc "Update your cardo to keep it fresh"
-  homepage "https://www.cardosystems.com/software-update-info/"
+  desc "Update your Cardo Communication System"
+  homepage "https://www.cardosystems.com/update"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   depends_on macos: ">= :el_capitan"
 
