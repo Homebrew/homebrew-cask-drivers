9b5c3aa94bf85c87470fe6665d9c916c92d3efc9
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-07-14 02:43:05 -0500

    ADD cardo-update cask version 3.6.0 (#2913)
    * ADD cardo-update cask version 3.6.0
    
    * Update cardo-update.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/cardo-update.rb
@@ -0,0 +1,21 @@
+cask "cardo-update" do
+  version "3.6.0.21600"
+  sha256 :no_check
+
+  url "https://update.cardosystems.com/cardo-app/cardo_updater_macOS_latest.pkg"
+  name "Cardo Update"
+  desc "Update your Cardo Communication System"
+  homepage "https://www.cardosystems.com/update"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  depends_on macos: ">= :el_capitan"
+
+  pkg "cardo_updater_macOS_latest.pkg"
+
+  uninstall pkgutil: "com.cardo.fwupdater",
+            delete:  "/Applications/Cardo Update.app"
+end
