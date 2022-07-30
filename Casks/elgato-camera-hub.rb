56ec2c3f87544345287549e0490b9a077151d01a
Roman Zhuzha <sanyer@users.noreply.github.com>
2022-07-30 06:14:10 +0200

    Update elgato-camera-hub from 1.2.2.397 to 1.2.3.409 (#2936)
    * Update elgato-camera-hub from 1.2.3.409 to 1.2.3.409
    
    * Update elgato-camera-hub.rb
    
    * Update elgato-camera-hub.rb
    
    * Update elgato-camera-hub.rb
    
    * Update elgato-camera-hub.rb
    
    * Update elgato-camera-hub.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/elgato-camera-hub.rb
+++ Casks/elgato-camera-hub.rb
@@ -1,18 +1,24 @@
 cask "elgato-camera-hub" do
-  version "1.2.2.397"
-  sha256 "2e35174461b01a22494c2f710160d76b7e1b44cac629d2ce63510d4518d65d7a"
+  version "1.2.3.409,2"
+  sha256 "1d7e062ae1c205cbae37ce49d7f16401f25d62a81f9230a99802ed49ae655c31"
 
-  url "https://edge.elgato.com/egc/macos/echm/#{version.major_minor_patch.delete_suffix(".0")}/Camera_Hub_#{version}.pkg"
+  url "https://edge.elgato.com/egc/macos/echm/#{version.major_minor_patch.delete_suffix(".0")}/Camera_Hub_#{version.csv.first}#{version.csv.second.prepend("%23") if version.csv.second}.pkg"
   name "elgato-camera-hub"
   desc "Elgato FACECAM configuration tool"
   homepage "https://www.elgato.com/en/facecam"
 
   livecheck do
     url "https://www.elgato.com/sites/default/files/downloads.json"
-    regex(%r{"downloadURL"\s*:\s*"[^"]*?/Camera[._-]Hub[._-]v?(\d+(?:\.\d+)+)\.pkg"}i)
+    regex(%r{"downloadURL"\s*:\s*"[^"]*?/Camera[._-]Hub[._-]v?(\d+(?:\.\d+)+)(?:%23)?(\d+)?\.pkg"}i)
+    strategy :page_match do |page, regex|
+      match = page.scan(regex).flatten
+      match if match.second.blank?
+
+      "#{match.first},#{match.second}"
+    end
   end
 
-  pkg "Camera_Hub_#{version}.pkg"
+  pkg "Camera_Hub_#{version.tr(",", "#")}.pkg"
 
   uninstall launchctl: "com.elgato.CameraHub",
             quit:      "com.elgato.CameraHub",
