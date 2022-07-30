b162b3aa547bb189e7d7f5d7e317ae8869f0ad16
Bevan Kay <email@bevankay.me>
2022-07-30 12:55:08 +1000

    Update elgato-camera-hub.rb

--- Casks/elgato-camera-hub.rb
+++ Casks/elgato-camera-hub.rb
@@ -1,15 +1,21 @@
 cask "elgato-camera-hub" do
-  version "1.2.3.409"
-  sha256 "826d92454fa35e3c5bb7b802cb69cfa098eb3f2df0958a7b5e5ac87cf167b6b2"
+  version "1.2.3.409,2"
+  sha256 "1d7e062ae1c205cbae37ce49d7f16401f25d62a81f9230a99802ed49ae655c31"
 
-  url "https://edge.elgato.com/egc/macos/echm/#{version.major_minor_patch.delete_suffix(".0")}/Camera_Hub_#{version}.pkg"
+  url "https://edge.elgato.com/egc/macos/echm/#{version.major_minor_patch.delete_suffix(".0")}/Camera_Hub_#{version.csv.first}#{version.csv.second.prepend("%23") if version.csv.second}.pkg"
   name "elgato-camera-hub"
   desc "Elgato FACECAM configuration tool"
   homepage "https://www.elgato.com/en/facecam"
 
   livecheck do
     url "https://www.elgato.com/sites/default/files/downloads.json"
-    regex(%r{"downloadURL"\s*:\s*"[^"]*?/Camera[._-]Hub[._-]v?(\d+(?:\.\d+)+)\.pkg"}i)
+    regex(%r{"downloadURL"\s*:\s*"[^"]*?/Camera[._-]Hub[._-]v?(\d+(?:\.\d+)+)(?:%23)?(\d+)?\.pkg"}i)
+    strategy :page_match do |page, regex|
+      match = page.scan(regex).flatten
+      match if match.second.blank?
+
+      "#{match.first},#{match.second}"
+    end
   end
 
   pkg "Camera_Hub_#{version}.pkg"
