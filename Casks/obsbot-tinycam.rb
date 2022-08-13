5ff9881a0d3d3cbaa86453327adc4c5c6090a00a
Malo Bourgon <mbourgon@gmail.com>
2022-08-13 10:52:36 -0700

    Update obsbot-tinycam from 1.3.6.12 to 1.3.8.15 (#2956)
    * Update obsbot-tinycam from 1.3.6.12 to 1.3.8.15
    
    * Update obsbot-tinycam homepage, fixing livecheck

--- Casks/obsbot-tinycam.rb
+++ Casks/obsbot-tinycam.rb
@@ -1,19 +1,18 @@
 cask "obsbot-tinycam" do
-  version "1.3.6.12"
-  sha256 "fb0d3245a77e11ebe1d0a9db2c879a35d1eb9a7844313420681c029bd1f2e731"
+  version "1.3.8.15"
+  sha256 "132955ccd96e82d198df25fcfcb39bcd0c8d97a14d7c8dcb3fd9023b7c250bd1"
 
   url "https://obsbot-static-resource.oss-cn-hongkong.aliyuncs.com/download/obsbot-tiny-4k/software/Obsbot_tiny_pc_OA_E_MacOS_#{version}_release.dmg",
       verified: "obsbot-static-resource.oss-cn-hongkong.aliyuncs.com/download/obsbot-tiny-4k/software/"
   name "OBSBOT TinyCam"
   desc "Configuration and firmware update utility for OBSBOT Tiny and Tiny 4K webcams"
-  homepage "https://www.obsbot.com/download"
+  homepage "https://www.obsbot.com/download/obsbot-tiny-series"
 
   livecheck do
     url :homepage
     regex(/Obsbot[._-]tiny[._-]pc[._-]OA[._-]E[._-]MacOS[._-](\d+(?:\.\d+)+)[._-]release\.dmg/i)
   end
 
-  auto_updates true
   depends_on macos: ">= :high_sierra"
 
   app "OBSBOT TinyCam.app"
