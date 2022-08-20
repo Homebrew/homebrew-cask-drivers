38c108baca1cc7a8e27a88e640005e611993c65b
Aleks Bunin <github@compuix.com>
2022-08-20 15:12:38 -0400

    Update mazda-toolbox from 2002149986 to 2022060400

--- Casks/mazda-toolbox.rb
+++ Casks/mazda-toolbox.rb
@@ -1,12 +1,14 @@
 cask "mazda-toolbox" do
-  version "2002149986,2020_08_20__12_30_00"
-  sha256 "0667c08422148770650c8b7951f304073c0ba72f7a554b27d716d3b284eea489"
+  version "2022060400,2022_02_10_11_36"
+  sha256 "f030d9e0242a2b4685a09eabcc22d6f2334bc8380b3e7730374b08f355b9c123"
 
-  url "https://azure.distrib.naviextras.com/content/!application/TOOLBOX/Agents/Mazda/OSX/#{version.csv.second}/Mazda-Toolbox.dmg",
-      verified: "azure.distrib.naviextras.com/content/!application/"
+
+  url "https://download.naviextras.com/content/!application/TOOLBOX/Agents/Mazda/#{version.csv.second}/Mazda-Toolbox.dmg",
+      verified: "download.naviextras.com/content/!application/"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.naviextras.com/to/MazdaToolboxOSX",
           must_contain: version.after_comma
   name "Mazda Toolbox"
+  desc "Map Updater for Mazda"
   homepage "https://infotainment.mazdahandsfree.com/navigation-updatemymaps"
 
   app "Mazda Toolbox.app"
