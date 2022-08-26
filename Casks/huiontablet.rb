9ca5acc1c77ac9de31c956daf56de9d912360a59
chiefmikey <wolfemikl@gmail.com>
2022-08-25 20:23:37 -0600

    Update huiontablet from 15.6.1.34 to 15.6.2.50

--- Casks/huiontablet.rb
+++ Casks/huiontablet.rb
@@ -1,6 +1,6 @@
 cask "huiontablet" do
-  version "15.6.1.34"
-  sha256 "bb802892e1d31d77cafe4e4a7e48343a3ef721907233f5512c6ab13a2863c535"
+  version "15.6.2.50"
+  sha256 "f381145306a0343c1c447b2123680390b104a7854a875d9d86b19cdedea753c8"
 
   url "https://driverdl.huion.com/driver/Mac/HuionTablet_MacDriver_v#{version}.dmg"
   name "Huion Tablet"
