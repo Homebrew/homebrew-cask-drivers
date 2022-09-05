5b2f930541b47d1aa13cc77424f18b893ad781fa
Jason Tedor <jason@tedor.me>
2022-09-05 15:56:49 -0400

    Correct download URL

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -2,7 +2,7 @@ cask "sonos" do
   version "14.14,69.1.32100"
   sha256 "f8c7a048d7e6185accab9dedaf0fb69c64c25a77ae2a68e7ac1141b3767c3dd6"
 
-  url "https://update-software.sonos.com/software/flfowmpo/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
+  url "https://update-software.sonos.com/software/hqppgqce/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
   name "Sonos"
   desc "Control your Sonos system"
   homepage "https://www.sonos.com/"
