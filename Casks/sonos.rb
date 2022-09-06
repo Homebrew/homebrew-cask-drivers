d54a8559337e711f70fd76590e8271ca2d69f003
Jason Tedor <jason@tedor.me>
2022-09-05 17:47:47 -0700

    Update sonos from 14.12 to 14.14 (#2992)
    * Update sonos from 14.12 to 14.14
    
    * Correct download URL
    
    * Fix SHA

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,8 +1,8 @@
 cask "sonos" do
-  version "14.12,69.1.31120"
-  sha256 "f8c7a048d7e6185accab9dedaf0fb69c64c25a77ae2a68e7ac1141b3767c3dd6"
+  version "14.14,69.1.32100"
+  sha256 "bd1cb1a3f3cd3324259754c583ff559ed4bf868db7a59ad66c39e870948b8f23"
 
-  url "https://update-software.sonos.com/software/flfowmpo/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
+  url "https://update-software.sonos.com/software/hqppgqce/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
   name "Sonos"
   desc "Control your Sonos system"
   homepage "https://www.sonos.com/"
