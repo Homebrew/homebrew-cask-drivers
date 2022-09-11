4e9477ceb9bf39c9b08449862b55cd00d1ad3acf
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-12 07:46:53 +0800

    Update kyocera-printer-drivers.rb

--- Casks/kyocera-printer-drivers.rb
+++ Casks/kyocera-printer-drivers.rb
@@ -1,26 +1,32 @@
 cask "kyocera-printer-drivers" do
-  version "5.1,2020.07.01"
+  version "5.4,2022.07.11,2022.06.01"
   sha256 "21bc52ac4a1d2d074a1c56bb17d76092dd0e2405648ffa960577e603eed7cd0c"
 
-  url "https://cdn.kyostatics.net/dlc/eu/driver/all/kyocera_os_x_10_6.-downloadcenteritem-Single-File.downloadcenteritem.tmp/MacPhase#{version.csv.first.no_dots}_#{version.csv.second.dots_to_underscores}.zip",
-      verified: "kyostatics.net/dlc/eu/driver/all/"
-  appcast "https://dlc.kyoceradocumentsolutions.eu/index/service/dlc.false._.TASKALFA5053CI._.EN.html"
+  url "https://dam.kyoceradocumentsolutions.com/content/dam/gdam_dc/dc_global/executables/web/Mac#{version.csv.first.no_dots}_#{version.csv.second}-KDC-en.zip"
   name "Kyocera Mac Driver"
-  homepage "https://dlc.kyoceradocumentsolutions.eu/index/service/dlc.false._.TASKALFA5053CI._.EN.html#MAC"
+  homepage "https://www.kyoceradocumentsolutions.com/download/"
 
-  container nested: "Mac#{version.before_comma.no_dots}_#{version.after_comma}-KDC/Kyocera OS X 10.9+ Web build #{version.after_comma}.dmg"
+  livecheck do
+    skip "No version information available"
+  end
 
-  pkg "Kyocera OS X 10.9+ Web build #{version.after_comma}.pkg"
+  container nested: "Kyocera OS X 10.9+ Web build #{version.csv.second}.dmg"
+
+  pkg "Kyocera OS X 10.9+ Web build #{version.csv.third}.pkg"
 
   uninstall pkgutil: [
-    "com.kyocera.kyoceraMacintosh.kyoPDFfilter_F.pkg",
-    "com.kyocera.kyoceraMacintosh.PreferencePanes.pkg",
-    "com.kyocera.kyoceraMacintosh.PCL_F.pkg",
-    "com.kyocera.kyoceraMacintosh.kyoprefilter_F.pkg",
-    "com.kyocera.kyoceraMacintosh.kyofilter_f.pkg",
-    "com.kyocera.kyoceraMacintosh.Print_Panel.pkg",
+    "com.kyocera.kyoceraMacintosh.Adjustment.pkg",
+    "com.kyocera.kyoceraMacintosh.Framework.pkg",
     "com.kyocera.kyoceraMacintosh.KfSummary.pkg",
+    "com.kyocera.kyoceraMacintosh.kpsl.pkg",
+    "com.kyocera.kyoceraMacintosh.kyofilter_f.pkg",
+    "com.kyocera.kyoceraMacintosh.kyoPDFfilter_F.pkg",
+    "com.kyocera.kyoceraMacintosh.kyoprefilter_F.pkg",
     "com.kyocera.kyoceraMacintosh.OSX10-1.pkg",
+    "com.kyocera.kyoceraMacintosh.OSX10-2.pkg",
     "com.kyocera.kyoceraMacintosh.OSX10.pkg",
+    "com.kyocera.kyoceraMacintosh.PCL_F.pkg",
+    "com.kyocera.kyoceraMacintosh.PreferencePanes.pkg",
+    "com.kyocera.kyoceraMacintosh.Print_Panel.pkg",
   ]
 end
