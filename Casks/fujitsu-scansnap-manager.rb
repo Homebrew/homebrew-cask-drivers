c6d23a84a8c8b35abdd42aeba64223be2280f9d9
Tom Hoover <tom@hisword.net>
2022-09-12 19:29:01 -0500

    Update fujitsu-scansnap-manager from 7.0L30 to 7.2L50 (#3001)
    * Update fujitsu-scansnap-manager from 7.0L30 to 7.2L50
    
    * Update fujitsu-scansnap-manager.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/fujitsu-scansnap-manager.rb
+++ Casks/fujitsu-scansnap-manager.rb
@@ -1,12 +1,17 @@
 cask "fujitsu-scansnap-manager" do
-  version "7.0L30"
-  sha256 "4458776ac2d117992fb4010e7d75363261cda5bc9cdf6a4abdc3126f3b0318d1"
+  version "7.2L50"
+  sha256 "82142858b9734ed61b7f5d816737502c565e37e61fd5f11fef0145e84c122fd7"
 
   url "https://origin.pfultd.com/downloads/IMAGE/driver/ss/inst/ix1500/m-software/MacManagerV#{version.no_dots}WW.dmg",
       verified: "origin.pfultd.com/"
   name "ScanSnap Manager for Mac"
   desc "Scan software for Fujitsu document scanners"
   homepage "https://scansnap.fujitsu.com/"
+
+  livecheck do
+    url "https://scansnap.fujitsu.com/global/dl/mac-ssinst2-list.html"
+    regex(/ScanSnap\sManager\sV?(\d+(?:.\d+)+)/i)
+  end
 
   pkg "ScanSnap Manager.pkg"
 
