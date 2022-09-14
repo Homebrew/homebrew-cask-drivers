fc57d5dc545f1f15e75b5d2c7846d63ea4b36749
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-14 10:29:28 +0800

    Update droidcam-obs.rb

--- Casks/droidcam-obs.rb
+++ Casks/droidcam-obs.rb
@@ -2,19 +2,15 @@ cask "droidcam-obs" do
   version "1.6.0"
   sha256 "040e80cfe5469ae98f50206f58a218bc9efed36b11cafdc447659ed42e37988e"
 
-  url "https://github.com/dev47apps/droidcam-obs-plugin/releases/download/1.6.0/DroidCamOBS_1.6.0_macos.pkg"
+  url "https://github.com/dev47apps/droidcam-obs-plugin/releases/download/#{version}/DroidCamOBS_#{version}_macos.pkg",
+      verified: "github.com/dev47apps/droidcam-obs-plugin/"
   name "DroidCam OBS"
   desc "Use your phone as a camera directly in OBS Studio"
   homepage "https://www.dev47apps.com/obs/"
 
-  livecheck do
-    url "https://www.dev47apps.com/go/DroidCamOBS.Setup.pkg"
-    strategy :header_match
-  end
-
   depends_on cask: "obs"
 
-  pkg "DroidCamOBS_Universal_#{version}.pkg"
+  pkg "DroidCamOBS_#{version}_macos.pkg"
 
   uninstall pkgutil: "com.dev47apps.obsdroidcam",
             delete:  "/Library/Application Support/obs-studio/plugins/droidcam-obs",
