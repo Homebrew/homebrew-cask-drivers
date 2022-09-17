790121fcaaa2b46f81575b0e03db50f1f1e084c2
Jason Tedor <jason@tedor.me>
2022-09-17 11:31:21 -0400

    Update sonos from 14.15,69.1.32152 to 14.15,69.1.32152

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,5 +1,5 @@
 cask "sonos" do
-  version "14.14,69.1.32100"
+  version "14.15,69.1.32152"
   sha256 "bd1cb1a3f3cd3324259754c583ff559ed4bf868db7a59ad66c39e870948b8f23"
 
   url "https://update-software.sonos.com/software/hqppgqce/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
