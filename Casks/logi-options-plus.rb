9a8b9accbc97391bc135cd8b5a66a9e26cab5161
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-09-20 12:46:25 +0530

    logi-options-plus 1.30.0.7349
    * Update logi-options-plus from latest to 1.22.5550
    * Update logi-options-plus.rb
    * Update logi-options-plus.rb
    * Update logi-options-plus from latest to 1.24.0.2581
    * Bump logi-options-plus from latest to 1.29.0.6100
    * Mark logi-options-plus as auto_updates
    * Bump logi-options-plus from latest to 1.30.0.7349
    
    Closes #3008.
    
    Signed-off-by: Moulick Aggarwal <moulickaggarwal@gmail.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/logi-options-plus.rb
+++ Casks/logi-options-plus.rb
@@ -1,5 +1,5 @@
 cask "logi-options-plus" do
-  version :latest
+  version "1.30.0.7349"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
@@ -8,6 +8,14 @@ cask "logi-options-plus" do
   desc "Software for Logitech devices"
   homepage "https://www.logitech.com/en-us/software/logi-options-plus.html"
 
+  livecheck do
+    url :url
+    strategy :extract_plist do |versions|
+      versions.values.map(&:version).compact.first
+    end
+  end
+
+  auto_updates true
   depends_on macos: ">= :catalina"
 
   installer manual: "logioptionsplus_installer.app"
@@ -15,28 +23,28 @@ cask "logi-options-plus" do
   uninstall launchctl: [
               "com.logi.cp-dev-mgr",
               "com.logi.optionsplus",
-              "com.logi.optionsplus.updater",
               "com.logi.optionsplus.agent",
+              "com.logi.optionsplus.updater",
             ],
             quit:      [
               "com.logi.cp-dev-mgr",
               "com.logi.optionsplus",
-              "com.logi.optionsplus.updater",
               "com.logi.optionsplus.agent",
+              "com.logi.optionsplus.updater",
             ],
             delete:    [
               "/Applications/logioptionsplus.app",
-              "/Library/LaunchDaemons/com.logi.optionsplus.updater.plist",
               "/Library/LaunchAgents/com.logi.optionsplus.plist",
+              "/Library/LaunchDaemons/com.logi.optionsplus.updater.plist",
             ]
 
   zap trash: [
-    "~/Library/Application Support/LogiOptionsPlus",
-    "~/Library/Preferences/com.logi.optionsplus.plist",
-    "~/Library/Preferences/com.logi.cp-dev-mgr.plist",
-    "~/Library/Application Support/logioptionsplus",
-    "~/Library/Saved Application State/com.logi.optionsplus.savedState",
     "/Users/Shared/LogiOptionsPlus",
+    "~/Library/Application Support/LogiOptionsPlus",
+    "~/Library/Application Support/logioptionsplus",
+    "~/Library/Preferences/com.logi.cp-dev-mgr.plist",
+    "~/Library/Preferences/com.logi.optionsplus.plist",
+    "~/Library/Saved Application State/com.logi.optionsplus.savedState",
   ]
 
   caveats do
