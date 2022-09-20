56b222d04e2e319868be0440db816dde5644ab08
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-09-20 12:46:25 +0530

    Update logi-options-plus from latest to 1.22.5550

--- Casks/logi-options-plus.rb
+++ Casks/logi-options-plus.rb
@@ -1,5 +1,5 @@
 cask "logi-options-plus" do
-  version :latest
+  version "1.22.5550"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
