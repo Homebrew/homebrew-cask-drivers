2aac57026173a54a353fc2599711cf9531536cd4
Leandro Facchinetti <git@leafac.com>
2022-09-24 11:49:20 +0100

    Update obs-ios-camera-source from 2.9.0 to 2.9.3

--- Casks/obs-ios-camera-source.rb
+++ Casks/obs-ios-camera-source.rb
@@ -1,8 +1,8 @@
 cask "obs-ios-camera-source" do
-  version "2.9.0"
-  sha256 "afef9b3b9b5a0494a79f3c3af84d090312141196782b3cd577b9086d1c3f9b9b"
+  version "2.9.3"
+  sha256 "18622d510da58bc903bf05a985c1cb0a6dc3f9591c698d89f37595b18ba79624"
 
-  url "https://github.com/wtsnz/obs-ios-camera-source/releases/download/v#{version}/obs-ios-camera-source-v#{version}-macos.pkg",
+  url "https://github.com/wtsnz/obs-ios-camera-source/releases/download/v#{version}/obs-ios-camera-source-#{version}-macos-universal.pkg",
       verified: "github.com/wtsnz/obs-ios-camera-source/"
   name "OBS iOS Camera Source"
   desc "iOS camera plugin for OBS Studio"
@@ -16,7 +16,7 @@ cask "obs-ios-camera-source" do
   depends_on cask: "obs"
   depends_on macos: ">= :high_sierra"
 
-  pkg "obs-ios-camera-source-v#{version}-macos.pkg"
+  pkg "obs-ios-camera-source-#{version}-macos-universal.pkg"
 
   uninstall pkgutil: "com.wtsnz.obs-ios-camera-source-plugin",
             delete:  "/Library/Application Support/obs-studio/plugins/obs-ios-camera-source",
