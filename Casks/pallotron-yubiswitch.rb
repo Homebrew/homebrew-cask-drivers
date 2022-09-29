d6fec7c8494bdc05906b7462ff24de5bfe2d0ba1
Steve Lessard <slessardjr@users.noreply.github.com>
2022-09-29 14:00:00 -0400

    Update for YubiSwitch 0.14 release
    https://github.com/pallotron/yubiswitch/releases/tag/v0.14

--- Casks/pallotron-yubiswitch.rb
+++ Casks/pallotron-yubiswitch.rb
@@ -1,6 +1,6 @@
 cask "pallotron-yubiswitch" do
-  version "0.12"
-  sha256 "691ff7017c000211587217c60a6a9f352b3cb8d9ee2ce099f01d096d794a01ff"
+  version "0.14"
+  sha256 "fe514658b6bab343840172921f70f9f5d953eb04bc8f96c30800ac5906711966"
 
   url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}/yubiswitch_#{version}.dmg"
   name "Yubiswitch"
