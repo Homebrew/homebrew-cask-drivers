8f97e8f94bbf49a646451e27b9be517de99dbd48
Jason Masker <masker@post.harvard.edu>
2022-10-11 01:36:37 -0400

    Update xerox-print-driver (#3039)
    * Update xerox-print-driver
    
    * Update xerox-print-driver.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/xerox-print-driver.rb
+++ Casks/xerox-print-driver.rb
@@ -7,15 +7,19 @@ cask "xerox-print-driver" do
     version "5.2.0_2115"
     sha256 "9989b6c127fca8c97b24bd86fd4d20035cd094c69e3fd41f6a243361f86483ec"
     url "https://download.support.xerox.com/pub/drivers/CQ8570/drivers/macos1012/pt_BR/XeroxPrintDriver_#{version}.dmg"
-  else
+    elseif MacOS.version <= :mojave
     version "5.5.9_2180"
     sha256 "24e700fedf39391d9df3310d34dcf98cfe4b6e46acfead09903c8befd10b1345"
     url "https://download.support.xerox.com/pub/drivers/BALTORO_HF/drivers/macOS10_13/pt_BR/XeroxDrivers_#{version}.dmg"
+  else
+    version "5.10.1_2333"
+    sha256 "c8d4ca41d939aee275831eeebcd0d84aa2f219e24d4100c63ef0b78110fe7680"
+    url "https://download.support.xerox.com/pub/drivers/ALB80XX/drivers/macOS10_15/pt_BR/XeroxDrivers_#{version}.dmg"
   end
 
   name "Xerox Print Driver"
   desc "Drivers for Xerox printers"
-  homepage "https://www.support.xerox.com/en-us/product/colorqube-8570/downloads"
+  homepage "https://www.support.xerox.com/en-us/product/phaser-6510/downloads"
 
   livecheck do
     url :homepage
@@ -31,6 +35,7 @@ cask "xerox-print-driver" do
   uninstall launchctl: [
               "com.aviatainc.powerengage.XRTK",
               "com.aviatainc.powerengage.helper.XRTK",
+              "com.xerox.AnalyticsAgent",
             ],
             quit:      "com.aviatainc.powerengage.EngageLauncher",
             pkgutil:   [
