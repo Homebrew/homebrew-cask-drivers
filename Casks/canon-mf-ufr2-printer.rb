874bcb1646f30bb01f987230d255d78b72a48d51
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-12 19:43:58 +0800

    Update canon-mf-ufr2-printer.rb

--- Casks/canon-mf-ufr2-printer.rb
+++ Casks/canon-mf-ufr2-printer.rb
@@ -2,7 +2,7 @@ cask "canon-mf-ufr2-printer" do
   version "10.19.13"
   sha256 "11e32221671b4622f97e89f6ec1c80b38f6f507558baf90f8bb82ca343583d64"
 
-  url "https://gdlp01.c-wss.com/gds/1/0100011351/05/mac-UFRII-LIPSLX-v#{version}-18.dmg",
+  url "https://gdlp01.c-wss.com/gds/1/0100011351/05/mac-UFRII-LIPSLX-v#{version.no_dots}-18.dmg",
       verified: "gdlp01.c-wss.com/gds/"
   name "Canon MF UFRII/UFRII LT Printer Driver & Utilities"
   desc "Printer UFRII/UFRII LT driver & utilities for Canon imageCLASS MF printers"
