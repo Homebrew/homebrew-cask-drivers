8dd21beea64cff533f4c5845cc36d0bcd3d1808d
Pavel Zwerschke <pavelzw@gmail.com>
2022-10-12 13:36:45 +0200

    Update canon-eos-webcam-utility from 1.0 to 1.1 (#3042)
    * Update canon-eos-webcam-utility from 1.0 to 1.1
    
    * Update canon-eos-webcam-utility.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/canon-eos-webcam-utility.rb
+++ Casks/canon-eos-webcam-utility.rb
@@ -1,21 +1,29 @@
 cask "canon-eos-webcam-utility" do
-  version "1.0"
-  sha256 "38b232bd9e91064bb291b26e831cee088c6ede2c4e3b1bb18c08280b11108304"
+  version "1.1"
+  sha256 "37b84335433ed585e477d0ab3c91ca99d1b578f138ba2d76660a270c6038be7b"
 
   url "https://downloads.canon.com/webcam/EOSWebcamUtility-MAC#{version}.pkg.zip"
   name "Canon EOS Webcam Utility"
   desc "Use a Canon camera as a webcam"
-  homepage "https://www.usa.canon.com/internet/portal/us/home/support/self-help-center/eos-webcam-utility/"
+  homepage "https://www.usa.canon.com/cameras/eos-webcam-utility"
+
+  livecheck do
+    url :homepage
+    regex(/MAC\sOFFICIAL\sRELEASE\sVERSION\sv?(\d+(?:\.\d+)+)/i)
+  end
 
   pkg "EOSWebcamUtility-MAC#{version}.pkg"
 
-  uninstall launchctl: "com.canon.usa.EOSWebcamUtility",
+  uninstall launchctl: [
+              "com.canon.usa.EOSWebcamUtility",
+              "com.canon.usa.EWCService_x86_64_nonSIMD",
+            ],
             pkgutil:   "com.canon.ciis.pkg.EOSWebcamUtility",
             delete:    [
               "/Applications/EOS Webcam Utility",
               "/Library/CoreMediaIO/Plug-Ins/DAL/EOSWebcamUtility.plugin",
-              "/Library/LaunchAgents/com.canon.usa.EOSWebcamUtility.plist",
               "/Library/CoreMediaIO/Plug-Ins/DAL/EOSWebcamUtility.plugin",
+              "/Library/LaunchAgents/com.canon.usa.EOSWebcamUtility.plist",
             ],
             rmdir:     "/Library/Frameworks/EDSDK.framework"
 end
