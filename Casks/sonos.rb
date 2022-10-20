1a36ccbe58d9876e3e2df0e572cd768b80665abd
Jason Tedor <jason@tedor.me>
2022-10-20 06:58:04 -0400

    Update Casks/sonos.rb

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,5 +1,5 @@
 cask "sonos" do
-  version "14.18,70.1-34112"
+  version "14.18,70.1.34112"
   sha256 "de091fcd1ac585260a3f4115a950031fd399114498bdfe5ac1e6a573484bc5ab"
 
   url "https://update-software.sonos.com/software/imaqiycw/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
