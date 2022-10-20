65eb7daadd6ea1a4ce10c0be0a350970d506ba8d
Grettir <30250+grettir@users.noreply.github.com>
2022-10-19 23:02:46 -0600

    Update concept2-utility from 7.10.11 to 7.10.15

--- Casks/concept2-utility.rb
+++ Casks/concept2-utility.rb
@@ -3,8 +3,8 @@ cask "concept2-utility" do
     version "7.09.02"
     sha256 "e4ebee8cde57c7ef63c3903285c3fc0ee8f87221e7c5529b9dcf97b3f9ebb57e"
   else
-    version "7.10.11"
-    sha256 "0d8705caed67c4859f43eb176265d6c954ee1867b54dbb5097203c77d486fc80"
+    version "7.10.15"
+    sha256 "e92bc06fc75fd08291ed08e8ae726fc7461d4b64c42887641977b5be2817af93"
   end
 
   url "https://software.concept2.com/utility/Concept2Utility#{version.no_dots}.dmg"
