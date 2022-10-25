6598f81f85b19e5cd4a9109655008d2e1460170a
Kęstutis Triponis <kestutis@triponis.lt>
2022-10-25 21:42:28 +0200

    Update elgato-camera-hub from 1.2.3.409 to 1.4.0.770 (#3067)
    * Update elgato-camera-hub from 1.2.3.409 to 1.4.0.770
    
    * Remove comma for single component versions
    
    * Update elgato-camera-hub.rb
    
    * Update elgato-camera-hub.rb
    
    * Update elgato-camera-hub.rb
    
    * Update elgato-camera-hub.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/elgato-camera-hub.rb
+++ Casks/elgato-camera-hub.rb
@@ -1,6 +1,6 @@
 cask "elgato-camera-hub" do
-  version "1.2.3.409,2"
-  sha256 "1d7e062ae1c205cbae37ce49d7f16401f25d62a81f9230a99802ed49ae655c31"
+  version "1.4.0.770"
+  sha256 "4a8d2a8265333977ba641ca6a076a558cb106b2ba99450f831701e11fc136f9c"
 
   url "https://edge.elgato.com/egc/macos/echm/#{version.major_minor_patch.delete_suffix(".0")}/Camera_Hub_#{version.csv.first}#{version.csv.second.prepend("%23") if version.csv.second}.pkg"
   name "elgato-camera-hub"
@@ -12,17 +12,16 @@ cask "elgato-camera-hub" do
     regex(%r{"downloadURL"\s*:\s*"[^"]*?/Camera[._-]Hub[._-]v?(\d+(?:\.\d+)+)(?:%23)?(\d+)?\.pkg"}i)
     strategy :page_match do |page, regex|
       match = page.scan(regex).flatten
-      match if match.second.blank?
-
-      "#{match.first},#{match.second}"
+      match.second.blank? ? match : "#{match.first},#{match.second}"
     end
   end
 
   pkg "Camera_Hub_#{version.tr(",", "#")}.pkg"
 
-  uninstall launchctl: "com.elgato.CameraHub",
-            quit:      "com.elgato.CameraHub",
-            pkgutil:   "com.elgato.CameraHub.Installer"
+  uninstall signal:    ["TERM", "com.elgato.CameraHub"],
+            launchctl: "com.elgato.CameraHub",
+            pkgutil:   "com.elgato.CameraHub.Installer",
+            delete:    "/Applications/Elgato Camera Hub.app"
 
   zap trash: [
     "~/Library/Logs/CameraHub",
