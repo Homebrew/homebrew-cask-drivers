e0eb368eeb95d5da99a417f5830b5ba80652fb87
Patrick Linnane <patrick@linnane.io>
2022-11-01 22:24:17 -0700

    Update motu-m-series from 89383 to 92136

--- Casks/motu-m-series.rb
+++ Casks/motu-m-series.rb
@@ -1,8 +1,8 @@
 cask "motu-m-series" do
-  version "89383"
-  sha256 "b4fc567e85baf959dc2387ea96f0fffcac7fa91329ca5e02260762abcf0e9f82"
+  version "92136"
+  sha256 "89d377f65619e929b11c71a6483dd62429cfa831cec609ad32e1fa08de3297ce"
 
-  url "https://cdn-data.motu.com/downloads/audio/mseries/driver/MW/RC/MOTU%20M%20Series%20Installer%20(#{version}).pkg"
+  url "https://cdn-data.motu.com/downloads/audio/mseries/driver/RC/MOTU%20M%20Series%20Installer%20(#{version}).pkg"
   name "MOTU M Series Installer"
   desc "Install MOTU M2 and M4 system extension"
   homepage "https://motu.com/en-us/download/product/408/"
