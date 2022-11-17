c33b07ebea578c3edf79793290d5d63547c00dc7
Jason Tedor <jason@tedor.me>
2022-11-17 14:52:10 -0500

    Update sonos from 14.19,70.3.35070 to 14.19,70.3.35070

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,8 +1,8 @@
 cask "sonos" do
-  version "14.18,70.1.34112"
-  sha256 "de091fcd1ac585260a3f4115a950031fd399114498bdfe5ac1e6a573484bc5ab"
+  version "14.19,70.3.35070"
+  sha256 "02bb81c1f24e40491511cf3e3e13c20256cbe9eb562c00d53dce9cd675be6347"
 
-  url "https://update-software.sonos.com/software/imaqiycw/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
+  url "https://update-software.sonos.com/software/epqbkaso/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
   name "Sonos"
   desc "Control your Sonos system"
   homepage "https://www.sonos.com/"
