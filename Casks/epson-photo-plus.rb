acac6565dcdf0ae5c75af88e10827cbe2c6f634d
Mr-TTOT <51176964+Mr-TTOT@users.noreply.github.com>
2022-11-18 23:11:44 -0700

    Create Epson Drivers/Utilities (#3125)
    * Create epson-photo-plus
    
    * Create epson-print-layout
    
    * Create epson-surecolor-p900

---
+++ Casks/epson-photo-plus.rb
@@ -0,0 +1,22 @@
+cask "epson-photo-plus" do
+  version "3.7.0"
+  sha256 "eaa02b81cb01aed495d2493fc062b909c9fb068a6ab8f867084fa4f227280c01"
+
+  url "https://ftp.epson.com/drivers/EPPlus_#{version.no_dots}.dmg"
+  name "Epson Photo+"
+  desc "Arrange and print photos, including direct printing to discs"
+  homepage "https://epson.com/"
+
+  livecheck do
+    url "https://epson.com/Support/Printers/Single-Function-Inkjet-Printers/SureColor-Series/Epson-SureColor-P900/s/SPT_C11CH37201"
+    strategy :page_match
+    regex(/\s*Epson\sPhoto\+[\s(?:&nbsp;)]+v(\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :el_capitan"
+
+  pkg "Epson Photo Plus.pkg"
+
+  uninstall pkgutil: "com.epson.pkg.EpsonPhotoPlus"
+end
