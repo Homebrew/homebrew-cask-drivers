6d251581e470a9a7d1648a76d817718f989d32a4
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-24 10:48:52 +0800

    Update yubico-authenticator.rb

--- Casks/yubico-authenticator.rb
+++ Casks/yubico-authenticator.rb
@@ -5,11 +5,11 @@ cask "yubico-authenticator" do
   url "https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-#{version}-mac.dmg"
   name "Yubico Authenticator"
   desc "Application for generating TOTP and HOTP codes"
-  homepage "https://developers.yubico.com/yubioath-desktop/"
+  homepage "https://developers.yubico.com/yubioath-flutter/"
 
   livecheck do
     url "https://developers.yubico.com/yubioath-flutter/Releases/"
-    regex(/href=.*?yubico-authenticator-(\d+(\.\d+)*[a-z]?)-mac\.dmg/i)
+    regex(/href=.*?yubico[._-]authenticator[._-]v?(\d+(?:\.\d+)+)[._-]mac\.dmg/i)
   end
 
   depends_on macos: ">= :big_sur"
@@ -17,7 +17,7 @@ cask "yubico-authenticator" do
   app "Yubico Authenticator.app"
 
   zap trash: [
-    "~/Library/Containers/com.yubico.authenticator",
     "~/Library/Application Scripts/com.yubico.authenticator",
+    "~/Library/Containers/com.yubico.authenticator",
   ]
 end
