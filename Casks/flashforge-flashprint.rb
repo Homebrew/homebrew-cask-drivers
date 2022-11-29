a8d960c96321255dcd8298f2937aa1370c2a05dc
Patrick Linnane <patrick@linnane.io>
2022-11-28 20:54:20 -0800

    flashforge-flashprint 5.5.2
    Update flashforge-flashprint from 5.5.1 to 5.5.2
    
    Closes #3141.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/flashforge-flashprint.rb
+++ Casks/flashforge-flashprint.rb
@@ -1,6 +1,6 @@
 cask "flashforge-flashprint" do
-  version "5.5.1"
-  sha256 "134ed9e79b80e576644dc83ee812f4e4a9ecfbf0148a18a87e0f4036845c6390"
+  version "5.5.2"
+  sha256 "a62213b3778fe9b4c9227fe6060284f183c56c496e88f5d9411bd74ec32d1495"
 
   url "http://www.ishare3d.com/3dapp/public/FlashPrint-5/FlashPrint/FlashPrint_#{version}_mac.zip",
       verified: "ishare3d.com/3dapp/public/FlashPrint-5/FlashPrint/"
