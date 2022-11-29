2c83835784cabb33da5a445a7866f397996fc6e4
Gregory DEPUILLE <gregory.depuille@gmail.com>
2022-11-30 00:23:05 +0100

    Add RJM Mastermind GT and Mini Effect Gizmo X editor software

---
+++ Casks/rjm-megxeditor.rb
@@ -0,0 +1,16 @@
+cask "rjm-megxeditor" do
+  version "1.1.1"
+  sha256 "a08d3f34b26dc8d1153319af64410c770c38abc4e29ed1ebd728bf7c14121c25"
+
+  url "https://www.rjmmusic.com/downloads/MEGX/MEGXEditor-#{version}.dmg"
+  name "RJM Music Mini Effect Gizmo X Editor"
+  desc "Editor software to configure RJM mini effect gizmo X MIDI audio loop switcher"
+  homepage "https://www.rjmmusic.com/"
+
+  livecheck do
+    url "https://www.rjmmusic.com/wp-json/wp/v2/pages/6614"
+    regex(/href=.*?MEGXEditor[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  app "MEGXEditor.app"
+end
