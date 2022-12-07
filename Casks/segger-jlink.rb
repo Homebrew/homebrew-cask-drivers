9f4eade5735f564b8e185bad186d43199892e25c
Shahar Hadas <shaharhd@gmail.com>
2022-12-07 11:28:49 +0200

    Update segger-jlink from 7.82d to 7.82e

--- Casks/segger-jlink.rb
+++ Casks/segger-jlink.rb
@@ -1,6 +1,6 @@
 cask "segger-jlink" do
-  version "7.82d"
-  sha256 "e5ec9ea7a6a35c97509b428c2912191f5f8472ba3d383171011b59f57bf24468"
+  version "7.82e"
+  sha256 "c5e3aed759d5bf12663e4daa68748ccccab39c63c9fa486f0dbef99c8b528018"
 
   url "https://www.segger.com/downloads/jlink/JLink_MacOSX_V#{version.no_dots}_universal.pkg",
       using: :post,
