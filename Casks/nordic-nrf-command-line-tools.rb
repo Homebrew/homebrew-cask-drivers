6342ea8f34266d9a7cba3a3f4bc31684a893deb5
Patrick Linnane <patrick@linnane.io>
2022-12-14 13:27:36 -0800

    nordic-nrf-command-line-tools: formatting fix (#3180)

--- Casks/nordic-nrf-command-line-tools.rb
+++ Casks/nordic-nrf-command-line-tools.rb
@@ -2,7 +2,8 @@ cask "nordic-nrf-command-line-tools" do
   version "10.18.1"
   sha256 "e060ddf2044677f08c4f825ee31dfc8525c26f557abcb700758e1193bc556c9f"
 
-  url "https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nrf-command-line-tools-#{version}-darwin.dmg", verified: "nsscprodmedia.blob.core.windows.net/"
+  url "https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nrf-command-line-tools-#{version}-darwin.dmg",
+      verified: "nsscprodmedia.blob.core.windows.net/"
   name "nRF Command Line Tools"
   desc "Command-line tools for Nordic nRF Semiconductors"
   homepage "https://www.nordicsemi.com/Software-and-Tools/Development-Tools/nRF-Command-Line-Tools"
