5f1cfbf0b624a16b71e6b779b288d671c4c96c57
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-25 15:23:45 +0800

    Update creative.rb

--- Casks/creative.rb
+++ Casks/creative.rb
@@ -3,13 +3,13 @@ cask "creative" do
   sha256 "efd98409681a7d860774c7b9a1c44508d13aa1af305d9f057367580c4e1957e9"
 
   url "https://files.creative.com/manualdn/Applications/100394/FELGxPhWie/Creative%20#{version.csv.first}%20build%20#{version.csv.second}.zip"
-  name "Creative App for Mac"
+  name "Creative"
   desc "Control panel for the Creative hardware"
   homepage "https://support.creative.com/"
 
   livecheck do
     url "https://support.creative.com/Products/ProductDetails.aspx?catID=1&subCatID=1258&prodID=23677"
-    regex(/Creative (\d+(?:[._]\d)+) build (\d+(?:\d)+)+\.zip/i)
+    regex(/Creative\s(\d+(?:[._]\d)+)\sbuild\s(\d+(?:\d)+)+\.zip/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
