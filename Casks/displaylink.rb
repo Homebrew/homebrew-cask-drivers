c7a3f4caff63254357410f18771ed7a0950b3b11
Bevan Kay <email@bevankay.me>
2022-12-28 17:36:44 +1100

    displaylink: convert to `on_system` blocks

--- Casks/displaylink.rb
+++ Casks/displaylink.rb
@@ -1,21 +1,25 @@
 cask "displaylink" do
-  if MacOS.version <= :sierra
+  on_sierra :or_older do
     version "4.3.1,2021-02"
     sha256 "d5cd6787d6c4ca6a2425984bcbab607e618e9803335455e24196e14e35657b97"
     url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20Mac%20OS%20X%20and%20macOS#{version.csv.first}-EXE.dmg"
-  elsif MacOS.version <= :high_sierra
+  end
+  on_high_sierra do
     version "5.1.1,2021-02"
     sha256 "1ac9093f8113af8c35d6f3ff5b1ae3f119a5aff0d5309d75c7a1742f159184b5"
     url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.csv.first}-EXE.dmg"
-  elsif MacOS.version <= :mojave
+  end
+  on_mojave do
     version "5.2.6,2021-05"
     sha256 "9f1854cd5720105d6d45c91172419c503358543e4a23d7113387aedf16a39cbb"
     url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20USB%20Graphics%20Software%20for%20macOS#{version.csv.first}-EXE.dmg"
-  elsif MacOS.version <= :catalina
+  end
+  on_catalina do
     version "1.5,2021-09"
     sha256 "d703cc8e9093e4d163c5e612326c0907a02c6d4eec6aaca8d0727503859ef95d"
     url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20Manager%20Graphics%20Connectivity#{version.csv.first}-EXE.pkg"
-  else
+  end
+  on_big_sur :or_newer do
     version "1.8,2022-10"
     sha256 "8cd7e7aeb393a707019a7222ce73f2abf2f662e8d1c00f5176b1f3b32ac7fb13"
     url "https://www.synaptics.com/sites/default/files/exe_files/#{version.csv.second}/DisplayLink%20Manager%20Graphics%20Connectivity#{version.csv.first}-EXE.pkg"
@@ -29,9 +33,10 @@ cask "displaylink" do
     skip "No version information available"
   end
 
-  if MacOS.version <= :mojave
+  on_mojave :or_older do
     pkg "DisplayLink Software Installer.pkg"
-  else
+  end
+  on_catalina :or_newer do
     pkg "DisplayLink Manager Graphics Connectivity#{version.csv.first}-EXE.pkg"
   end
 
