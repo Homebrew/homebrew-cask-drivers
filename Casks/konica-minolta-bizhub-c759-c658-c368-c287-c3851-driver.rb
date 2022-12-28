7d1f19d47dd99239870128cad25b887a9477c71d
Bevan Kay <email@bevankay.me>
2022-12-29 02:48:51 +1100

    konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver: convert to `on_system` blocks (#3210)
    * konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver: convert to `on_system` blocks
    
    * Update konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb

--- Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
+++ Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
@@ -1,10 +1,11 @@
 cask "konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver" do
-  if MacOS.version <= :catalina
+  on_catalina :or_older do
     version "11.6.0"
     sha256 :no_check
 
     url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=08e8308b1e134d6bb6837e3b046b126b&tx_kmanacondaimport_downloadproxy[documentId]=122431&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
-  else
+  end
+  on_big_sur :or_newer do
     version "11.8.0A"
     sha256 :no_check
 
@@ -21,9 +22,10 @@ cask "konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver" do
 
   depends_on macos: ">= :sierra"
 
-  if MacOS.version <= :catalina
+  on_catalina :or_older do
     pkg "C759_C658_C368_C287_C3851_Series_v#{version}_Letter/C759_C658_C368_C287_C3851.pkg"
-  else
+  end
+  on_big_sur :or_newer do
     pkg "C759_C658_C368_C287_C3851_#{version.major}.pkg"
   end
 
