aed1708291dad61d3763bbbaec326775743836fb
Bevan Kay <email@bevankay.me>
2022-12-28 17:42:29 +1100

    konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver: convert to `on_system` blocks

--- Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
+++ Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
@@ -1,10 +1,11 @@
 cask "konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver" do
-  if MacOS.version <= :catalina
+  on_catalina :or_older do
     version "11.6.0"
     sha256 :no_check
 
     url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=08e8308b1e134d6bb6837e3b046b126b&tx_kmanacondaimport_downloadproxy[documentId]=122431&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
-  else
+  end
+  on_big_sur :or_newer do
     version "11.8.0A"
     sha256 :no_check
 
