f2b9de10c682aae994f55a98c52da57c40d9ea22
Bevan Kay <email@bevankay.me>
2022-12-28 17:46:37 +1100

    logitech-options: convert to `on_system` blocks

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -1,5 +1,5 @@
 cask "logitech-options" do
-  if MacOS.version <= :sierra
+  on_sierra :or_older do
     version "7.14.77"
     sha256 "e4df55642e04139fc93d955e949bf736196a404ed067d87f8de7eb9ac9117ece"
 
@@ -10,7 +10,8 @@ cask "logitech-options" do
       url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.12"
       regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
     end
-  elsif MacOS.version <= :high_sierra
+  end
+  on_high_sierra do
     version "8.30.293"
     sha256 "db5f2cd94960223bdf74f0db6fc009f82f80928fe2ce849202754bbdb720eb87"
 
@@ -21,7 +22,8 @@ cask "logitech-options" do
       url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.13"
       regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
     end
-  elsif MacOS.version <= :mojave
+  end
+  on_mojave do
     version "8.54.147"
     sha256 "7b7a8d7a498d868c90b4ffe7dfc50a7a39c25e1f61350702e87d4c771b3d6459"
 
@@ -32,7 +34,8 @@ cask "logitech-options" do
       url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.14"
       regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
     end
-  else
+  end
+  on_catalina :or_newer do
     version "10.00.75"
     sha256 :no_check
 
@@ -52,9 +55,10 @@ cask "logitech-options" do
   auto_updates true
   depends_on macos: ">= :sierra"
 
-  if MacOS.version <= :high_sierra
+  on_high_sierra :or_older do
     pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
-  else
+  end
+  on_mojave :or_newer do
     pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
   end
 
