d7e6265331dd8d9a3ebdec44e5b5d15885e3b0e9
Bevan Kay <email@bevankay.me>
2022-12-29 15:51:03 +1100

    elgato-control-center 1.4.1.10463
    Update elgato-control-center from 1.4.0.10462 to 1.4.1.10463
    
    Closes #3218.
    
    Signed-off-by: George Adams <george.adams@microsoft.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/elgato-control-center.rb
+++ Casks/elgato-control-center.rb
@@ -1,8 +1,8 @@
 cask "elgato-control-center" do
-  version "1.4.0.10462"
-  sha256 "19fb311146a92a19b1a754bb10f5062ee1fe1cd63d141e80a84a31726fc10773"
+  version "1.4.1.10463"
+  sha256 "8811db60de8a8865f5253d3c7827a47031616410d92450fb9b6b83deea6dcc0b"
 
-  url "https://edge.elgato.com/egc/macos/eccm/#{version.major_minor}/ControlCenterMac-#{version}.app.zip"
+  url "https://edge.elgato.com/egc/macos/eccm/#{version.major_minor_patch.chomp(".0")}/ControlCenterMac-#{version}.app.zip"
   name "Elgato Control Center"
   desc "Control your Elgato Key Lights"
   homepage "https://www.elgato.com/en/gaming/key-light"
