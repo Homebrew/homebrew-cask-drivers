2f2142e9b94f3fc1f8d1d043e9afb2128a9e21fb
i0ntempest <szf1234@me.com>
2023-01-06 20:45:30 -0500

    Update logitech-g-hub from 2022.11.345634 to 2022.12.348746

--- Casks/logitech-g-hub.rb
+++ Casks/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2022.11.345634"
+  version "2022.12.348746"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
