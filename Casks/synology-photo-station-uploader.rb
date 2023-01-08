5759f47fb6181675226847c1f8a41305116db8ce
Teko012 <112829523+Teko012@users.noreply.github.com>
2023-01-08 19:03:14 +0100

    Bump synology-photo-station-uploader from 1.4.5 to 1.4.6

--- Casks/synology-photo-station-uploader.rb
+++ Casks/synology-photo-station-uploader.rb
@@ -1,8 +1,8 @@
 cask "synology-photo-station-uploader" do
-  version "1.4.5,093"
-  sha256 "16191318254ad221f52fa1c2b6ed1509d9cc22349c3129935f717fca7f0d44f1"
+  version "1.4.6,098"
+  sha256 "6969aa9d75e8b8a57e64db6ff81e9fc8826c5a08432ccea7713b589cad9f7895"
 
-  url "https://global.download.synology.com/download/Tools/PhotoStationUploader/#{version.csv.first}-#{version.csv.second}/Mac/SynologyPhotoStationUploader-#{version.csv.second}-Mac-Installer.dmg"
+  url "https://global.download.synology.com/download/Utility/PhotoStationUploader/#{version.csv.first}-#{version.csv.second}/Mac/SynologyPhotoStationUploader-#{version.csv.second}-Mac-Installer.dmg"
   appcast "https://www.synology.com/en-us/releaseNote/PhotoStationUploader"
   name "Synology Photo Station Uploader"
   desc "Bulk upload photos and videos to Synology Photo Station"
@@ -10,11 +10,15 @@ cask "synology-photo-station-uploader" do
 
   pkg "SynologyPhotoStationUploader-#{version.after_comma}-Mac-Installer.pkg"
 
-  uninstall pkgutil:   "com.synology.photostationuploader.installer",
+  uninstall pkgutil:   [
+              "com.synology.photostationuploader.installer",
+              "inc.synology.photostationuploader",
+            ],
             quit:      "com.synology.PhotoStationUploader",
             launchctl: [
               "com.synology.PhotoUploaderFinderSync",
               "com.synology.PhotoUploaderShellApp",
+              "com.synology.PhotoUploaderUninstaller",
               "com.synology.SynoSIMBL_RefreshFinder",
             ]
 end
