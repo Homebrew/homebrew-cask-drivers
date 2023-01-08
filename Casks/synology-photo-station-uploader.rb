3c0407dafb84439d78337a0d3f0e094a3f23cab1
Patrick Linnane <patrick@linnane.io>
2023-01-08 11:16:58 -0800

    add livecheck

--- Casks/synology-photo-station-uploader.rb
+++ Casks/synology-photo-station-uploader.rb
@@ -3,10 +3,18 @@ cask "synology-photo-station-uploader" do
   sha256 "6969aa9d75e8b8a57e64db6ff81e9fc8826c5a08432ccea7713b589cad9f7895"
 
   url "https://global.download.synology.com/download/Utility/PhotoStationUploader/#{version.csv.first}-#{version.csv.second}/Mac/SynologyPhotoStationUploader-#{version.csv.second}-Mac-Installer.dmg"
-  appcast "https://www.synology.com/en-us/releaseNote/PhotoStationUploader"
   name "Synology Photo Station Uploader"
   desc "Bulk upload photos and videos to Synology Photo Station"
   homepage "https://www.synology.com/"
+
+  livecheck do
+    url "https://www.synology.com/en-us/releaseNote/PhotoStationUploader"
+    strategy :patch_match do |page|
+      match = page.match(/<h3>Version:\s(\d+(?:\.\d+)+)\-(\d+)/i)
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   pkg "SynologyPhotoStationUploader-#{version.after_comma}-Mac-Installer.pkg"
 
