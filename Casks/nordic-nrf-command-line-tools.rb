c75f7c2b5e81f00ba25bb56aa4d35e28be2dd950
Bevan Kay <email@bevankay.me>
2023-01-25 11:23:16 +1100

    nordic-nrf-command-line-tools 10.19.0
    Update nordic-nrf-command-line-tools from 10.18.1 to 10.19.0
    
    Closes #3258.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/nordic-nrf-command-line-tools.rb
+++ Casks/nordic-nrf-command-line-tools.rb
@@ -1,6 +1,6 @@
 cask "nordic-nrf-command-line-tools" do
-  version "10.18.1"
-  sha256 "e060ddf2044677f08c4f825ee31dfc8525c26f557abcb700758e1193bc556c9f"
+  version "10.19.0"
+  sha256 "33b2de58640ecfe2997ffddc002cc9e0374507657151c2276e591ac6adce8500"
 
   url "https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nrf-command-line-tools-#{version}-darwin.dmg",
       verified: "nsscprodmedia.blob.core.windows.net/"
