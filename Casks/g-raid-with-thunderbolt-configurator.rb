2d8d6424545b7c9c6c3d4732b2d41d2d7f3a5a69
Markus Reiter <me@reitermark.us>
2023-02-08 16:41:16 +0100

    Remove appcast.

--- Casks/g-raid-with-thunderbolt-configurator.rb
+++ Casks/g-raid-with-thunderbolt-configurator.rb
@@ -3,9 +3,13 @@ cask "g-raid-with-thunderbolt-configurator" do
   sha256 "b3fd3050fa86725f0f801a2c368a9be9fd67e035fc2ff57ce5198950e91ed66c"
 
   url "http://download.g-technology.com/software/gtc-#{version}.zip"
-  appcast "https://support.g-technology.com/sites/default/files/downloads/grrtu/mac/appcast.xml"
   name "G-RAID with Thunderbolt Configurator"
   homepage "https://support-en.g-technology.com/app/products/detailnew/p/91/~/g-raid-with-thunderbolt-3"
+
+  livecheck do
+    url "https://support.g-technology.com/sites/default/files/downloads/grrtu/mac/appcast.xml"
+    strategy :sparkle
+  end
 
   app "G-RAID with Thunderbolt Configurator.app"
 end
