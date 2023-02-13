db0a763a87cf25cf4cc3a66aa9bde3dad13b5b29
Bevan Kay <email@bevankay.me>
2023-02-13 21:53:50 +1100

    Update elecom-mouse-assistant from 5.2.8.000 to 5.2.8.001

--- Casks/elecom-mouse-assistant.rb
+++ Casks/elecom-mouse-assistant.rb
@@ -1,6 +1,6 @@
 cask "elecom-mouse-assistant" do
-  version "5.2.8.000"
-  sha256 "9e33882a6f37312ec36dcb8b041f204a0bd8576d76c576540fd165db0d6497bd"
+  version "5.2.8.001"
+  sha256 "319df0179dfa696ef1f0e76d736a33d7d5596361d3c329beb583f86e959ef708"
 
   url "https://dl.elecom.co.jp/support/download/peripheral/mouse/assistant/mac/ELECOM_Mouse_Installer_#{version}.zip"
   name "ELECOM Mouse Assistant"
