652abe7885a3084e5485303bc0f9d85b0ea6a449
Markus Reiter <me@reitermark.us>
2023-02-19 16:44:39 +0100

    Update hp-easy-start from latest to 2.13.3

--- Casks/hp-easy-start.rb
+++ Casks/hp-easy-start.rb
@@ -1,5 +1,5 @@
 cask "hp-easy-start" do
-  version :latest
+  version "2.13.3,221209"
   sha256 :no_check
 
   url "https://ftp.hp.com/pub/softlib/software12/HP_Quick_Start/osx/Applications/HP_Easy_Start.app.zip"
