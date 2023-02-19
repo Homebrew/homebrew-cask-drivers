81da38fb8b54bb3d44b690712b6db5ed7a9c1e20
Markus Reiter <me@reitermark.us>
2023-02-19 17:18:32 +0100

    Update ricoh-theta from latest to 3.19.1 (#3299)
    * Update ricoh-theta from latest to 3.19.1
    
    * Update ricoh-theta.rb
    
    * Update ricoh-theta.rb

--- Casks/ricoh-theta.rb
+++ Casks/ricoh-theta.rb
@@ -1,11 +1,17 @@
 cask "ricoh-theta" do
-  version :latest
+  version "3.19.1,4"
   sha256 :no_check
 
   url "https://theta360-statics.s3.amazonaws.com/app/viewer/RICOH%20THETA.dmg",
       verified: "theta360-statics.s3.amazonaws.com/app/viewer/"
   name "RICOH THETA"
+  desc "Companion software for 360° cameras"
   homepage "https://theta360.com/en/support/download/pcmac/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   app "RICOH THETA.app"
 end
