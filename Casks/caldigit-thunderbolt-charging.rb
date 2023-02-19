9642465f62b7560ce8fd3ceddf9c1a95d6be2beb
Markus Reiter <me@reitermark.us>
2023-02-19 17:18:17 +0100

    Update caldigit-thunderbolt-charging from latest to 1.2 (#3298)
    * Update caldigit-thunderbolt-charging from latest to 1.2
    
    * Update caldigit-thunderbolt-charging.rb

--- Casks/caldigit-thunderbolt-charging.rb
+++ Casks/caldigit-thunderbolt-charging.rb
@@ -1,11 +1,16 @@
 cask "caldigit-thunderbolt-charging" do
-  version :latest
+  version "1.2"
   sha256 :no_check
 
   url "https://downloads.caldigit.com/CalDigit-Thunderbolt-Station-Mac-Drivers.zip"
   name "CalDigit Thunderbolt Station USB Charging & SuperDrive Support Driver"
   desc "Improved Apple device support"
   homepage "https://www.caldigit.com/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
 
   pkg "CalDigit-Thunderbolt-Station-Mac-Drivers.pkg"
 
