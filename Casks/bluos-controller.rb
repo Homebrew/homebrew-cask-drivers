523935c6f13e392880b1f16ae858a9766a108f3b
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 22:13:54 +0800

    Update bluos-controller.rb

--- Casks/bluos-controller.rb
+++ Casks/bluos-controller.rb
@@ -1,16 +1,20 @@
 cask "bluos-controller" do
-  version "3.20.4"
+  version "3.20.4,2023,02"
   sha256 "955377dfa953d45155773b6ce49cb1623abbce7bb8104b4653c8c4f155edaf0e"
 
-  url "https://content-bluesound-com.s3.amazonaws.com/uploads/2023/02/BluOS-Controller-#{version}.dmg",
+  url "https://content-bluesound-com.s3.amazonaws.com/uploads/#{version.csv.second}/#{version.csv.third}/BluOS-Controller-#{version.csv.first}.dmg",
       verified: "content-bluesound-com.s3.amazonaws.com/uploads/"
   name "BluOS Controller"
   desc "Manage audio systems"
   homepage "https://www.bluesound.com/"
 
   livecheck do
-    url "https://www.bluesound.com/downloads"
-    regex(%r{href=".*?/BluOS[._-]Controller[._-]v?(\d+(?:\.\d+)+)\.dmg"}i)
+    strategy :page_match do |page|
+      match = page.match(%r{uploads/(\d+)/(\d+)/BluOS[._-]Controller[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+      next if match.blank?
+
+      "#{match[3]},#{match[1]},#{match[2]}"
+    end
   end
 
   depends_on macos: ">= :high_sierra"
