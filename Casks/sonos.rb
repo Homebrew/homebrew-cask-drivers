217730f77d519b782862b693321126c3c8da9ab0
Jason Tedor <jason@tedor.me>
2023-02-23 13:25:13 -0500

    Update Casks/sonos.rb

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,5 +1,5 @@
 cask "sonos" do
-  version "15.1,71.1-38080"
+  version "15.1,71.1.38080"
   sha256 "d547325770c8f3e89b68dd97a28e2bd5f92a1ae3ef5bebf7b3d64cd0170a88c0"
 
   url "https://update-software.sonos.com/software/aomqdjww/Sonos_#{version.csv.second.sub(/(\d+)\.(\d+)\.(\d+)/, '\1.\2-\3')}.dmg"
