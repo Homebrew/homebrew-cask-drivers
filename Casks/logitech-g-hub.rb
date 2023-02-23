8eb5d800d864f65bb3036ab6a3a02dbcfbf363e0
Bevan Kay <email@bevankay.me>
2023-02-24 08:07:53 +1100

    Update logitech-g-hub from 2023.1.364056 to 2023.2.373424

--- Casks/logitech-g-hub.rb
+++ Casks/logitech-g-hub.rb
@@ -1,5 +1,5 @@
 cask "logitech-g-hub" do
-  version "2023.1.364056"
+  version "2023.2.373424"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.zip",
