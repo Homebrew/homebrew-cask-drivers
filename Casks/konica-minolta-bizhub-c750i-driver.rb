2bab4e7b1f0b039bb22440d23457ba2d71a1cd38
Markus Reiter <me@reitermark.us>
2023-02-27 14:22:11 +0100

    Fix URL for `konica-minolta-bizhub-c750i-driver`.

--- Casks/konica-minolta-bizhub-c750i-driver.rb
+++ Casks/konica-minolta-bizhub-c750i-driver.rb
@@ -16,7 +16,7 @@ cask "konica-minolta-bizhub-c750i-driver" do
     pkg "C750i_C650i_C360i_C287i_C286i_C4050i_C4000i_C3320i_11.pkg"
 
     livecheck do
-      url "https://dl.konicaminolta.eu/en/?tx_kmdownloadcenter_dlajaxservice[action]=getDocuments&tx_kmdownloadcenter_dlajaxservice[controller]=AjaxService&tx_kmdownloadcenter_dlajaxservice[productId]=103745&tx_kmdownloadcenter_dlajaxservice[system]=KonicaMinolta&cHash=dd72618a38434b6cb3edfc20595d58c5&type=1527583889"
+      url "https://dl.konicaminolta.eu/en?tx_kmdownloadcenter_dlajaxservice[action]=getDocuments&tx_kmdownloadcenter_dlajaxservice[controller]=AjaxService&tx_kmdownloadcenter_dlajaxservice[productId]=103745&tx_kmdownloadcenter_dlajaxservice[system]=KonicaMinolta&cHash=dd72618a38434b6cb3edfc20595d58c5&type=1527583889"
       strategy :page_match do |page|
         items = JSON.parse(page).select do |i|
           i["TypeOfApplicationName_textS"]&.match?(/driver/i) &&
@@ -32,7 +32,7 @@ cask "konica-minolta-bizhub-c750i-driver" do
     end
   end
 
-  url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=#{version.csv[1]}&tx_kmanacondaimport_downloadproxy[documentId]=#{version.csv[2]}&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
+  url "https://dl.konicaminolta.eu/en?tx_kmdownloadcentersite_downloadproxy[fileId]=#{version.csv[1]}&tx_kmdownloadcentersite_downloadproxy[documentId]=#{version.csv[2]}&tx_kmdownloadcentersite_downloadproxy[system]=KonicaMinolta&tx_kmdownloadcentersite_downloadproxy[language]=EN&type=1558521685"
   name "Konica Minolta Bizhub C750i/C650i/C360i/C287i/C286i/C4050i/C4000i/C3320i Printer Driver"
   desc "PostScript printer driver"
   homepage "https://www.konicaminolta.eu/eu-en/support/download-centre"
