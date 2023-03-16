63d61927775a729b862b22709902242edd488737
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-16 09:49:57 +0800

    Update asix-ax88179.rb

--- Casks/asix-ax88179.rb
+++ Casks/asix-ax88179.rb
@@ -50,7 +50,7 @@ cask "asix-ax88179" do
   on_monterey :or_newer do
     version "2.3.0,1372"
 
-    container nested: "ASIX_USB_Device_Installer_macOS_12_above_Driver_v#{version.csv.first}_20221201/ASIX_USB_Device_Installer_v#{version.csv.first}.dmg"
+    container nested: "ASIX_USB_Device_Installer_macOS_12_above_Driver_v#{version.csv.first}/ASIX_USB_Device_Installer_v#{version.csv.first}.dmg"
     pkg "ASIX_USB_Device_Installer_v#{version.csv.first}.pkg"
 
     livecheck do
