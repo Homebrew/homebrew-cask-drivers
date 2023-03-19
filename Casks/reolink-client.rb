126f146fb56049fb9696e34bfbd38fefe229bcd5
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-03-19 18:24:36 -0400

    Update reolink-client from 8.8.4 to 8.8.5

--- Casks/reolink-client.rb
+++ Casks/reolink-client.rb
@@ -1,5 +1,5 @@
 cask "reolink-client" do
-  version "8.8.4,2023,02,011051551675248715,3375"
+  version "8.8.5,2023,03,170224401679019880,5577"
   sha256 :no_check
 
   url "https://home-cdn.reolink.us/wp-content/uploads/#{version.csv[1]}/#{version.csv[2]}/#{version.csv[3]}.#{version.csv[4]}.dmg",
