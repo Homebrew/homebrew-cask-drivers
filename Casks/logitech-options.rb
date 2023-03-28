30fbeef4b20739a64232fbaea12ba5e3f4762b16
Bevan Kay <email@bevankay.me>
2023-03-29 09:26:07 +1100

    Fix install

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -6,6 +6,8 @@ cask "logitech-options" do
     url "https://download01.logi.com/web/ftp/pub/techsupport/options/Options_#{version}.zip",
         verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
 
+    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
+
     livecheck do
       url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.12"
       regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
@@ -18,13 +20,12 @@ cask "logitech-options" do
     url "https://download01.logi.com/web/ftp/pub/techsupport/options/Options_#{version}.zip",
         verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
 
+    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
+
     livecheck do
       url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.13"
       regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
     end
-  end
-  on_high_sierra :or_older do
-    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
   end
   on_mojave do
     version "8.54.147"
@@ -33,13 +34,12 @@ cask "logitech-options" do
     url "https://download01.logi.com/web/ftp/pub/techsupport/options/Options_#{version}.zip",
         verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
 
+    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
+
     livecheck do
       url "https://support.logi.com/api/v2/help_center/en-us/articles.json?label_names=webcontent=productdownload,webos=mac-macos-x-10.14"
       regex(%r{/Options[._-]?v?(\d+(?:\.\d+)+)\.zip}i)
     end
-  end
-  on_mojave :or_newer do
-    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
   end
   on_catalina :or_newer do
     version "10.00.75"
@@ -47,6 +47,8 @@ cask "logitech-options" do
 
     url "https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.zip",
         verified: "download01.logi.com/web/ftp/pub/techsupport/options/"
+
+    pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.pkg"
 
     livecheck do
       url "https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.zip"
