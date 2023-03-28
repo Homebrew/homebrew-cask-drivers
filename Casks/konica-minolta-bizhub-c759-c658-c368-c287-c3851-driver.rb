96fbb9f22fc9a0196ae103670a26a1aaccff2eda
Bevan Kay <email@bevankay.me>
2023-03-29 09:16:03 +1100

    konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver: update download links

--- Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
+++ Casks/konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver.rb
@@ -3,7 +3,7 @@ cask "konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver" do
     version "11.6.0"
     sha256 :no_check
 
-    url "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=08e8308b1e134d6bb6837e3b046b126b&tx_kmanacondaimport_downloadproxy[documentId]=122431&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
+    url "https://dl.konicaminolta.eu/en?tx_kmdownloadcentersite_downloadproxy[fileId]=08e8308b1e134d6bb6837e3b046b126b&tx_kmdownloadcentersite_downloadproxy[documentId]=122431&tx_kmdownloadcentersite_downloadproxy[system]=KonicaMinolta&tx_kmdownloadcentersite_downloadproxy[language]=EN&type=1558521685"
 
     pkg "C759_C658_C368_C287_C3851_Series_v#{version}_Letter/C759_C658_C368_C287_C3851.pkg"
   end
@@ -11,7 +11,7 @@ cask "konica-minolta-bizhub-c759-c658-c368-c287-c3851-driver" do
     version "11.8.0A"
     sha256 :no_check
 
-    url "https://dl.konicaminolta.eu/en?tx_kmanacondaimport_downloadproxy[fileId]=7228bd01e7674417c6a223ce7a186487&tx_kmanacondaimport_downloadproxy[documentId]=130160&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
+    url "https://dl.konicaminolta.eu/en?tx_kmdownloadcentersite_downloadproxy[fileId]=7228bd01e7674417c6a223ce7a186487&tx_kmdownloadcentersite_downloadproxy[documentId]=130160&tx_kmdownloadcentersite_downloadproxy[system]=KonicaMinolta&tx_kmdownloadcentersite_downloadproxy[language]=EN&type=1558521685"
 
     pkg "C759_C658_C368_C287_C3851_#{version.major}.pkg"
   end
