666a478ec78b99f5304b31abf963630442fb20a5
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-29 13:22:32 +0800

    konica-minolta-bizhub-c750i-driver: various updates

--- Casks/konica-minolta-bizhub-c750i-driver.rb
+++ Casks/konica-minolta-bizhub-c750i-driver.rb
@@ -32,7 +32,7 @@ cask "konica-minolta-bizhub-c750i-driver" do
     end
   end
 
-  url "https://dl.konicaminolta.eu/en?tx_kmdownloadcentersite_downloadproxy[fileId]=#{version.csv[1]}&tx_kmdownloadcentersite_downloadproxy[documentId]=#{version.csv[2]}&tx_kmdownloadcentersite_downloadproxy[system]=KonicaMinolta&tx_kmdownloadcentersite_downloadproxy[language]=EN&type=1558521685"
+  url "https://dl.konicaminolta.eu/en?tx_kmdownloadcentersite_downloadproxy[fileId]=#{version.csv.second}&tx_kmdownloadcentersite_downloadproxy[documentId]=#{version.csv.third}&tx_kmdownloadcentersite_downloadproxy[system]=KonicaMinolta&tx_kmdownloadcentersite_downloadproxy[language]=EN&type=1558521685"
   name "Konica Minolta Bizhub C750i/C650i/C360i/C287i/C286i/C4050i/C4000i/C3320i Printer Driver"
   desc "PostScript printer driver"
   homepage "https://www.konicaminolta.eu/eu-en/support/download-centre"
