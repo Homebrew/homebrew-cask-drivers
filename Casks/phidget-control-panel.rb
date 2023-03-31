9d8aff757a9a336a1dbb2e87f1e8b8cc146db6e5
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-31 12:02:38 +0800

    Update phidget-control-panel from 22,1.13.20230224 to 22,1.13.20230330 (#3391)
    * Update phidget-control-panel from 22,1.13.20230224 to 22,1.13.20230330
    
    * Update phidget-control-panel.rb
    
    * phidget-control-panel: update livecheck
    
    ---------
    
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Casks/phidget-control-panel.rb
+++ Casks/phidget-control-panel.rb
@@ -1,6 +1,6 @@
 cask "phidget-control-panel" do
-  version "22,1.13.20230224"
-  sha256 "59ba3a96b0b3e81b55be55e822b4fe2109dfbe5d024eaebe3741a0852c3ef833"
+  version "22,1.13.20230330"
+  sha256 "6d4b0fb6fc03b7d633e6f362d0ef80fef93bdc8644936f7294ba28bc7854c442"
 
   url "https://www.phidgets.com/downloads/phidget#{version.csv.first}/libraries/macos/Phidget#{version.csv.first}/Phidget#{version.csv.first}_#{version.csv.second}.dmg"
   name "Phidget Control Panel"
@@ -8,10 +8,24 @@ cask "phidget-control-panel" do
   homepage "https://www.phidgets.com/"
 
   livecheck do
-    url "https://www.phidgets.com/downloads/phidget#{version.csv.first}/libraries/macos/Phidget#{version.csv.first}.dmg"
+    url "https://www.phidgets.com/docs/Phidgets_Drivers"
     regex(%r{/Phidget(\d+)_(\d+(?:\.\d+)+)\.dmg}i)
-    strategy :header_match do |headers, regex|
-      match = headers["location"].match(regex)
+    strategy :page_match do |page, regex|
+      # Identify the latest main version from the Phidgets Drivers page
+      main_version = page.scan(%r{/Phidget(\d+)\.dmg}i)
+                         .flatten
+                         .uniq
+                         .map { |v| Version.new(v) }
+                         .max
+      next if main_version.blank?
+
+      # Collect the response headers from the generic URL to the latest dmg file
+      headers = Homebrew::Livecheck::Strategy.page_headers(
+        "https://www.phidgets.com/downloads/phidget#{main_version}/libraries/macos/Phidget#{main_version}.dmg",
+      ).reduce(&:merge)
+
+      # Match the version from the filename in the `location` header
+      match = headers["location"]&.match(regex)
       next if match.blank?
 
       "#{match[1]},#{match[2]}"
@@ -23,10 +37,10 @@ cask "phidget-control-panel" do
   pkg "Phidgets.pkg"
 
   uninstall pkgutil: [
-              "com.phidgets.phidget#{version.csv.first}java",
-              "com.phidgets.phidget#{version.csv.first}extra",
-              "com.phidgets.phidget#{version.csv.first}networkserver",
               "com.phidgets.controlpanel#{version.csv.first}",
+              "com.phidgets.phidget#{version.csv.first}extra",
+              "com.phidgets.phidget#{version.csv.first}java",
+              "com.phidgets.phidget#{version.csv.first}networkserver",
               "com.phidgets.phidgetframework#{version.csv.first}",
               "com.phidgets.signeddriver",
             ],
