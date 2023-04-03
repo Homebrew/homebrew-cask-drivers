a36aabfa123e6be6dacb7ca0c11f8e5afbb198f6
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-03 08:29:24 +0800

    cycliq-cycliqplus 2.78 (#3395)

--- Casks/cycliq-cycliqplus.rb
+++ Casks/cycliq-cycliqplus.rb
@@ -1,6 +1,6 @@
 cask "cycliq-cycliqplus" do
-  version "2.75"
-  sha256 "2f027e427bffc86d03d52bb078396467b77856db87848cd29681cd69df6d367d"
+  version "2.78"
+  sha256 "b5c08af21c59f875e53dd8c095b0f038b5330e48a75eb9be203b82155b12f866"
 
   url "https://cycliq.com/files/software/downloads/CycliqPlus-#{version}.dmg"
   name "CycliqPlus"
