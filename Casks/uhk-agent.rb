59fa51e12a652f5201e52abb9253d6f842320952
Andrew Herr <lobsterless@gmail.com>
2023-04-04 16:09:09 -0600

    uhk-agent 2.1.2 (#3397)

--- Casks/uhk-agent.rb
+++ Casks/uhk-agent.rb
@@ -1,6 +1,6 @@
 cask "uhk-agent" do
-  version "2.1.1"
-  sha256 "c27ac00a1818fdbda3c12ac4323a3fe017ea172c1b175906d34aff086828a3dc"
+  version "2.1.2"
+  sha256 "28345eb41b33dc3d5d40675013c7ee39ed8de4da05e64ea05c13d5436cc5acbf"
 
   url "https://github.com/UltimateHackingKeyboard/agent/releases/download/v#{version}/UHK.Agent-#{version}-mac.dmg"
   name "Ultimate Hacking Keyboard Agent"
