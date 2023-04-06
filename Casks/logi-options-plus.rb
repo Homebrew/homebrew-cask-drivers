a66f7562b7059d4a34b9180e6c28feed1ef4a5b1
Matt Coster <matt@mtcoster.net>
2023-04-06 09:48:55 +0100

    logi-options-plus 1.36.0.387585 (#3400)

--- Casks/logi-options-plus.rb
+++ Casks/logi-options-plus.rb
@@ -1,5 +1,5 @@
 cask "logi-options-plus" do
-  version "1.34.0.376143"
+  version "1.36.0.387585"
   sha256 :no_check
 
   url "https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip",
