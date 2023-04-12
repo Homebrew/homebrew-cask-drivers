cd27cc4f58b3064a61a99533974c49df7ff7a11c
Issy Long <me@issyl0.co.uk>
2023-04-12 18:05:38 +0100

    nvidia-cuda-toolkit: Autofix `Cask/StanzaGrouping` offenses in `on_*` blocks

--- Casks/nvidia-cuda-toolkit.rb
+++ Casks/nvidia-cuda-toolkit.rb
@@ -2,11 +2,13 @@ cask "nvidia-cuda-toolkit" do
   on_sierra :or_older do
     version "9.0.176"
     sha256 "8fad950098337d2611d64617ca9f62c319d97c5e882b8368ed196e994bdaf225"
+
     url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac-dmg"
   end
   on_high_sierra :or_newer do
     version "10.1.243"
     sha256 "432a2f07a793f21320edc5d10e7f68a8e4e89465c31e1696290bdb0ca7c8c997"
+
     url "https://developer.download.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac.dmg"
   end
 
