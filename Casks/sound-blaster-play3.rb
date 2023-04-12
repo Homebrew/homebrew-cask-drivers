db2a26021773bf87eae38a1433a1a224d38aeea5
Issy Long <me@issyl0.co.uk>
2023-04-12 18:06:26 +0100

    sound-blaster-play3: Autofix `Cask/StanzaGrouping` offenses in `on_*` blocks

--- Casks/sound-blaster-play3.rb
+++ Casks/sound-blaster-play3.rb
@@ -5,6 +5,7 @@ cask "sound-blaster-play3" do
     sha256 "0550328b70fc214fcffc418fba3e5140c853e827bc92227a73864d62ba5e8415"
 
     url "https://files.creative.com/manualdn/Drivers/AVP/13795/0x3F213863/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg.zip"
+
     pkg "Install.pkg"
 
     livecheck do
@@ -20,6 +21,7 @@ cask "sound-blaster-play3" do
     sha256 "3dbbdc627f5ee72f87403dc471e6718c258594e0fb0b208d900a5f174ce02915"
 
     url "https://files.creative.com/manualdn/Drivers/100289/MqrOwTo9a7/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg"
+
     pkg "INSTALL.pkg"
 
     livecheck do
