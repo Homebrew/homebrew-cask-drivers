fc6e6093ff2eba29e500a6bc603437bea165375d
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-04 20:38:31 -0400

    hdhomerun 20230503 (#3441)

--- Casks/hdhomerun.rb
+++ Casks/hdhomerun.rb
@@ -1,6 +1,6 @@
 cask "hdhomerun" do
-  version "20230323"
-  sha256 "924aad08d9878960ad53d186dda219d2cce67954978862cbd57c2cc5d8656fcf"
+  version "20230503"
+  sha256 "46db4a8dfa95c9290eddd5513c639b9bdef92cd8dc1ea64f8376d09fc38e4c74"
 
   url "https://download.silicondust.com/hdhomerun/hdhomerun_mac_#{version}.dmg"
   name "HDHomeRun"
